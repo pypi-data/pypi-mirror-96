d387854611fd01c2447a87f447344c40c8657e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:21 +0000

    Add package policyuniverse/1.3.3.20210223/policyuniverse-1.3.3.20210223.tar.gz

---
+++ packages/policyuniverse/policyuniverse-1.3.3.20210223.tar.gz/policyuniverse-1.3.3.20210223/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup
+import os
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
+
+tests_require = ["pytest", "coveralls"]
+dev_require = ["pre-commit", "black"]
+
+setup(
+    name="policyuniverse",
+    version="1.3.3.20210223",
+    description="Parse and Process AWS IAM Policies, Statements, ARNs, and wildcards.",
+    long_description=open(os.path.join(ROOT, "README.md")).read(),
+    long_description_content_type="text/markdown",
+    author="Patrick Kelley",
+    author_email="patrickbarrettkelley@gmail.com",
+    url="https://github.com/Netflix-Skunkworks/policyuniverse",
+    keywords=[
+        "iam",
+        "arn",
+        "action_groups",
+        "condition",
+        "policy",
+        "statement",
+        "wildcard",
+    ],
+    packages=["policyuniverse"],
+    package_data={"policyuniverse": ["data.json"]},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    extras_require={"tests": tests_require, "dev": dev_require},
+)
