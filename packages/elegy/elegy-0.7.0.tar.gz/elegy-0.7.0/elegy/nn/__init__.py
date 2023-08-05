972852270ac8371dacaa29349585f1de4de47275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:59 +0000

    Add package elegy/0.7.0/elegy-0.7.0.tar.gz

---
+++ packages/elegy/elegy-0.7.0.tar.gz/elegy-0.7.0/elegy/nn/__init__.py
@@ -0,0 +1,33 @@
+from .batch_normalization import BatchNormalization
+from .conv import Conv1D, Conv2D, Conv3D, ConvND
+from .dropout import Dropout
+from .flatten import Flatten, Reshape
+from .linear import Linear
+from .sequential_module import Sequential, sequential
+
+from .layer_normalization import LayerNormalization, InstanceNormalization
+from .embedding import Embedding, EmbedLookupStyle
+from .pool import MaxPool, AvgPool
+from .moving_averages import EMAParamsTree
+from . import transformers
+
+__all__ = [
+    "EMAParamsTree",
+    "BatchNormalization",
+    "Conv1D",
+    "Conv2D",
+    "Conv3D",
+    "ConvND",
+    "Dropout",
+    "Flatten",
+    "Reshape",
+    "Linear",
+    "Sequential",
+    "sequential",
+    "LayerNormalization",
+    "InstanceNormalization",
+    "Embedding",
+    "EmbedLookupStyle",
+    "MaxPool",
+    "AvgPool",
+]
