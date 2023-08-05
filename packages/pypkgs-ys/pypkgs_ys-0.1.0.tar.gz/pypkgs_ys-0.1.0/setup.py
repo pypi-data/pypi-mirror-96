5c38b6507789b9712d448051805ca6504ed82ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:31 +0000

    Add package pypkgs-ys/0.1.0/pypkgs_ys-0.1.0.tar.gz

---
+++ packages/pypkgs-ys/pypkgs_ys-0.1.0.tar.gz/pypkgs_ys-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypkgs_ys']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pypkgs-ys',
+    'version': '0.1.0',
+    'description': 'Python package that eases the pain of concatenating Pandas categoricals!',
+    'long_description': None,
+    'author': 'Yiki Su',
+    'author_email': 'sueyi@student.ubc.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
