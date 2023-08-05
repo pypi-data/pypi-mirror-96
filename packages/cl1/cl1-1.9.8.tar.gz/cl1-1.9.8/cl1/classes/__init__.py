2f74749a6521a39af1570b0eb2c8ef7d0e71a9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:54 +0000

    Add package cl1/1.9.8/cl1-1.9.8.tar.gz

---
+++ packages/cl1/cl1-1.9.8.tar.gz/cl1-1.9.8/cl1/classes/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from cl1.classes.config import version
+from cl1.classes.cl1 import CLI, get_argument, argument_present, arguments_present
+
