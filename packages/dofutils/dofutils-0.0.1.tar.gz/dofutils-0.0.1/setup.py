3b9ec7ae119ab69e686cf787761415af868867f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:59 +0000

    Add package dofutils/0.0.1/dofutils-0.0.1.tar.gz

---
+++ packages/dofutils/dofutils-0.0.1.tar.gz/dofutils-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='dofutils',
+    version='0.0.1',
+    packages=find_packages(exclude="tests"),
+    url='https://github.com/Dysta/Dofutils',
+    license='',
+    author='Dysta',
+    author_email='',
+    description='Collection of useful things for build Dofus Retro bot/emulator ',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
