4e18e7fc9dafe3ec134c85b566b42a3b6ba5e04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package nginx-language-server/0.4.0/nginx-language-server-0.4.0.tar.gz

---
+++ packages/nginx-language-server/nginx-language-server-0.4.0.tar.gz/nginx-language-server-0.4.0/nginx_language_server/cli.py
@@ -0,0 +1,17 @@
+"""Cli for nginx language server."""
+
+import click
+
+from .server import SERVER
+
+
+@click.command()
+@click.version_option()
+def cli() -> None:
+    """Nginx language server.
+
+    Examples:
+
+        Run from stdio : nginx-language-server
+    """
+    SERVER.start_io()
