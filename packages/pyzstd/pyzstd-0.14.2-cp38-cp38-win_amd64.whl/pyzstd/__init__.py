7b7e660f8556d49981ac282d4e6ee6c6640fd971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:41 +0000

    Add package pyzstd/0.14.2/pyzstd-0.14.2-cp38-cp38-win_amd64.whl

---
+++ packages/pyzstd/pyzstd-0.14.2-cp38-cp38-win_amd64.whl/pyzstd/__init__.py
@@ -0,0 +1,11 @@
+from .pyzstd import *
+
+__version__ = '0.14.2'
+
+__doc__ = '''\
+Python bindings for Zstandard (zstd) compression algorithm, the API is similar
+to Python's bz2/lzma/zlib module.
+
+Documentation: https://pyzstd.readthedocs.io
+GitHub: https://github.com/animalize/pyzstd
+PyPI: https://pypi.org/project/pyzstd'''
