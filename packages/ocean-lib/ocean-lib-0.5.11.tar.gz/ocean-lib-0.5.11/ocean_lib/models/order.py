f27d9a79432d40a3d22a3ded94179979a41aebeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:12 +0000

    Add package ocean-lib/0.5.11/ocean-lib-0.5.11.tar.gz

---
+++ packages/ocean-lib/ocean-lib-0.5.11.tar.gz/ocean-lib-0.5.11/ocean_lib/models/order.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+Order = namedtuple(
+    "Order",
+    (
+        "datatoken",
+        "amount",
+        "timestamp",
+        "transactionId",
+        "did",
+        "payer",
+        "consumer",
+        "serviceId",
+        "serviceType",
+    ),
+)
