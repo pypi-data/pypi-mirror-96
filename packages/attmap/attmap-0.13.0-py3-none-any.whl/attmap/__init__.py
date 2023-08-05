d7e3ed8890d8d069afae29f11637666fdec1807e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:44 +0000

    Add package attmap/0.13.0/attmap-0.13.0-py3-none-any.whl

---
+++ packages/attmap/attmap-0.13.0-py3-none-any.whl/attmap/__init__.py
@@ -0,0 +1,19 @@
+""" Package-scope definitions """
+
+from ._att_map_like import AttMapLike
+from .attmap import AttMap
+from .attmap_echo import *
+from .helpers import *
+from .ordattmap import OrdAttMap
+from .pathex_attmap import PathExAttMap
+from ._version import __version__
+
+AttributeDict = AttMap
+AttributeDictEcho = AttMapEcho
+
+__all__ = ["AttMapLike", "AttMap", "AttMapEcho", "AttributeDict",
+           "AttributeDictEcho", "EchoAttMap", "OrdAttMap", "PathExAttMap",
+           "get_data_lines"]
+__aliases__ = {"AttMap": ["AttributeDict"],
+               "AttMapEcho": ["AttributeDictEcho"],
+               "EchoAttMap": ["AttMapEcho"]}
