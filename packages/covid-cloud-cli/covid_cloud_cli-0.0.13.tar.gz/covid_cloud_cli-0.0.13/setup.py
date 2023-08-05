7441c8c6417d90b8c9311e635a625a430654b17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package covid-cloud-cli/0.0.13/covid_cloud_cli-0.0.13.tar.gz

---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.13.tar.gz/covid_cloud_cli-0.0.13/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='covid_cloud_cli',
+    packages=find_packages(exclude=['covid_cloud.cli', 'covid_cloud.client']),
+    # packages=find_packages(),
+    # packages=['covid_cloud'],
+    version='0.0.13',
+    description='DNAstack COVID Cloud CLI Library',
+    author='Derek',
+    license='MIT',
+    install_requires=['search-python-client>=0.1.7',
+                      'urllib3==1.26.3',
+                      'requests==2.25.1',
+                      'click==7.1.2'
+                      ],
+)
