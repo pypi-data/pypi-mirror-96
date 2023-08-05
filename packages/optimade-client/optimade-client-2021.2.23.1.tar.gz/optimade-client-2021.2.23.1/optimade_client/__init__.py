5533a5914f0d13d95af2150d8f6c522ebe6d54a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:24 +0000

    Add package optimade-client/2021.2.23.1/optimade-client-2021.2.23.1.tar.gz

---
+++ packages/optimade-client/optimade-client-2021.2.23.1.tar.gz/optimade-client-2021.2.23.1/optimade_client/__init__.py
@@ -0,0 +1,20 @@
+"""
+OPTIMADE Client
+
+Voilà/Jupyter client for searching through OPTIMADE databases.
+"""
+from .informational import OptimadeClientFAQ, HeaderDescription, OptimadeLog
+from .query_provider import OptimadeQueryProviderWidget
+from .query_filter import OptimadeQueryFilterWidget
+from .summary import OptimadeSummaryWidget
+
+
+__version__ = "2021.2.23.1"
+__all__ = (
+    "HeaderDescription",
+    "OptimadeClientFAQ",
+    "OptimadeLog",
+    "OptimadeQueryProviderWidget",
+    "OptimadeQueryFilterWidget",
+    "OptimadeSummaryWidget",
+)
