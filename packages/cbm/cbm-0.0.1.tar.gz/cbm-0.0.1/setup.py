0b8716bcba895f33dbb54b3087b3c899f487c386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:26 +0000

    Add package cbm/0.0.1/cbm-0.0.1.tar.gz

---
+++ packages/cbm/cbm-0.0.1.tar.gz/cbm-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cbm',
+    version='0.0.01',
+    python_requires='>=3.6',
+    description='Checks by Monitoring (CbM)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ec-jrc/cbm',
+    author='Guido Lemoine, Konstantinos Anastasakis',
+    author_email='',
+    license='BSD 3-clause',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'boto3',
+        'botocore',
+        'bqplot',
+        'Fiona',
+        'osgeo',
+        'ipyleaflet',
+        'ipywidgets',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'psycopg2-binary',
+        'python-json-logger',
+        'rasterio',
+        'rasterstats',
+        'requests',
+        'tqdm',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
