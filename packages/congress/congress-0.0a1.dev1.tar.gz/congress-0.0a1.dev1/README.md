c9b9ae213190b734a728797bcb84b248d0112486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:24 +0000

    Add package congress/0.0a1.dev1/congress-0.0a1.dev1.tar.gz

---
+++ packages/congress/congress-0.0a1.dev1.tar.gz/congress-0.0a1.dev1/README.md
@@ -0,0 +1,14 @@
+![POLITICO](https://www.politico.com/interactives/cdn/images/badge.svg)
+
+# congress 🇺🇸
+
+A package for working with U.S. Congressional metadata. Like [python-us](https://github.com/unitedstates/python-us), but for bills, committee assignments and more!
+
+[![PyPI version](https://badge.fury.io/py/congress.svg)](https://badge.fury.io/py/congress)
+
+
+### Quickstart
+
+```
+$ pip install congress
+```
---
+++ packages/congress/congress-0.0a1.dev1.tar.gz/congress-0.0a1.dev1/setup.py
@@ -0,0 +1,47 @@
+# Imports from python.
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+
+# Imports from us-elections.
+from congress import __appname__, __version__
+
+
+REPO_URL = "https://github.com/The-Politico/us-congress/"
+
+PYPI_VERSION = ".".join(str(v) for v in __version__)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+setup(
+    name=__appname__,
+    version=PYPI_VERSION,
+    packages=find_packages(exclude=["db", "docs", "tests"]),
+    license="MIT",
+    description="U.S. Congressional metadata (beta; more coming soon)",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url=REPO_URL,
+    download_url="{repo_url}archive/{version}.tar.gz".format(
+        **{"repo_url": REPO_URL, "version": PYPI_VERSION}
+    ),
+    author="POLITICO Interactive News",
+    author_email="interactives@politico.com",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    keywords="",
+    platforms=["any"],
+    include_package_data=True,
+    install_requires=["us>=1,<2"],
+    zip_safe=False,
+    extras_require={"test": ["pytest"]},
+)
