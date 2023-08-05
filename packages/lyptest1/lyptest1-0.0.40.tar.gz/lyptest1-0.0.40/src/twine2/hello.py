e2a419de51316da71b367090f2205df8ee15891a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package lyptest1/0.0.40/lyptest1-0.0.40.tar.gz

---
+++ packages/lyptest1/lyptest1-0.0.40.tar.gz/lyptest1-0.0.40/src/twine2/hello.py
@@ -0,0 +1,42 @@
+# from utils import saybye
+import argparse
+import logging
+import os.path
+from typing import Dict, List, cast
+
+
+
+# def main(args: List[str]) -> None:
+#     parser = argparse.ArgumentParser(prog="twine upload")
+#     # settings.Settings.register_argparse_arguments(parser)
+#     parser.add_argument(
+#         "dists",
+#         nargs="+",
+#         metavar="dist",
+#         help="The distribution files to upload to the repository "
+#              "(package index). Usually dist/* . May additionally contain "
+#              "a .asc file to include an existing signature with the "
+#              "file upload.",
+#     )
+#
+#     parsed_args = parser.parse_args(args)
+#
+#     print("upload main ")
+#     # upload_settings = settings.Settings.from_argparse(parsed_args)
+#
+#     # Call the upload function with the arguments from the command line
+#     # return upload(upload_settings, parsed_args.dists)
+
+
+def main():
+    print('he111llo')
+    # saybye()
+
+def main2():
+    print('hello222')
+    # saybye()
+
+
+#
+# if __name__=='__main__':
+#     main()
