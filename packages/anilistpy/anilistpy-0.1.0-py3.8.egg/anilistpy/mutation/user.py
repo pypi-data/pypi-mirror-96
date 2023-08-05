1e52da14a65540c4ba885a6daaa5c1fe53e45738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:16 +0000

    Add package anilistpy/0.1.0/anilistpy-0.1.0-py3.8.egg

---
+++ packages/anilistpy/anilistpy-0.1.0-py3.8.egg/anilistpy/mutation/user.py
@@ -0,0 +1,78 @@
+import requests
+import json
+
+class User:
+    def username(accessToken):
+        mutation = '''
+query ($id: Int) {
+  Viewer{
+    name
+  }
+  Media(id: $id){
+    id
+  }
+}    '''
+        variables = {
+            'id' : 1
+        }
+        headers = {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        url = 'https://graphql.anilist.co'
+        response = requests.post(url, json={'query': mutation, 'variables': variables}, headers=headers)
+
+        raw = json.loads(response.text)
+
+        return raw["data"]["Viewer"]["name"]      
+    '''
+    user mutation api
+    def UpdateUser()
+    '''
+    
+    def userid(username):
+        
+        query = '''
+query ($name: String) {
+	User(name: $name){
+    id
+  }
+}
+        '''
+        variables = {
+            'name' : username
+        }
+
+        url = 'https://graphql.anilist.co'
+        response = requests.post(url, json={'query': query, 'variables': variables})
+
+        raw = json.loads(response.text)
+        
+        return raw["data"]["User"]["id"]
+    
+    def updatebio(accessToken, bio):
+        mutation = '''
+mutation ($bio: String) {
+    UpdateUser (about: $bio) {
+        about
+    }
+}
+        '''
+        variables = {
+            'bio' : bio
+        }
+
+        headers = {
+            'Authorization': 'Bearer ' + accessToken,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        url = 'https://graphql.anilist.co'
+        response = requests.post(url, json={'query': mutation, 'variables': variables}, headers=headers)
+
+        raw = json.loads(response.text)
+
+        return raw
