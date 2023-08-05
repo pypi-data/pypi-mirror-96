a7eb793909ea15aa7151ddde4bdb9352fe5e2da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:52 +0000

    Add package fairscale/0.3.0/fairscale-0.3.0.tar.gz

---
+++ packages/fairscale/fairscale-0.3.0.tar.gz/fairscale-0.3.0/fairscale/nn/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .data_parallel import FullyShardedDataParallel, ShardedDataParallel
+from .misc import FlattenParamsWrapper
+from .moe import MOELayer, Top2Gate
+from .pipe import Pipe, PipeRPCWrapper
+
+__all__ = [
+    "FlattenParamsWrapper",
+    "FullyShardedDataParallel",
+    "LazyModule",
+    "Pipe",
+    "PipeRPCWrapper",
+    "ShardedDataParallel",
+    "Top2Gate",
+]
