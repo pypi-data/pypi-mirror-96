063785e3ef2e04dde42d9265ef32934e60787b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:45 +0000

    Add package xmq-python/0.0.3/xmq_python-0.0.3-py3-none-any.whl

---
+++ packages/xmq-python/xmq_python-0.0.3-py3-none-any.whl/xmq_python/comsumer.py
@@ -0,0 +1,21 @@
+from .adapters.comsumer_amqp import ComsumerAMQP
+from .mq_type import *
+
+
+class Comsumer(object):
+    def __init__(self, mq_type='AMQP', comsumer_group_id='', lookup_address='', node_address='', access_key='', access_secret='', thread_num=None, batch_size=None, username=None, password=None, **config):
+        if mq_type == MQ_TYPE_AMQP:
+            self.__agent = ComsumerAMQP(comsumer_group_id=comsumer_group_id, lookup_address=lookup_address,
+                                        node_address=node_address, access_key=access_key, access_secret=access_secret, thread_num=thread_num, batch_size=batch_size, username=None, password=None, **config)
+        else:
+            self.__agent = ComsumerAMQP(comsumer_group_id=comsumer_group_id, lookup_address=lookup_address,
+                                        node_address=node_address, access_key=access_key, access_secret=access_secret, thread_num=thread_num, batch_size=batch_size, username=None, password=None, **config)
+
+    def Register(self, topic_group: str, topic: str, callback, **route):
+        self.__agent.Register(topic_group, topic, callback, **route)
+
+    def Start(self):
+        self.__agent.Start()
+
+    def Stop(self):
+        self.__agent.Stop()
