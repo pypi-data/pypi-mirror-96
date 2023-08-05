6f842b1525b413a681f4454f80a81a5235b07407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:55 +0000

    Add package django-postgres-extra/2.0.3rc2/django-postgres-extra-2.0.3rc2.tar.gz

---
+++ packages/django-postgres-extra/django-postgres-extra-2.0.3rc2.tar.gz/django-postgres-extra-2.0.3rc2/psqlextra/indexes/__init__.py
@@ -0,0 +1,4 @@
+from .case_insensitive_unique_index import CaseInsensitiveUniqueIndex
+from .conditional_unique_index import ConditionalUniqueIndex
+
+__all__ = ["ConditionalUniqueIndex", "CaseInsensitiveUniqueIndex"]
