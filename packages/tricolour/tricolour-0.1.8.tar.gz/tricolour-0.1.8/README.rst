236543dd393d99aa61bdcfd6fd6e14af7f545d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:32 +0000

    Add package tricolour/0.1.8/tricolour-0.1.8.tar.gz

---
+++ packages/tricolour/tricolour-0.1.8.tar.gz/tricolour-0.1.8/README.rst
@@ -0,0 +1,6 @@
+=========
+Tricolour
+=========
+
+A variant of the Science Data Processing flagging code, wrapped in dask,
+operating on Measurement Sets.
---
+++ packages/tricolour/tricolour-0.1.8.tar.gz/tricolour-0.1.8/tricolour/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Tricolour."""
+
+from donfig import Config
+
+__version__ = '0.1.8'
+__author__ = """Simon Perkins"""
+__email__ = 'sperkins@ska.ac.za'
+
+config = Config("tricolour")
