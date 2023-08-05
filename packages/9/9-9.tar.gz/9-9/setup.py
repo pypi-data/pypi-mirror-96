45b00c96084a8517a54d218f4cf23e2d901f47c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:34 +0000

    Add package 9/9/9-9.tar.gz

---
+++ packages/9/9-9.tar.gz/9-9/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='9',
+    version='9',
+    description='For future projects.',
+)
