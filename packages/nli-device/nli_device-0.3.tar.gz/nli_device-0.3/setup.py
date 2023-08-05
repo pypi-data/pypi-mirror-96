4c259f752d043addadb4f8c52baf6097f5ca83a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package nli-device/0.3/nli_device-0.3.tar.gz

---
+++ packages/nli-device/nli_device-0.3.tar.gz/nli_device-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nli_device',
+      version='0.3',
+      description='natural language inference for all devices',
+      packages=['nli_device'],
+      zip_safe=False)
