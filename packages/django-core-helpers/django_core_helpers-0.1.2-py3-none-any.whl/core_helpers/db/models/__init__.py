5cf6da59887e1db5baa5e1ef2dcb0c94c221ccc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:36 +0000

    Add package django-core-helpers/0.1.2/django_core_helpers-0.1.2-py3-none-any.whl

---
+++ packages/django-core-helpers/django_core_helpers-0.1.2-py3-none-any.whl/core_helpers/db/models/__init__.py
@@ -0,0 +1,19 @@
+from .core import *
+
+__all__ = [
+    "UUIDModel",
+    "ActiveModel",
+    "TimeStampedModel",
+    "DeletableModel",
+    "DeletableUUIDModel",
+    "ActiveUUIDModel",
+    "ActiveTimeStampedModel",
+    "ActiveDeletableModel",
+    "ActiveTimeStampedUUIDModel",
+    "ActiveDeletableUUIDModel",
+    "ActiveTimeStampedDeletableModel",
+    "ActiveTimeStampedDeletableUUIDModel",
+    "TimeStampedUUIDModel",
+    "TimeStampedDeletableModel",
+    "TimeStampedDeletableUUIDModel",
+]
