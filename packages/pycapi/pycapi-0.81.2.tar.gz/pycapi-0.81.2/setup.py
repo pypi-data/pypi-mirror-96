d6be770f9bcfb85249b5788232271b78a4abd9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:16 +0000

    Add package pycapi/0.81.2/pycapi-0.81.2.tar.gz

---
+++ packages/pycapi/pycapi-0.81.2.tar.gz/pycapi-0.81.2/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from setuptools import Extension, setup  # type: ignore
+
+
+if __name__ == "__main__":
+
+    with (Path(__file__).parent / "README.md").open(encoding="UTF-8") as file:
+        long_description = file.read()
+
+    setup(
+        author="Brandt Bucher",
+        author_email="brandtbucher@gmail.com",
+        description="Over 600 fast Python bindings to the CPython C API.",
+        ext_modules=[Extension(name="pycapi", sources=["pycapi.c"])],
+        keywords="API C CPython Python",
+        license="MIT",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        name="pycapi",
+        url="https://github.com/brandtbucher/pycapi",
+        version="0.81.2",
+    )
