bc36bc050ba413ddea50d8198f706e009ba24115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:56 +0000

    Add package nli/0.4/nli-0.4.tar.gz

---
+++ packages/nli/nli-0.4.tar.gz/nli-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nli',
+      version='0.4',
+      description='natural language inference using tpu device',
+      packages=['nli'],
+      zip_safe=False)
