3575b7c8ad69a6acf1570335d7cb56d8b7fa7019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package xso/0.0.1/xso-0.0.1.tar.gz

---
+++ packages/xso/xso-0.0.1.tar.gz/xso-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+      name='xso',
+      version='0.0.1',
+      description='',
+      long_description='framework for building models based on differential equations, an extension of xarray-simlab',
+      url='https://github.com/ben1post/phydra',
+      author='Benjamin Post',
+      author_email='ben@anoutpost.com',
+      packages=[],
+      classifiers=['Development Status :: 1 - Planning'],
+      )
