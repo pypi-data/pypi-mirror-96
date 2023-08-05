8e8655e432825575b1aac65db84a5bff855c49fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:33 +0000

    Add package metlibvi/0.1.0/metlibvi-0.1.0.tar.gz

---
+++ packages/metlibvi/metlibvi-0.1.0.tar.gz/metlibvi-0.1.0/README.md
@@ -0,0 +1,2 @@
+# metlib
+Library for metropolized variational inference.
---
+++ packages/metlibvi/metlibvi-0.1.0.tar.gz/metlibvi-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='metlibvi',
+    packages=find_packages(include=['metlibvi']),
+    version='0.1.0',
+    description='Library for metropolized variational inference.',
+    author='Nikita Kotelevskii, Achille Thin',
+    author_email='Nikita.Kotelevskii@skoltech.ru',
+    url='https://github.com/nkotelevskii/metlib',
+    install_requires=['torch'],
+    license='MIT',
+    long_description="https://github.com/nkotelevskii/metlib/blob/master/README.md",
+
+)
