63902d7cdaad020222888a7359d405c06f882206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package FlyBrainLab/1.1.4/FlyBrainLab-1.1.4.tar.gz

---
+++ packages/FlyBrainLab/FlyBrainLab-1.1.4.tar.gz/FlyBrainLab-1.1.4/flybrainlab/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+import pkg_resources
+__version__ = pkg_resources.require('flybrainlab')[0].version
+__min_NeuroMynerva_version_supported__ = '0.2.10'
+__min_NeuroArch_version_supported__ = '0.3.0'
+from .Client import *
+from .widget import WidgetManager
+
+_initialized = False
+
+
+def init():
+    """Initialize FBLClient for current kernel"""
+    global _initialized
+    global widget_manager
+    global client_manager
+    global get_client
+    if _initialized:
+        return
+    widget_manager = WidgetManager()
+    client_manager = MetaClient()
+    def get_client(id = 0):
+        keys = list(client_manager.clients.keys())
+        key = keys[id]
+        client = client_manager.clients[key]['client']
+        return client
+    _initialized = True
