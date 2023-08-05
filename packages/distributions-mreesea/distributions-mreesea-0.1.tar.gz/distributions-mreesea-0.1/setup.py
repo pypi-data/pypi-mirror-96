1b5fad8a7ce43254e9b2d5a89249eabd14113302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:19 +0000

    Add package distributions-mreesea/0.1/distributions-mreesea-0.1.tar.gz

---
+++ packages/distributions-mreesea/distributions-mreesea-0.1.tar.gz/distributions-mreesea-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-mreesea',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions-mreesea'],
+      zip_safe=False)
