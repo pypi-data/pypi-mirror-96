bd3b9a48986fd24f3ebd3d4b9a2439a15238f73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:30 +0000

    Add package distributions-z1/0.1/distributions_z1-0.1.tar.gz

---
+++ packages/distributions-z1/distributions_z1-0.1.tar.gz/distributions_z1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_z1',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_z1'],
+      author = 'Richmond Yeboah',
+      author_email = 'Richmond.k.yeboah@gmail.com',
+      zip_safe=False)
