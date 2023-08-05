694d56418c22b7c256a62aedd8fc0fd6bc9f667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:52 +0000

    Add package pipedown/0.0.2/pipedown-0.0.2-py3-none-any.whl

---
+++ packages/pipedown/pipedown-0.0.2-py3-none-any.whl/pipedown/nodes/metrics/mean_squared_error.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+from pipedown.nodes.base.metric import Metric
+
+
+class MeanSquaredError(Metric):
+    def run(self, y_pred: pd.Series, y_true: pd.Series):
+        return np.mean(np.square(y_pred - y_true))
+
+    def get_metric_name(self):
+        return "mean_squared_error"
