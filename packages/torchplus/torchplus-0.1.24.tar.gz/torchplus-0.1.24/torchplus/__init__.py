0d1eb4571598e48e56185b3d3b9486925f95250a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:11 +0000

    Add package torchplus/0.1.24/torchplus-0.1.24.tar.gz

---
+++ packages/torchplus/torchplus-0.1.24.tar.gz/torchplus-0.1.24/torchplus/__init__.py
@@ -0,0 +1,10 @@
+#! python3.8 -u
+#  -*- coding: utf-8 -*-
+
+##############################
+## Project PyCTLib
+## Package torchplus
+##############################
+
+from .tensor import *
+from .tensorfunc import *
