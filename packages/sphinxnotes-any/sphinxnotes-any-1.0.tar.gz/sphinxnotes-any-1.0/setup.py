3a03e0fa812eead0a87fcba1e3ebdd083b3c93be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package sphinxnotes-any/1.0/sphinxnotes-any-1.0.tar.gz

---
+++ packages/sphinxnotes-any/sphinxnotes-any-1.0.tar.gz/sphinxnotes-any-1.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from doc import project
+
+with open('README.rst') as f:
+    long_desc = f.read()
+
+setup(
+    name=project.name,
+    version=project.version,
+    url=project.url,
+    download_url=project.download_url,
+    project_urls=project.project_urls,
+    license=project.license,
+    author=project.author,
+    description=project.description,
+    long_description=long_desc,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    keywords=project.keywords,
+    platforms='any',
+    python_requires='>=3',
+    packages=find_packages(),
+    include_package_data=True,
+    # sphinx.util.compat.Directive class is now deprecated in 1.6
+    install_requires=['Sphinx>=1.6', 'Jinja2'],
+    namespace_packages=['sphinxnotes'],
+)
