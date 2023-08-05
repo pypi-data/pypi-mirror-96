5159b5ad72bc5936cd1e991cb00ccf1489f1dd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:12 +0000

    Add package ocean-lib/0.5.11/ocean_lib-0.5.11-py2.py3-none-any.whl

---
+++ packages/ocean-lib/ocean_lib-0.5.11-py2.py3-none-any.whl/ocean_lib/__init__.py
@@ -0,0 +1,5 @@
+__author__ = """OceanProtocol"""
+__version__ = "__version__ = '0.5.11'"
+
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
---
+++ packages/ocean-lib/ocean_lib-0.5.11-py2.py3-none-any.whl/ocean_lib/config_provider.py
@@ -0,0 +1,25 @@
+#  Copyright 2018 Ocean Protocol Foundation
+#  SPDX-License-Identifier: Apache-2.0
+
+
+class ConfigProvider:
+    """Provides the Config instance."""
+
+    _config = None
+
+    @staticmethod
+    def get_config():
+        """ Get a Config instance."""
+        if not ConfigProvider._config:
+            raise AssertionError("set_config first.")
+        return ConfigProvider._config
+
+    @staticmethod
+    def set_config(config):
+        """
+         Set a Config instance.
+
+        :param config: Config
+        :return:  New Config instance.
+        """
+        ConfigProvider._config = config
