20cbabec6d34bf250b009a40627a443cf4bd6d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package pmlab-lite/0.4.5/pmlab_lite-0.4.5-py3-none-any.whl

---
+++ packages/pmlab-lite/pmlab_lite-0.4.5-py3-none-any.whl/pmlab_lite/alignments/variables.py
@@ -0,0 +1,9 @@
+open_list=[]
+closed_list=[]
+solutions=[]
+
+#Storing the specification of SP net
+incidence_matrix = []
+init_mark_vector = []
+final_mark_vector = []
+transitions_by_index = dict()
