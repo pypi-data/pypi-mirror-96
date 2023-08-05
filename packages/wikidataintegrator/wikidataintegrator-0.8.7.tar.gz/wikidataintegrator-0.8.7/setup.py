1a71ad50aa61415fe922e99704c6618bd237e01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package wikidataintegrator/0.8.7/wikidataintegrator-0.8.7.tar.gz

---
+++ packages/wikidataintegrator/wikidataintegrator-0.8.7.tar.gz/wikidataintegrator-0.8.7/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.8.7"
+
+setup(
+    name='wikidataintegrator',
+    version=VERSION,
+    author='Andra Waagmeester, Greg Stupp, Sebastian Burgstaller-Muehlbacher ',
+    author_email='andra@micel.io',
+    description='Python package for reading and writing to/from Wikidata',
+    license='MIT',
+    keywords='Wikidata genewiki biology chemistry medicine ShEx citations',
+    url='https://github.com/sulab/WikidataIntegrator',
+    packages=find_packages(),
+    # packages=['wikidataintegrator',
+    #          'wikidataintegrator.ref_handlers', 'wikidataintegrator.wdi_helpers'],
+    include_package_data=True,
+    ## long_description=read('README.md'),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'requests',
+        'python-dateutil',
+        'simplejson',
+        'pandas',
+        'tqdm',
+        'mwoauth',
+        'oauthlib',
+        'jsonasobj',
+        'pyshex',
+        'backoff',
+    ],
+)
---
+++ packages/wikidataintegrator/wikidataintegrator-0.8.7.tar.gz/wikidataintegrator-0.8.7/wikidataintegrator/wdi_config.py
@@ -0,0 +1,36 @@
+"""
+Config global options
+Options can be changed at run time. See tests/test_backoff.py for usage example
+
+Options:
+BACKOFF_MAX_TRIES: maximum number of times to retry failed request to wikidata endpoint.
+        Default: None (retry indefinitely)
+        To disable retry, set value to 1
+BACKOFF_MAX_VALUE: maximum number of seconds to wait before retrying. wait time will increase to this number
+        Default: 3600 (one hour)
+USER_AGENT_DEFAULT: default user agent string used for http requests. Both to wikibase api, query service and others.
+    See: https://meta.wikimedia.org/wiki/User-Agent_policy
+"""
+import pkg_resources
+
+try:
+    __version__ = pkg_resources.get_distribution("wikidataintegrator").version
+except Exception as e:
+    __version__ = ""
+
+config = {
+    'BACKOFF_MAX_TRIES': None,
+    'BACKOFF_MAX_VALUE': 3600,
+    'USER_AGENT_DEFAULT': 'wikidataintegrator/{}'.format(__version__),
+    'MAXLAG': 5,
+    'PROPERTY_CONSTRAINT_PID': 'P2302',
+    'DISTINCT_VALUES_CONSTRAINT_QID': 'Q21502410',
+    'COORDINATE_GLOBE_QID': 'http://www.wikidata.org/entity/Q2',
+    'CALENDAR_MODEL_QID': 'http://www.wikidata.org/entity/Q1985727',
+    'MEDIAWIKI_API_URL': 'https://www.wikidata.org/w/api.php',
+    'MEDIAWIKI_INDEX_URL': 'https://www.wikidata.org/w/index.php',
+    'SPARQL_ENDPOINT_URL': 'https://query.wikidata.org/sparql',
+    "ENTITY_SCHEMA_REPO": "https://www.wikidata.org/wiki/Special:EntitySchemaText/",
+    'WIKIBASE_URL': 'http://www.wikidata.org',
+    'CONCEPT_BASE_URI': 'http://www.wikidata.org/entity/'
+}
