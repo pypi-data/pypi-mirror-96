74dcb04db7c798f9675481c575fd2aaf4c08517c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:44 +0000

    Add package scrape-search-engine/0.1.9/scrape-search-engine-0.1.9.tar.gz

---
+++ packages/scrape-search-engine/scrape-search-engine-0.1.9.tar.gz/scrape-search-engine-0.1.9/setup.py
@@ -0,0 +1,34 @@
+__author__ = 'Sujit Mandal'
+#Date : 22-08-2020
+from setuptools import setup 
+
+def readme():
+    with open('README.md') as files:
+        README = files.read()
+
+    return(README)
+
+setup(
+    name = 'scrape-search-engine',
+    version = '0.1.9',
+    description = "Search anything on the different Search Engine's it will collect all the links.",
+    long_description = readme(),
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/sujitmandal/scrape-search-engine',
+    author = 'Sujit Mandal',
+    author_email = 'mandals974@gmail.com',
+    license = 'MIT',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    packages = ['ScrapeSearchEngine'],
+    include_package_data = True,
+)
