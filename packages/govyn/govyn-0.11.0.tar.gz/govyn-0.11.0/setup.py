39a2565c43cc3dde82b4406187a8a5d718dda544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:34 +0000

    Add package govyn/0.11.0/govyn-0.11.0.tar.gz

---
+++ packages/govyn/govyn-0.11.0.tar.gz/govyn-0.11.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open('readme.md', 'r', encoding = 'utf-8') as fh:
+	long_description = fh.read()
+
+setup(
+	name = 'govyn',
+	version = '0.11.0',
+	author = 'Ruan Pearce-Authers',
+	author_email = 'ruanpa@outlook.com',
+	description = 'HTTP APIs in typed Python',
+	url = 'https://github.com/returnString/govyn',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = [ 'govyn' ],
+	package_data = {
+		'govyn': [ 'py.typed' ],
+	},
+	zip_safe = False,
+	install_requires = [
+		'chardet < 4.0.0',
+		'starlette >= 0.13',
+		'uvicorn >= 0.11',
+		'dacite >= 1.5.1',
+		'requests >= 2.23',
+		'aioprometheus[aiohttp] >= 20.0',
+	],
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	python_requires = '>= 3.8',
+)
