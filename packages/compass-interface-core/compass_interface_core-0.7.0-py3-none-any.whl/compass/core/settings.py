16cbec21cff4c7a296858686bd53326290221a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:50 +0000

    Add package compass-interface-core/0.7.0/compass_interface_core-0.7.0-py3-none-any.whl

---
+++ packages/compass-interface-core/compass_interface_core-0.7.0-py3-none-any.whl/compass/core/settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    base_url = "https://compass.scouts.org.uk"
+    date_format = "%d %B %Y"  # dd Month YYYY
+    org_number = 10000001
+    total_requests = 0
+    wcf_json_endpoint = "/JSon.svc"  # Windows communication foundation JSON service endpoint
+    web_service_path = base_url + wcf_json_endpoint
