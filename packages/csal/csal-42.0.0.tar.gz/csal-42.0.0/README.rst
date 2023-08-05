23d9cd413e65c37eeb000a30ffc4ae4d8391bb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:36 +0000

    Add package csal/42.0.0/csal-42.0.0.tar.gz

---
+++ packages/csal/csal-42.0.0.tar.gz/csal-42.0.0/README.rst
@@ -0,0 +1,5 @@
+=====
+csal
+=====
+
+This is a placeholder for the ``csal`` package.
---
+++ packages/csal/csal-42.0.0.tar.gz/csal-42.0.0/csal/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for csal."""
+
+__author__ = """Bo Bayles"""
+__email__ = 'bo@bbayles.com'
+__version__ = '42.0.0'
