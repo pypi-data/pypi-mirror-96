1eb3b815252f05b498a89a57f0cf87ed28597363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:06 +0000

    Add package figenv/0.2.1/figenv-0.2.1-py3-none-any.whl

---
+++ packages/figenv/figenv-0.2.1-py3-none-any.whl/figenv.py
@@ -0,0 +1,54 @@
+import os
+
+
+class MetaConfig(type):
+
+    def __init__(cls, name, bases, dict):
+        super().__init__(name, bases, dict)
+        cls.name = name
+        cls._dict = dict
+
+    def __getattribute__(cls, name):
+        '''
+        Return value of ``name`` and ``_dict``
+
+        Fall back to getattr for everything else
+        '''
+        if name in ('name', '_dict'):
+            return super().__getattribute__(name)
+        raise AttributeError('Fallback to environment')
+
+    def __dir__(cls):
+        return list(cls._dict)
+
+    def __getattr__(cls, name):
+        '''
+        Check if attribute is available on the class. If it is, then check
+        the environment variables for that. If it is not in the environment
+        variables, then return the default set on the class. Otherwise raise
+        an AttributeError.
+        '''
+        prefix = cls._dict.get('ENV_PREFIX', '')
+        load_all = cls._dict.get('ENV_LOAD_ALL', False)
+
+        if (not load_all and name not in cls._dict) or (name not in cls._dict and prefix + name not in os.environ):
+            raise AttributeError(f"type object {cls.name} has no attribute '{name}'")
+
+        if prefix + name in os.environ:
+            value = os.environ[prefix + name]
+        else:
+            value = cls._dict[name]
+
+        if callable(value):
+            value = value(cls)
+
+        if not isinstance(value, str):
+            return value
+        elif value.lower() in ('true', 'false'):
+            value = True if value.lower() == 'true' else False
+        elif value.count('.') == 1 and ''.join(filter(lambda x: x != '.', value)).isdigit():
+            value = float(value)
+        elif value.isdigit():
+            value = int(value)
+
+        return value
