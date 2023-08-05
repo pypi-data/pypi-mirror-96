f19cb5e8cc713595fbd19e15a2a7cb9bb1010fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:58 +0000

    Add package lyptest1/0.0.37/lyptest1-0.0.37.tar.gz

---
+++ packages/lyptest1/lyptest1-0.0.37.tar.gz/lyptest1-0.0.37/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup( name = 'lyptest1',
+       version = '0.0.37', description = 'python 3.7-->3.6 ', long_description = 'library ',
+       author = 'pylyp456', author_email = '25506871@qq.com', url = 'https://github.com/ligaopan/lgp-library', license = 'MIT Licence', keywords = 'testing testautomation', platforms = 'any',
+
+       python_requires = '>=3.6.*', install_requires = [],
+       package_dir = {'': 'src'}, packages = find_packages('src')
+,
+       entry_points = {
+              'console_scripts': [
+                     # 'say4 = twine2.hello:main',
+                     'say4 = twine2.__main__:main',
+              ]
+              ,
+              'say4.registered_commands': [
+                     'upload = twine2.commands.upload:main',
+              ],
+       }
+
+
+       )
