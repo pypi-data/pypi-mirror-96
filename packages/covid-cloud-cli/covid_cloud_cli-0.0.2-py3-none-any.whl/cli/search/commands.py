f7a06b43d836216becc9aefcb71c861e65433f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:12 +0000

    Add package covid-cloud-cli/0.0.2/covid_cloud_cli-0.0.2-py3-none-any.whl

---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.2-py3-none-any.whl/cli/search/commands.py
@@ -0,0 +1,23 @@
+from .tables import commands as tables_commands
+from cli.utils import assert_config
+from client import *
+
+
+@click.group()
+@click.pass_context
+def search(ctx):
+    assert_config(ctx, 'search-url')
+    ctx.obj["search-client"] = SearchClient(ctx.obj['search-url'])
+
+
+@search.command()
+@click.pass_context
+@click.argument("q")
+@click.option('-d', '--download', is_flag=True)
+@click.option('-j', '--use-json', '--json', is_flag=True)
+@click.option('-r', '--raw', is_flag=True)
+def query(ctx, q, download, use_json, raw):
+    click.echo(search_client.query(ctx.obj['search-url'], q, download, use_json, raw))
+
+
+search.add_command(tables_commands.tables)
---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.2-py3-none-any.whl/client/covid_cloud_client.py
@@ -0,0 +1,22 @@
+from client import *
+
+
+class CovidCloudClient:
+    def __init__(self, client_id, client_secret, personal_access_token, search_url=None, drs_url=None):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.personal_access_token = personal_access_token
+        self.search_url = search_url
+        self.drs_url = drs_url
+
+    def query(self, q, download=False, use_json=False, raw=False):
+        return query(self.search_url, q, download, use_json, raw)
+
+    def list_tables(self):
+        return get_tables(self.search_url)
+
+    def get_table(self, table_name):
+        return get_table(self.search_url, table_name)
+
+    def download(self, urls, output_dir=downloads_directory):
+        return download_files(self.drs_url, urls, output_dir)
