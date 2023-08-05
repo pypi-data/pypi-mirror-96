0b305dc8a6cd24345866a8c7d514ba87eb5ddc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:27 +0000

    Add package data-repo-client/1.27.1/data-repo-client-1.27.1.tar.gz

---
+++ packages/data-repo-client/data-repo-client-1.27.1.tar.gz/data-repo-client-1.27.1/data_repo_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from data_repo_client.api.data_repository_service_api import DataRepositoryServiceApi
+from data_repo_client.api.repository_api import RepositoryApi
+from data_repo_client.api.resources_api import ResourcesApi
+from data_repo_client.api.unauthenticated_api import UnauthenticatedApi
