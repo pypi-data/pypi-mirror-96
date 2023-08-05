c711b5912f6449c6d35d06861d11c33cdac07e51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package exoplasim/2.1.5/exoplasim-2.1.5.tar.gz

---
+++ packages/exoplasim/exoplasim-2.1.5.tar.gz/exoplasim-2.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+ 
+setup(
+    name='exoplasim',
+    version='2.1.5',
+    packages=['exoplasim',],
+    zip_safe=False,
+    install_requires=["numpy","netCDF4","matplotlib","scipy"],
+    include_package_data=True,
+    author='Adiv Paradise',
+    author_email='paradise.astro@gmail.com',
+    license='GNU General Public License',
+    license_files="LICENSE.TXT",
+    url='https://github.com/alphaparrot/ExoPlaSim',
+    description='Exoplanet GCM',
+    long_description_content_type='text/x-rst',
+    long_description=open('README.rst').read(),
+    )
