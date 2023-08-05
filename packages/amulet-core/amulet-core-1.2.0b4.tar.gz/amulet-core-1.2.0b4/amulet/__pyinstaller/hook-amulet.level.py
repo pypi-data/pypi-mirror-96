50df6fa7b5527bc01c2b7a0a8e0513b17d1a442f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:12 +0000

    Add package amulet-core/1.2.0b4/amulet-core-1.2.0b4.tar.gz

---
+++ packages/amulet-core/amulet-core-1.2.0b4.tar.gz/amulet-core-1.2.0b4/amulet/__pyinstaller/hook-amulet.level.py
@@ -0,0 +1,7 @@
+from PyInstaller.utils.hooks import collect_data_files
+import pkgutil
+from amulet import level
+
+hiddenimports = [
+    name for _, name, _ in pkgutil.walk_packages(level.__path__, level.__name__ + ".")
+]
