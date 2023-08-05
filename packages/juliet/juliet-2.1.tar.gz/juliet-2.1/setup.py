5aa0ea1c9071ac9fba8dfaa2ddc575edd4aba051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:15 +0000

    Add package juliet/2.1/juliet-2.1.tar.gz

---
+++ packages/juliet/juliet-2.1.tar.gz/juliet-2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='juliet',
+      version='2.1',
+      description='juliet: a versatile modelling tool for transiting exoplanets, radial-velocity systems or both',
+      url='http://github.com/nespinoza/juliet',
+      author='Nestor Espinoza',
+      author_email='espinoza@mpia.de',
+      license='MIT',
+      packages=['juliet'],
+      install_requires=['batman-package','radvel','dynesty','george','celerite','astropy','numpy','scipy'],
+      python_requires='>=2.7',
+      extras_requires={
+            'seaborn':['seaborn'],
+            'pymultinest':['pymultinest'],
+            'matplotlib':['matplotlib'],},
+      entry_points={
+            'console_scripts': [
+                 'juliet=juliet.__main__:main'
+            ]},
+      zip_safe=False)
