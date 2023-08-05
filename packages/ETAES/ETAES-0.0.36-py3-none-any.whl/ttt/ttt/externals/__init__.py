e16a428cc19f629441b055fe71d7346b68a1d55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:48 +0000

    Add package ETAES/0.0.36/ETAES-0.0.36-py3-none-any.whl

---
+++ packages/ETAES/ETAES-0.0.36-py3-none-any.whl/ttt/ttt/externals/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+# @File     :__init__.py.py
+# @Time     :2021/2/22
+# @Author   :jiawei.li
+# @Software :PyCharm
+# @Desc     :None
+
+from .my import *
+
+__all__ = []
