dde518424ef3336e9f067c517fbedcfd7fb44bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package fastapi-limiter/0.1.2/fastapi_limiter-0.1.2-py3-none-any.whl

---
+++ packages/fastapi-limiter/fastapi_limiter-0.1.2-py3-none-any.whl/fastapi_limiter/__init__.py
@@ -0,0 +1,49 @@
+from math import ceil
+from typing import Callable
+
+import aioredis
+from fastapi import HTTPException
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.status import HTTP_429_TOO_MANY_REQUESTS
+
+
+async def default_identifier(request: Request):
+    forwarded = request.headers.get("X-Forwarded-For")
+    if forwarded:
+        return forwarded.split(",")[0]
+    return request.client.host
+
+
+async def default_callback(request: Request, response: Response, pexpire: int):
+    """
+    default callback when too many requests
+    :param request:
+    :param pexpire: The remaining milliseconds
+    :return:
+    """
+    expire = ceil(pexpire / 1000)
+
+    raise HTTPException(
+        HTTP_429_TOO_MANY_REQUESTS, "Too Many Requests", headers={"Retry-After": str(expire)}
+    )
+
+
+class FastAPILimiter:
+    redis: aioredis.Redis = None
+    prefix: str = None
+    identifier: Callable = None
+    callback: Callable = None
+
+    @classmethod
+    def init(
+        cls,
+        redis: aioredis.Redis,
+        prefix: str = "fastapi-limiter",
+        identifier: Callable = default_identifier,
+        callback: Callable = default_callback,
+    ):
+        cls.redis = redis
+        cls.prefix = prefix
+        cls.identifier = identifier
+        cls.callback = callback
