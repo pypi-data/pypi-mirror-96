d9e439167e432201271ceee2c27de5a38a84f0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:09 +0000

    Add package statmach/1.0.8/statmach-1.0.8.tar.gz

---
+++ packages/statmach/statmach-1.0.8.tar.gz/statmach-1.0.8/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+import statmach
+
+
+def read_text(file_name: str):
+    with open(file_name, "r") as fh:
+        return fh.read()
+
+
+setuptools.setup(
+    name='statmach',
+    version='1.0.8',
+    url=statmach.__repository__,
+    license=read_text('LICENSE'),
+    author=statmach.__author__,
+    author_email='howard.lovatt@gmail.com',
+    description=statmach.__description__,
+    # read_text('README.md') doesn't work because PyPi can't render README.md - PyPI bug.
+    long_description=f'See {statmach.__repository__} for detailed description.\n',  # read_text('README.md'),
+    long_description_content_type='text/markdown',
+    py_modules=['statmach'],
+    platforms=['any'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
