dccbbbfdb6e080d5e0f0bec61a889a912f5e3886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package appmap/0.1.0.dev9/appmap-0.1.0.dev9.tar.gz

---
+++ packages/appmap/appmap-0.1.0.dev9.tar.gz/appmap-0.1.0.dev9/appmap/_implementation/env.py
@@ -0,0 +1,54 @@
+"""Initialize from the environment"""
+import logging
+import logging.config
+import os
+
+
+def enabled():
+    return os.getenv("APPMAP", "false") == "true"
+
+
+def _configure_logging():
+    log_level = os.getenv("APPMAP_LOG_LEVEL", "warning").upper()
+
+    log_config = os.getenv("APPMAP_LOG_CONFIG")
+    log_stream = os.getenv("APPMAP_LOG_STREAM", "stderr")
+    log_stream = 'ext://sys.%s' % (log_stream)
+    config_dict = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'style': '{',
+                'format': '[{asctime}] {levelname} {name}: {message}'
+            }
+        },
+        'handlers': {
+            'default': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default'
+            }
+        },
+        'loggers': {
+            'appmap': {
+                'level': log_level,
+                'handlers': ['default'],
+                'propagate': False
+            }
+        }
+    }
+    if log_config is not None:
+        name, level = log_config.split('=', 2)
+        config_dict['loggers'].update({
+            name: {
+                'level': level.upper(),
+                'handlers': ['default'],
+                'propagate': False
+            }
+        })
+    logging.config.dictConfig(config_dict)
+
+
+def initialize():
+    _configure_logging()
+    logging.info('appmap enabled: %s', enabled())
---
+++ packages/appmap/appmap-0.1.0.dev9.tar.gz/appmap-0.1.0.dev9/appmap/test/test_recording.py
@@ -0,0 +1,37 @@
+"""Test recording context manager"""
+import json
+import os
+import sys
+
+from .appmap_test_base import AppMapTestBase
+
+
+class TestRecording(AppMapTestBase):
+    def test_recording_works(self, data_dir, monkeypatch):
+        with open(os.path.join(data_dir, 'expected.appmap.json')) as f:
+            expected_appmap = json.load(f)
+
+        sys.path.append(data_dir)
+
+        monkeypatch.setenv("APPMAP", "true")
+        monkeypatch.setenv("APPMAP_CONFIG",
+                           os.path.join(data_dir, 'appmap.yml'))
+
+        import appmap
+        # Reinitialize to pick up the environment variables just set
+        appmap._implementation.initialize()  # pylint: disable=protected-access
+        r = appmap.Recording()
+        with r:
+            from example_class import ExampleClass  # pylint: disable=import-error
+            ExampleClass.static_method()
+            ExampleClass.class_method()
+            ExampleClass().instance_method()
+            ExampleClass.what_time_is_it()
+            try:
+                ExampleClass().test_exception()
+            except:  # pylint: disable=bare-except  # noqa: E722
+                pass
+
+        generated_appmap = self.normalize_appmap(appmap.generation.dump(r))
+
+        assert generated_appmap == expected_appmap
