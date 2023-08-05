b28c65e8525c150130052f7525247c0f3cd06a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package idom-dash/0.0.2/idom_dash-0.0.2.tar.gz

---
+++ packages/idom-dash/idom_dash-0.0.2.tar.gz/idom_dash-0.0.2/idom_dash/IdomDashComponent.py
@@ -0,0 +1,30 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class IdomDashComponent(Component):
+    """An IdomDashComponent component.
+
+
+Keyword arguments:
+- viewId (string; optional): The view ID for this component instance"""
+    @_explicitize_args
+    def __init__(self, viewId=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['viewId']
+        self._type = 'IdomDashComponent'
+        self._namespace = 'idom_dash'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['viewId']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(IdomDashComponent, self).__init__(**args)
