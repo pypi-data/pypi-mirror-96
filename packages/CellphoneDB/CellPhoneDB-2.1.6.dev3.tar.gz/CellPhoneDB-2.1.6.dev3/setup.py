c324a5c55de89b6a66c46c93c4102aca0fafe8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:11 +0000

    Add package CellphoneDB/2.1.6.dev3/CellPhoneDB-2.1.6.dev3.tar.gz

---
+++ packages/CellphoneDB/CellPhoneDB-2.1.6.dev3.tar.gz/CellPhoneDB-2.1.6.dev3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "rt", encoding="utf-8") as fh:
+    install_requirements_txt = [line.strip() for line in fh.readlines()]
+
+setup(
+    name='CellPhoneDB',
+    author='TeichLab',
+    author_email='contact@cellphonedb.org',
+    version='2.1.6.dev3',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    maintainer='TeichLab',
+    maintainer_email='contact@cellphonedb.org',
+    url='https://cellphonedb.org',
+    license='MIT',
+    exclude_package_data={'': ['tools']},
+    entry_points={
+        'console_scripts':
+            [
+                'cellphonedb = cellphonedb.cellphonedb_cli:cli'
+            ]
+    },
+    install_requires=install_requirements_txt,
+)
