4c7cdaebf0fd6f093e07e70f244380a0159686b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:19 +0000

    Add package python-amazon-sp-api/0.2.4/python-amazon-sp-api-0.2.4.tar.gz

---
+++ packages/python-amazon-sp-api/python-amazon-sp-api-0.2.4.tar.gz/python-amazon-sp-api-0.2.4/sp_api/api/__init__.py
@@ -0,0 +1,25 @@
+from .finances.finances import Finances
+from .notifications.notifications import Notifications
+from .orders.orders import Orders
+from .product_fees.product_fees import ProductFees
+from .sellers.sellers import Sellers
+from .reports.reports import Reports
+from .products.products import Products
+from .sales.sales import Sales
+from .catalog.catalog import Catalog
+from .feeds.feeds import Feeds
+from .inventories.inventories import Inventories
+
+__all__ = [
+    "Sales",
+    "Products",
+    "Reports",
+    "Orders",
+    "Sellers",
+    "Notifications",
+    "ProductFees",
+    "Finances",
+    "Catalog",
+    "Feeds",
+    "Inventories"
+]
---
+++ packages/python-amazon-sp-api/python-amazon-sp-api-0.2.4.tar.gz/python-amazon-sp-api-0.2.4/sp_api/auth/access_token_client.py
@@ -0,0 +1,97 @@
+import requests
+
+import logging
+from cachetools import TTLCache
+
+from .credentials import Credentials
+from .access_token_response import AccessTokenResponse
+from sp_api.base import BaseClient
+import hashlib
+
+cache = TTLCache(maxsize=10, ttl=3600)
+grantless_cache = TTLCache(maxsize=10, ttl=3600)
+
+logger = logging.getLogger(__name__)
+
+
+class AccessTokenClient(BaseClient):
+    host = 'api.amazon.com'
+    grant_type = 'refresh_token'
+    path = '/auth/o2/token'
+
+    def __init__(self, refresh_token=None, account='default', credentials=None):
+        super().__init__(account, credentials)
+        self.cred = Credentials(refresh_token, self.credentials)
+
+    def get_auth(self) -> AccessTokenResponse:
+        """
+        Get's the access token
+        :return:AccessTokenResponse
+        """
+        global cache
+
+        cache_key = self._get_cache_key()
+        try:
+            access_token = cache[cache_key]
+            logger.debug('from cache')
+        except KeyError:
+            response = requests.post(self.scheme + self.host + self.path, data=self.data, headers=self.headers)
+            logger.debug('token refreshed')
+            cache = TTLCache(maxsize=10, ttl=3600)
+            cache[cache_key] = response.json()
+            access_token = response.json()
+
+        return AccessTokenResponse(**access_token)
+
+    def get_grantless_auth(self):
+        """
+        POST /auth/o2/token HTTP/l.l
+        Host: api.amazon.com
+        Content-Type: application/x-www-form-urlencoded;charset=UTF-8
+        grant_type=client_credentials
+        &scope=sellingpartnerapi::notifications
+        &client_id=foodev
+        &client_secret=Y76SDl2F
+        :return:
+        """
+        global grantless_cache
+        cache_key = self._get_cache_key()
+        try:
+            access_token = grantless_cache[cache_key]
+            logger.debug('from_cache')
+        except KeyError:
+            response = requests.post(self.scheme + self.host + self.path, data=self.grantless_data, headers=self.headers)
+            logger.debug('token_refreshed')
+            grantless_cache = TTLCache(maxsize=10, ttl=3600)
+            grantless_cache[cache_key] = response.json()
+            access_token = response.json()
+
+        return AccessTokenResponse(**access_token)
+
+    @property
+    def grantless_data(self):
+        return {
+            'grant_type': 'client_credentials',
+            'client_id': self.cred.client_id,
+            'scope': 'sellingpartnerapi::notifications',
+            'client_secret': self.cred.client_secret
+        }
+
+    @property
+    def data(self):
+        return {
+            'grant_type': self.grant_type,
+            'client_id': self.cred.client_id,
+            'refresh_token': self.cred.refresh_token,
+            'client_secret': self.cred.client_secret
+        }
+
+    @property
+    def headers(self):
+        return {
+            'User-Agent': self.user_agent,
+            'content-type': self.content_type
+        }
+
+    def _get_cache_key(self):
+        return 'access_token_' + hashlib.md5(self.cred.refresh_token.encode('utf-8')).hexdigest()
