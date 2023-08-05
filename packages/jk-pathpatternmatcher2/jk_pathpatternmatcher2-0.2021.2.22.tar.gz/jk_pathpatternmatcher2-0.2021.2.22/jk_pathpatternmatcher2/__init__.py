cfa2f733c092353c9958e6ae7771c7f5dd8f05d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:34 +0000

    Add package jk-pathpatternmatcher2/0.2021.2.22/jk_pathpatternmatcher2-0.2021.2.22.tar.gz

---
+++ packages/jk-pathpatternmatcher2/jk_pathpatternmatcher2-0.2021.2.22.tar.gz/jk_pathpatternmatcher2-0.2021.2.22/jk_pathpatternmatcher2/__init__.py
@@ -0,0 +1,16 @@
+﻿
+
+
+__version__ = "0.2021.2.22"
+
+
+
+from .PathPatternMatcher import PathPatternMatcher
+from .PathPatternMatcherCollection import PathPatternMatcherCollection
+from .Entry import Entry
+from .pm import compilePattern
+from .walk import walk
+
+
+
+
