d465c04a584d5c6fff7fbe971647f13fb2cf0c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package qrshare/0.4.0/qrshare-0.4.0.tar.gz

---
+++ packages/qrshare/qrshare-0.4.0.tar.gz/qrshare-0.4.0/qrshare/client/src/pages/main/components/FilterIcon.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Funnel from "../../../module/icons/Funnel.svelte";
+    import { searchCollapsed } from "../store";
+</script>
+
+<div class:active={!$searchCollapsed}>
+    <Funnel />
+</div>
+
+<style>
+    div {
+        display: flex;
+        align-items: center;
+    }
+
+    div.active :global(svg) {
+        fill: var(--color-primary);
+    }
+</style>
---
+++ packages/qrshare/qrshare-0.4.0.tar.gz/qrshare-0.4.0/qrshare/client/src/pages/main/store/index.js
@@ -0,0 +1,16 @@
+export {
+    meta,
+    title,
+    subtitle,
+    currentRoute,
+    routes,
+    qrMarkup,
+    isSearching,
+    searchInfo,
+    searchCollapsed,
+    searchResults,
+    searchQuery,
+    isSorted,
+    processedResults,
+} from "./store";
+export { updateStore, search } from "./actions";
