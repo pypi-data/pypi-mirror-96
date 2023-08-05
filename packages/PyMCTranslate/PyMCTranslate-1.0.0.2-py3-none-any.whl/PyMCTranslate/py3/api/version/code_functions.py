357687d5947dfd680be33adb1c56fbf40b94a85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:15 +0000

    Add package PyMCTranslate/1.0.0.2/PyMCTranslate-1.0.0.2-py3-none-any.whl

---
+++ packages/PyMCTranslate/PyMCTranslate-1.0.0.2-py3-none-any.whl/PyMCTranslate/py3/api/version/code_functions.py
@@ -0,0 +1,36 @@
+import glob
+import os
+import importlib.util
+
+code_functions = {}
+
+for code_file in glob.iglob(
+    os.path.join(
+        glob.escape(os.path.dirname(__file__)),
+        "..",
+        "..",
+        "..",
+        "code_functions",
+        "**",
+        "*.py",
+    ),
+    recursive=True,
+):
+    code_function_name = os.path.splitext(os.path.basename(code_file))[0]
+
+    spec = importlib.util.spec_from_file_location(
+        code_function_name,
+        code_file,
+    )
+    code_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(code_module)
+
+    assert hasattr(code_module, "main")
+    code_functions[code_function_name] = code_module.main
+
+
+def run(function_name, inputs):
+    assert (
+        function_name in code_functions
+    ), f"Function {function_name} could not be found"
+    return code_functions[function_name](*inputs)
