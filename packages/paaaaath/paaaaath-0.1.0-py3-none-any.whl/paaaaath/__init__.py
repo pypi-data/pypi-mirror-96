5c040334fdb1cac7504aced89f87c5b157c8971e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:49 +0000

    Add package paaaaath/0.1.0/paaaaath-0.1.0-py3-none-any.whl

---
+++ packages/paaaaath/paaaaath-0.1.0-py3-none-any.whl/paaaaath/__init__.py
@@ -0,0 +1,6 @@
+from .common import Path, PurePath
+from .http import HttpPath, PureHttpPath
+from .posix import PosixPath, PurePosixPath
+from .s3 import PureS3Path, S3Path
+from .uri import PureUriPath
+from .windows import PureWindowsPath, WindowsPath
