773cab8da06fbd0a65e04c34126130d897090a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:23 +0000

    Add package APNNetwork/0.0.1/APNNetwork-0.0.1.tar.gz

---
+++ packages/APNNetwork/APNNetwork-0.0.1.tar.gz/APNNetwork-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='APNNetwork',
+    version='0.0.1',
+    packages=['APN_Net'],
+    url='',
+    license='',
+    author='Yubadboy',
+    author_email='2025677540@qq.com',
+    description="My English is bad,APN's coder in bilibili,if it has bug,then write email to me."
+)
