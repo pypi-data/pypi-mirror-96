909580ba8ab9f056a335d80b4e0fd42792adc803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package ora2/3.1.5/ora2-3.1.5.tar.gz

---
+++ packages/ora2/ora2-3.1.5.tar.gz/ora2-3.1.5/openassessment/xblock/static/js/src/lms/oa_file_upload.js
@@ -0,0 +1,55 @@
+/*
+Upload a file using a one-time URL.
+This object doesn't do much work, but it makes it
+easier to stub out the upload in tests.
+
+This request requires appropriate CORS configuration for AJAX
+PUT requests on the server.
+
+Args:
+    url (string): The one-time URL we're uploading to.
+    file (File): The HTML5 file reference.
+
+Returns:
+    JQuery promise
+
+*/
+export class FileUploader {
+  upload(url, file) {
+    // eslint-disable-next-line new-cap
+    return $.Deferred((defer) => {
+      $.ajax({
+        url,
+        type: 'PUT',
+        data: file,
+        async: false,
+        processData: false,
+        contentType: file.type,
+        headers: { 'Content-Disposition': `attachment; filename*=UTF-8''${encodeURI(file.name)}` },
+      }).done(() => {
+        // Log an analytics event
+        Logger.log(
+          'openassessment.upload_file',
+          {
+            fileName: file.name,
+            fileSize: file.size,
+            fileType: file.type,
+          },
+        );
+
+        // Return control to the caller
+        defer.resolve();
+      }).fail((data, textStatus) => {
+        Logger.log(
+          'openassessment.upload_file_error',
+          {
+            statusText: data.statusText,
+          },
+        );
+        defer.rejectWith(this, [textStatus]);
+      });
+    }).promise();
+  }
+}
+
+export default FileUploader;
