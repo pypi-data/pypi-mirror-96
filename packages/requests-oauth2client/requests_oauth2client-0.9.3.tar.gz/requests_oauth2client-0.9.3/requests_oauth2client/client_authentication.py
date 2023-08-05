dd35c1e5d631820f7c8548ba7ec1a7b2a287ede0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:21 +0000

    Add package requests-oauth2client/0.9.3/requests_oauth2client-0.9.3.tar.gz

---
+++ packages/requests-oauth2client/requests_oauth2client-0.9.3.tar.gz/requests_oauth2client-0.9.3/requests_oauth2client/client_authentication.py
@@ -0,0 +1,205 @@
+from datetime import datetime
+from typing import Any, Callable
+from uuid import uuid4
+
+import furl  # type: ignore[import]
+import requests
+from jwcrypto.jwk import JWK  # type: ignore[import]
+from jwcrypto.jwt import JWT  # type: ignore[import]
+from requests.auth import _basic_auth_str
+
+from requests_oauth2client.utils import b64u_encode
+
+
+class ClientAuthenticationMethod(requests.auth.AuthBase):
+    """
+    Base class for the Client Authentication methods.
+    """
+
+    def __call__(self, request: requests.PreparedRequest) -> requests.PreparedRequest:
+        if (
+            request.method != "POST"
+            or request.headers["Content-Type"] != "application/x-www-form-urlencoded"
+        ):
+            raise RuntimeError(
+                "This request is not suitable to add OAuth2.0 Client Authentication"
+            )
+        return request
+
+
+class ClientSecretBasic(ClientAuthenticationMethod):
+    """
+    Handles client_secret_basic authentication (client_id and client_secret passed as Basic authentication)
+    """
+
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = str(client_id)
+        self.client_secret = str(client_secret)
+
+    def __call__(self, request: requests.PreparedRequest) -> requests.PreparedRequest:
+        request = super().__call__(request)
+        request.headers["Authorization"] = _basic_auth_str(self.client_id, self.client_secret)
+        return request
+
+
+class ClientSecretPost(ClientAuthenticationMethod):
+    """
+    Handles client_secret_post client authentication method (client_id and client_secret
+    passed as part of the request form data).
+    """
+
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = str(client_id)
+        self.client_secret = str(client_secret)
+
+    def __call__(self, request: requests.PreparedRequest) -> requests.PreparedRequest:
+        request = super().__call__(request)
+        data = furl.Query(request.body)
+        data.set([("client_id", self.client_id), ("client_secret", self.client_secret)])
+        request.prepare_body(data.params, files=None)
+        return request
+
+
+class ClientAssertionAuthenticationMethod(ClientAuthenticationMethod):
+    """
+    Base class for assertion based client authentication methods.
+    """
+
+    def __init__(self, client_id: str, alg: str, lifetime: int, jti_gen: Callable[[], str]):
+        self.client_id = str(client_id)
+        self.alg = alg
+        self.lifetime = lifetime
+        self.jti_gen = jti_gen
+
+    def client_assertion(self, audience: str) -> str:
+        raise NotImplementedError()
+
+    def __call__(self, request: requests.PreparedRequest) -> requests.PreparedRequest:
+        request = super().__call__(request)
+        token_endpoint = request.url
+        assert token_endpoint is not None
+        data = furl.Query(request.body)
+        client_assertion = self.client_assertion(token_endpoint)
+        data.set(
+            [
+                ("client_id", self.client_id),
+                ("client_assertion", client_assertion),
+                (
+                    "client_assertion_type",
+                    "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                ),
+            ]
+        )
+        request.prepare_body(data.params, files=None)
+        return request
+
+
+class ClientSecretJWT(ClientAssertionAuthenticationMethod):
+    """
+    Handles client_secret_jwt client authentication method (client_assertion symmetrically signed with the client_secret).
+    """
+
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        alg: str = "HS256",
+        lifetime: int = 60,
+        jti_gen: Callable[[], Any] = lambda: uuid4(),
+    ) -> None:
+        super().__init__(client_id, alg, lifetime, jti_gen)
+        self.client_secret = str(client_secret)
+
+    def client_assertion(self, audience: str) -> str:
+        iat = int(datetime.now().timestamp())
+        exp = iat + self.lifetime
+        jti = str(self.jti_gen())
+
+        jwk = JWK(kty="oct", k=b64u_encode(self.client_secret))
+
+        jwt = JWT(
+            header={"alg": self.alg},
+            claims={
+                "iss": self.client_id,
+                "sub": self.client_id,
+                "aud": audience,
+                "iat": iat,
+                "exp": exp,
+                "jti": jti,
+            },
+        )
+        jwt.make_signed_token(jwk)
+        assertion: str = jwt.serialize()
+        return assertion
+
+
+class PrivateKeyJWT(ClientAssertionAuthenticationMethod):
+    """
+    Handles private_key_jwt client authentication method (client_assertion asymmetrically signed with a private key).
+    """
+
+    def __init__(
+        self,
+        client_id: str,
+        private_jwk: dict,
+        alg: str = "RS256",
+        lifetime: int = 60,
+        kid: str = None,
+        jti_gen: Callable[[], Any] = lambda: uuid4(),
+    ):
+        alg = private_jwk.get("alg", alg)
+        if not alg:
+            raise ValueError(
+                "Asymmetric signing requires an alg, either as part of the private JWK, or passed as parameter"
+            )
+        kid = private_jwk.get("kid", kid)
+        if not kid:
+            raise ValueError(
+                "Asymmetric signing requires a kid, either as part of the private JWK, or passed as parameter"
+            )
+
+        super().__init__(client_id, alg, lifetime, jti_gen)
+        self.private_jwk = JWK(**private_jwk)
+        self.kid = kid
+
+    def client_assertion(self, audience: str, lifetime: int = 60, jti: str = None) -> str:
+        iat = int(datetime.now().timestamp())
+        exp = iat + lifetime
+        if jti is None:
+            jti = str(uuid4())
+        elif callable(jti):
+            jti = jti()
+
+        if not isinstance(jti, str):
+            jti = str(jti)
+
+        jwt = JWT(
+            header={"alg": self.alg, "kid": self.kid},
+            claims={
+                "iss": self.client_id,
+                "sub": self.client_id,
+                "aud": audience,
+                "iat": iat,
+                "exp": exp,
+                "jti": jti,
+            },
+        )
+        jwt.make_signed_token(self.private_jwk)
+        assertion: str = jwt.serialize()
+        return assertion
+
+
+class PublicApp(ClientAuthenticationMethod):
+    """
+    Handles the "none" authentication method (client only sends its client_id).
+    """
+
+    def __init__(self, client_id: str) -> None:
+        self.client_id = client_id
+
+    def __call__(self, request: requests.PreparedRequest) -> requests.PreparedRequest:
+        request = super().__call__(request)
+        data = furl.Query(request.body)
+        data.set([("client_id", self.client_id)])
+        request.prepare_body(data.params, files=None)
+        return request
---
+++ packages/requests-oauth2client/requests_oauth2client-0.9.3.tar.gz/requests_oauth2client-0.9.3/requests_oauth2client/token_response.py
@@ -0,0 +1,205 @@
+import base64
+import json
+import pprint
+import zlib
+from datetime import datetime, timedelta
+from typing import TYPE_CHECKING, Any, Callable, Dict, Optional, Union
+
+if TYPE_CHECKING:
+    from .client import OAuth2Client
+
+
+class BearerToken:
+    """
+    A wrapper around a Bearer Token and associated expiration date and refresh token,
+    as returned by an OAuth20 or OIDC Token Endpoint.
+    """
+
+    def __init__(
+        self,
+        access_token: str,
+        expires_at: datetime = None,
+        scope: str = None,
+        refresh_token: str = None,
+        token_type: str = "Bearer",
+        **kwargs: Any,
+    ):
+        if token_type != "Bearer":
+            raise ValueError("This is not Bearer Token!", token_type)
+        self.access_token = access_token
+        self.expires_at = expires_at
+        self.scope = scope
+        self.refresh_token = refresh_token
+        self.other = kwargs
+
+    def is_expired(self) -> Optional[bool]:
+        """
+        Returns true if the access token is expired at the time of the call.
+        :return:
+        """
+        if self.expires_at:
+            return datetime.now() > self.expires_at
+        return None
+
+    def authorization_header(self) -> str:
+        """
+        Returns the Authorization Header value containing this access token, correctly formatted as per RFC6750.
+        :return: the value to use in a HTTP Authorization Header
+        """
+        return f"Bearer {self.access_token}"
+
+    def __str__(self) -> str:
+        """
+        Returns the access token value, as a string
+        :return: the access token string
+        """
+        return self.access_token
+
+    def __contains__(self, key: str) -> bool:
+        """
+        Check existence of a key in the token response.
+        Allows testing like `assert "refresh_token" in token_response`.
+        :param key: a key
+        :return: True if the key exists in the token response, False otherwise
+        """
+        if key == "access_token":
+            return True
+        elif key == "refresh_token":
+            return self.refresh_token is not None
+        elif key == "scope":
+            return self.scope is not None
+        elif key == "token_type":
+            raise ValueError("token_type is always Bearer, explicitly or implicitly")
+        elif key == "expires_in":
+            return self.expires_at is not None
+        elif key is None:
+            return False
+        else:
+            return key in self.other
+
+    def __getattr__(self, key):
+        """
+        Returns items from this Token Response.
+        Allows `token_response.expires_in` or `token_response.any_custom_attribute`
+        :param key: a key
+        :return: the associated value in this token response
+        :raises:
+        """
+        if key == "expires_in":
+            return int(self.expires_at.timestamp() - datetime.now().timestamp())
+        elif key == "token_type":
+            return "Bearer"
+        return self.other.get(key) or super().__getattr__(self, key)
+
+    def as_dict(self, expires_at: bool = False) -> Dict[str, Any]:
+        r: Dict[str, Any] = {
+            "access_token": self.access_token,
+            "token_type": "Bearer",
+        }
+        if self.expires_at:
+            if expires_at:
+                r["expires_at"] = self.expires_at
+            else:
+                r["expires_in"] = self.expires_in
+        if self.scope:
+            r["scope"] = self.scope
+        if self.refresh_token:
+            r["refresh_token"] = self.refresh_token
+        if self.other:
+            r.update(self.other)
+        return r
+
+    def __repr__(self) -> str:
+        return pprint.pformat(self.as_dict())
+
+
+class BearerTokenEndpointResponse(BearerToken):
+    """
+    Like a BearerToken, but includes all the attributes returned by the token endpoint (id_token, etc.)
+    """
+
+    def __init__(
+        self,
+        access_token: str,
+        expires_in: int = None,
+        token_type: str = "Bearer",
+        scope: str = None,
+        refresh_token: str = None,
+        id_token: str = None,
+        client: "OAuth2Client" = None,
+        **kwargs: Union[str, int, bool],
+    ) -> None:
+        if token_type != "Bearer":
+            raise ValueError("token types other than Bearer are not supported")
+        expires_at = None
+        if expires_in:
+            expires_at = datetime.now() + timedelta(seconds=expires_in)
+        super().__init__(access_token, expires_at, scope, refresh_token, token_type, **kwargs)
+        self.client = client
+        self._id_token = id_token
+
+    def as_dict(self, expires_at: bool = False) -> Dict[str, Any]:
+        r = super().as_dict(expires_at)
+        if self._id_token:
+            r["id_token"] = self._id_token
+        return r
+
+    @classmethod
+    def from_requests_response(cls, client, resp):
+        return cls(client=client, **resp.json())
+
+
+class TokenSerializer:
+    def __init__(
+        self,
+        dumper: Callable[[BearerToken], str] = None,
+        loader: Callable[[str], BearerToken] = None,
+        token_class=BearerToken,
+    ):
+        self.token_class = token_class
+        self.dumper = dumper or self.default_dumper
+        self.loader = loader or self.default_loader
+
+    @staticmethod
+    def default_dumper(token: BearerToken) -> str:
+        """
+        Serializes as JSON, encodes as base64url of zlib compression of JSON representation of the Access Token,
+        with expiration date represented as expires_at.
+        :param token: the :class:`BearerToken` to serialize
+        :return: the serialized value
+        """
+        return base64.urlsafe_b64encode(
+            zlib.compress(
+                json.dumps(
+                    token.as_dict(True), default=lambda d: d.strftime("%Y-%m-%dT%H:%M:%S")
+                ).encode()
+            )
+        ).decode()
+
+    def default_loader(self, serialized: str, token_class=BearerToken) -> BearerToken:
+        """
+        Default deserializer for tokens.
+        :param serialized: the serialized token
+        :return: a
+        """
+        attrs = json.loads(zlib.decompress(base64.urlsafe_b64decode(serialized)).decode())
+        expires_at = attrs.get("expires_at")
+        if expires_at:
+            attrs["expires_at"] = datetime.strptime(expires_at, "%Y-%m-%dT%H:%M:%S")
+        return token_class(**attrs)
+
+    def dumps(self, token: BearerToken) -> str:
+        """
+        Serialize and compress a given token for easier storage
+        :param token: a BearerToken to serialize
+        :return: the serialized token, as a str
+        """
+        return self.dumper(token)
+
+    def loads(self, serialized: str) -> BearerToken:
+        """
+        Deserialize a serialized token
+        :param serialized: the serialized token
+        :return: the deserialized token
+        """
+        return self.loader(serialized)
