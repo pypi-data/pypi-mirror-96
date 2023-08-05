71db3f9d5dd9cfd958db9a6e4965b9b170230758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:10 +0000

    Add package mttf/0.0.3/mttf-0.0.3-py3-none-any.whl

---
+++ packages/mttf/mttf-0.0.3-py3-none-any.whl/mt/tf/init.py
@@ -0,0 +1,19 @@
+'''Initialises TensorFlow, monkey-patching if necessary.'''
+
+__all__ = ['import_tf']
+
+def init():
+    '''Initialises tensorflow, monkey-patching if necessary.'''
+
+    import tensorflow
+
+    if tensorflow.__version__.startswith('2.') and tensorflow.__version__ < '2.4':
+        import h5py as _h5
+        if _h5.__version__.startswith('3.'): # hack because h5py>=3.0.0 behaves differently from h5py<3.0.0
+            from .keras_engine import hdf5_format
+            tensorflow.python.keras.saving.hdf5_format = hdf5_format
+            tensorflow.python.keras.engine.training.hdf5_format = hdf5_format
+    
+    return tensorflow
+
+init()
