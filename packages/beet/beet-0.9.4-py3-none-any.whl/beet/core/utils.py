3d16690a2267e292bdb93db49a128f9a3089be73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package beet/0.9.4/beet-0.9.4-py3-none-any.whl

---
+++ packages/beet/beet-0.9.4-py3-none-any.whl/beet/core/utils.py
@@ -0,0 +1,45 @@
+__all__ = [
+    "JsonDict",
+    "FileSystemPath",
+    "Sentinel",
+    "SENTINEL_OBJ",
+    "dump_json",
+    "extra_field",
+    "intersperse",
+]
+
+
+import json
+import os
+from dataclasses import field
+from typing import Any, Dict, Iterable, Iterator, List, TypeVar, Union
+
+T = TypeVar("T")
+
+JsonDict = Dict[str, Any]
+FileSystemPath = Union[str, "os.PathLike[str]"]
+TextComponent = Union[str, List[Any], JsonDict]
+
+
+class Sentinel:
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}>"
+
+
+SENTINEL_OBJ = Sentinel()
+
+
+def dump_json(value: Any) -> str:
+    return json.dumps(value, indent=2) + "\n"
+
+
+def extra_field(**kwargs: Any) -> Any:
+    return field(repr=False, hash=False, compare=False, **kwargs)
+
+
+def intersperse(iterable: Iterable[T], delimitter: T) -> Iterator[T]:
+    it = iter(iterable)
+    yield next(it)
+    for x in it:
+        yield delimitter
+        yield x
