61489d32ad91379523da490073514c71bfa76457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:38 +0000

    Add package msgspec/0.1.0/msgspec-0.1.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/msgspec/msgspec-0.1.0-cp38-cp38-manylinux1_x86_64.whl/msgspec/__init__.py
@@ -0,0 +1,15 @@
+from .core import (
+    Struct,
+    Encoder,
+    Decoder,
+    MsgspecError,
+    EncodingError,
+    DecodingError,
+    encode,
+    decode,
+)
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
