904fa175ba0bb27f4209820075a6d131274a1caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:33 +0000

    Add package aleatools/20.2.23.0/aleatools-20.2.23.0-py3-none-any.whl

---
+++ packages/aleatools/aleatools-20.2.23.0-py3-none-any.whl/aleatools-20.2.23.0.data/scripts/pullall.py
@@ -0,0 +1,11 @@
+#!python
+"""Pulls repositories using git."""
+
+import logging
+import a107
+import aleatools
+
+SIMULATION = False
+
+if __name__ == "__main__":
+    aleatools.gitaux_main(False, SIMULATION)
