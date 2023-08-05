3903f26fcb8d56bd5d95edf7c8bf4adbaa78cf71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:18 +0000

    Add package w3bsite/4.1.8/w3bsite-4.1.8.tar.gz

---
+++ packages/w3bsite/w3bsite-4.1.8.tar.gz/w3bsite-4.1.8/w3bsite/classes/config/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+import os, sys, ast, json, glob, platform, subprocess, random, requests, urllib, threading, time, string, traceback
+
+# inc imports.
+import cl1, netw0rk, syst3m, ssht00ls
+from fil3s import *
+from r3sponse import r3sponse
+
+# source.	
+ALIAS = "w3bsite"
+SOURCE_PATH = syst3m.defaults.source_path(__file__, back=3)
+BASE = syst3m.defaults.source_path(SOURCE_PATH)
+OS = syst3m.defaults.operating_system(supported=["linux", "macos"])
+LOG_LEVEL = syst3m.defaults.log_level(default=0)
+#syst3m.defaults.alias(alias=ALIAS, executable=SOURCE_PATH)
+
+# version.
+try: version = Files.load(f"{SOURCE_PATH}/.version")
+except: version = "unkown"
+
+# network info.
+NETWORK_INFO = netw0rk.network.info()
+if not NETWORK_INFO["success"]: raise ValueError(NETWORK_INFO["error"])
+
