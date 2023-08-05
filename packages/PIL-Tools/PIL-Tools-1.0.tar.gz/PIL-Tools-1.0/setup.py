b74ae20076b286147983e167bc62055a8dc8ca96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:20 +0000

    Add package PIL-Tools/1.0/PIL-Tools-1.0.tar.gz

---
+++ packages/PIL-Tools/PIL-Tools-1.0.tar.gz/PIL-Tools-1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md") as README:
+    long_description = README.read()
+
+setup(
+    name='PIL-Tools',
+    version='1.0',
+    description='An extension module for Pillow to add functions that help simplify some processes.',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Steven Shrewsbury',
+    author_email='',
+    url='https://pypi.python.org/pypi/PILTools',
+    packages=['PILTools'],
+    install_requires=['Pillow', 'requests'],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+    ],
+    project_urls={
+        "Source": "https://github.com/stshrewsburyDev/PIL-Tools",
+        "Documentation": "https://pil-tools.readthedocs.io/en/latest/",
+    },
+)
