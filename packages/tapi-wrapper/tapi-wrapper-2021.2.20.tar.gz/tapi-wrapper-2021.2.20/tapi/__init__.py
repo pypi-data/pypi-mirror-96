3badc59374de7793ff45d653099457daae9c0a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package tapi-wrapper/2021.2.20/tapi-wrapper-2021.2.20.tar.gz

---
+++ packages/tapi-wrapper/tapi-wrapper-2021.2.20.tar.gz/tapi-wrapper-2021.2.20/tapi/__init__.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+__author__ = 'Filipe Ximenes'
+__email__ = 'filipeximenes@gmail.com'
+__version__ = '2021.2.20'
+
+from .adapters import (
+    generate_wrapper_from_adapter,
+    TapiAdapter,
+    JSONAdapterMixin
+)
