13007564d7d25cf2b5beaf053e8d62dca5934c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:11 +0000

    Add package pygdaltools/1.3.2/pygdaltools-1.3.2.tar.gz

---
+++ packages/pygdaltools/pygdaltools-1.3.2.tar.gz/pygdaltools-1.3.2/gdaltools/basetypes.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+'''
+    gvSIG Online.
+    Copyright (C) 2015-2016 gvSIG Association.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+'''
+@author: Cesar Martinez Izquierdo - Scolab <http://scolab.es>
+'''
+
+import subprocess
+import logging
+import os, platform
+import sys
+try:
+	sys_encoding = sys.stdout.encoding or 'utf-8'
+except:
+	sys_encoding = 'utf-8'
+
+class GdalToolsError(Exception):
+    def __init__(self, code=-1, message=None):
+        self.code = code
+        self.message=message
+
+
+class ConnectionString():
+    def encode(self):
+        pass
+
+
+class PgConnectionString(ConnectionString):
+    conn_string_tpl = u"PG:host='{host}' port='{port}' user='{user}' dbname='{dbname}' password='{password}'"
+    def __init__(self, host=None, port=None, dbname=None, schema=None, user=None, password=None):
+        self.host = host
+        self.port = port
+        self.dbname = dbname
+        self.schema = schema
+        self.user = user
+        self.password = password
+
+    def encode(self):
+        return self.conn_string_tpl.format(host=self.host, port=self.port, user=self.user, dbname=self.dbname, password=self.password, schema=self.schema)
+
+    def __unicode__(self):
+        return '"' + self.conn_string_tpl.format(host=self.host, port=self.port, user=self.user, dbname=self.dbname, password='xxxxxx', schema=self.schema) + '"'
+    
+    def __str__(self):
+        return self.__unicode__()
+
+class FileConnectionString():
+    
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def encode(self):
+        return self.file_path
+
+    def __unicode__(self):
+        return '"' + self.file_path + '"'
+
+    def __str__(self):
+        return self.__unicode__()
+
+class Wrapper():
+    BASEPATH = "/usr/bin"
+    CMD = None
+    def __init__(self, version=1, command_path=None):
+        self.version = version
+        self._command = command_path
+    
+    def _get_command(self):
+        if self._command:
+            return self._command
+        if platform.system()=='Windows':
+            cmd = self.CMD + ".exe"
+        else:
+            cmd = self.CMD
+        return os.path.join(self.BASEPATH, cmd)
+    
+    def _do_execute(self, args):
+        self.args = args
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=-1)
+        output, err = p.communicate()
+        rc = p.returncode
+        self.returncode = rc
+        try:
+            self.stdout = output.decode(sys_encoding)
+            self.stderr = err.decode(sys_encoding)
+        except:
+            self.stdout = "Error decoding stdout"
+            self.stderr = "Error decoding stderr"
+        logging.debug("return code: " + str(rc))
+        if rc>0:
+            logging.error(err)
+            raise GdalToolsError(rc, err)
+        return output
---
+++ packages/pygdaltools/pygdaltools-1.3.2.tar.gz/pygdaltools-1.3.2/tests/test_gdaltools.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import pytest
+from gdaltools.basetypes import GdalToolsError
+
+@pytest.yield_fixture
+def ogr():
+    import gdaltools
+    ogr = gdaltools.ogr2ogr()
+    yield ogr
+
+
+"""
+@pytest.yield_fixture
+def tmpdir():
+    import tempfile
+    tmp_path = tempfile.mkdtemp()
+    yield tmp_path
+    import shutil
+    shutil.rmtree(tmp_path)
+"""
+
+
+@pytest.yield_fixture
+def tmp_sqlite(tmpdir):
+    f = str(tmpdir.join("db.sqlite"))
+    yield f
+    import os
+    if os.path.exists(f):
+        os.remove(f)
+
+
+def test_spatialite_output(ogr, tmp_sqlite):
+    ogr.set_input("tests/data/areas.shp", srs="EPSG:4258")
+    ogr.set_encoding("UTF-8")
+    ogr.set_output(tmp_sqlite)
+    ogr.execute()
+    assert ogr.returncode == 0
+    import os
+    assert os.path.isfile(tmp_sqlite)
+
+
+def test_creation_mode(ogr, tmp_sqlite):
+    ogr.set_input("tests/data/areas.shp", srs="EPSG:4258")
+    ogr.set_encoding("UTF-8")
+    ogr.set_output(tmp_sqlite)
+    # create areas layer in tmp_sqlite
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    """
+    Not true depending on the ogr2ogr version
+    # should fail now, as it already exists
+    from gdaltools import GdalToolsError
+    with pytest.raises(GdalToolsError):
+        ogr.execute()
+    assert ogr.returncode != 0
+    """
+
+    # should also fail if explicitly using MODE_LAYER_CREATE and MODE_DS_CREATE_OR_UPDATE
+    ogr.set_output_mode(
+            layer_mode=ogr.MODE_LAYER_CREATE,
+            data_source_mode=ogr.MODE_DS_CREATE_OR_UPDATE)
+    with pytest.raises(GdalToolsError):
+        ogr.execute()
+    assert ogr.returncode != 0
+
+    # should work with MODE_LAYER_OVERWRITE
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_OVERWRITE, data_source_mode=ogr.MODE_DS_CREATE_OR_UPDATE)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # should fail with MODE_LAYER_CREATE & MODE_DS_UPDATE
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_UPDATE)
+    with pytest.raises(GdalToolsError):
+        ogr.execute()
+    assert ogr.returncode != 0
+
+    # should work
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_OVERWRITE, data_source_mode=ogr.MODE_DS_UPDATE)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # I think it should fail according ogr2ogr docs, but works at least in ogr 1.11 so excluding from testing
+    # ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_OVERWRITE, data_source_mode=ogr.MODE_DS_CREATE)
+    # ogr.execute()
+
+    # I think it should fail according ogr2ogr docs, but works at least in ogr 2.2.3 so excluding from testing
+    #ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_CREATE)
+    #with pytest.raises(GdalToolsError):
+    #    ogr.execute()
+    #assert ogr.returncode != 0
+
+    # should fail because the db exist and using MODE_DS_CREATE
+    # but works at least in ogr 2.2.3 so excluding from testing
+    #ogr.set_output(tmp_sqlite, table_name="areas01")
+    #ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_CREATE)
+    #with pytest.raises(GdalToolsError):
+    #    ogr.execute()
+    #assert ogr.returncode != 0
+
+    # should fails, the layer exists
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_CREATE_OR_UPDATE)
+    with pytest.raises(GdalToolsError):
+        ogr.execute()
+    assert ogr.returncode != 0
+
+    # should work
+    ogr.set_output(tmp_sqlite, table_name="areas02")
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_UPDATE)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # should work
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_APPEND, data_source_mode=ogr.MODE_DS_UPDATE)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # should work
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_APPEND, data_source_mode=ogr.MODE_DS_CREATE_OR_UPDATE)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # I think it should fail according ogr2ogr docs, but works at least in ogr 1.11 so excluding from testing
+    # ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_APPEND, data_source_mode=ogr.MODE_DS_CREATE)
+    # with pytest.raises(GdalToolsError):
+    #    ogr.execute()
+
+
+def test_shape_encoding(ogr, tmpdir, tmp_sqlite):
+    # import utf-8 and latin1 layers in s spatialite db
+    ogr.set_input("tests/data/pointsutf8.shp", srs="EPSG:4258")
+    ogr.set_output_mode(layer_mode=ogr.MODE_LAYER_CREATE, data_source_mode=ogr.MODE_DS_CREATE_OR_UPDATE)
+    ogr.set_encoding("UTF-8")
+    ogr.set_output(tmp_sqlite)
+    ogr.execute()
+    assert ogr.returncode == 0
+    ogr.set_input("tests/data/pointslatin1.shp", srs="EPSG:4258")
+    ogr.set_encoding("ISO-8859-1")
+    ogr.set_output(tmp_sqlite)
+    ogr.execute()
+    assert ogr.returncode == 0
+
+    # export as utf8 a layer having utf8 chars
+    ogr.set_input(tmp_sqlite, table_name="pointsutf8", srs="EPSG:4258")
+    out_shp = tmpdir.join("pointsutf8_01.shp")
+    ogr.set_output(str(out_shp))
+    ogr.set_encoding("UTF-8")
+    ogr.execute()
+    assert ogr.returncode == 0
+    assert out_shp.check(file=1)
+    out_cpg = tmpdir.join("pointsutf8_01.cpg")
+    assert out_cpg.read() =='UTF-8'
+    
+
+    # export as latin1 a layer having utf8 chars
+    out_shp = tmpdir.join("pointsutf8_02.shp")
+    ogr.set_output(str(out_shp))
+    ogr.set_encoding("ISO-8859-1")
+    output = ogr.execute()
+    assert ogr.returncode == 0
+    
+    assert out_shp.check(file=1)
+    assert "Warning" in ogr.stderr
+    assert "One or several characters couldn't be converted correctly from UTF-8 to ISO-8859-1" in ogr.stderr
+    
+    out_cpg = tmpdir.join("pointsutf8_02.cpg")
+    assert out_cpg.read() == "ISO-8859-1"
