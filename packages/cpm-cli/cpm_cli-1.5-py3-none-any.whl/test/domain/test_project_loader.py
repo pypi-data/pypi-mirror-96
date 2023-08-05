a9268fa9cc84e3947be7ef2202c64a33109045d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:04 +0000

    Add package cpm-cli/1.5/cpm_cli-1.5-py3-none-any.whl

---
+++ packages/cpm-cli/cpm_cli-1.5-py3-none-any.whl/test/domain/test_project_loader.py
@@ -0,0 +1,37 @@
+import unittest
+import mock
+
+from cpm.domain.project import project_loader
+from cpm.domain.project.project_descriptor import ProjectDescriptor, DeclaredBit
+
+
+class TestProjectLoader(unittest.TestCase):
+    @mock.patch('cpm.domain.project.project_loader.project_descriptor_parser')
+    @mock.patch('cpm.domain.project.project_loader.project_composer')
+    def test_project_loader_without_bits(self, project_composer, project_descriptor_parser):
+        # Given
+        project_descriptor_parser.parse_from.return_value = ProjectDescriptor()
+        loader = project_loader.ProjectLoader()
+        # When
+        loader.load('.', 'default')
+        # Then
+        project_descriptor_parser.parse_from.assert_called_once_with('.')
+        project_composer.compose.assert_called_once_with(project_descriptor_parser.parse_from.return_value, 'default')
+
+    @mock.patch('cpm.domain.project.project_loader.project_descriptor_parser')
+    @mock.patch('cpm.domain.project.project_loader.project_composer')
+    def test_project_loader_with_declared_bits(self, project_composer, project_descriptor_parser):
+        # Given
+        project_descriptor = ProjectDescriptor()
+        project_descriptor.build.declared_bits = [DeclaredBit('bit', '2.2')]
+        project_descriptor_parser.parse_from.return_value = project_descriptor
+        loader = project_loader.ProjectLoader()
+        # When
+        loader.load('.', 'default')
+        # Then
+        project_descriptor_parser.parse_from.assert_has_calls([
+            mock.call('.'),
+            mock.call('bits/bit')
+        ])
+        project_composer.compose.assert_called_once_with(project_descriptor, 'default')
+
