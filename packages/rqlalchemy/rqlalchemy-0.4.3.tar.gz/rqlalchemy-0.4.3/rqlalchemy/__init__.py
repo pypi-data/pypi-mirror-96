6483d6d5dda6330caaddc3aa108589b1b6c7d1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:14 +0000

    Add package rqlalchemy/0.4.3/rqlalchemy-0.4.3.tar.gz

---
+++ packages/rqlalchemy/rqlalchemy-0.4.3.tar.gz/rqlalchemy-0.4.3/rqlalchemy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+from rqlalchemy.query import RQLQueryMixIn, RQLQueryError
+
+
+__title__ = "rqlalchemy"
+__version__ = "0.4.3"
+__author__ = "Pedro Werneck"
+__license__ = "MIT"
+
+
+__all__ = ["RQLQueryMixIn", "RQLQueryError"]
