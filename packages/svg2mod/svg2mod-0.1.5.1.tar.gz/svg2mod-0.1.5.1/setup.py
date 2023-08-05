9a01f66bd6b5b1263a2de19bc7934c5c77865f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:46 +0000

    Add package svg2mod/0.1.5.1/svg2mod-0.1.5.1.tar.gz

---
+++ packages/svg2mod/svg2mod-0.1.5.1.tar.gz/svg2mod-0.1.5.1/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+tag = ""
+
+try:
+    tag = os.popen("git describe --tag")._stream.read().strip()
+except:
+    tag = "development"
+
+requirements = [
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='svg2mod',
+    version=tag,
+    description="Convert an SVG file to a KiCad footprint.",
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    author='https://github.com/svg2mod',
+    author_email='',
+    url='https://github.com/svg2mod/svg2mod',
+    packages=setuptools.find_packages(),
+    entry_points={'console_scripts':['svg2mod = svg2mod.svg2mod:main']},
+    package_dir={'svg2mod':'svg2mod'},
+    include_package_data=True,
+    package_data={'kipart': ['*.gif', '*.png']},
+    scripts=[],
+    install_requires=requirements,
+    license="CC0-1.0",
+    zip_safe=False,
+    keywords='svg2mod, KiCAD, inkscape',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
