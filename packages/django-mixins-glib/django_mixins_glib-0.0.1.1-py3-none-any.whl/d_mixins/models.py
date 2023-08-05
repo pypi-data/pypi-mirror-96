c774325aa11e7c08409cd465ad3090485937aac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:53 +0000

    Add package django-mixins-glib/0.0.1.1/django_mixins_glib-0.0.1.1-py3-none-any.whl

---
+++ packages/django-mixins-glib/django_mixins_glib-0.0.1.1-py3-none-any.whl/d_mixins/models.py
@@ -0,0 +1,28 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class TimeStampMixin(models.Model):
+    # START SYSTEM DEFINED ---
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                   blank=True, related_name='%(class)s_created_by')
+    modified_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                    blank=True, related_name='%(class)s_modified_by')
+
+    # END SYSTEM DEFINED ---
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    # START SYSTEM DEFINED ---
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    # END SYSTEM DEFINED ---
+    class Meta:
+        abstract = True
