fb57b107e591902136f0eb233ae1f409a842ec2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package devpill/1.0.1/devpill-1.0.1.tar.gz

---
+++ packages/devpill/devpill-1.0.1.tar.gz/devpill-1.0.1/setup.py
@@ -0,0 +1,9 @@
+import distutils.core
+
+distutils.core.setup(
+    name="devpill",
+    packages=["devpill"],
+    version="1.0.1",
+    author="Perzan",
+    author_email="PerzanDevelopment@gmail.com"
+)
