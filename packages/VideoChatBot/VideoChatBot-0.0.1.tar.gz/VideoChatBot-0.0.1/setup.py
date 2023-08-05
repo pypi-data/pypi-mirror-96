ea4ce5686bec18a801d1c08c5e780f13cace3830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:33 +0000

    Add package VideoChatBot/0.0.1/VideoChatBot-0.0.1.tar.gz

---
+++ packages/VideoChatBot/VideoChatBot-0.0.1.tar.gz/VideoChatBot-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name='VideoChatBot',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+                ],
+    install_requires=['ChatterBot', 'opencv-python', 'SpeechRecognition', 'matplotlib', 'imageio','pyttsx3', 'text2emotion', 'tensorflow', 'fer'],
+    python_requires='==3.6'
+        )
