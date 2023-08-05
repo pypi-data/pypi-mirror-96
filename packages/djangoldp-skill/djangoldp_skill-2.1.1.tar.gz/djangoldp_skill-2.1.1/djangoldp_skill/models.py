e29f54a87a4f8d9ae19e32721120d64d00acff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package djangoldp-skill/2.1.1/djangoldp_skill-2.1.1.tar.gz

---
+++ packages/djangoldp-skill/djangoldp_skill-2.1.1.tar.gz/djangoldp_skill-2.1.1/djangoldp_skill/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from djangoldp_i18n.views import I18nLDPViewSet
+
+from djangoldp.models import Model
+
+class Skill(Model):
+    name = models.CharField(max_length=255, default='', null=True, blank=True)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name="skills")
+
+    class Meta:
+        serializer_fields=["@id", "name"]
+        nested_fields=[]
+        anonymous_perms = ['view']
+        container_path = 'skills/'
+        rdf_type = 'hd:skill'
+        view_set = I18nLDPViewSet
+
+
+    def __str__(self):
+        return self.name
