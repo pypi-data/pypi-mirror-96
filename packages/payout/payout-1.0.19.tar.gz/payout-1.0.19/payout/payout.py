d8ba1b8df0b1f9b14515d36f52cbfe0044d1761c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:16 +0000

    Add package payout/1.0.19/payout-1.0.19.tar.gz

---
+++ packages/payout/payout-1.0.19.tar.gz/payout-1.0.19/payout/payout.py
@@ -0,0 +1,45 @@
+import http.client
+import json
+
+from payout.create_payout_account import CreatePayoutAccount
+from payout.update_payout_account import UpdatePayoutAccount
+
+
+class Payout:
+    def __init__(self, app, key) -> None:
+        self.app = app
+        self.key = key
+        # Get app endpoints
+        conn = http.client.HTTPSConnection("reddash.azurewebsites.net")
+        route = "/app/endpoints/" + self.app
+        headers = {"Accept": "application/json"}
+        conn.request("POST", route, "", headers)
+        response = json.loads(conn.getresponse().read().decode())
+        # Parse and set dataEndpoints
+        if "dataEndpoints" in response:
+            self.dataEndpoints = response["dataEndpoints"]
+        else:
+            self.dataEndpoints = "ERROR Unable to get dataEndpoints from server"
+        # print("dataEndpoints -> " + self.dataEndpoints)
+        # Parse and set redpayEndpoints
+        if "redpayEndpoints" in response:
+            self.redpayEndpoints = response["redpayEndpoints"]
+        else:
+            self.redpayEndpoints = "ERROR Unable to get redpayEndpoints from server"
+        # print("redpayEndpoints -> " + self.redpayEndpoints)
+
+    def CreatePayoutAccount(self, request):
+        create_payout_account = CreatePayoutAccount(
+            self.app, self.key, self.dataEndpoints)
+        return create_payout_account.Process(request)
+
+    def UpdatePayoutAccount(self, request):
+        update_payout_account = UpdatePayoutAccount(
+            self.app, self.key, self.dataEndpoints)
+        return update_payout_account.Process(request)
+
+    def AddPaymentRate(self, request):
+        pass
+
+    def AddPaymentToken(self, request):
+        pass
---
+++ packages/payout/payout-1.0.19.tar.gz/payout-1.0.19/payout/update_payout_account.py
@@ -0,0 +1,48 @@
+import http.client
+import urllib.parse as urlparse
+import json
+
+
+class UpdatePayoutAccount:
+    def __init__(self, app, key, endpoint) -> None:
+        self.app = app
+        self.key = key
+        self.endpoint = endpoint
+        self.route = "/payoutaccount/update/" + app + "/"
+
+    def Process(self, request):
+        headers = {"Content-type": "application/json",
+                   "Accept": "application/json"}
+
+        id = ""
+        if "id" in request:
+            id = request["id"]
+        else:
+            return {
+                "code": 400,
+                "error": "id is required for update"
+            }
+
+        url_info = urlparse.urlparse(self.endpoint)
+        if url_info.scheme == "https":
+            conn = http.client.HTTPSConnection(
+                url_info.hostname, url_info.port or 443)
+        else:
+            conn = http.client.HTTPSConnection(
+                url_info.hostname, url_info.port or 80)
+
+        try:
+            conn.request('POST', (self.route + id), request, headers)
+            response = conn.getresponse()
+            print("response status >>>" + response.status)
+            text = response.read().decode()
+            print("response text >>>" + text)
+            if response.status == 200:
+                return json.loads(text)
+            else:
+                return {
+                    "code": response.status,
+                    "error": response.reason
+                }
+        finally:
+            conn.close()
