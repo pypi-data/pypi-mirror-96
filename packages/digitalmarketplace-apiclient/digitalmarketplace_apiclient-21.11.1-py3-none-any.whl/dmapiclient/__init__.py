2000c1fb7ed68b6f0b3042b8cc336a92d8eaf0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package digitalmarketplace-apiclient/21.11.1/digitalmarketplace_apiclient-21.11.1-py3-none-any.whl

---
+++ packages/digitalmarketplace-apiclient/digitalmarketplace_apiclient-21.11.1-py3-none-any.whl/dmapiclient/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '21.11.1'
+
+from .errors import APIError, HTTPError, InvalidResponse  # noqa
+from .errors import REQUEST_ERROR_STATUS_CODE, REQUEST_ERROR_MESSAGE  # noqa
+
+from .antivirus import AntivirusAPIClient  # noqa
+from .data import DataAPIClient  # noqa
+from .search import SearchAPIClient  # noqa
