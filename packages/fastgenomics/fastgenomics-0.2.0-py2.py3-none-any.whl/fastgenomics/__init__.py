bbf2e40273085bb5cd5f668a50a1206cf081e352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package fastgenomics/0.2.0/fastgenomics-0.2.0-py2.py3-none-any.whl

---
+++ packages/fastgenomics/fastgenomics-0.2.0-py2.py3-none-any.whl/fastgenomics/__init__.py
@@ -0,0 +1,5 @@
+"""FASTGenomics base library"""
+
+__version__ = "0.2.0"
+
+from .fg import FASTGenomicsClient, FASTGenomicsLargeFileStorageClient, FASTGenomicsDatasetClient, ToolConfiguration, run_zip
