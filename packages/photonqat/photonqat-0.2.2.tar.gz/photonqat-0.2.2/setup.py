4a9cc7f2d7d9967deb615b7825aeef10621f8b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:47 +0000

    Add package photonqat/0.2.2/photonqat-0.2.2.tar.gz

---
+++ packages/photonqat/photonqat-0.2.2.tar.gz/photonqat-0.2.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("install_requirements.txt", "r", encoding="utf-8") as f:
+    install_requires = list(map(str.strip, f))
+
+setuptools.setup(
+    name="photonqat",
+    version="0.2.2",
+    author="The Photonqat Developers",
+    author_email="nagairic@gmail.com",
+    description="Library for photonic continuous variable quantum programming",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/Blueqat/Photonqat",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
