163f8152e487eb13dc1a7e337e0386ef71de12f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:50 +0000

    Add package pip-pin/0.0.6.post1/pip-pin-0.0.6.post1.tar.gz

---
+++ packages/pip-pin/pip-pin-0.0.6.post1.tar.gz/pip-pin-0.0.6.post1/pip_pin/commands.py
@@ -0,0 +1,141 @@
+import distutils.cmd
+import sys
+import os
+from enum import Enum
+
+from pkg_resources import DistributionNotFound, Requirement, get_distribution
+
+
+class Env(Enum):
+    INSTALL = "install"
+    TESTS = "tests"
+    DEVELOP = "develop"
+
+    @property
+    def path(self):
+        return f"./.pip-pin/{self.value}.txt"
+
+class Command(distutils.cmd.Command):
+    user_options = [
+        ("install", "i", "Use install dependencies"),
+        ("tests", "t", "Use test dependencies"),
+        ("develop", "d", "Use develop dependencies"),
+    ]
+
+    def initialize_options(self):
+        self.install = False
+        self.tests = False
+        self.develop = False
+
+    def finalize_options(self):
+        self.install = self.install or not any([self.tests, self.develop])
+
+    @property
+    def envs(self):
+        if self.tests:
+            yield Env.TESTS
+
+        if self.develop:
+            yield Env.DEVELOP
+
+        if self.install:
+            yield Env.INSTALL
+
+    @property
+    def reqs(self):
+        def parse(reqs):
+            return [Requirement.parse(r) for r in reqs]
+
+        return {
+            Env.INSTALL: parse(self.distribution.install_requires),
+            Env.TESTS: parse(self.distribution.tests_require),
+            Env.DEVELOP: parse(self.distribution.develop_requires),
+        }
+
+
+class Sync(Command):
+    description = "Pippin sync"
+    user_options = Command.user_options + [
+        "update", None, "Install non-pinned versions (honoring constraints from setup.py)"
+    ]
+
+    def initialize_options(self):
+        super().initialize_options()
+        self.init = False
+
+    def run(self):
+        cmd = [
+            sys.executable,
+            "-m",
+            "pip",
+            "install",
+        ]
+
+        for env in self.envs:
+            if not self.reqs[env]:
+                continue
+
+            if self.init:
+                reqs = [str(r) for r in self.reqs[env]]
+                self.announce(f"{env.value}: {reqs}")
+                sef.spawn(cmd + reqs)
+            else:
+                self.announce(f"{env.value}: -r {env.path}")
+                self.spawn(
+                    cmd + ["-r", os.path.abspath(env.path)]
+                )
+
+
+class Pin(Command):
+    description = "Pippin pin"
+
+    def walk(self, req, pins):
+        # don't pin setuptools
+        if req.name == 'setuptools':
+            return
+
+        dist = get_distribution(req.name)
+
+        pins.update({self.walk(r, pins) for r in dist.requires(extras=req.extras)})
+
+        # ignore version if req specifies a direct url
+        req.specifier = dist.as_requirement().specifier if req.url is None else None
+        req.marker = None
+        return req
+
+    def run(self):
+        for env in self.envs:
+            pins = set()
+
+            for r in self.reqs[env]:
+                pins.add(self.walk(r, pins))
+
+            self.announce(f"# {env.path}")
+
+            pins = list(sorted(set(str(pin) for pin in pins if pin)))
+            for pin in pins:
+                self.announce(pin)
+
+            if self.dry_run:
+                sys.stderr.write(f"# {env.path}\n")
+                sys.stderr.write("\n".join(pins))
+                sys.stderr.write("\n")
+                continue
+
+            try:
+                os.mkdir(os.path.dirname(env.path))
+            except FileExistsError:
+                pass
+
+            with open(env.path, "w") as f:
+                if env == Env.TESTS:
+                    f.write(f"-r install.txt\n-c install.txt\n")
+                if env == Env.DEVELOP:
+                    f.write(f"-r tests.txt\n-c tests.txt\n")
+
+                f.write("\n".join(pins))
+                f.write("\n")
+
+
+def validate_develop_requires(*args):
+    pass
---
+++ packages/pip-pin/pip-pin-0.0.6.post1.tar.gz/pip-pin-0.0.6.post1/setup.py
@@ -0,0 +1,57 @@
+#!env python
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# fmt: off
+setup(
+    name='pip-pin',
+    version='0.0.6post1',
+    packages=find_packages(),
+    url='https://github.com/mrzechonek/python-pip-pin',
+    license='MIT',
+    author='Michał Lowas-Rzechonek',
+    author_email='michal@rzechonek.net',
+    description='Pippin',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Testing",
+    ],
+    python_requires='>= 3.6',
+    setup_requires=[
+        'pytest-runner',
+        'pip-pin',
+    ],
+    install_requires=[
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    develop_requires=[
+        'black',
+        'isort',
+    ],
+    entry_points={
+        "distutils.commands": [
+            "sync = pip_pin:Sync",
+            "pin = pip_pin:Pin",
+        ],
+        "distutils.setup_keywords": [
+            "develop_requires = pip_pin:validate_develop_requires",
+        ],
+        'egg_info.writers': [
+            'tests_require.txt = pip_pin:tests_require',
+            'develop_requires.txt = pip_pin:develop_requires',
+        ],
+    },
+)
+# fmt: on
