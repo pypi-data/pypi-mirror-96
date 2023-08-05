dcd907a712175d2fb9d620c1be2cfcc6cd390ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:53 +0000

    Add package netw0rk/1.5.4/netw0rk-1.5.4.tar.gz

---
+++ packages/netw0rk/netw0rk-1.5.4.tar.gz/netw0rk-1.5.4/netw0rk/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from netw0rk.classes import *
---
+++ packages/netw0rk/netw0rk-1.5.4.tar.gz/netw0rk-1.5.4/netw0rk/classes/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from netw0rk.classes.config import version
+from netw0rk.classes.firewall import firewall
+from netw0rk.classes.network import network
+from netw0rk.classes import curl
