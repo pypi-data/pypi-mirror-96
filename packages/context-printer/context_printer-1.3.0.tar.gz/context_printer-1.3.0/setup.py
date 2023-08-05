f2107dd8b7be26d87ebef306aa6d2f2530679b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:45 +0000

    Add package context-printer/1.3.0/context_printer-1.3.0.tar.gz

---
+++ packages/context-printer/context_printer-1.3.0.tar.gz/context_printer-1.3.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='context_printer',
+   version='1.3.0',
+   description='Tool for simple and appealing display of context in which each operation is executer',
+   license="",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author="Bonvin Etienne, Rey Valérian",
+   author_email="etienne.bonvin@epfl.ch, valerian.rey@epfl.ch",
+   url="",
+   package_dir = {"context_printer": "context_printer"},
+   packages=["context_printer"],
+   include_package_data=True,
+)
