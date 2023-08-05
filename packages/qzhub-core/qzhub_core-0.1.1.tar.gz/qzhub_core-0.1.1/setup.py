efd20a10027585f74f3a5dc0cbdff9481a59dc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:48 +0000

    Add package qzhub-core/0.1.1/qzhub_core-0.1.1.tar.gz

---
+++ packages/qzhub-core/qzhub_core-0.1.1.tar.gz/qzhub_core-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['qzhub_core']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'qzhub-core',
+    'version': '0.1.1',
+    'description': 'CORE Library for QZHub Team',
+    'long_description': None,
+    'author': 'Aitemir Kuandyk',
+    'author_email': 'mooniron.k@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
