27b7c5093467577d113a6a211eef31e5ea254018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:14 +0000

    Add package randomlib0/0.1/randomlib0-0.1.tar.gz

---
+++ packages/randomlib0/randomlib0-0.1.tar.gz/randomlib0-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name = "randomlib0",
+    version = "0.1",
+    packages = ['randomlib0'],
+    install_requires=['scipy','numpy','sounddevice']
+)
