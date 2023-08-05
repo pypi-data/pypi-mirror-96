c97abd683a972b305c2f945dd4ca082224730a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:27 +0000

    Add package extractcode-libarchive-system-provided/3.5.1.210223/extractcode_libarchive_system_provided-3.5.1.210223.tar.gz

---
+++ packages/extractcode-libarchive-system-provided/extractcode_libarchive_system_provided-3.5.1.210223.tar.gz/extractcode_libarchive_system_provided-3.5.1.210223/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from glob import glob
+from os.path import basename
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+desc = '''A ScanCode path provider plugin to provide a system package provided libarchive shared library.'''
+
+setup(
+    name='extractcode_libarchive_system_provided',
+    version='3.5.1.210223',
+    license='apache-2.0 AND bsd-simplified',
+    description=desc,
+    long_description=desc,
+    author='nexB',
+    author_email='info@aboutcode.org',
+    url='https://github.com/nexB/scancode-plugins',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+    ],
+    keywords=[
+        'open source', 'extractcode', 'libarchive'
+    ],
+    entry_points={
+        'scancode_location_provider': [
+            'extractcode_libarchive = extractcode_libarchive:LibarchivePaths',
+        ],
+    },
+)
