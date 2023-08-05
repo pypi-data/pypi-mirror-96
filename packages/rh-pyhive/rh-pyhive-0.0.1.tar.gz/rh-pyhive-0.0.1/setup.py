d503e8cee61a5c54d1571352881a08e3d9ce25d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:44 +0000

    Add package rh-pyhive/0.0.1/rh-pyhive-0.0.1.tar.gz

---
+++ packages/rh-pyhive/rh-pyhive-0.0.1.tar.gz/rh-pyhive-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="rh-pyhive",
+    version="0.0.1",
+    description="Relative Humidity PyHive",
+    author="ssatia",
+    packages=["rh-pyhive"],
+    include_package_data=True,
+)
