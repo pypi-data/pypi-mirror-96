5dc7cdfc383323477e69cd7bf93fd5d96f3c3be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:27 +0000

    Add package depp/0.1.14/depp-0.1.14-py3-none-any.whl

---
+++ packages/depp/depp-0.1.14-py3-none-any.whl/depp-0.1.14.data/scripts/distance_depp.sh
@@ -0,0 +1,24 @@
+while getopts q:b:m:t:o:r: flag
+do
+    case "${flag}" in
+	q) query_sequence=${OPTARG};;
+	b) backbone_sequence=${OPTARG};;
+	m) model_path=${OPTARG};;
+	t) backbone_tree=${OPTARG};;
+	o) outdir=${OPTARG};;
+	r) replicate_seq=${OPTARG};;
+    esac
+done
+depp_distance.py query_seq_file=$query_sequence model_path=$model_path backbone_seq_file=$backbone_sequence outdir=$outdir replicate_seq=$replicate_seq
+echo "distance correcting..."
+#mkdir $outdir/depp_tmp
+run_apples.py -d "./${outdir}/depp.csv" -t "./${backbone_tree}" -o "./${outdir}/depp_tmp/tmp.jplace"
+gappa examine graft --jplace-path "./${outdir}/depp_tmp/tmp.jplace" --out-dir "./${outdir}/depp_tmp" --allow-file-overwriting --fully-resolve > /dev/null 2>&1 
+#perl -ne 'if(/^>(\S+)/){print "$1\n"}' $query_sequence > "${outdir}/depp_tmp/seq_name.txt"
+while read p; do
+    sed -e's/);/,XXXXX:0)ROOT:0;/g' "${outdir}/depp_tmp/tmp.newick"|nw_reroot - $p| nw_clade - ROOT|nw_labels -I -|grep -v XXXXX > "${outdir}/depp_tmp/${p}_leaves.txt";
+done < "${outdir}/depp_tmp/seq_name.txt"
+distance_correction.py outdir=$outdir backbone_tree=$backbone_tree
+echo "finish correction!"
+#run_apples.py -d "./${ourdir}/depp_correction.csv" -t "./${backbone_tree}" -o "./${outdir}/depp_tmp/tmp.jplace"
+rm -rf "./${outdir}/depp_tmp/"
