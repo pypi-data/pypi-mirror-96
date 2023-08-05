41e0ebbedfb81cfa49b02ae9ffa3b3a8355bd602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:57 +0000

    Add package roo-data-storage/0.1/roo_data_storage-0.1.tar.gz

---
+++ packages/roo-data-storage/roo_data_storage-0.1.tar.gz/roo_data_storage-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='roo_data_storage',
+    version='0.1',
+    packages=['roo_data_storage',],
+    license='LICENSE',
+    long_description=open('README.txt').read(),
+    author='Deliveroo',
+    author_email='diego.vazqueznanini@deliveroo.co.uk',
+    url='http://pypi.python.org/pypi/roo_data_storage/',
+)
