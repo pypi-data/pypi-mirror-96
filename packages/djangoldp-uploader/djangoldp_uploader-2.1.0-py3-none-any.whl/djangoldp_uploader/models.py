b2bed5d4ca3639ac7ab7b934564db28edf29aea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:07 +0000

    Add package djangoldp-uploader/2.1.0/djangoldp_uploader-2.1.0-py3-none-any.whl

---
+++ packages/djangoldp-uploader/djangoldp_uploader-2.1.0-py3-none-any.whl/djangoldp_uploader/models.py
@@ -0,0 +1,8 @@
+from djangoldp.models import Model
+from django.db import models
+
+
+class Document(Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
