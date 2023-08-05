e30dbf1c1c06299c08257373a50c1368aa9973f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package decidim-electionguard/0.1.0/decidim-electionguard-0.1.0.tar.gz

---
+++ packages/decidim-electionguard/decidim-electionguard-0.1.0.tar.gz/decidim-electionguard-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="decidim-electionguard",
+    version="0.1.0",
+    description="ElectionGuard wrappers to be used by Decidim eVoting project",
+    url="http://github.com/codegram/decidim.electionguard",
+    author="Codegram",
+    author_email="leo@codegram.com",
+    license="MIT",
+    package_dir={"": "src", "tests": "tests"},
+    packages=find_packages("src", "tests"),
+    install_requires=["electionguard==1.1.15", "jsons==1.2"],
+)
