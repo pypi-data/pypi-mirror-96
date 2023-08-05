f9c13f77d6f2b74e5d795ed8426cee23d827333b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:23 +0000

    Add package ssht00ls/3.11.6/ssht00ls-3.11.6.tar.gz

---
+++ packages/ssht00ls/ssht00ls-3.11.6.tar.gz/ssht00ls-3.11.6/ssht00ls/classes/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+# keep this import style so you can also use the keys.Keys() etc directly.
+from ssht00ls.classes.config import version, SOURCE_PATH
+try:
+	from ssht00ls.classes.config import encryption
+except ImportError:
+	encryption = None
+
+# import classes.
+from ssht00ls.classes import utils, installation, sshd, sshfs, scp, agent, keys, connections, smart_cards, aliases, ssh, ssync
