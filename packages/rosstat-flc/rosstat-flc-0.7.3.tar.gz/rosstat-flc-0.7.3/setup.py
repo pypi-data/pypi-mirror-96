6297f7e3b9880480362827502c35e570a54115e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:20 +0000

    Add package rosstat-flc/0.7.3/rosstat-flc-0.7.3.tar.gz

---
+++ packages/rosstat-flc/rosstat-flc-0.7.3.tar.gz/rosstat-flc-0.7.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rosstat-flc',
+    version='0.7.3',
+    description='Tool for format-logistic control of reports sent to RosStat',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    author='Nikita Ryabinin',
+    author_email='ryabinin.ne@gmail.com',
+    install_requires=['lxml', 'ply'],
+    url='https://github.com/WoolenSweater/rosstat_flc',
+    packages=find_packages(),
+    python_requires='>=3.7'
+)
