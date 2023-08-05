63c1710e020b5ed47dcbdb4c48a4aea152218590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:40 +0000

    Add package robotpy-commands-v2/2021.2.2.2/robotpy_commands_v2-2021.2.2.2-cp39-cp39-win_amd64.whl

---
+++ packages/robotpy-commands-v2/robotpy_commands_v2-2021.2.2.2-cp39-cp39-win_amd64.whl/commands2/src/cpp/frc2/command/Subsystem.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "frc2/command/Subsystem.h"
+
+using namespace frc2;
+Subsystem::~Subsystem() {
+  CommandScheduler::GetInstance().UnregisterSubsystem(this);
+}
+
+void Subsystem::Periodic() {}
+
+void Subsystem::SimulationPeriodic() {}
+
+std::shared_ptr<Command> Subsystem::GetDefaultCommand() {
+  return CommandScheduler::GetInstance().GetDefaultCommand(shared_from_this());
+}
+
+std::shared_ptr<Command> Subsystem::GetCurrentCommand() {
+  return CommandScheduler::GetInstance().Requiring(shared_from_this());
+}
+
+void Subsystem::Register() {
+  return CommandScheduler::GetInstance().RegisterSubsystem(this);
+}
