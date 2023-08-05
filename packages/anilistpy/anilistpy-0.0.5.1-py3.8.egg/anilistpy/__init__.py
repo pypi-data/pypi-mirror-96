04a0ef39fc3ca4f4c97ef35c591c9f5ffdce11b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:16 +0000

    Add package anilistpy/0.0.5.1/anilistpy-0.0.5.1-py3.8.egg

---
+++ packages/anilistpy/anilistpy-0.0.5.1-py3.8.egg/anilistpy/__init__.py
@@ -0,0 +1,33 @@
+from anilistpy.anime import Anime
+from anilistpy.manga import Manga
+from anilistpy.char import Character
+from anilistpy.staff import Staff
+from anilistpy.studio import Studio
+from anilistpy.search.manga import mangaSearch
+from anilistpy.search.anime import animeSearch
+from anilistpy.search.char import charSearch
+from anilistpy.search.staff import searchStaff
+from anilistpy.search.studio import searchStudio
+from anilistpy.utils import videoLink
+
+from anilistpy.mutation.media import Media
+from anilistpy.mutation.user import User
+
+from anilistpy.user.medialist import MediaList 
+'''
+    easy to use wrapper for anilist apiv2
+    Copyright (C) 2020  0kalekale
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
