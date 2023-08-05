e937d337ca12e6611229b248a97740715d0aa2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:55 +0000

    Add package monstereo/0.2.3/monstereo-0.2.3.tar.gz

---
+++ packages/monstereo/monstereo-0.2.3.tar.gz/monstereo-0.2.3/monstereo/__init__.py
@@ -0,0 +1,4 @@
+
+"""Open implementation of MonoLoco++ / MonStereo."""
+
+__version__ = '0.2.3'
---
+++ packages/monstereo/monstereo-0.2.3.tar.gz/monstereo-0.2.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+# extract version from __init__.py
+with open('monstereo/__init__.py', 'r') as f:
+    VERSION_LINE = [l for l in f if l.startswith('__version__')][0]
+    VERSION = VERSION_LINE.split('=')[1].strip()[1:-1]
+
+setup(
+    name='monstereo',
+    version=VERSION,
+    packages=[
+        'monstereo',
+        'monstereo.network',
+        'monstereo.eval',
+        'monstereo.train',
+        'monstereo.prep',
+        'monstereo.visuals',
+        'monstereo.utils'
+    ],
+    license='GNU AGPLv3',
+    description=' Perceiving Humans: from Monocular 3D Localization to Social Distancing '
+                '/ MonStereo: When Monocular and Stereo Meet at the Tail of 3D Human Localization',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lorenzo Bertoni',
+    author_email='lorenzo.bertoni@epfl.ch',
+    url='https://github.com/vita-epfl/monstereo',
+    zip_safe=False,
+
+    install_requires=[
+        'openpifpaf==v0.12.1',
+        'matplotlib'
+    ],
+    extras_require={
+        'eval': [
+            'tabulate==0.8.3',
+            'sklearn',
+            'pandas',
+            'pylint',
+            'pytest',
+        ],
+        'prep': [
+            'nuscenes-devkit==1.0.2',
+        ],
+    },
+)
