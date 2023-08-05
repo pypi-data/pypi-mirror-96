6fd84b6d14345fce6a10d0dfefc9dea780fca012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:14 +0000

    Add package dttxml/1.1.3/dttxml-1.1.3-py2.py3-none-any.whl

---
+++ packages/dttxml/dttxml-1.1.3-py2.py3-none-any.whl/dttxml/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+from .parse import dtt_read
+from .access import DiagAccess
+from .dtt2bunch import dtt2bunch
+
+from .version import (
+    version,
+    __version__,
+)
+
+__all__ = [
+    dtt_read,
+    DiagAccess,
+    dtt2bunch,
+    version,
+    __version__,
+]
