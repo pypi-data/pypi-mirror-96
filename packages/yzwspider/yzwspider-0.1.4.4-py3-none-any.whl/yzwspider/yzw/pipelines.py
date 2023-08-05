5ebdf4aaab390bd16f8604601dba15615166119e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:45 +0000

    Add package yzwspider/0.1.4.4/yzwspider-0.1.4.4-py3-none-any.whl

---
+++ packages/yzwspider/yzwspider-0.1.4.4-py3-none-any.whl/yzwspider/yzw/pipelines.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+import pymysql
+import xlwt
+import logging
+import os
+from twisted.enterprise import adbapi
+import traceback
+from yzwspider.yzw.items import YzwItem
+
+logger = logging.getLogger("YzwPipeline")
+
+
+class YzwPipeline(object):
+    def __init__(self, pool, settings):
+        self.dbpool = pool
+        self.settings = settings
+        self.excelstyle = self.getExcelStyle()
+        excel_path = os.getcwd() if settings.get("EXCEL_FILE_PATH") == '.' else settings.get("EXCEL_FILE_PATH")
+        excel_file = settings.get("EXCEL_FILE_NAME") + '.xls'
+        self.excelFile = os.path.join(excel_path, excel_file)
+
+    @classmethod
+    def from_settings(cls, settings):
+        params = dict(
+            host=settings.get("HOST"),
+            port=settings.get("PORT"),
+            db=settings.get("DATABASE"),
+            user=settings.get("USER"),
+            passwd=settings.get("PASSWORD"),
+            charset=settings.get("CHARSET"),
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        db_connect_pool = None
+        if settings.get("MYSQL"):
+            YzwPipeline.__test_mysql_settings(**params)
+            db_connect_pool = adbapi.ConnectionPool('pymysql', **params)
+        obj = cls(db_connect_pool, settings)
+        return obj
+
+    def _create_table(self, txn):
+        try:
+            sql = "DROP TABLE IF EXISTS `{0}`".format(self.settings.get("TABLE"))
+            re = txn.execute(sql)
+            sql = self.settings.get("CREATE_TEBLE_SQL").format(self.settings.get("TABLE"))
+            re = txn.execute(sql)
+            logger.info("创建表:'%s'成功." % self.settings.get('TABLE'))
+        except Exception as e:
+            logger.critical(traceback.format_exc())
+
+    def open_spider(self, spider):
+        if self.dbpool:
+            obj = self.dbpool.runInteraction(self._create_table)
+        else:
+            self.newExcelFile()
+
+    def close_spider(self, spider):
+        try:
+            if self.dbpool:
+                self.dbpool.close()
+                logger.info("数据已存储于数据库" + self.settings.get("DATABASE") + "， 表：" + self.settings.get("TABLE"))
+            else:
+                self.wbk.save(self.excelFile)
+                logger.info("excel文件已存储于 " + self.excelFile)
+        except Exception as e:
+            logger.error(traceback.format_exc())
+
+    def process_item(self, item, spider):
+        try:
+            if self.dbpool:
+                self.process_mysql(item)
+            else:
+                self.process_excel(item)
+        except Exception as e:
+            logger.critical(traceback.format_exc())
+
+    def process_mysql(self, item):
+        result = self.dbpool.runInteraction(self.insert, item)
+        # 给result绑定一个回调函数，用于监听错误信息
+        result.addErrback(self.error)
+
+    def insert(self, cursor, item):
+        insert_sql = "insert into {0} (`id`, `招生单位`, `院校特性`, `院系所`, `专业`,`研究方向`,`学习方式`, `拟招生人数`" \
+                     ", `备注`, `业务课一`, `业务课二`, `外语`, `政治`, `所在地`, `专业代码`,`指导老师`, `门类`, `一级学科` ) " \
+                     "VALUES ('{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}', '{15}', '{16}','{17}','{18}')" \
+            .format(self.settings.get('TABLE'), item['id'], item['招生单位'], item['院校特性'], item['院系所'], item['专业'],
+                    item['研究方向'], item['学习方式'], item['拟招生人数'], item['备注'],
+                    item['业务课一'], item['业务课二'], item['外语'], item['政治'], item['所在地'], item['专业代码'], item['指导老师'],
+                    item['门类'], item['一级学科'])
+        cursor.execute(insert_sql)
+
+    def error(self, reason):
+        # 跳过主键重复error
+        if reason.value.args[0] != 1062:
+            logger.error("insert to database err: -------------\n" + reason.getErrorMessage() + "\n" + str(
+                reason.getTraceback()))
+
+    def process_excel(self, item):
+        flag = False if (self.row & 1 == 0) else True
+        style = xlwt.XFStyle()
+        if flag:
+            style = self.excelstyle
+        else:
+            style = xlwt.XFStyle()
+            alignment = xlwt.Alignment()
+            alignment.horz = xlwt.Alignment.HORZ_CENTER
+            style.alignment = alignment
+        for i in range(0, YzwItem.fields.__len__()):
+            ret = self.sheet.write(self.row, i, item[self.list[i]], style)
+        self.row += 1
+
+    def getExcelStyle(self):
+        style = xlwt.XFStyle()
+        pattern = xlwt.Pattern()
+        pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+        pattern.pattern_fore_colour = 42
+        style.pattern = pattern
+        borders = xlwt.Borders()
+        borders.top = 1
+        borders.bottom = 1
+        borders.top_colour = 17
+        borders.bottom_colour = 17
+        style.borders = borders
+        alignment = xlwt.Alignment()
+        alignment.horz = xlwt.Alignment.HORZ_CENTER
+        style.alignment = alignment
+        return style
+
+    def getExcelTitleStyle(self):
+        style = xlwt.XFStyle()
+        pattern = xlwt.Pattern()
+        pattern.pattern = xlwt.Pattern.SOLID_PATTERN
+        pattern.pattern_fore_colour = 17
+        style.pattern = pattern
+        fnt = xlwt.Font()
+        fnt.name = u'黑体'
+        fnt.height = 0X00D9
+        fnt.colour_index = 1
+        fnt.bold = True
+        style.font = fnt
+        alignment = xlwt.Alignment()
+        alignment.horz = xlwt.Alignment.HORZ_CENTER
+        style.alignment = alignment
+        return style
+
+    def newExcelFile(self):
+        self.wbk = xlwt.Workbook()
+        self.sheet = self.wbk.add_sheet('Sheet1')
+        self.row = 1
+        self.sheet.col(0).width = 3000
+        self.sheet.col(1).width = 8000
+        self.sheet.col(2).width = 3000
+        self.sheet.col(3).width = 10000
+        self.sheet.col(4).width = 7000
+        self.sheet.col(5).width = 10000
+        self.sheet.col(6).width = 7000
+        self.sheet.col(7).width = 3000
+        self.sheet.col(8).width = 5000
+        self.sheet.col(9).width = 11000
+        self.sheet.col(10).width = 4000
+        self.sheet.col(11).width = 7000
+        self.sheet.col(12).width = 2000
+        self.sheet.col(13).width = 2000
+        self.sheet.col(14).width = 2000
+        self.sheet.col(15).width = 2000
+        self.sheet.col(16).width = 6000
+        self.sheet.col(17).width = 10000
+        self.list = ['id', '招生单位', '院校特性', '院系所', '专业', '研究方向', '学习方式', '拟招生人数'
+            , '业务课一', '业务课二', '外语', '政治', '所在地', '专业代码', '指导老师', '门类', '一级学科', '备注']
+        style = self.getExcelTitleStyle()
+        for i in range(0, YzwItem.fields.__len__()):
+            self.sheet.write(0, i, self.list[i], style)
+
+    @staticmethod
+    def __test_mysql_settings(**params):
+        try:
+            db = pymysql.connect(**params)
+            db.close()
+        except Exception as e:
+            logger.critical(str(e))
+            os._exit(1)
---
+++ packages/yzwspider/yzwspider-0.1.4.4-py3-none-any.whl/yzwspider/yzw/spiders/schools.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import os
+import traceback
+from yzwspider.yzw.items import YzwItem
+
+
+class SchoolsSpider(scrapy.Spider):
+    name = 'schools'
+    allowed_domains = ['chsi.com.cn']
+    start_urls = []
+    firstClassSubjectIndex = {}
+    st = {}
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    custom_settings = {
+        'STATS_CLASS': 'yzwspider.yzw.collector.YzwCollector',
+    }
+
+    def start_requests(self):
+        self.st = {i : self.settings.attributes[i].value for i in self.settings.attributes.keys() }
+        path = os.path.join(self.PROJECT_ROOT, self.settings.get('FCSI_FILE'))
+        with open(path, 'r', encoding='utf-8') as f:
+            self.firstClassSubjectIndex = eval(f.read())
+        gen = self.__ssdm_yjxk(self.settings.get('SSDM'), self.settings.get('YJXKDM'))
+        for ssdm, yjxkdm in gen:
+            url =  'https://yz.chsi.com.cn/zsml/queryAction.do?ssdm={}&dwmc=&mldm={}&mlmc=&yjxkdm={}&pageno=1'\
+                .format(ssdm, self.settings.get('MLDM'), yjxkdm)
+            yield scrapy.Request(url, meta={'ssdm':ssdm}, callback=self.parse)
+
+    # 爬取学校目录
+    def parse(self, response):
+        for tr in response.xpath('//tbody/tr[not(@class="noResult")]'):
+            try:
+                school = tr.xpath('.//a[re:test(@href,"/zsml/querySchAction.do?")]/text()').get()
+                schName = school[7:]
+                url = re.sub(r'queryAction', 'querySchAction', response.url)
+                url = re.sub(r'dwmc=', 'dwmc=' + schName, url)
+                yield scrapy.Request(url, meta={'ssdm':response.meta['ssdm']}, callback=self.parse_school)
+            except Exception as e:
+                self.logger.error(traceback.format_exc())
+                continue
+        # 翻页
+        url = self.__next_page_url(response)
+        if url:
+            yield scrapy.Request(url, meta={'ssdm':response.meta['ssdm']}, callback=self.parse)
+
+    # 爬取学校页面专业信息
+    def parse_school(self, response):
+        majorInfo = response.css('table').css('tr')
+        n = len(majorInfo)
+        for i in range(1, n):
+            try:
+                str = majorInfo[i].css('td::text')[2].extract()
+                majorCode = re.findall(r'\(.*?\)', str)[0][1:-1]
+                url = 'https://yz.chsi.com.cn' + majorInfo[i].css('td')[7].css('a::attr(href)')[0].extract()
+                yield scrapy.Request(url, meta={'ssdm':response.meta['ssdm']}, callback=self.parse_major)
+            except Exception as e:
+                self.logger.error(traceback.format_exc())
+                continue
+        # 翻页
+        url = self.__next_page_url(response)
+        if url:
+            yield scrapy.Request(url, meta={'ssdm':response.meta['ssdm']}, callback=self.parse_school)
+
+    # 爬取专业信息
+    def parse_major(self, response):
+        try:
+            item = YzwItem()
+            province = response.meta['ssdm']
+            majorInfo = response.css('table')[0].css('tr')
+            examRange = response.xpath('//tbody[re:test(@class,"zsml-res-items")]')
+            for num in range(0,len(examRange)):
+                body = examRange[num]
+                item['id'] = response.url[-19:] + str(num+1).zfill(3)
+                item['招生单位'] = majorInfo[0].css('td::text')[1].extract()[7:]
+                item['院校特性'] = self.__getSchoolFeature(item['招生单位'])
+                item['院系所'] = majorInfo[1].css('td::text')[1].extract()[5:]
+                item['专业'] = majorInfo[2].css('td::text')[1].extract()
+                item['研究方向'] = majorInfo[3].css('td::text')[1].extract()
+                item['学习方式'] = majorInfo[2].css('td::text')[3].extract()
+                item['拟招生人数'] = majorInfo[4].css('td::text')[1].extract()
+                comments = majorInfo[5].css('.zsml-bz::text')
+                item['备注'] = comments[1].get() if len(comments) > 1 else ""
+                item['政治'] = re.sub(r'\s', '', body.css('td::text')[0].extract())
+                item['外语'] = re.sub(r'\s', '', body.css('td::text')[2].extract())
+                item['业务课一'] = re.sub(r'\s', '', body.css('td::text')[3].extract())
+                item['业务课二'] = re.sub(r'\s', '', body.css('td::text')[4].extract())
+                item['所在地'] = self.settings.get('PROVINCE_DICT')[province]
+                item['指导老师'] = majorInfo[3].xpath('td')[3].xpath('text()').extract()
+                item['指导老师'] = item['指导老师'][0] if item['指导老师'] else ''
+                item['专业代码'] = item['专业'][1:7]
+                item['门类'] = self.settings.get('SUBJECT_INDEX')[item['专业代码'][:2]]
+                item['一级学科'] = self.firstClassSubjectIndex[item['专业代码'][:4]]
+                self.logger.info(item)
+                yield item
+        except Exception as e:
+            self.logger.error(traceback.format_exc())
+
+    # 生成省市代码， 一级学科代码
+    def __ssdm_yjxk(self, ssdm, yjxkdm):
+        if yjxkdm == '' and ssdm == '':
+            for province in self.settings.get('PROVINCE_LISE'):
+                for key in self.firstClassSubjectIndex.keys():
+                    if str(key).startswith(self.settings.get('MLDM')): yield province, key
+        elif yjxkdm == '':
+            for key in self.firstClassSubjectIndex.keys():
+                if str(key).startswith(self.settings.get('MLDM')): yield ssdm, key
+        elif ssdm == '':
+            for province in self.settings.get('PROVINCE_LISE'): yield province, yjxkdm
+        else:
+            yield ssdm, yjxkdm
+
+    # 判断学校性质
+    def __getSchoolFeature(self, schName):
+        return self.settings.get('SCHOOL_FEATURE').get(schName, "")
+
+    # 获取下一页url
+    def __next_page_url(self, response):
+        url = ''
+        page = response.xpath('//div[re:test(@class,"zsml-page-box")]/ul/li').css('a::attr(onclick)').extract()
+        page = page[len(page) - 1]
+        pageButtonLabel = response.xpath('//li[re:test(@class,"lip unable ")]').css('li::attr(class)').extract()
+        # 非最后一页
+        if pageButtonLabel == [] or pageButtonLabel == ['lip unable lip-first']:
+            try:
+                nextPage = re.findall(r'\(.*?\)', page)[0][1:-1]
+                url = re.sub(r'pageno=\d*', 'pageno=' + nextPage, response.url)
+            except Exception as e:
+                self.logger.error(traceback.format_exc())
+        return url
+
