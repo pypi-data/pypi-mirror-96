bd18c892701e47e5c845f797118e82a5e28c56f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package jmcm/0.1.3/jmcm-0.1.3-py3-none-any.whl

---
+++ packages/jmcm/jmcm-0.1.3-py3-none-any.whl/jmcm/__init__.py
@@ -0,0 +1,22 @@
+name = "jmcm"
+
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("math", "numpy", "pandas",
+                     "scipy", "collections", "patsy","warnings")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append("{0}: {1}".format(dependency, str(e)))
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" +
+        "\n".join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
+
+from .joint_model import JointModel
+
