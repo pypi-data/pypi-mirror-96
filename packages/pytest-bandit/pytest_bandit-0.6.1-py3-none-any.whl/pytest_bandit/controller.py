28719db3852d0c0aa328e88a4722895c533776f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package pytest-bandit/0.6.1/pytest_bandit-0.6.1-py3-none-any.whl

---
+++ packages/pytest-bandit/pytest_bandit-0.6.1-py3-none-any.whl/pytest_bandit/controller.py
@@ -0,0 +1,96 @@
+import logging
+import os
+import pytest
+import sys
+from bandit.core.config import BanditConfig
+from bandit.core.manager import BanditManager
+from bandit.core import constants
+from pytest_bandit.errors import BanditError
+
+bandit_config_params = []
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+
+
+class BanditItem(pytest.Item):
+    CACHE_KEY = 'bandit/mtimes'
+    MARKER = 'bandit'
+
+    @classmethod
+    def from_parent(cls, parent, name, **kwargs):
+        super_ = super()
+        if hasattr(super_, "from_parent"):
+            new = super_.from_parent(parent=parent, name=name)
+        else:
+            new = super_(name, parent)
+            new.config = parent.config
+        return new
+
+    def setup(self):
+        old_mtime = self.config.cache.get(
+                self.CACHE_KEY, {}
+            ).get(
+                str(self.fspath), -1
+            )
+        mtime = self.fspath.mtime()
+        if old_mtime == mtime:
+            pytest.skip('previously passed bandit checks')
+
+    def runtest(self):
+        b_conf = BanditConfig()
+        b_mgr = BanditManager(
+            b_conf,
+            self.config.getini('bandit_aggregate_by'),
+            debug=self.config.getini('bandit_debug'),
+            profile=self.config.getini('bandit_profile'),
+            verbose=self.config.getini('bandit_verbose'),
+            ignore_nosec=self.config.getini('bandit_ignore_nosec'))
+        b_mgr.discover_files(
+            self.config.getini('bandit_targets'),
+            self.config.getini('bandit_recurse'),
+            ', '.join(self.config.getini('bandit_exclude')))
+
+        if not b_mgr.b_ts.tests:
+            LOG.error(
+                'No tests would be run, please check your targets and add '
+                'recurse'
+            )
+            return 5
+
+        b_mgr.run_tests()
+
+        # trigger output of results by Bandit Manager
+        sev_level = constants.RANKING[
+            int(self.config.getini('bandit_sev_level'))
+        ]
+        conf_level = constants.RANKING[
+            int(self.config.getini('bandit_conf_level'))
+        ]
+        sys.stdout = sys.__stdout__
+        # pytest doesn't terminate the last line before invoking `runtest`
+        sys.stdout.write(os.linesep)
+        b_mgr.output_results(self.config.getini('bandit_context_lines'),
+                             sev_level,
+                             conf_level,
+                             sys.stdout,
+                             'screen')
+
+        # return an exit code of 1 if there are results, 0 otherwise
+        LOG.debug(sev_level)
+        LOG.debug(conf_level)
+        if b_mgr.results_count(
+            sev_filter=sev_level, conf_filter=conf_level
+        ) > 0:
+            return 1
+
+        return 0
+
+    def repr_failure(self, excinfo):
+        if excinfo.errisinstance(BanditError):
+            return excinfo.value.args[0]
+
+        return super().repr_failure(excinfo)
+
+    def reportinfo(self):
+        return self.fspath, None, 'bandit-check'
---
+++ packages/pytest-bandit/pytest_bandit-0.6.1-py3-none-any.whl/pytest_bandit/plugin.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+import logging
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+import pytest
+
+from pytest_bandit.controller import BanditItem
+
+logging.getLogger("bandit.core").setLevel(logging.WARNING)
+LOG = logging.getLogger(__name__)
+
+StringIO  # pyflakes, this is for re-export
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('bandit')
+    group.addoption(
+        '--bandit',
+        action='store_true',
+        default=False,
+        dest='run_bandit',
+        help='Run bandit'
+    )
+    parser.addini(
+        'bandit_targets',
+        type='args',
+        help='Path to analyzed code'
+    )
+    parser.addini(
+        'bandit_recurse',
+        type='bool',
+        default=True,
+        help='Path to analyzed code'
+    )
+    parser.addini(
+        'bandit_aggregate_by',
+        type='args',
+        default='vuln',
+        help='Aggregate output by vulnerability (default) or by filename'
+    )
+    parser.addini(
+        'bandit_context_lines',
+        type='args',
+        default=3,
+        help='Maximum number of code lines to output for each issue'
+    )
+    parser.addini(
+        'bandit_config',
+        type='args',
+        help=(
+            'Optional config file to use for selecting plugins and overriding '
+            'defaults'
+        )
+    )
+    parser.addini(
+        'bandit_profile',
+        type='args',
+        default=None,
+        help='Profile to use (defaults to executing all tests)'
+    )
+    parser.addini(
+        'bandit_run_tests',
+        type='args',
+        help='Comma-separated list of test IDs to run'
+    )
+    parser.addini(
+        'bandit_skip_tests',
+        type='args',
+        help='Comma-separated list of test IDs to skip'
+    )
+    parser.addini(
+        'bandit_sev_level',
+        default=1,
+        help=(
+            'Report only issues of a given severity level or higher '
+            '(1 = LOW, 2 = MEDIUM, 3 = HIGH)'
+        )
+    )
+    parser.addini(
+        'bandit_conf_level',
+        default=1,
+        help=(
+            'Report only issues of a given confidence level or higher '
+            '(1 = LOW, 2 = MEDIUM, 3 = HIGH)'
+        )
+    )
+    parser.addini(
+        'bandit_output_format',
+        type='args',
+        default='screen',
+        help='(CURRENTLY NOT IMPLEMENTED) Specify output format'
+    )
+    parser.addini(
+        'bandit_msg_template',
+        type='args',
+        help=(
+            '(CURRENTLY NOT IMPLEMENTED) Specify output message template '
+            '(only usable with --format custom), see CUSTOM FORMAT section '
+            'for list of available values'
+        )
+    )
+    parser.addini(
+        'bandit_verbose',
+        type='bool',
+        default=False,
+        help='Output extra information like excluded and included files'
+    )
+    parser.addini(
+        'bandit_debug',
+        type='bool',
+        default=False,
+        help='Turn on debug mode'
+    )
+    parser.addini(
+        'bandit_ignore_nosec',
+        type='bool',
+        help='Do not skip lines with # nosec comments'
+    )
+    parser.addini(
+        'bandit_exclude',
+        type='args',
+        help='Comma-separated list of paths to exclude from scan'
+    )
+    parser.addini(
+        'bandit_baseline',
+        type='args',
+        help=(
+            'Path of baseline report to compare against (only JSON-formatted '
+            'files are accepted)'
+        )
+    )
+    parser.addini(
+        'bandit_ini',
+        type='args',
+        help='Path to a .bandit file that supplies command line arguments'
+    )
+
+
+def pytest_configure(config):
+    """Register a custom marker for BanditItems."""
+    config.addinivalue_line(
+        'markers',
+        '{marker}: mark tests to be checked by bandit.'.format(
+            marker=BanditItem.MARKER,
+        ),
+    )
+
+
+if hasattr(pytest, 'hookimpl'):
+    hookwrapper = pytest.hookimpl(hookwrapper=True)
+else:
+    hookwrapper = pytest.mark.hookwrapper
+
+
+class SessionWrapper(object):
+    def __init__(self, session):
+        self.session = session
+        self.config = self.session.config
+        self.nodeid = self.session.nodeid
+        if hasattr(self.session, 'testsfailed'):
+            self._attr = 'testsfailed'
+        else:
+            self._attr = '_testsfailed'
+
+    @property
+    def testsfailed(self):
+        return getattr(self.session, self._attr)
+
+    @testsfailed.setter
+    def testsfailed(self, value):
+        setattr(self.session, self._attr, value)
+
+    @property
+    def shouldfail(self):
+        return getattr(self.session, 'shouldfail')
+
+    @shouldfail.setter
+    def shouldfail(self, value):
+        # Only set when currently False
+        if not self.shouldfail:
+            setattr(self.session, 'shouldfail', value)
+
+
+@hookwrapper
+def pytest_runtestloop(session):
+    yield
+    compat_session = SessionWrapper(session)
+    bandit_failures = BanditItem.from_parent(
+        parent=session, name='bandit'
+    ).runtest()
+    LOG.debug(compat_session.shouldfail)
+    compat_session.testsfailed += bandit_failures
+    compat_session.shouldfail = bool(bandit_failures)
+    LOG.debug(compat_session.shouldfail)
