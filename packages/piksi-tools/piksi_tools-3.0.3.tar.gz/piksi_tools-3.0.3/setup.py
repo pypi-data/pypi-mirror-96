bbb3a4d830af70f87cc7eb7ce6d37975a5095f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:16 +0000

    Add package piksi-tools/3.0.3/piksi_tools-3.0.3.tar.gz

---
+++ packages/piksi-tools/piksi_tools-3.0.3.tar.gz/piksi_tools-3.0.3/setup.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+import re
+import os
+
+from setuptools import setup
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Operating System :: POSIX :: Linux',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.7',
+]
+
+PACKAGES = ['piksi_tools', 'piksi_tools.console', 'piksi_tools.ardupilot']
+
+PLATFORMS = [
+    'linux',
+    'osx',
+    'win32',
+]
+
+PACKAGE_DATA = {
+    'piksi_tools': [
+        'console/settings.yaml',
+        'console/images',
+        'console/images/icon.png',
+        'console/images/fontawesome/download.svg',
+        'console/images/fontawesome/exclamation-triangle.svg',
+        'console/images/fontawesome/floppy-o.svg',
+        'console/images/fontawesome/refresh.svg',
+        'console/images/fontawesome/stop.svg',
+        'console/images/iconic/fullscreen.svg',
+        'console/images/iconic/arrows_blue.png',
+        'console/images/iconic/arrows_grey.png',
+        'console/images/iconic/move.svg',
+        'console/images/iconic/pause.svg',
+        'console/images/iconic/play.svg',
+        'console/images/iconic/stop.svg',
+        'console/images/iconic/target.svg',
+        'console/images/iconic/x.svg',
+    ]
+}
+
+
+def scmtools_parse(root,
+                   describe_command=None,
+                   config=None):
+    """
+    rewriting of setuptools_scm.git.parse method to remove -branch string
+    from any tags.  This library is clearly not designed for people to adjust
+    its function so I had to lift entire function from Aug 8 master with SHA
+    a91b40c99ea9bfc4289272285f17e1d43c243b76
+    """
+
+    from setuptools_scm.git import GitWorkdir, _git_parse_describe
+    from setuptools_scm.config import Configuration
+    from setuptools_scm.utils import has_command
+    from setuptools_scm.version import meta
+
+    if describe_command is None:
+        from setuptools_scm.git import DEFAULT_DESCRIBE
+        describe_command = DEFAULT_DESCRIBE
+
+    if not config:
+        config = Configuration(root=root)
+
+    if not has_command("git"):
+        return
+
+    wd = GitWorkdir.from_potential_worktree(config.absolute_root)
+    if wd is None:
+        return
+
+    out, unused_err, ret = wd.do_ex(describe_command)
+    if ret:
+        # If 'git describe' failed, try to get the information otherwise.
+        rev_node = wd.node()
+        dirty = wd.is_dirty()
+
+        if rev_node is None:
+            return meta("0.0", distance=0, dirty=dirty, config=config)
+
+        return meta(
+            "0.0",
+            distance=wd.count_all_nodes(),
+            node="g" + rev_node,
+            dirty=dirty,
+            branch=wd.get_branch(),
+            config=config,
+        )
+    else:
+        tag, number, node, dirty = _git_parse_describe(out)
+        branch = wd.get_branch()
+        if number:
+            return meta(
+                tag.replace('-branch', ''),
+                config=config,
+                distance=number,
+                node=node,
+                dirty=dirty,
+                branch=branch,
+            )
+        else:
+            return meta(tag.replace('-branch', ''), config=config, node=node, dirty=dirty, branch=branch)
+
+
+def version_scheme_add_v(version):
+    from setuptools_scm.version import guess_next_dev_version
+    scm_version = guess_next_dev_version(version)
+    v_version = scm_version if scm_version[0] == 'v' else "v" + scm_version
+    return v_version
+
+
+if __name__ == '__main__':
+
+    import io
+
+    cwd = os.path.abspath(os.path.dirname(__file__))
+
+    with io.open(cwd + '/README.rst', encoding='utf8') as f:
+        readme = f.read()
+
+    with open(cwd + '/requirements.txt') as fp:
+
+        INSTALL_REQUIRES = [L.strip() for L in fp if (not L.startswith('git+') and not L.startswith('--extra-index-url'))]
+
+        def transform(link):
+            link = link.strip()
+            spl = re.split('[&#]', link)
+            return str.join('#', spl)
+
+        DEPENDENCY_LINKS = [transform(L) for L in fp if L.startswith('git+')]
+
+    setup(
+        name='piksi_tools',
+        description='Python tools for the Piksi GNSS receiver.',
+        long_description=readme,
+        use_scm_version={
+            'write_to': 'piksi_tools/_version.py',
+            'parse': scmtools_parse,
+            'version_scheme': version_scheme_add_v
+        },
+        setup_requires=['setuptools_scm'],
+        author='Swift Navigation',
+        author_email='dev@swiftnav.com',
+        url='https://github.com/swift-nav/piksi_tools',
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        platforms=PLATFORMS,
+        install_requires=INSTALL_REQUIRES,
+        dependency_links=DEPENDENCY_LINKS,
+        include_package_data=True,
+        use_2to3=False,
+        zip_safe=False)
---
+++ packages/piksi-tools/piksi_tools-3.0.3.tar.gz/piksi_tools-3.0.3/tests/test_utils.py
@@ -0,0 +1,107 @@
+# -*- python -*-
+
+from piksi_tools.utils import Time
+
+
+def test_time_repr():
+    t1 = Time(1, 0)
+    assert repr(t1) == "Time(s=1,ms=0)"
+
+
+def test_time_eq():
+    t1 = Time(1, 0)
+    t2 = Time(2, 0)
+    assert t1 != t2
+    t3 = Time(1, 0)
+    assert t1 == t3
+
+
+def test_time_add():
+    t4 = Time(1, 500)
+    t5 = Time(1, 500)
+    assert (t4 + t5) == Time(3, 0)
+
+
+def test_time_sub():
+    t6 = Time(1, 400)
+    t7 = Time(2, 500)
+    t8 = (t7 - t6)
+    # confirm positive subraction result makes sense
+    assert t8 == Time(1, 100)
+    # adding then subtracking t6 should cancel out back to t7
+    assert (t8 + t6) == t7
+    # confirm negative result makes sense
+    t9 = t6 - t7
+    assert t9 == Time(-2, 900)
+    # TODO: result for t9 maybe should be normalized to -1 -100 in the util
+    # But at the very least float representation should match
+    assert t8.to_float() == -t9.to_float()
+    # adding then subtracking t7 should cancel out back to t6
+    assert (t9 + t7) == t6
+    # Confirm identity property of addition / subtraction
+    assert t8 + t9 == Time(0, 0)
+    # Confirm adding negative number is equivalent to subtracting positive number
+    t10 = Time(-1, -100)
+    t11 = Time(1,  100)
+    assert (t7 + t10) == Time(1, 400)
+    assert (t7 - t11) == Time(1, 400)
+
+
+def test_time_to_float():
+    t6 = Time(1, 400)
+    t6.to_float() -1.4 <= 0.00001
+
+
+def test_time_ge():
+    t4 = Time(1, 500)
+    t5 = Time(1, 500)
+    assert t4 >= t5
+    t4 += Time(0, 1)
+    assert t4 >= t5
+    t5 += Time(0, 2)
+    assert not (t4 >= t5)
+
+
+def test_time_gt():
+    t4 = Time(1, 500)
+    t5 = Time(1, 500)
+    assert not (t4 > t5)
+    t4 += Time(0, 1)
+    assert (t4 > t5)
+
+
+def test_time_le():
+    t1 = Time(1, 500)
+    t2 = Time(1, 500)
+    assert t1 <= t2
+    t2 += Time(0, 1)
+    assert t1 <= t2
+    t1 += Time(0, 2)
+    assert not (t1 <= t2)
+
+
+def test_time_lt():
+    t1 = Time(1, 500)
+    t2 = Time(1, 500)
+    assert not (t1 > t2)
+    t1 += Time(0, 1)
+    assert (t2 < t1)
+
+def test_time():
+    import time
+    time.sleep(1)
+    t1 = Time.now()
+    t2 = Time.now()
+    # For epoch time, seconds will "never" be zero
+    assert (    ((t1._seconds == 0 and t1._millis != 0) or (t1._seconds != 0))
+            and ((t2._seconds == 0 and t2._millis != 0) or (t2._seconds != 0))
+            and (t1._millis != 0 or t2._millis != 0)) # Could land on 0 milliseconds, but probably never twice
+
+
+def test_iter():
+    t1 = Time(1, 500)
+    t2 = Time(1, 505)
+    ls = [Time(1, 501), Time(1, 502), Time(1, 503), Time(1, 504), Time(1, 505)]
+    for iter_t in Time.iter_since(t1, t2):
+        assert ls.pop(0) == iter_t
+    assert len(ls) == 0
