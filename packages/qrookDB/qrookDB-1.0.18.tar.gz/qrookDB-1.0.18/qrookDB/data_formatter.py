e9099c31fbcfa47a9dd20beb041d2baaea00daf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package qrookDB/1.0.18/qrookDB-1.0.18.tar.gz

---
+++ packages/qrookDB/qrookDB-1.0.18.tar.gz/qrookDB-1.0.18/qrookDB/data_formatter.py
@@ -0,0 +1,35 @@
+# todo load from config data format -> refactor
+from error_handlers import log_error
+
+
+class DataFormatter:
+    def __init__(self):
+        self.format = 'list'
+
+    # result type -> 'all', 'one'
+    @log_error
+    def format_data(self, data, used_fields, result_type):
+        print('DATA FORMATTER:', data, used_fields, result_type) # todo delete
+        if result_type is None or data is None:
+            return None
+
+        if self.format == 'list':
+            return data
+        elif self.format == 'dict':
+            res = []
+            n = len(used_fields)
+            if result_type == 'one':
+                return {used_fields[i]: data[i] for i in range(n)}
+            else:
+                for d in data:
+                    res.append({used_fields[i]: d[i] for i in range(n)})
+                return res
+
+    def set_list_format(self):
+        self.format = 'list'
+
+    def set_dict_format(self):
+        self.format = 'dict'
+
+
+defaultDataFormatter = DataFormatter()
