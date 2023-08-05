4003991ea6de89bf584306fdceed39c75c6dd19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:52 +0000

    Add package covid-cloud-cli/0.0.15/covid_cloud_cli-0.0.15.tar.gz

---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.15.tar.gz/covid_cloud_cli-0.0.15/covid_cloud/client/constants.py
@@ -0,0 +1,5 @@
+import os
+
+config_file_path = f"{os.path.expanduser('~')}/.covid-cloud/config.yaml"
+cli_directory = f"{os.path.expanduser('~')}/.covid-cloud"
+downloads_directory = f"{os.path.expanduser('~')}/.covid-cloud/downloads"
