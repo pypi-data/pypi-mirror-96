ec7d6916fa652248a98d956c40098d00160a0bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:29 +0000

    Add package anilistpy/0.0.5.2/anilistpy-0.0.5.2.tar.gz

---
+++ packages/anilistpy/anilistpy-0.0.5.2.tar.gz/anilistpy-0.0.5.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anilistpy", 
+    version="0.0.5.2",
+    author="kalekale",
+    author_email="kalekale.anon@gmail.com",
+    description="python API wrapper for anilist.co",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anilistpy/anilistpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
