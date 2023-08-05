162c0793a58ea7c0004505336b081ddf980d56ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:34 +0000

    Add package appmap/0.1.0.dev10/appmap-0.1.0.dev10-py3-none-any.whl

---
+++ packages/appmap/appmap-0.1.0.dev10-py3-none-any.whl/appmap/_implementation/configuration.py
@@ -0,0 +1,272 @@
+"""
+Manage Configuration AppMap recorder for Python.
+"""
+
+import logging
+import os
+import sys
+from contextlib import contextmanager
+from functools import wraps, WRAPPER_ASSIGNMENTS
+import time
+
+import yaml
+
+from . import env
+from . import event
+from .recording import Recorder, Filter
+from .utils import appmap_tls, split_function_name, fqname
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    """ Singleton Config class """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            logger.debug('Creating the Config object')
+            cls._instance = super(Config, cls).__new__(cls)
+
+            # If we're not enabled, no more initialization needs to be
+            # done.
+            cls._instance._initialized = not env.enabled()
+
+        return cls._instance
+
+    def __init__(self):
+        if self.__getattribute__('_initialized'):  # keep pylint happy
+            return
+
+        config_file = os.getenv("APPMAP_CONFIG", "appmap.yml")
+        with open(config_file) as file:
+            self._config = yaml.load(file, Loader=yaml.BaseLoader)
+            logger.debug('self._config %s', self._config)
+
+        out_dir = os.getenv("APPMAP_OUTPUT_DIR", os.path.join('tmp', 'appmap'))
+        self._output_dir = os.path.abspath(out_dir)
+
+        self._initialized = True
+
+    @classmethod
+    def initialize(cls):
+        cls._instance = None
+
+    @property
+    def output_dir(self):
+        return self._output_dir
+
+    @property
+    def name(self):
+        return self._config['name']
+
+    @property
+    def packages(self):
+        return self._config['packages']
+
+
+@contextmanager
+def recording_disabled():
+    tls = appmap_tls()
+    tls['instrumentation_disabled'] = True
+    try:
+        yield
+    finally:
+        tls['instrumentation_disabled'] = False
+
+
+def is_instrumentation_disabled():
+    return appmap_tls().setdefault('instrumentation_disabled', False)
+
+
+def wrap(fn, isstatic):
+    """return a wrapped function"""
+    logger.debug('hooking %s', '.'.join(split_function_name(fn)))
+
+    make_call_event = event.CallEvent.make(fn, isstatic)
+    make_receiver = event.CallEvent.make_receiver(fn, isstatic)
+
+    # django depends on being able to find the cache_clear attribute
+    # on functions. Make sure it gets copied from the original to the
+    # wrapped function.
+    #
+    # Going forward, we should consider how to make this more general.
+    @wraps(fn, assigned = WRAPPER_ASSIGNMENTS + tuple(['cache_clear']))
+    def wrapped_fn(*args, **kwargs):
+        if not Recorder().enabled or is_instrumentation_disabled():
+            return fn(*args, **kwargs)
+
+        with recording_disabled():
+            logger.debug('%s args %s kwargs %s', fn, args, kwargs)
+            call_event = make_call_event(receiver=make_receiver(args, kwargs),
+                                         parameters=[])
+        call_event_id = call_event.id
+        Recorder().add_event(call_event)
+        start_time = time.time()
+        try:
+            ret = fn(*args, **kwargs)
+            elapsed_time = time.time() - start_time
+
+            return_event = event.ReturnEvent(parent_id=call_event_id, elapsed=elapsed_time)
+            Recorder().add_event(return_event)
+            return ret
+        except Exception:  # noqa: E722
+            elapsed_time = time.time() - start_time
+            Recorder().add_event(event.ExceptionEvent(parent_id=call_event_id,
+                                                      elapsed=elapsed_time,
+                                                      exc_info=sys.exc_info()))
+            raise
+    setattr(wrapped_fn, '_appmap_wrapped', True)
+    return wrapped_fn
+
+
+def name_in_set(name, which):
+    return any(filter(lambda n: name.startswith(n + '.') or name == n,
+                      which))
+
+
+def class_in_set(class_name, which):
+    return name_in_set(class_name + '.', which)
+
+
+def function_in_set(fn, which):
+    class_name, fn_name = split_function_name(fn)
+    if class_name is None:
+        # fn isn't in a class, ignore it.
+        return False
+
+    ret = (class_in_set(class_name, which)
+           or name_in_set('%s.%s' %(class_name, fn_name), which))
+
+    logger.debug(('function_in_set, class_name %s'
+                  ' fnname %s'
+                  ' which %s'
+                  ' ret %s'),
+                 class_name,
+                 fn_name,
+                 which,
+                 ret)
+    return ret
+
+
+class ConfigFilter(Filter):
+    """
+    During initialization, the ConfigFilter class reads the `package`
+    entries from the config file.  Based on these entries, it creates
+    a set of paths that should be included, and another set of paths
+    that should be excluded.
+
+    When checking to see if a function should be included,
+    ConfigFilter looks for the function in both sets.  If the function
+    is found in the included set, it's wrapped for instrumentation.
+    If it's found in the excluded set, it's used without modification.
+    If it isn't found in either set, ConfigFilter passes the function
+    on to the next Filter.
+
+    To determine whether a function is in one of the sets, the
+    fully-qualified name of the function is considered, including the
+    packages(s), module, and class that contain it.  As an example,
+    consider a function named `pkg1.pkg2.mod1.Class1.func`.  Any of
+    `pkg1`, `pkg1.pkg2.`, `pkg1.pkg2.mod1`, `pkg1.pkg2.mod1.Class1`,
+    or `pkg1.pkg2.mod1.Class1.func` will match this function (causing
+    it to be included or excluded, as appropriate).  Note that only
+    full names will match.  (This is achieved in the code by appending
+    `.` to the name retrieved from the config file.)
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._includes = set()
+        self._excludes = set()
+
+        if not env.enabled():
+            return
+
+        for package in Config().packages:
+            path = package['path']
+            self._includes.add(path)
+            if 'exclude' in package:
+                if not isinstance(package['exclude'], list):
+                    raise RuntimeError('Excludes for package'
+                                       ' "%s" must be a list' % (path))
+                excludes = ['%s.%s' % (path, e) for e in package['exclude']]
+                self._excludes.update(excludes)
+        logger.info('ConfigFilter, includes %s', self._includes)
+        logger.info('ConfigFilter, excludes %s', self._excludes)
+
+    def excluded(self, fn):
+        ret = function_in_set(fn, self._excludes)
+        logger.debug('ConfigFilter, %s excluded? %s', fn, ret)
+        return ret
+
+    def included(self, fn):
+        ret = function_in_set(fn, self._includes)
+        logger.debug('ConfigFilter, %s included? %s', fn, ret)
+        return ret
+
+    def filter(self, class_):
+        name = fqname(class_)
+        if class_in_set(name, self._excludes):
+            logger.info('excluded class %s', name)
+            return False
+        if class_in_set(name, self._includes):
+            logger.info('included class %s', name)
+            return True
+
+        logger.debug('  undecided')
+        return self.next_filter.filter(class_)
+
+    def wrap(self, fn, isstatic):
+        logger.debug('ConfigFilter.wrap, fn %s', fn)
+
+        fn_name = '.'.join(split_function_name(fn))
+        if self.excluded(fn):
+            logger.info('excluded function %s', fn_name)
+            return fn
+
+        if self.included(fn):
+            logger.info('included function %s', fn_name)
+            wrapped = getattr(fn, '_appmap_wrapped', None)
+            logger.debug('  wrapped %s', wrapped)
+            if wrapped is None:
+                logger.debug('  wrapping %s', fn)
+                ret = wrap(fn, isstatic)
+            else:
+                logger.debug('  already wrapped %s', fn)
+                ret = fn
+            return ret
+
+        return self.next_filter.wrap(fn, isstatic)
+
+
+class BuiltinFilter(Filter):
+    def __init__(self, *args):
+        super().__init__(*args)
+        if not env.enabled():
+            self._includes = set()
+            return
+
+        self._includes = {'os.read', 'os.write'}
+
+    def included(self, fn):
+        return function_in_set(fn, self._includes)
+
+    def filter(self, class_):
+        name = class_.__name__
+        if class_in_set(name, self._includes):
+            return True
+        return self.next_filter.filter(class_)
+
+    def wrap(self, fn, isstatic):
+        if self.included(fn):
+            return wrap(fn, isstatic)
+        return self.next_filter.wrap(fn, isstatic)
+
+
+def initialize():
+    Config().initialize()
+    Recorder().use_filter(BuiltinFilter)
+    Recorder().use_filter(ConfigFilter)
+
+
+initialize()
---
+++ packages/appmap/appmap-0.1.0.dev10-py3-none-any.whl/appmap/_implementation/event.py
@@ -0,0 +1,192 @@
+import inspect
+import threading
+from itertools import chain
+from functools import partial
+
+from .utils import appmap_tls, split_function_name, fqname
+
+
+class _EventIds:
+    id = 1
+    lock = threading.Lock()
+
+    @classmethod
+    def next_id(cls):
+        with cls.lock:
+            cls.id += 1
+            return cls.id
+
+    # The identifiers returned by threading.get_ident() aren't
+    # guaranteed to be unique: they may be recycled after the thread
+    # exits. We need a unique id, so we'll manage it ourselves.
+    _next_thread_id = 0
+    _next_thread_id_lock = threading.Lock()
+
+    @classmethod
+    def next_thread_id(cls):
+        with cls._next_thread_id_lock:
+            cls._next_thread_id += 1
+            return cls._next_thread_id
+
+    @classmethod
+    def get_thread_id(cls):
+        tls = appmap_tls()
+        return (tls.get('thread_id')
+                or tls.setdefault('thread_id', cls.next_thread_id()))
+
+    @classmethod
+    def reset(cls):
+        cls.id = 1
+        cls._next_thread_id = 0
+
+
+class Event:
+    __slots__ = ['id', 'event', 'thread_id']
+
+    def __init__(self, event):
+        self.id = _EventIds.next_id()
+        self.event = event
+        self.thread_id = _EventIds.get_thread_id()
+
+    def to_dict(self):
+        return {
+            k: getattr(self, k, None)
+            for k in chain.from_iterable(getattr(cls, '__slots__', [])
+                                         for cls in type(self).__mro__)
+        }
+
+    def __repr__(self):
+        return repr(self.to_dict())
+
+
+class CallEvent(Event):
+    __slots__ = ['defined_class', 'method_id', 'path', 'lineno',
+                 'static', 'receiver', 'parameters']
+
+    @staticmethod
+    def make(fn, isstatic):
+        """
+        Return a factory for creating new CallEvents based on
+        introspecting the given function.
+        """
+        defined_class, method_id = split_function_name(fn)
+
+        try:
+            path = inspect.getsourcefile(fn)
+        except TypeError:
+            path = '<builtin>'
+
+        try:
+            __, lineno = inspect.getsourcelines(fn)
+        except OSError:
+            lineno = 0
+
+        return partial(CallEvent, defined_class,
+                       method_id, path, lineno, isstatic)
+
+    @staticmethod
+    def make_receiver(fn, isstatic):
+        """
+        Create the receiver object that should be part of the call
+        event for the given function.
+        """
+        defined_class, __ = split_function_name(fn)
+        if isstatic:
+            cls = "class"
+            value = defined_class
+            object_id = id(defined_class)
+        else:
+            cls = defined_class
+            value = None
+            object_id = None
+
+        def make(cls, value, object_id, *args):
+            if not isstatic:
+                object_id = id(args[0][0])
+                slf = args[0][0]
+                # Make a best-effort attempt to get a string value for
+                # the receiver. If repr() raises, formulate a
+                # value from the class and id.
+                try:
+                    value = repr(slf)
+                except Exception:  # pylint: disable=broad-except
+                    value = '<%s object at %#x>' % (defined_class, object_id)
+            return {
+                "class": cls,
+                "value": value,
+                "object_id": object_id
+            }
+        return partial(make, cls, value, object_id)
+
+    def __init__(self, defined_class, method_id, path, lineno,
+                 static, receiver, parameters):
+        super().__init__('call')
+        self.defined_class = defined_class
+        self.method_id = method_id
+        self.path = path
+        self.lineno = lineno
+        self.static = static
+        self.receiver = receiver
+        self.parameters = parameters
+
+
+class SqlEvent(Event):
+    __slots__ = ['sql_query']
+
+    def __init__(self, sql):
+        super().__init__('call')
+        self.sql_query = {
+            'sql': sql
+        }
+
+
+class HttpRequestEvent(Event):
+    __slots__ = ['http_server_request']
+
+    def __init__(self, request_method, path_info, protocol):
+        super().__init__('call')
+        self.http_server_request = {
+            'request_method': request_method,
+            'path_info': path_info,
+            'protocol': protocol
+        }
+
+
+class ReturnEvent(Event):
+    __slots__ = ['parent_id', 'elapsed']
+
+    def __init__(self, parent_id, elapsed):
+        super().__init__('return')
+        self.parent_id = parent_id
+        self.elapsed = elapsed
+
+
+class HttpResponseEvent(ReturnEvent):
+    __slots__ = ['http_server_response']
+
+    def __init__(self, status_code, mime_type, **kwargs):
+        super().__init__(**kwargs)
+        self.http_server_response = {
+            'status_code': status_code,
+            'mime_type': mime_type
+        }
+
+
+class ExceptionEvent(ReturnEvent):
+    __slots__ = ['exceptions']
+
+    def __init__(self, parent_id, elapsed, exc_info):
+        super().__init__(parent_id, elapsed)
+        class_, exc, __ = exc_info
+        self.exceptions = [{
+            'exceptions': {
+                'class': fqname(class_),
+                'message': str(exc),
+                'object_id': id(exc),
+            }
+        }]
+
+
+def initialize():
+    appmap_tls().pop('thread_id', None)
+    _EventIds.reset()
