91f5f2d90af5a93dfbe066371330820e580ef262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:01 +0000

    Add package portdatasplitter/1.3/portdatasplitter-1.3-py3-none-any.whl

---
+++ packages/portdatasplitter/portdatasplitter-1.3-py3-none-any.whl/portdatasplitter/settings.py
@@ -0,0 +1,5 @@
+# Список weight_list по умолчанию содержит код no_data_code
+no_data_code = '5'
+
+# Код, который попадает в weight_list, если весовой терминал вдруг отключился
+scale_disconnected_code = '17'
