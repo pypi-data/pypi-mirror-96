8f998522db530230f11325d1cee40047dce23f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:35 +0000

    Add package py-pkg-jraza19/0.1.0/py_pkg_jraza19-0.1.0.tar.gz

---
+++ packages/py-pkg-jraza19/py_pkg_jraza19-0.1.0.tar.gz/py_pkg_jraza19-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['py_pkg_jraza19']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'py-pkg-jraza19',
+    'version': '0.1.0',
+    'description': 'Python package that eases the pain of concatenating Pandas categoricals!',
+    'long_description': None,
+    'author': 'Javairia Raza',
+    'author_email': 'jr.jem19@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
