510006b3cf5add832ab4ffa47e0eba8726f1d2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package nuvpy/1.0.6/nuvpy-1.0.6-py3-none-any.whl

---
+++ packages/nuvpy/nuvpy-1.0.6-py3-none-any.whl/nuvpy/__init__.py
@@ -0,0 +1,7 @@
+from . import nuviot_auth
+from . import nuviot_data
+from . import nuviot_device
+from . import nuviot_srvc
+from . import nuviot_util
+from . import nuviot_jobs
+from . import nuviot_report_builder
