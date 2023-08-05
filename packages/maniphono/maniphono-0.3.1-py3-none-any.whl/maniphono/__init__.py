35ce8c731410a852f5e004130388fdeddb43883b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:48 +0000

    Add package maniphono/0.3.1/maniphono-0.3.1-py3-none-any.whl

---
+++ packages/maniphono/maniphono-0.3.1-py3-none-any.whl/maniphono/__init__.py
@@ -0,0 +1,31 @@
+# __init__.py
+
+"""
+maniphono __init__.py
+"""
+
+__version__ = "0.3.1"
+__author__ = "Tiago Tresoldi"
+__email__ = "tiago.tresoldi@lingfil.uu.se"
+
+# Build the namespace
+from maniphono.phonomodel import (
+    PhonoModel,
+    model_mipa,
+    model_tresoldi,
+)  # pyflakes.ignore
+from maniphono.sound import Sound  # noqa: F401
+from maniphono.segment import (
+    Segment,
+    SoundSegment,
+    BoundarySegment,
+    parse_segment,
+)  # noqa: F401
+from maniphono.segsequence import SegSequence, parse_sequence  # noqa: F401
+from maniphono.utils import (
+    codepoint2glyph,
+    replace_codepoints,
+    read_distance_matrix,
+    parse_constraints,
+    split_fvalues_str,
+)  # pyflakes.ignore
