4245e07123c5e6cf39ad01a244e9719dcf0dec47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:27 +0000

    Add package cricketpy/1.2.0/cricketpy-1.2.0.tar.gz

---
+++ packages/cricketpy/cricketpy-1.2.0.tar.gz/cricketpy-1.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+
+# This call to setup() does all the work
+setup(
+    name="cricketpy",
+    version="1.2.0",
+    description="View cricket scores,commentary and scorecard from the command line",
+    url="https://github.com/sAksham-Ar/cricketpy",
+    author="Saksham Arya",
+    author_email="aryasaksham@gmail.com",
+    license="GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["cricketpy"],
+    include_package_data=True,
+    install_requires=["criapi","pyfiglet"],
+    entry_points={
+        "console_scripts": [
+            "cricketpy=cricketpy.__main__:cricpy",
+        ]
+    },
+)
