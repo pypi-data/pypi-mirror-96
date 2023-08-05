2a6c9af06dc6edc1dda367ce8259989a5a0b62b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:00 +0000

    Add package geomapviz/0.4/geomapviz-0.4.tar.gz

---
+++ packages/geomapviz/geomapviz-0.4.tar.gz/geomapviz-0.4/geomapviz/__init__.py
@@ -0,0 +1,9 @@
+from . import geomapviz
+__version__ = "0.4"
+__all__ = ["geomapviz"]
+
+# bound to upper level
+from .geomapviz import *
+
+# Author declaration (optional)
+__author__ = "Thomas Bury"
