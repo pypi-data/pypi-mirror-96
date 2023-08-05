66a2d61b455ac962a2d095ac35c4026c7f02c292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:51 +0000

    Add package pax2graphml/0.5.2/pax2graphml-0.5.2.tar.gz

---
+++ packages/pax2graphml/pax2graphml-0.5.2.tar.gz/pax2graphml-0.5.2/pax2graphml/math.py
@@ -0,0 +1,31 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import graph_tool.all as gt
+import pandas as pd
+import argparse
+
+def hyperbolicTangent(n):
+    return np.tanh(x)
+
+def sigmoid(x, derivative=False):
+    sigm = 1 / (1 + np.exp(-x))
+    if derivative:
+        return sigm * (1 - sigm)
+    return sigm
+ 
+#https://fr.wikipedia.org/wiki/Sigmo%C3%AFde_(math%C3%A9matiques)
+
+
+
+
+def sigmoidTransform(x):
+    x=np.double(x)
+    zr=np.double(0)
+    if x < zr:
+          r=sigmoid(-x)
+          return -r
+    else:    
+          r=sigmoid(x)
+          return r
