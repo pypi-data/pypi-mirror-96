67b4556c23030f3835e96c0d5ddc0d932723587c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package mqtt-io/2.0.0b4/mqtt-io-2.0.0b4.tar.gz

---
+++ packages/mqtt-io/mqtt-io-2.0.0b4.tar.gz/mqtt-io-2.0.0b4/mqtt_io/types.py
@@ -0,0 +1,15 @@
+"""
+Types used in MQTT IO type hints.
+"""
+
+from typing import TYPE_CHECKING, Any, Dict, Union
+
+if TYPE_CHECKING:
+    import asyncio
+    from concurrent.futures import Future as ConcurrentFuture
+
+ConfigType = Dict[str, Any]
+PinType = Union[str, int]
+SensorValueType = Union[float, int, None]
+CerberusSchemaType = Dict[str, Any]
+UnawaitedTaskType = Union["asyncio.Future[Any]", "ConcurrentFuture[Any]"]
