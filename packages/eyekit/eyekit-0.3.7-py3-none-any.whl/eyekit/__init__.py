79c947f2a849bbfb054e29ca1feef7db5320ec77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package eyekit/0.3.7/eyekit-0.3.7-py3-none-any.whl

---
+++ packages/eyekit/eyekit-0.3.7-py3-none-any.whl/eyekit/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. include:: ../.GUIDE.md
+   :start-line: 4
+"""
+
+from .fixation import FixationSequence
+from .text import TextBlock
+from . import analysis
+from . import io
+from . import tools
+from . import vis
+
+del fixation, text
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "???"
