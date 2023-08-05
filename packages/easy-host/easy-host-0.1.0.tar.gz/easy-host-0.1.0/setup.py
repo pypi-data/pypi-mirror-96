04313d8e643845c08465979d83c2bcfeff9523fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:07 +0000

    Add package easy-host/0.1.0/easy-host-0.1.0.tar.gz

---
+++ packages/easy-host/easy-host-0.1.0.tar.gz/easy-host-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="easy-host",
+    version="0.1.0",
+    author="toor",
+    author_email="sh9901@163.com",
+    description="",
+    long_description="",
+    url="https://pypi.org/project/ease-host/",
+    packages=['src'],
+    include_package_data=True,
+    data_files=[('files', [
+        'resources/javaparse-sample.zip',
+        'resources/javaparservisited.pdf',
+        'resources/swagger-core.zip'
+    ])],
+    install_requires=[],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
