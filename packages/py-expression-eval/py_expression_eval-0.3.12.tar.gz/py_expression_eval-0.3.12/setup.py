e758516ed6a2358fb9efc63eb16a53b34ac59b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:58 +0000

    Add package py-expression-eval/0.3.12/py_expression_eval-0.3.12.tar.gz

---
+++ packages/py-expression-eval/py_expression_eval-0.3.12.tar.gz/py_expression_eval-0.3.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='py_expression_eval',
+    version='0.3.12',
+    description='Python Mathematical Expression Evaluator',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/AxiaCore/py-expression-eval/',
+    author='vero4ka',
+    author_email='vero4ka.ru@gmail.com',
+    license='MIT',
+    packages=['py_expression_eval'],
+    zip_safe=False,
+    test_suite='py_expression_eval.tests',
+)
