5c62d8b596e5469e974f3cf123017ecd818cc718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package convertbase/0.1.3/convertbase-0.1.3.tar.gz

---
+++ packages/convertbase/convertbase-0.1.3.tar.gz/convertbase-0.1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="convertbase",
+    version="0.1.3",
+    author="Johan Lahti",
+    author_email="ccie60702@gmail.com",
+    description="Convert between bases, initailly intended for RFC4648 base32",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johan-lahti/convertbase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.7',
+)
