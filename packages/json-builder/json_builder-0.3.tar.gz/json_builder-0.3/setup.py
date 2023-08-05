3423ae14413cd21d347c8e018d9f10598e10b4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:35 +0000

    Add package json-builder/0.3/json_builder-0.3.tar.gz

---
+++ packages/json-builder/json_builder-0.3.tar.gz/json_builder-0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='json_builder',
+    packages=find_packages(),
+    version='0.3',
+    license='MIT',
+    description='This module is built to provide straightforward way to build and modify json objects',
+    long_description=open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    author='Albert Dorcioman',
+    author_email='albert.dorcioman@gmail.com',
+    url='https://github.com/adorcioman/json_builder',
+    download_url='https://github.com/adorcioman/json_builder/archive/v_0.3.tar.gz',
+    keywords=['Json', 'Builder'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
