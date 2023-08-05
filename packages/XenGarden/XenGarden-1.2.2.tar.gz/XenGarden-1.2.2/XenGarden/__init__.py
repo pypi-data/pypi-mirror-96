9dcc7486d6586a4fcd378a42d727f39d29a46ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:55 +0000

    Add package XenGarden/1.2.2/XenGarden-1.2.2.tar.gz

---
+++ packages/XenGarden/XenGarden-1.2.2.tar.gz/XenGarden-1.2.2/XenGarden/__init__.py
@@ -0,0 +1,23 @@
+from XenGarden.SR import SR
+from XenGarden.VBD import VBD
+from XenGarden.VDI import VDI
+from XenGarden.VIF import VIF
+from XenGarden.Console import Console
+from XenGarden.GuestMetrics import GuestMetrics
+from XenGarden.Host import Host
+from XenGarden.VM import VM
+from XenGarden.session import create_session
+
+__all__ = [
+    "SR",
+    "VBD",
+    "VDI",
+    "VIF",
+    "Console",
+    "GuestMetrics",
+    "Host",
+    "VM",
+    "session",
+]
+__version__ = "1.2.0"
+__copyright__ = "Copyright (c) 2020-2021 Stella IT Inc."
