ee7e61693cab8e6cb86a44245f05cb32654a2a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:34 +0000

    Add package PyAutoMaker/0.1.2/PyAutoMaker-0.1.2.tar.gz

---
+++ packages/PyAutoMaker/PyAutoMaker-0.1.2.tar.gz/PyAutoMaker-0.1.2/PyAutoMaker/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from glob import glob
+from os.path import dirname
+
+rootDir = dirname(__file__)
+
+sys.path.append(rootDir)
+os.environ["PATH"] = os.environ["PATH"] + os.pathsep + os.path.join(rootDir, "dlls")
+os.environ["DNN_DATA_FOLDER"] = os.path.join(rootDir, "models")
+os.environ["IMG_DATA_FOLDER"] = os.path.join(rootDir, "imgs")
+
+
+import dd
+import image
+import record
