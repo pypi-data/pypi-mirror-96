a56ff87337b8dc760ade1e90d289955fdba8b27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:11 +0000

    Add package pynsodm/0.3.1/pynsodm-0.3.1-py3-none-any.whl

---
+++ packages/pynsodm/pynsodm-0.3.1-py3-none-any.whl/pynsodm/fields/id_fields.py
@@ -0,0 +1,21 @@
+from .base_field import BaseField
+
+
+class IDField(BaseField):
+    def __init__(self, **kwargs):
+        if 'is_primary' not in kwargs:
+            kwargs['is_primary'] = True
+            kwargs['is_index'] = True
+
+        if 'is_sensitive' not in kwargs:
+            kwargs['is_sensitive'] = True
+
+        BaseField.__init__(self, **kwargs)
+
+    def __set__(self, obj, value):
+        self.value = str(value) if value else None
+
+    def __get__(self, obj, type):
+        if not obj:
+            return self
+        return self.value if self.value else None
