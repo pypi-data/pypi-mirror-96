b3dd8544b5778f5a0a1e71d7c9c485c30ed966f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package meshio/4.3.10/meshio-4.3.10.tar.gz

---
+++ packages/meshio/meshio-4.3.10.tar.gz/meshio-4.3.10/test/test_svg.py
@@ -0,0 +1,22 @@
+import os
+import tempfile
+
+import helpers
+import pytest
+
+import meshio
+
+test_set = [
+    helpers.empty_mesh,
+    helpers.line_mesh,
+    helpers.tri_mesh,
+    helpers.tri_mesh_2d,
+    helpers.quad_mesh,
+]
+
+
+@pytest.mark.parametrize("mesh", test_set)
+def test(mesh):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        filepath = os.path.join(temp_dir, "out.svg")
+        meshio.write_points_cells(filepath, mesh.points, mesh.cells)
