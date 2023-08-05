a47ca387caa7572d17cf7987ed7daac5c05c44da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:16 +0000

    Add package covid-cloud-cli/0.0.3/covid_cloud_cli-0.0.3-py3-none-any.whl

---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.3-py3-none-any.whl/cli/__init__.py
@@ -0,0 +1,3 @@
+from .cli_config import commands as config_commands
+from .search import commands as search_commands
+from .files import commands as file_commands
---
+++ packages/covid-cloud-cli/covid_cloud_cli-0.0.3-py3-none-any.whl/client/__init__.py
@@ -0,0 +1,3 @@
+from .constants import *
+from .files_client import *
+from .search_client import *
