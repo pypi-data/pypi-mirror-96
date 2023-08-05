1c0cb91f5ebc477cc958c3f7c483fbe150f8dbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:15 +0000

    Add package c-formatter-42/0.0.3/c_formatter_42-0.0.3-py3-none-any.whl

---
+++ packages/c-formatter-42/c_formatter_42-0.0.3-py3-none-any.whl/c_formatter_42/formatters/helper.py
@@ -0,0 +1,36 @@
+# ############################################################################ #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    helper.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cacharle <me@cacharle.xyz>                 +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/10/04 11:38:00 by cacharle          #+#    #+#              #
+#    Updated: 2021/02/08 18:22:06 by charles          ###   ########.fr        #
+#                                                                              #
+# ############################################################################ #
+
+import re
+
+
+# regex for a type
+REGEX_TYPE      = r"([a-z]+\s+)*[a-zA-Z_]\w*"
+# regex for a c variable/function name
+REGEX_NAME      = r"\**[a-zA-Z_]\w*"
+# regex for a name in a declaration context (with array and function ptr)
+REGEX_DECL_NAME = r"\(?{name}(\[.*\])*(\)\(.*\))?".format(name=REGEX_NAME)
+
+
+def locally_scoped(func):
+    """ Apply the formatter on every local scopes of the content """
+
+    def wrapper(content: str) -> str:
+        return re.sub(
+            # `*?` is the non greedy version of `*`
+            # https://docs.python.org/3/howto/regex.html#greedy-versus-non-greedy
+            r"\)\n\{\n(?P<body>.*?)\n\}\n".replace(r"\n", "\n"),
+            lambda match: ")\n{\n" + func(match.group("body")) + "\n}\n",
+            content,
+            flags=re.DOTALL
+        )
+    return wrapper
