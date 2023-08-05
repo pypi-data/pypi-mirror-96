0408dc41220ff52a833b30008ad1bbcef7f35ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:37 +0000

    Add package prepextend/2020.2.1.5/prepextend-2020.2.1.5.tar.gz

---
+++ packages/prepextend/prepextend-2020.2.1.5.tar.gz/prepextend-2020.2.1.5/prepextend/run/api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  1 15:33:08 2019
+
+@author: WEIHAO
+"""
+
+from prepextend._config.config import config_set
+from prepextend.run.run_prep import prep_run
+
+config = config_set()
