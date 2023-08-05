2fa722dfc486609d059ab8858b02ca2619a722a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:22 +0000

    Add package undent/0.2/undent-0.2.tar.gz

---
+++ packages/undent/undent-0.2.tar.gz/undent-0.2/undent/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#
+# Undent - Dedent and format multiline strings into human-readable output
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: MIT
+#
+
+
+from undent.api import undent
+from undent.__version__ import (
+    __title__,
+    __version__,
+    __license__,
+    __author__,
+    __contact__,
+    __url__,
+    __description__)
---
+++ packages/undent/undent-0.2.tar.gz/undent-0.2/undent/__version__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#
+# Undent - Dedent and format multiline strings into human-readable output
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: MIT
+#
+
+__title__ = 'undent'
+__version__ = '0.2'
+__license__ = 'MIT'
+__author__ = 'Ansgar Grunseid'
+__contact__ = 'grunseid@gmail.com'
+__url__ = 'https://github.com/gruns/undent'
+__description__ = (
+    'Dedent and format multiline strings into human-readable output.')
