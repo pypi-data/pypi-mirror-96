8316fa0c577c600e12adcb28c4412e76571d4e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:31 +0000

    Add package us-senate/0.0a1.dev1/us_senate-0.0a1.dev1-py3-none-any.whl

---
+++ packages/us-senate/us_senate-0.0a1.dev1-py3-none-any.whl/senate/__init__.py
@@ -0,0 +1,8 @@
+# Imports from us-elections.
+# from elections.models.election_years import ElectionYear
+
+
+__appname__ = "us-senate"
+__version__ = (0, "0a1", "dev1")
+
+# __all__ = ["__appname__", "__version__", "ElectionYear"]
