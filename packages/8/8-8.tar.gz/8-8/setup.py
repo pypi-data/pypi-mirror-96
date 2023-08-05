86764e590677189fd20e7a8002cf8de84507c59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:33 +0000

    Add package 8/8/8-8.tar.gz

---
+++ packages/8/8-8.tar.gz/8-8/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='8',
+    version='8',
+    description='For future projects.',
+)
