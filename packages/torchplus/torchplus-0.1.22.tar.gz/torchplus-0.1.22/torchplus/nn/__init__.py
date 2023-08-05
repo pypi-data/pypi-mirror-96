bdf14478a20a3a58c12172b1d5dc1b959e9bd9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:43 +0000

    Add package torchplus/0.1.22/torchplus-0.1.22.tar.gz

---
+++ packages/torchplus/torchplus-0.1.22.tar.gz/torchplus-0.1.22/torchplus/nn/__init__.py
@@ -0,0 +1,10 @@
+#! python3.8 -u
+#  -*- coding: utf-8 -*-
+
+##############################
+## Project PyCTLib
+## Package torchplus.nn
+##############################
+
+from .modules import *
+from .parameter import Parameter
