7672b4aacbaba5838db250b82d2b8d543a7aeb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package NlpToolkit-NamedEntityRecognition/1.0.7/NlpToolkit-NamedEntityRecognition-1.0.7.tar.gz

---
+++ packages/NlpToolkit-NamedEntityRecognition/NlpToolkit-NamedEntityRecognition-1.0.7.tar.gz/NlpToolkit-NamedEntityRecognition-1.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-NamedEntityRecognition',
+    version='1.0.7',
+    packages=['NamedEntityRecognition'],
+    url='https://github.com/StarlangSoftware/TurkishNamedEntityRecognition-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='NER Corpus Processing Library',
+    install_requires=['NlpToolkit-Corpus']
+)
