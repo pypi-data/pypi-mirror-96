a0b70b5c7b6f855444a6785e9d45f80649cd5f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:12 +0000

    Add package logevo/1.0.0/logevo-1.0.0.tar.gz

---
+++ packages/logevo/logevo-1.0.0.tar.gz/logevo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='logevo',
+    version='1.0.0',
+    description='Facke Logevo',
+    packages=['logevo'],
+    author_email='volodymyr.volodymyrovych@gmail.com',
+    zip_safe=False
+)
