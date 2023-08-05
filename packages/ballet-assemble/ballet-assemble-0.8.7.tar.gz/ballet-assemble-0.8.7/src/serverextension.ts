ed43c0e89e7c48dc5825c742cec9fc1e41c70cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package ballet-assemble/0.8.7/ballet-assemble-0.8.7.tar.gz

---
+++ packages/ballet-assemble/ballet-assemble-0.8.7.tar.gz/ballet-assemble-0.8.7/src/serverextension.ts
@@ -0,0 +1,82 @@
+import { URLExt } from '@jupyterlab/coreutils';
+
+import { ServerConnection } from '@jupyterlab/services';
+
+export interface ISubmissionRequest {
+  codeContent: string;
+}
+
+export interface ISubmissionResponse {
+  result: boolean;
+  url?: string;
+  message?: string;
+  tb?: string;
+}
+
+export interface IAuthenticatedResponse {
+  result: boolean;
+}
+
+export async function submit(
+  cellContents: string
+): Promise<ISubmissionResponse> {
+  const endPoint = 'submit';
+  const init = {
+    method: 'POST',
+    body: JSON.stringify({
+      codeContent: cellContents
+    })
+  };
+
+  try {
+    return request<ISubmissionResponse>(endPoint, init);
+  } catch (error) {
+    console.error(error);
+    return { result: false };
+  }
+}
+
+export async function checkStatus(): Promise<void> {
+  return request<void>('status');
+}
+
+export async function isAuthenticated(): Promise<boolean> {
+  const response = await request<IAuthenticatedResponse>('auth/authenticated');
+  return response.result;
+}
+
+export function getEndpointUrl(endPoint: string): string {
+  const settings = ServerConnection.makeSettings();
+  return URLExt.join(settings.baseUrl, 'assemble', endPoint);
+}
+
+/**
+ * Call the API extension
+ *
+ * @param endPoint API REST end point for the extension
+ * @param init Initial values for the request
+ * @returns The response body interpreted as JSON
+ */
+export async function request<T>(
+  endPoint: string = '',
+  init: RequestInit = {}
+): Promise<T> {
+  // Make request to Jupyter API
+  const requestUrl = getEndpointUrl(endPoint);
+
+  let response: Response;
+  try {
+    const settings = ServerConnection.makeSettings();
+    response = await ServerConnection.makeRequest(requestUrl, init, settings);
+  } catch (error) {
+    throw new ServerConnection.NetworkError(error);
+  }
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new ServerConnection.ResponseError(response, data.message);
+  }
+
+  return data;
+}
---
+++ packages/ballet-assemble/ballet-assemble-0.8.7.tar.gz/ballet-assemble-0.8.7/src/widgets.tsx
@@ -0,0 +1,58 @@
+import { ReactWidget } from '@jupyterlab/apputils';
+import React from 'react';
+
+export class ConfirmWidget extends ReactWidget {
+  code: string;
+
+  constructor(code: string) {
+    super();
+    this.code = code;
+    this.addClass('jp-ReactWidget');
+  }
+
+  render() {
+    return (
+      <div className="assemble-featureSubmittedConfirm">
+        <p>
+          {' '}
+          The following feature would be submitted to the upstream Ballet
+          project:{' '}
+        </p>
+        <div>
+          <pre>
+            <code>{this.code}</code>
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class FeatureSubmittedOkayWidget extends ReactWidget {
+  url: string;
+
+  constructor(url: string) {
+    super();
+    this.url = url;
+    this.addClass('jp-ReactWidget');
+  }
+
+  render() {
+    return (
+      <div className="assemble-featureSubmittedOkay">
+        <p> Your feature was submitted! </p>
+        <br />
+        <p>
+          {' '}
+          The associated pull request is visible at{' '}
+          <a href={this.url} target="_blank">
+            {this.url}
+          </a>
+          .{' '}
+        </p>
+        <br />
+        <p> Please do not submit this same feature more than once. </p>
+      </div>
+    );
+  }
+}
