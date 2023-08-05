ab2a13be58282c8b8aea8513ee4515cb54d6d150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:23 +0000

    Add package chainsyncer/0.0.1a16/chainsyncer-0.0.1a16-py3-none-any.whl

---
+++ packages/chainsyncer/chainsyncer-0.0.1a16-py3-none-any.whl/chainsyncer/error.py
@@ -0,0 +1,12 @@
+class SyncDone(Exception):
+    """Exception raised when a syncing is complete.
+    """
+    pass
+
+
+class RequestError(Exception):
+    pass
+
+
+class BackendError(Exception):
+    pass
