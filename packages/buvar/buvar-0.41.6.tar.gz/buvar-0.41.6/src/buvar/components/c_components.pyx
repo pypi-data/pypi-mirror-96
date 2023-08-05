0381f4715e00e25ade4b87d953f89b83c51e9c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:25 +0000

    Add package buvar/0.41.6/buvar-0.41.6.tar.gz

---
+++ packages/buvar/buvar-0.41.6.tar.gz/buvar-0.41.6/src/buvar/components/c_components.pyx
@@ -0,0 +1,98 @@
+# cython: language_level=3
+"""Self made components.
+
+stacked::
+    - each layer is the destination of additions, no inferior layer is affected
+    - find collects all stacked namespaces, where superior names overwrite
+      inferior ones
+"""
+import itertools
+
+missing = object()
+
+
+cdef class ComponentLookupError(Exception):
+    pass
+
+
+cdef class Components:
+    def __init__(self, *stack):
+        self._stack = list(stack) or [{}]  # always at least one map
+        self.namespaces = self._stack[0]
+
+    @property
+    def stack(self):
+        return self._stack
+
+    cdef _add(self, object item, namespace=None, name=None):
+        cdef dict space
+
+        if isinstance(item, type):
+            raise ValueError("A component should be an instance.")
+
+        if namespace is None:
+            namespace = type(item)
+
+        if isinstance(namespace, type):
+            mro = namespace.__mro__
+        else:
+            mro = [namespace]
+
+        # we add the whole MRO to have a fast lookup later
+        for namespace in mro:
+            if namespace in self.namespaces:
+                self.namespaces[namespace][name] = item
+            else:
+                space = {name: item}
+                self.namespaces[namespace] = space
+        return item
+
+    def add(self, item, namespace=None, *, name=None):
+        if isinstance(item, type):
+            raise ValueError("A component should be an instance.")
+        return self._add(item, namespace, name=name)
+
+    cdef _push(self, namespaces, stack):
+        if namespaces is None:
+            namespaces = {}
+
+        components = self.__class__(namespaces, *itertools.chain(stack, self._stack))
+        return components
+
+    def push(self, namespaces=None, *stack):
+        """Push a whole stack on top of the actual components or just a new
+        bare namespaces one.
+        """
+        return self._push(namespaces, stack)
+
+    cpdef pop(self):
+        return self.__class__(*self._stack[1:])
+
+    cdef dict _find(self, namespace):
+        cdef dict merged = {}
+        for namespaces in self._stack[::-1]:
+            try:
+                space = namespaces[namespace]
+                merged.update(space)
+            except KeyError:
+                pass
+        return merged
+
+    def find(self, namespace):
+        return self._find(namespace)
+
+    cdef _get(self, namespace, name=None, default=missing):
+        for namespaces in self._stack:
+            try:
+                item = namespaces[namespace][name]
+                return item
+            except KeyError:
+                pass
+
+        if default is missing:
+            raise ComponentLookupError("Component not found", namespace, name, default)
+        return default
+
+    def get(self, namespace, *, name=None, default=missing):
+        item = self._get(namespace, name=name, default=default)
+        return item
---
+++ packages/buvar/buvar-0.41.6.tar.gz/buvar-0.41.6/src/buvar/util.py
@@ -0,0 +1,92 @@
+import functools
+import importlib
+import inspect
+import sys
+import types
+import typing
+
+
+def methdispatch(func):
+    # https://stackoverflow.com/questions/24601722/how-can-i-use-functools-singledispatch-with-instance-methods
+    dispatcher = functools.singledispatch(func)
+
+    def wrapper(*args, **kw):
+        return dispatcher.dispatch(args[1].__class__)(*args, **kw)
+
+    wrapper.register = dispatcher.register
+    functools.update_wrapper(wrapper, func)
+    return wrapper
+
+
+class adict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+class reify:
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        functools.update_wrapper(self, wrapped)
+
+    def __get__(self, inst, objtype=None):
+        if inst is None:
+            return self
+        val = self.wrapped(inst)
+        setattr(inst, self.wrapped.__name__, val)
+        return val
+
+
+def merge_dict(*sources, dest=None):
+    """Merge `sources` into `dest`.
+
+    `dest` is altered in place.
+    """
+    if dest is None:
+        dest = {}
+    for source in sources:
+        for key, value in source.items():
+            if isinstance(value, dict):
+                # get node or create one
+                node = dest.setdefault(key, {})
+                merge_dict(value, dest=node)
+            else:
+                dest[key] = value
+    return dest
+
+
+def resolve_dotted_name(name, *, caller: typing.Union[types.FrameType, int] = 0):
+    """Use pkg_resources style dotted name to resolve a name."""
+    # skip resolving for module and coroutine
+    if inspect.ismodule(name) or inspect.isroutine(name):
+        return name
+
+    # relative import
+    if name.startswith("."):
+        # find coller package
+        frame = (
+            caller if isinstance(caller, types.FrameType) else sys._getframe(1 + caller)
+        )
+        caller_package = frame.f_globals["__package__"]
+    else:
+        caller_package = None
+
+    part = ":"
+    module_name, _, attr_name = name.partition(part)
+
+    if part in attr_name:
+        raise ValueError(f"Invalid name: {name}", name)
+
+    try:
+        resolved = importlib.import_module(module_name, caller_package)
+    except ValueError as ex:
+        raise ImportError(*ex.args)
+
+    if attr_name:
+        resolved = getattr(resolved, attr_name)
+
+    return resolved
+
+
+def fqdn(obj):
+    return f"{obj.__module__}.{obj.__qualname__}"
