1e98b964a666ae4027e37aac517c5fdf1c8ee890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:53 +0000

    Add package drf-psq/1.1.0/drf_psq-1.1.0.tar.gz

---
+++ packages/drf-psq/drf_psq-1.1.0.tar.gz/drf_psq-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+description = (
+    "The simplest and most general way to manage action-based "
+    "permissions, serializers, and querysets dependent on permission-based "
+    "rules for the Django REST framework!"
+)
+
+setup(
+    name='drf_psq',
+    version='1.1.0',
+    packages=find_packages(),
+
+    description=description,
+    license='MIT',
+    author='AminHP',
+    author_email='mdan.hagh@gmail.com',
+
+    install_requires=[
+        'django',
+        'djangorestframework',
+    ],
+)
