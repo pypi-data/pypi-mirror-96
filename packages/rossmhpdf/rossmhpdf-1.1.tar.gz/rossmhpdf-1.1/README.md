ccbcc7181ef861cab49a775542f7d20b255f6849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:22 +0000

    Add package rossmhpdf/1.1/rossmhpdf-1.1.tar.gz

---
+++ packages/rossmhpdf/rossmhpdf-1.1.tar.gz/rossmhpdf-1.1/README.md
@@ -0,0 +1,1 @@
+"this is our programe home page"
---
+++ packages/rossmhpdf/rossmhpdf-1.1.tar.gz/rossmhpdf-1.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="rossmhpdf",
+    version=1.1,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(exclude=["tests", "data"])
+)
