3195a026b9a879a640d83cdb3a2850eae4f7257e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:27 +0000

    Add package stu2077-distributions/0.12/stu2077_distributions-0.12.tar.gz

---
+++ packages/stu2077-distributions/stu2077_distributions-0.12.tar.gz/stu2077_distributions-0.12/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stu2077_distributions',
+      version='0.12',
+      description='Gaussian distributions',
+      packages=['stu2077_distributions'],
+      zip_safe=False)
