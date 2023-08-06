47f300d4bc68555e2ec869694e11f6969368aa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:26 +0000

    Add package pyctlib/0.1.22/pyctlib-0.1.22.tar.gz

---
+++ packages/pyctlib/pyctlib-0.1.22.tar.gz/pyctlib-0.1.22/setup.py
@@ -0,0 +1,28 @@
+'''
+@File       :   setup.py
+@Author     :   Yuncheng Zhou & Yiteng Zhang
+@Time       :   2020-10
+@Version    :   1.0
+@Contact    :   2247501256@qq.com
+@Dect       :   None
+'''
+ 
+from setuptools import setup, find_packages
+ 
+setup(
+    name = "pyctlib",
+    version = "0.1.22",
+    keywords = ("pip", "pyctlib"),
+    description = "This is A foundamental package containing some basic self-designed functions and types for Project PyCTLib. ",
+    long_description = "This is A foundamental package containing some basic self-designed functions and types for Project PyCTLib. ",
+    license = "MIT Licence",
+ 
+    url = "https://github.com/Bertie97/pyctlib/tree/main/pyctlib",
+    author = "All contributors of PyCTLib",
+    author_email = "2247501256@qq.com",
+ 
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["pyoverload"]
+)
