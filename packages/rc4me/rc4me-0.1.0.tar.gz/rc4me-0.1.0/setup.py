33dff3fd9222b67d8262042fe555244cc0b6d3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package rc4me/0.1.0/rc4me-0.1.0.tar.gz

---
+++ packages/rc4me/rc4me-0.1.0.tar.gz/rc4me-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rc4me",
+    author="Michael Stefferson/Jeffrey Moore",
+    author_email="mstefferson@gmail.com",
+    description="Description",
+    url="",
+    packages=find_packages(),
+    install_requires=["click>=7.1.2", "pick>=1.0.0"],
+    python_requires=">=3.8",
+    license="MIT License",
+    entry_points="""
+        [console_scripts]
+        rc4me=rc4me.cli:cli
+    """,
+)
