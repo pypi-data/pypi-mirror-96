46a87b44e7604b1e7b3cb82d909590d9739b3558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:14 +0000

    Add package gm/3.0.134/gm-3.0.134-cp39-cp39-win_amd64.whl

---
+++ packages/gm/gm-3.0.134-cp39-cp39-win_amd64.whl/gm-3.0.134.data/purelib/gm/pb/strategy_service_pb2.pyi
@@ -0,0 +1,13 @@
+from typing import Any
+
+DESCRIPTOR: Any
+SetAccountsReq: Any
+GetAccountsReq: Any
+GetAccountsRsp: Any
+GetStrategiesToAccountReq: Any
+GetStrategiesReq: Any
+GetStrategiesOfStageReq: Any
+DelStrategiesReq: Any
+GetStartCommandsReq: Any
+GetStrategyStatusesReq: Any
+GetStrategyLogsReq: Any
