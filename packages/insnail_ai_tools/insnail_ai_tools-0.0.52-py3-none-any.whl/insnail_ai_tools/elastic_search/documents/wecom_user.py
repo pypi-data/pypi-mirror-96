d9e2b6230c2c776a26c2374edda9589412f8e083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:32 +0000

    Add package insnail_ai_tools/0.0.52/insnail_ai_tools-0.0.52-py3-none-any.whl

---
+++ packages/insnail_ai_tools/insnail_ai_tools-0.0.52-py3-none-any.whl/insnail_ai_tools/elastic_search/documents/wecom_user.py
@@ -0,0 +1,14 @@
+from elasticsearch_dsl import Document
+
+from insnail_ai_tools.elastic_search.mixin import AioMixin
+from insnail_ai_tools.elastic_search.mongodb_plugin import MongoToEs
+from insnail_ai_tools.mongodb.model.wecom.user import WecomUser
+
+
+@MongoToEs(
+    WecomUser,
+    fields=["userid", "name", "mobile", "department", "position", "email", "is_delete"],
+)
+class WecomUserIndex(Document, AioMixin):
+    class Index:
+        name = f"wecom_user"
