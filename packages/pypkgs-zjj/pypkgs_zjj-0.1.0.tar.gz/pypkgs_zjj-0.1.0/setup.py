b1fed602b72f237cf2394dc4f864dea384ff8eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:04 +0000

    Add package pypkgs-zjj/0.1.0/pypkgs_zjj-0.1.0.tar.gz

---
+++ packages/pypkgs-zjj/pypkgs_zjj-0.1.0.tar.gz/pypkgs_zjj-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypkgs_zjj']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pypkgs-zjj',
+    'version': '0.1.0',
+    'description': 'Python package that eases the pain of concatenating Pandas categoricals!',
+    'long_description': None,
+    'author': 'lenovo',
+    'author_email': 'jingjingzhi66@163.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
