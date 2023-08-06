da06ccb9acb2dbd1d06611a7e2d5bf47b4b5a869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:19 +0000

    Add package graiax-sayamod-nbnhhsh/0.1.3/graiax-sayamod-nbnhhsh-0.1.3.tar.gz

---
+++ packages/graiax-sayamod-nbnhhsh/graiax-sayamod-nbnhhsh-0.1.3.tar.gz/graiax-sayamod-nbnhhsh-0.1.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['graiax_sayamod_nbnhhsh']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiosonic>=0.9.5,<0.10.0',
+ 'graia-application-mirai>=0.16.1,<0.17.0',
+ 'graia-saya>=0.0.4,<0.0.5']
+
+setup_kwargs = {
+    'name': 'graiax-sayamod-nbnhhsh',
+    'version': '0.1.3',
+    'description': '[Saya模块]能不能好好说话——缩写查询',
+    'long_description': None,
+    'author': 'Eric',
+    'author_email': 'erictianc@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
