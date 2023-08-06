7d548f46177728c8b4b3d5b867f11ef356f9d0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:22 +0000

    Add package mkbak/1.3.0/mkbak-1.3.0.tar.gz

---
+++ packages/mkbak/mkbak-1.3.0.tar.gz/mkbak-1.3.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from mkbak import VERSION  # type: ignore
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mkbak",
+    version=VERSION,
+    author="Julia A M",
+    author_email="jlearning@tuta.io",
+    description="a commandline utility to create file backups",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sudo-julia/mkbak",
+    packages=find_packages(),
+    modules=["mkbak_args"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: Terminals",
+    ],
+    install_requires=[
+        "mkbak-iterfzf>=0.7",
+        "rich>=9.8.2",
+    ],
+    python_requires=">=3.7",
+    entry_points={"console_scripts": ["mkbak = mkbak.__main__:main"]},
+)
