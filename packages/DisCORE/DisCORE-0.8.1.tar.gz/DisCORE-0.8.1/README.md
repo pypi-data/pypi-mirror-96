cdb785f30048deb9e4ef30e8ce2442fe23c34cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:25 +0000

    Add package DisCORE/0.8.1/DisCORE-0.8.1.tar.gz

---
+++ packages/DisCORE/DisCORE-0.8.1.tar.gz/DisCORE-0.8.1/README.md
@@ -0,0 +1,3 @@
+# DisCORE
+
+A library of scripts that are designed to manipulate data in both JSON and SQL formats, in addition to using Webhooks to extend your code into Discord.
---
+++ packages/DisCORE/DisCORE-0.8.1.tar.gz/DisCORE-0.8.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+version = "0.8.1"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DisCORE",
+    version=version,
+    author="LDShadowLord",
+    author_email="ldshadowlord@gmail.com",
+    description="A library of scripts for manipulating data and webhooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LDShadowLord/DisCORE",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
