9687557309268345c169ffba71a6708c1e3af5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:14 +0000

    Add package superfilter/0.0.0/superfilter-0.0.0.tar.gz

---
+++ packages/superfilter/superfilter-0.0.0.tar.gz/superfilter-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='superfilter',
+    version='0.0.0',
+    author='Perzan',
+    packages=["superfilter"],
+    author_email='PerzanDevelopment@gmail.com'
+)
