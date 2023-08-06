eb85d4649134a572c8188140235aab2ecc297a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:01 +0000

    Add package django-querysetsequence/0.14/django-querysetsequence-0.14.tar.gz

---
+++ packages/django-querysetsequence/django-querysetsequence-0.14.tar.gz/django-querysetsequence-0.14/tests/settings.py
@@ -0,0 +1,39 @@
+SECRET_KEY = 'not_empty'
+SITE_ID = 1
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages'
+            ],
+        },
+    },
+]
+
+MIDDLEWARE_CLASSES = tuple()
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+INSTALLED_APPS = (
+    'tests',
+)
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+# Avoid a warning on Django >= 3.2.
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
