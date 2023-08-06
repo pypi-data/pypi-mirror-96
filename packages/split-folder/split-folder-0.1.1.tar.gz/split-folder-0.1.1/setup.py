d1e5e96fe8d7cedafffdd7cd04e2af94823fdc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package split-folder/0.1.1/split-folder-0.1.1.tar.gz

---
+++ packages/split-folder/split-folder-0.1.1.tar.gz/split-folder-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['split_folder']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['nb-utils>=0.1.0,<0.2.0']
+
+setup_kwargs = {
+    'name': 'split-folder',
+    'version': '0.1.1',
+    'description': 'Spilt folder into train/val/test subset',
+    'long_description': None,
+    'author': 'Nivratti',
+    'author_email': 'boyanenivratti@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
