ce3a36d0c137586f5a20115a9f32162ec2dd1d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:14 +0000

    Add package azure-mgmt-netapp/0.16.0/azure-mgmt-netapp-0.16.0.zip

---
+++ packages/azure-mgmt-netapp/azure-mgmt-netapp-0.16.0.zip/azure-mgmt-netapp-0.16.0/tests/setup.py
@@ -0,0 +1,22 @@
+# resource constants
+# some minor setup required to run in record mode
+#  - actual subsid to be used
+#  - rg must exist
+#  - vnet must exist
+
+TEST_RG = 'sdk-net-test-qa7'
+TEST_REPL_RG = 'sdk-test-qa7'
+TEST_REMOTE_RG = 'sdk-test-qa7-R'
+TEST_ACC_1 = 'sdk-py-tests-acc-10'
+TEST_ACC_2 = 'sdk-py-tests-acc-20'
+TEST_POOL_1 = 'sdk-py-tests-pool-1'
+TEST_POOL_2 = 'sdk-py-tests-pool-2'
+TEST_VOL_1 = 'sdk-py-tests-vol-1'
+TEST_VOL_2 = 'sdk-py-tests-vol-2'
+SERVICE_LEVEL = 'Premium'
+LOCATION = 'eastus2'
+REMOTE_LOCATION = 'centralus'
+VNET = 'sdknettestqa7vnet464'
+REPL_VNET = 'sdktestqa7vnet464'
+REMOTE_VNET = 'sdktestqa7vnet464-R'
+SERVICE_LEVEL = 'Premium'
---
+++ packages/azure-mgmt-netapp/azure-mgmt-netapp-0.16.0.zip/azure-mgmt-netapp-0.16.0/tests/test_snapshot.py
@@ -0,0 +1,94 @@
+import time
+from azure.mgmt.resource import ResourceManagementClient
+from devtools_testutils import AzureMgmtTestCase
+from azure.mgmt.netapp.models import Volume
+from test_volume import create_volume, wait_for_volume, delete_volume
+from test_pool import delete_pool
+from test_account import delete_account
+from setup import *
+import azure.mgmt.netapp.models
+
+TEST_SNAPSHOT_1 = 'sdk-py-tests-snapshot-1'
+TEST_SNAPSHOT_2 = 'sdk-py-tests-snapshot-2'
+
+
+def create_snapshot(client, rg=TEST_RG, account_name=TEST_ACC_1, pool_name=TEST_POOL_1, volume_name=TEST_VOL_1,
+                    snapshot_name=TEST_SNAPSHOT_1, location=LOCATION, snapshot_only=False):
+    if not snapshot_only:
+        volume = create_volume(client, rg, account_name, pool_name, volume_name)
+        # be sure the volume is really available
+        wait_for_volume(client, rg, account_name, pool_name, volume_name)
+    else:
+        # we need to get the volume id if we didn't just create it
+        volume = client.volumes.get(rg, account_name, pool_name, volume_name)
+
+    snapshot = client.snapshots.create(rg, account_name, pool_name, volume_name, snapshot_name, location=location,
+                                       file_system_id=volume.file_system_id).result()
+
+    return snapshot
+
+
+def delete_snapshot(client, rg, account_name, pool_name, volume_name, snapshot_name, live=False):
+    client.snapshots.delete(rg, account_name, pool_name, volume_name, snapshot_name).wait()
+
+    # wait to be sure it has gone - a workaround for the async nature of certain ARM processes
+    co = 0
+    while co < 10:
+        co += 1
+        if live:
+            time.sleep(20)
+        try:
+            client.snapshots.get(rg, account_name, pool_name, volume_name, snapshot_name)
+        except:
+            # not found is an exception case (status code 200 expected)
+            # but is what we are waiting for
+            break
+
+
+class NetAppAccountTestCase(AzureMgmtTestCase):
+    def setUp(self):
+        super(NetAppAccountTestCase, self).setUp()
+        self.client = self.create_mgmt_client(azure.mgmt.netapp.AzureNetAppFilesManagementClient)
+
+    def test_create_delete_snapshot(self):
+        create_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1, LOCATION)
+
+        snapshot_list = self.client.snapshots.list(TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1)
+        self.assertEqual(len(list(snapshot_list)), 1)
+
+        self.client.snapshots.delete(TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1).wait()
+        snapshot_list = self.client.snapshots.list(TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1)
+        self.assertEqual(len(list(snapshot_list)), 0)
+
+        delete_volume(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, self.is_live)
+        delete_pool(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, self.is_live)
+        delete_account(self.client, TEST_RG, TEST_ACC_1, self.is_live)
+
+    def test_list_snapshots(self):
+        create_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1, LOCATION)
+        create_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_2, LOCATION, snapshot_only=True)
+        snapshots = [TEST_SNAPSHOT_1, TEST_SNAPSHOT_2]
+
+        snapshot_list = self.client.snapshots.list(TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1)
+        self.assertEqual(len(list(snapshot_list)), 2)
+        idx = 0
+        for snapshot in snapshot_list:
+            self.assertEqual(snapshot.name, snapshots[idx])
+            idx += 1
+
+        delete_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1, self.is_live)
+        delete_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_2, self.is_live)
+        delete_volume(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, self.is_live)
+        delete_pool(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, self.is_live)
+        delete_account(self.client, TEST_RG, TEST_ACC_1, self.is_live)
+
+    def test_get_snapshot_by_name(self):
+        create_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1, LOCATION)
+
+        snapshot = self.client.snapshots.get(TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1)
+        self.assertEqual(snapshot.name, TEST_ACC_1 + '/' + TEST_POOL_1 + '/' + TEST_VOL_1+ '/' + TEST_SNAPSHOT_1)
+
+        delete_snapshot(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, TEST_SNAPSHOT_1, self.is_live)
+        delete_volume(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, TEST_VOL_1, self.is_live)
+        delete_pool(self.client, TEST_RG, TEST_ACC_1, TEST_POOL_1, self.is_live)
+        delete_account(self.client, TEST_RG, TEST_ACC_1)
