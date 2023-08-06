8c57bd13963446cce7d9e3f174f20a0a1fcc86f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package pyspcalt/1.0.3/pyspcalt-1.0.3.tar.gz

---
+++ packages/pyspcalt/pyspcalt-1.0.3.tar.gz/pyspcalt-1.0.3/pyspc/ccharts/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .cusum import cusum
+from .ewma import ewma, mewma
+from .mr import mr, xmr
+from .xbar_sbar import xbar_sbar, sbar
+from .xbar_rbar import xbar_rbar, rbar
+from .imrx import imrx
+from .imrr import imrr
+from .imrmr import imrmr
+from .imrstd import imrstd
+from .hotelling import Tsquare_single, Tsquare, variation
+from .np import np
+from .p import p
+from .c import c
+from .u import u
+from .tables import *
---
+++ packages/pyspcalt/pyspcalt-1.0.3.tar.gz/pyspcalt-1.0.3/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2016  Carlos Henrique Silva <carlosqsilva@outlook.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(name='pyspcalt',
+      version='1.0.3',
+      description='Statistical Process Control Charts Library for Humans',
+      long_description='Statistical Process Control Charts Library for Humans',
+      url='https://github.com/carlosqsilva/pyspc',
+      author='Carlos Silva',
+      author_email='carlosqsilva@outlook.com',
+      license='GPLv3',
+      packages=find_packages(),
+      package_dir={ "pyspc": "pyspc" },
+      package_data={
+      "pyspc": ["sampledata/*.csv"]},
+      install_requires=['pandas', 'matplotlib', 'numpy', 'scipy'],
+      test_suite="tests",
+      keywords='SPC QCC CEQ CEP UEPA',
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+      'Topic :: Scientific/Engineering',
+      'Operating System :: Microsoft :: Windows',
+      'Operating System :: POSIX :: Linux',
+      'Operating System :: MacOS',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Topic :: Software Development :: Libraries :: Python Modules'],
+      zip_safe=False)
