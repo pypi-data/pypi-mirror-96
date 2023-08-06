eac3643a31e243b36d9014ef01570a5acfe6d5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:52 +0000

    Add package contique/0.1.6/contique-0.1.6-py3-none-any.whl

---
+++ packages/contique/contique-0.1.6-py3-none-any.whl/contique/helpers.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 17 14:31:04 2021
+
+@author: Andreas
+"""
+
+import numpy as np
+
+
+def needle(j, m):
+    "Return zero-vector with needle at component `j` and length `m`."
+    n = np.zeros(m)
+    n[abs(j) - 1] = 1
+    return n
+
+
+def control(x):
+    "Signed index of greatest absolute component of vector x (1-indexed!)."
+    j = abs(x).argmax()
+    return int((j + 1) * np.sign(x[j]))
