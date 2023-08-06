b48c3d7296c5a0171b695b6f4f88c3dea07389ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:47 +0000

    Add package gtfs-lite/0.1.8/gtfs-lite-0.1.8.tar.gz

---
+++ packages/gtfs-lite/gtfs-lite-0.1.8.tar.gz/gtfs-lite-0.1.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gtfs-lite",
+    version="0.1.8",
+    author="Willem Klumpenhouwer",
+    author_email="willem@klumpentown.com",
+    description="A lightweight Pandas-driven package for analyzing static GTFS feeds.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wklumpen/gtfs-lite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas>=1.0',
+    ]
+)
