0c2686fa325a7d85bf18e3826b23b19cd214498b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:52 +0000

    Add package couplet-composer/1.7.6/couplet_composer-1.7.6-py2-none-any.whl

---
+++ packages/couplet-composer/couplet_composer-1.7.6-py2-none-any.whl/couplet_composer/util/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 Antti Kivi
+# Licensed under the MIT License
+
+"""
+The utilities are helpful modules that are required to run
+Couplet Composer.
+"""
---
+++ packages/couplet-composer/couplet_composer-1.7.6-py2-none-any.whl/tests/util/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2019 Antti Kivi
+# Licensed under the MIT License
