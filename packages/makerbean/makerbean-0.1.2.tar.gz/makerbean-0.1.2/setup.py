8b4718ad31a06283dfbf9b374ff9ad2d4e7c6e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:24 +0000

    Add package makerbean/0.1.2/makerbean-0.1.2.tar.gz

---
+++ packages/makerbean/makerbean-0.1.2.tar.gz/makerbean-0.1.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @Author: Anderson
+# @Date:   2019-11-14 17:45:03
+# @Last Modified by:   ander
+# @Last Modified time: 2021-02-25 13:54:41
+import setuptools
+
+
+setuptools.setup(
+    name="makerbean",
+    version="0.1.2",
+    author="MakerBi",
+    author_email="andersonby@163.com",
+    description="A small educational purpose package",
+    long_description_content_type="text/markdown",
+    url="https://makerbean.com",
+    packages=setuptools.find_packages(),
+    install_requires=['openpyxl', 'requests', 'beautifulsoup4', 'lxml', 'jieba', 'pyecharts', 'pdfplumber', 'PyPDF2', 'python-docx', 'pandas'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
