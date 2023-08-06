6a0e066b42fb186046a49198cfeae24053352d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:07 +0000

    Add package strato-query/3.9.8/strato_query-3.9.8.tar.gz

---
+++ packages/strato-query/strato_query-3.9.8.tar.gz/strato_query-3.9.8/setup.py
@@ -0,0 +1,35 @@
+"""
+StratoDem Analytics : setup
+Principal Author(s) : Eric Linden
+Secondary Author(s) :
+Description :
+
+Notes :
+
+December 26, 2018
+"""
+
+import os
+from setuptools import setup
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='strato_query',
+    version='3.9.8',
+    author='Michael Clawar, Raaid Arshad, Eric Linden',
+    author_email='tech@stratodem.com',
+    packages=[
+        'strato_query',
+    ],
+    license='MIT',
+    description='StratoDem Analytics API tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/StratoDem/strato-query',
+)
