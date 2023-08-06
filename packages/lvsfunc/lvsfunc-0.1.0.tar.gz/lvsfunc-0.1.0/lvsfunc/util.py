662d56aa9331cd259c11b799109c277618b46c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:01 +0000

    Add package lvsfunc/0.1.0/lvsfunc-0.1.0.tar.gz

---
+++ packages/lvsfunc/lvsfunc-0.1.0.tar.gz/lvsfunc-0.1.0/lvsfunc/util.py
@@ -0,0 +1,99 @@
+"""
+    Helper functions for the main functions in the script.
+"""
+from typing import Any, Callable, Sequence, Type, TypeVar, Union, cast
+
+import vapoursynth as vs
+from vsutil import depth
+
+core = vs.core
+
+
+def quick_resample(clip: vs.VideoNode,
+                   function: Callable[[vs.VideoNode], vs.VideoNode]
+                   ) -> vs.VideoNode:
+    """
+    A function to quickly resample to 16/8 bit and back to the original depth.
+    Useful for filters that only work in 16 bit or lower when you're working in float.
+
+    :param clip:      Input clip
+    :param function:  Filter to run after resampling (accepts and returns clip)
+
+    :return:          Filtered clip in original depth
+    """
+    if clip.format is None:
+        raise ValueError("quick_resample: 'Variable-format clips not supported'")
+    try:
+        down = depth(clip, 16)
+        filtered = function(down)
+    except:  # noqa: E722
+        down = depth(clip, 8)
+        filtered = function(down)
+    return depth(filtered, clip.format.bits_per_sample)
+
+
+# TODO: Merge pick_repair and pick_removegrain?
+def pick_repair(clip: vs.VideoNode) -> Callable[..., vs.VideoNode]:
+    """
+    Returns rgvs.Repair if the clip is 16 bit or lower, else rgsf.Repair.
+    This is done because rgvs doesn't work with float, but rgsf does for whatever reason.
+
+    Dependencies: rgsf
+
+    :param clip: Input clip
+
+    :return:     Appropriate repair function for input clip's depth
+    """
+    if clip.format is None:
+        raise ValueError("pick_repair: 'Variable-format clips not supported'")
+    return core.rgvs.Repair if clip.format.bits_per_sample < 32 else core.rgsf.Repair
+
+
+def pick_removegrain(clip: vs.VideoNode) -> Callable[..., vs.VideoNode]:
+    """
+    Returns rgvs.RemoveGrain if the clip is 16 bit or lower, else rgsf.RemoveGrain.
+    This is done because rgvs doesn't work with float, but rgsf does for whatever reason.
+
+    Dependencies: rgsf
+
+    :param clip: Input clip
+
+    :return:     Appropriate RemoveGrain function for input clip's depth
+    """
+    if clip.format is None:
+        raise ValueError("pick_removegrain: 'Variable-format clips not supported'")
+    return core.rgvs.RemoveGrain if clip.format.bits_per_sample < 32 else core.rgsf.RemoveGrain
+
+
+VideoProp = Union[
+    int, Sequence[int],
+    float, Sequence[float],
+    str, Sequence[str],
+    vs.VideoNode, Sequence[vs.VideoNode],
+    vs.VideoFrame, Sequence[vs.VideoFrame],
+    Callable[..., Any], Sequence[Callable[..., Any]]
+]
+
+T = TypeVar("T", bound=VideoProp)
+
+
+def get_prop(frame: vs.VideoFrame, key: str, t: Type[T]) -> T:
+    """
+    Gets gets FrameProp ``prop`` from frame ``frame`` with expected type ``t``
+    to satisfy the type checker.
+
+    :param frame:   Frame containing props
+    :param key:     Prop to get
+    :param t:       Type of prop
+
+    :return:        frame.prop[key]
+    """
+    try:
+        prop = frame.props[key]
+    except KeyError:
+        raise KeyError(f"get_prop: 'Key {key} not present in props'")
+    real_type = type(prop)
+    if real_type is not t:
+        raise ValueError(f"get_prop: 'Key {key} did not contain expected type: Expected {t} got {real_type}'")
+
+    return cast(T, prop)
---
+++ packages/lvsfunc/lvsfunc-0.1.0.tar.gz/lvsfunc-0.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    install_requires = fh.read()
+
+name = "lvsfunc"
+version = "0.1.0"
+release = "0.1.0"
+
+setuptools.setup(
+    name=name,
+    version=release,
+    author="LightArrowsEXE",
+    author_email="Lightarrowsreboot@gmail.com",
+    description="Light's Vapoursynth Functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["lvsfunc"],
+    url="https://github.com/Irrational-Encoding-Wizardry/lvsfunc",
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    command_options={
+        "build_sphinx": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+            "release": ("setup.py", release),
+            "source_dir": ("setup.py", "docs")
+        }
+    }
+)
