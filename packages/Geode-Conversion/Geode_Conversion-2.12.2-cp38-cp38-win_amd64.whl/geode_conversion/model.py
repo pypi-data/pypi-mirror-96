c5eaf97c5d51b4af60b495d55bea3d79f86602c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:07 +0000

    Add package Geode-Conversion/2.12.2/Geode_Conversion-2.12.2-cp38-cp38-win_amd64.whl

---
+++ packages/Geode-Conversion/Geode_Conversion-2.12.2-cp38-cp38-win_amd64.whl/geode_conversion/model.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2021 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+
+from geode_conversion import mesh
+from .geode_conversion_py_model import *
