8bda0668ecda5481dac8ae9b6bf7b6487a4fdfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:58 +0000

    Add package twacapic/0.1.2/twacapic-0.1.2.tar.gz

---
+++ packages/twacapic/twacapic-0.1.2.tar.gz/twacapic-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['twacapic']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['TwitterAPI>=2.6.9,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['twacapic = twacapic.main:run']}
+
+setup_kwargs = {
+    'name': 'twacapic',
+    'version': '0.1.2',
+    'description': 'A Twitter Academic API Client',
+    'long_description': '# twacapic\nTwitter Academic API Client\n',
+    'author': 'Felix Victor Münch',
+    'author_email': 'f.muench@leibniz-hbi.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Leibniz-HBI/twacapic',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
