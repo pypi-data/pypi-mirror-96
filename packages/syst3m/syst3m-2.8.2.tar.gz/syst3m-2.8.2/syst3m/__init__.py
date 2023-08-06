16380f8f859d00df6626acd0c40897de0e76955b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:12 +0000

    Add package syst3m/2.8.2/syst3m-2.8.2.tar.gz

---
+++ packages/syst3m/syst3m-2.8.2.tar.gz/syst3m-2.8.2/syst3m/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from syst3m.classes import *
+
+# version.
+import fil3s
+try: version = int(fil3s.Files.load(fil3s.gfp.base(__file__)+".version.py").replace("\n","").replace(" ",""))
+except: version = None
