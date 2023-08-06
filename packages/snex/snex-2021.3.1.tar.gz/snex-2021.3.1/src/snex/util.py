1789d11f1f7874f481864d0f398cb7106c322ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:02 +0000

    Add package snex/2021.3.1/snex-2021.3.1.tar.gz

---
+++ packages/snex/snex-2021.3.1.tar.gz/snex-2021.3.1/src/snex/util.py
@@ -0,0 +1,55 @@
+from pyhocon import ConfigFactory, ConfigTree
+from itertools import dropwhile
+from pathlib import Path
+import logging
+import re
+
+logger = logging.getLogger(__name__)
+
+
+VALID_PARAM_KEYS = ["name", "lang"]
+VALID_NAME_RE = r"[^-0-9A-Za-z_.%~äßöüÄ§ÖÜ€áàâãéêíóôõúçÁÀÂÃÉÊÍÓÔÕÚÇ]+"
+
+
+def parse_params(snippet_param_match):
+    params = {}
+    for kv in re.split(r"\s+", snippet_param_match.strip()):
+        (k, v) = kv.split("=", 2)
+        params[k.strip()] = v
+    return params
+
+
+def sanitize_params(params):
+    if not ("name" in params and params["name"]):
+        raise KeyError("name key not set")
+    params["name"] = re.sub(VALID_NAME_RE, "_", params["name"])
+    return {k: v for k, v in params.items() if k in VALID_PARAM_KEYS}
+
+
+def find_files(root, glob):
+    logger.info(f"{root} -> {glob}")
+    for p in Path(root).glob(glob):
+        if p.is_file():
+            yield p
+
+
+def det_indent_lvl(lines):
+    return min([len(x) - len(x.lstrip()) for x in lines if x])
+
+
+def dropwhile_right(pred, seq):
+    return reversed(list(dropwhile(pred, reversed(seq))))
+
+
+def merge_with_default_conf(conf, default_conf=None, global_default=None):
+    if global_default is None:
+        global_default = {}
+    if default_conf is None:
+        default_conf = ConfigTree()
+
+    tmp = ConfigTree.merge_configs(ConfigFactory.from_dict(global_default), default_conf, copy_trees=True)
+    return ConfigTree.merge_configs(tmp, conf)
+
+
+def is_empty(x):
+    return not bool(x)
