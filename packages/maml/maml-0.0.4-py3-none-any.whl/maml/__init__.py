ca8c8777f4df793d5c26312ac9554b88603aba2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:29 +0000

    Add package maml/0.0.4/maml-0.0.4-py3-none-any.whl

---
+++ packages/maml/maml-0.0.4-py3-none-any.whl/maml/__init__.py
@@ -0,0 +1,9 @@
+"""
+maml - materials machine learning
+"""
+
+__version__ = "0.0.4"
+
+from .base import BaseDataSource  # noqa
+from .base import BaseDescriber  # noqa
+from .base import BaseModel, SKLModel, KerasModel, SequentialDescriber  # noqa
