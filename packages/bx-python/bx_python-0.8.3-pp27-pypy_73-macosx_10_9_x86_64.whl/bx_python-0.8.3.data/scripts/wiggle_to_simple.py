a5c587d0bb4b975ac38888dcedc668f6d031556a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:24 +0000

    Add package bx-python/0.8.3/bx_python-0.8.3-pp27-pypy_73-macosx_10_9_x86_64.whl

---
+++ packages/bx-python/bx_python-0.8.3-pp27-pypy_73-macosx_10_9_x86_64.whl/bx_python-0.8.3.data/scripts/wiggle_to_simple.py
@@ -0,0 +1,25 @@
+#!python
+
+"""
+Read a wiggle track and print out a series of lines containing
+"chrom position score". Ignores track lines, handles bed, variableStep
+and fixedStep wiggle lines.
+"""
+from __future__ import print_function
+
+import sys
+
+import bx.wiggle
+import psyco_full
+
+if len( sys.argv ) > 1: in_file = open( sys.argv[1] )
+else: in_file = sys.stdin
+
+if len( sys.argv ) > 2: out_file = open( sys.argv[2], "w" )
+else: out_file = sys.stdout
+
+for fields in bx.wiggle.Reader( in_file ):
+    print(" ".join( map( str, fields ) ))
+
+in_file.close()
+out_file.close()
