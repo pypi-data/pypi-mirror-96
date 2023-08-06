2760cb9336f964b7827e7bf07541df1630183a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package csv-manager/0.11.5/csv_manager-0.11.5.tar.gz

---
+++ packages/csv-manager/csv_manager-0.11.5.tar.gz/csv_manager-0.11.5/csv_manager/__init__.py
@@ -0,0 +1,5 @@
+from .plotter import Plotter
+from .datafile import DataFile
+from . import writer
+from .database import Database
+from .misc import *
---
+++ packages/csv-manager/csv_manager-0.11.5.tar.gz/csv_manager-0.11.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='csv_manager',
+      version='0.11.5',
+      description='A simple CSV importer/exporter with plotting capabilities with matplotlib',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='csv export import plot matplotlib',
+      url='https://github.com/AdelKS/csv_manager',
+      author='Adel KARA SLIMANE',
+      author_email='adel.ks@zegrapher.com',
+      license='The Unlicense',
+      packages=['csv_manager'],
+      install_requires=[
+          'matplotlib', 'py_expression_eval'
+      ],
+      include_package_data=True,
+      zip_safe=False)
