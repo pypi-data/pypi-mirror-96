cae33436e5d09f898f94675ee497fa8911be3c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package bluedot-rest-framework/2.2.49/bluedot_rest_framework-2.2.49.tar.gz

---
+++ packages/bluedot-rest-framework/bluedot_rest_framework-2.2.49.tar.gz/bluedot_rest_framework-2.2.49/bluedot_rest_framework/event/survey/views.py
@@ -0,0 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from bluedot_rest_framework import import_string
+from django.db.models import Q
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from bluedot_rest_framework.event.survey.models import SurveyUser
+from bluedot_rest_framework.event.survey.serializers import SurveyUserSerializer
+from bluedot_rest_framework.utils.crypto import AESEncrypt
+from bluedot_rest_framework.utils.viewsets import CustomModelViewSet, user_perform_create
+from bluedot_rest_framework.utils.jwt_token import jwt_get_wechatid_handler, jwt_get_userid_handler
+
+
+class EventSurveyUserView(CustomModelViewSet):
+    model_class = SurveyUser
+    serializer_class = SurveyUserSerializer
+    filterset_fields = {
+        'event_id': {
+            'field_type': 'int',
+            'lookup_expr': ''
+        }
+    }
