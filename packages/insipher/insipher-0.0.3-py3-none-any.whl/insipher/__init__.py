e2807f01bd97197ea7950463eadf43797cd8db66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:18 +0000

    Add package insipher/0.0.3/insipher-0.0.3-py3-none-any.whl

---
+++ packages/insipher/insipher-0.0.3-py3-none-any.whl/insipher/__init__.py
@@ -0,0 +1,2 @@
+from .algorithm import InsipherAlgorithm
+from .error import InsipherError
---
+++ packages/insipher/insipher-0.0.3-py3-none-any.whl/insipher/error.py
@@ -0,0 +1,16 @@
+############################################################################
+# Copyright 2020, Anteris Technologies LLC d/b/a Insipher.
+# All rights reserved.
+# info@insipher.com
+# 
+# NOTICE:  All information contained herein is, and remains
+# the property of Anteris Technologies LLC d/b/a Insipher ("Insipher").
+# The intellectual and technical concepts contained herein are proprietary
+# to Insipher and may be covered by U.S. and foreign patents or
+# patent applications, trade secret, or copyright. Dissemination of
+# this information or reproduction of this material is strictly
+# forbidden unless prior written permission is obtained from Insipher.
+#
+
+class InsipherError(Exception):
+    pass
