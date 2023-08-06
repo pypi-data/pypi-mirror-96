902974315becae195f100c7c63a5b6e438442e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:06 +0000

    Add package hestia-earth-validation/0.1.17/hestia_earth.validation-0.1.17.tar.gz

---
+++ packages/hestia-earth-validation/hestia_earth.validation-0.1.17.tar.gz/hestia_earth.validation-0.1.17/hestia_earth/validation/validators/infrastructure.py
@@ -0,0 +1,16 @@
+from hestia_earth.validation.utils import _filter_list_errors, _list_has_props, _diff_in_years
+
+
+def validate_lifespan(infrastructure: list):
+    def validate(values):
+        value = values[1]
+        index = values[0]
+        lifespan = _diff_in_years(value.get('startDate'), value.get('endDate'))
+        return lifespan == round(value.get('lifespan'), 1) or {
+            'level': 'error',
+            'dataPath': f".infrastructure[{index}].lifespan",
+            'message': f"must equal to endDate - startDate in decimal years (~{lifespan})"
+        }
+
+    results = list(map(validate, enumerate(_list_has_props(infrastructure, ['lifespan', 'startDate', 'endDate']))))
+    return _filter_list_errors(results)
