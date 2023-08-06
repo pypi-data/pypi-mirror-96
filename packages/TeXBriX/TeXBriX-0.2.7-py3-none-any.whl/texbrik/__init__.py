e5409b3511e618118dd7bdc01a4d512dad6dffd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:10 +0000

    Add package TeXBriX/0.2.7/TeXBriX-0.2.7-py3-none-any.whl

---
+++ packages/TeXBriX/TeXBriX-0.2.7-py3-none-any.whl/texbrik/__init__.py
@@ -0,0 +1,2 @@
+from . import texbrik
+from . import mathbrik
---
+++ packages/TeXBriX/TeXBriX-0.2.7-py3-none-any.whl/texbrix/main.py
@@ -0,0 +1,24 @@
+from texbrix.texbrik import brikFromDoc
+import argparse
+from pathlib import Path
+
+__version__ = "0.2.4"
+parser = argparse.ArgumentParser(description='create LaTeX file from brix')
+parser.add_argument(
+    'top_brik',
+    metavar='Top Brik',
+    type=str,
+    help='top .brik document to work with')
+parser.add_argument('--template', nargs=1)
+parser.add_argument('--version', action='version', version="%(prog)s {v}".format(v=__version__))
+
+
+def main():
+    args = parser.parse_args()
+    p = Path(args.top_brik).resolve()
+    b = brikFromDoc(p)
+    if args.template:
+        s = b.make_TeX_file(template=Path(args.template[0]).resolve())
+    else:
+        s = b.make_TeX_file()
+    p.with_suffix('.tex').write_text(s)
