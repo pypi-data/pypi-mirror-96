c01c2e19a0ed3d5c5b4c5a47fe00e76880a8f17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package viledatools/1.0.3/viledatools-1.0.3.tar.gz

---
+++ packages/viledatools/viledatools-1.0.3.tar.gz/viledatools-1.0.3/setup.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+"""
+viledatools
+"""
+
+from setuptools import setup
+
+setup(name='viledatools',
+      version='1.0.3',
+      packages=['viledatools'],
+      url='https://www.vileda-professional.com/',
+      license='Creative Commons Attribution 4.0 International',
+      author='FHCS GmbH',
+      author_email='support@fhcs.zendesk.com',
+      description='Classes and functions to use for development of intregration software',
+      install_requires=['aiohttp>=3.7.3', 'openpyxl>=3.0.6'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Other Environment',
+                   'License :: Other/Proprietary License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.8',
+                   'Topic :: Utilities'],
+      zip_safe=False,
+      python_requires='>=3.8'
+      )
