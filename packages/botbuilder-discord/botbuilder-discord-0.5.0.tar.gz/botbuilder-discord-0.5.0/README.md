08ca8ecb876510d0b0312aa4437f1c288a3e3ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:45 +0000

    Add package botbuilder-discord/0.5.0/botbuilder-discord-0.5.0.tar.gz

---
+++ packages/botbuilder-discord/botbuilder-discord-0.5.0.tar.gz/botbuilder-discord-0.5.0/README.md
@@ -0,0 +1,15 @@
+
+# Botbuilder offline
+
+[![Generic badge](https://img.shields.io/badge/python-3.8%20%7C%203.9-blue)](https://www.python.org/downloads/release/python-380/)
+![Travis-CI](https://www.travis-ci.com/Joffreybvn/botbuilder-discord.svg)
+![ReadTheDocs](https://readthedocs.org/projects/botbuilder-discord/badge/?version=latest)
+
+Unofficial Discord.py connector for Microsoft's Bot Framework
+
+### Installing
+Install and update using pip:
+
+```Shell
+pip install botbuilder-discord
+```
---
+++ packages/botbuilder-discord/botbuilder-discord-0.5.0.tar.gz/botbuilder-discord-0.5.0/botbuilder_discord/connector.py
@@ -0,0 +1,27 @@
+
+from typing import List
+from .client import Client
+
+
+class OfflineConnector:
+
+    def __init__(self, bot_api_url: str = "http://127.0.0.1:3978/api/messages",
+                 listener_host: str = "127.0.0.1", listener_port: int = 5789):
+        self.client = Client(bot_api_url, listener_host, listener_port)
+
+        self.has_started = []
+
+    async def send_message(self, user_id: int, text: str) -> List[str]:
+
+        # List to store the responses from the bot
+        responses = []
+
+        # If the user is speaking for the first time, create a conversation
+        if user_id not in self.has_started:
+            responses.append(await self.client.start_conversation(user_id))
+            self.has_started.append(user_id)
+
+        # Send its message to the bot
+        responses.append(await self.client.send_message(user_id, text))
+
+        return responses
