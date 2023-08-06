de1f5df8a05773805851730f62e9d0928bdc0a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:11 +0000

    Add package bamboo-core/0.4.1/bamboo_core-0.4.1-py3-none-any.whl

---
+++ packages/bamboo-core/bamboo_core-0.4.1-py3-none-any.whl/bamboo/__init__.py
@@ -0,0 +1,38 @@
+
+__version__ = "0.4.1"
+
+from bamboo.api import (
+    ApiData, ValidationFailedError, BinaryApiData, InvalidAnnotationError,
+    JsonApiData, XWWWFormUrlEncodedData,
+)
+
+from bamboo.app import (
+    App, ParcelConfig, ATTR_PARCEL, VersionConfig, ATTR_VERSION,
+)
+
+from bamboo.base import (
+    ContentType, ContentTypeHolder, MediaTypes, HTTPMethods, HTTPStatus,
+)
+
+from bamboo.endpoint import (
+    StatusCodeAlreadySetError, Endpoint, ATTR_DATA_FORMAT, DataFormatInfo,
+    get_data_format_info, data_format, ATTR_HEADERS_REQUIRED, 
+    RequiredHeaderInfo, get_required_header_info, has_header_of,
+    ATTR_CLIENT_RESTRICTED, get_restricted_ip_info, restricts_client,
+    ATTR_ERRORS, get_errors_info, may_occur, _get_bamboo_attr,
+)
+
+from bamboo.error import (
+    ErrInfoBase, ApiErrInfo, DEFAULT_NOT_FOUND_ERROR, 
+    DEFUALT_INCORRECT_DATA_FORMAT_ERROR, DEFAULT_HEADER_NOT_FOUND_ERROR,
+    DEFAULT_NOT_APPLICABLE_IP_ERROR,
+)
+
+from bamboo.location import (
+    FlexibleLocation, is_flexible_uri, is_duplicated_uri,
+    AsciiDigitLocation, AnyStringLocation,
+)
+
+from bamboo.router import DuplicatedUriRegisteredError, Router
+
+from bamboo.test import ServerForm, TestExecutor
