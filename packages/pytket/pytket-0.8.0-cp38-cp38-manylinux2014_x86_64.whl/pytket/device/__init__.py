4c465c9033d963ebbf69739404b0dbb4c48ce34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:17 +0000

    Add package pytket/0.8.0/pytket-0.8.0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/pytket/pytket-0.8.0-cp38-cp38-manylinux2014_x86_64.whl/pytket/device/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2019-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+
+"""
+The device module provides access to the tket :py:class:`Device` structure and methods
+for providing gate errors amongst other device information. This module is provided in
+binary form during the PyPI installation.
+"""
+
+from pytket._tket.device import *  # type: ignore
