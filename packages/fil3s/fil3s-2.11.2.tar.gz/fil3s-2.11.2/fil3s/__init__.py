a75cdcc06db9f15512f1d28c1d225a5791936c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:51 +0000

    Add package fil3s/2.11.2/fil3s-2.11.2.tar.gz

---
+++ packages/fil3s/fil3s-2.11.2.tar.gz/fil3s-2.11.2/fil3s/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from fil3s.classes.defaults import Files,Formats,Code,gfp,gdate
+
+# shortcuts.
+FilePath = Formats.FilePath 
+String = Formats.String 
+Boolean = Formats.Boolean 
+Integer = Formats.Integer 
+Date = Formats.Date
+File = Files.File
+Directory = Files.Directory
+Zip = Files.Zip
+Image = Files.Image
+Bytes = Files.Bytes
+Dictionary = Files.Dictionary
+Array = Files.Array
+Script = Code.Script
+Python = Code.Python
+
+# source path & version.
+source_path = gfp.base(__file__)
+try: version = Files.load(source_path+".version.py").replace("\n","").replace(" ","")
+except: version = None
