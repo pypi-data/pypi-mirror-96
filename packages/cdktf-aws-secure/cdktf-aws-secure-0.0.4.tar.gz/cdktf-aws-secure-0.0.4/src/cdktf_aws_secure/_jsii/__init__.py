6f8a150fab5346ef83703d4d3b6744cc0a5b4f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package cdktf-aws-secure/0.0.4/cdktf-aws-secure-0.0.4.tar.gz

---
+++ packages/cdktf-aws-secure/cdktf-aws-secure-0.0.4.tar.gz/cdktf-aws-secure-0.0.4/src/cdktf_aws_secure/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdktf._jsii
+import cdktf_cdktf_provider_aws._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdktf-aws-secure", "0.0.4", __name__[0:-6], "cdktf-aws-secure@0.0.4.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
