36d484a5c98d344071384fbd7c1f8bc6b7342244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:49 +0000

    Add package marvelapp-cli/0.1.6/marvelapp-cli-0.1.6.tar.gz

---
+++ packages/marvelapp-cli/marvelapp-cli-0.1.6.tar.gz/marvelapp-cli-0.1.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="marvelapp-cli",
+    version="0.1.6",
+    author="Sophie Fitzpatrick",
+    author_email="sophierfitzpatrick@gmail.com",
+    description="A CLI wrapper for Marvelapp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sophiefitzpatrick/marvelcli/archive/v_0.1.6.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        marvelcli=marvelcli.marvelcli:marvelcli
+    ''',
+)
