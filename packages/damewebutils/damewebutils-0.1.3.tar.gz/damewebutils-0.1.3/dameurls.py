3fff9df2457aec9d93ab9855ee56f9528b72274e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:40 +0000

    Add package damewebutils/0.1.3/damewebutils-0.1.3.tar.gz

---
+++ packages/damewebutils/damewebutils-0.1.3.tar.gz/damewebutils-0.1.3/dameurls.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with damewebutils; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+# This program returns a list of broken links in an url
+
+import requests
+from lxml import html
+from pprint import pprint
+import os,re
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="url to analize broken links")
+parser.add_argument('--verify', default=False, action="store_true")
+args = parser.parse_args()
+
+
+start_url = args.url
+response = requests.get(start_url, verify=args.verify)
+tree = html.fromstring(response.text)
+links = tree.cssselect('a')  # or tree.xpath('//a')
+
+out = []
+for link in links:
+    # we use this if just in case some <a> tags lack an href attribute
+    if 'href' in link.attrib:
+        out.append(link.attrib['href'])
+
+absoluteurls = []
+for o in out:
+    if 'http' in o:
+        print(o)
+        absoluteurls.append(o)
+    else:
+        o = start_url + '/' + o
+        print(o)
+        absoluteurls.append(o)
+
+buggyurls = []
+for l in absoluteurls:
+    print(l)
+    try:
+        r = requests.get(l)
+        r.raise_for_status()
+        print(r.status_code)
+    except:
+        buggyurls.append(l)
+        print("Error")
+nbuggyurls = len(buggyurls)
+if (nbuggyurls == 1):
+    print("%s url with troubles:" % str(nbuggyurls))
+else:
+    print("%s url with troubles:" % str(nbuggyurls))    
+
+print(buggyurls)
+nurls = len(links) - len(buggyurls) 
+if (nurls == 1):
+    print(str(nurls) + " good url")
+else:
+    print(str(nurls) + " good urls")    
---
+++ packages/damewebutils/damewebutils-0.1.3.tar.gz/damewebutils-0.1.3/damewebutils/app/TCPServer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with dameweblibraries; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
