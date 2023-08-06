a5bdcd9c32985f8c947ff07c7eaa1f474dbfce7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:41 +0000

    Add package xor/0.0/xor-0.0.tar.gz

---
+++ packages/xor/xor-0.0.tar.gz/xor-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='xor',
+      version='0.0',
+      description='Logical xor.',
+      packages=['xor'],
+      author_email='roman.dik.06@mail.ru',
+      zip_safe=False)
---
+++ packages/xor/xor-0.0.tar.gz/xor-0.0/xor/__init__.py
@@ -0,0 +1,5 @@
+def xor(a, b):  # a, b - bool
+    if a == b:
+        return False
+    elif a != b:
+        return True
