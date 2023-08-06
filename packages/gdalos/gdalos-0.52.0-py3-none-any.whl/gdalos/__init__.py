1e4a246593262d545216c2134bb7dbcf230469d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:06 +0000

    Add package gdalos/0.52.0/gdalos-0.52.0-py3-none-any.whl

---
+++ packages/gdalos/gdalos-0.52.0-py3-none-any.whl/gdalos/__init__.py
@@ -0,0 +1,7 @@
+__pacakge_name__ = "gdalos"
+__version__ = "0.52.0"
+__author__ = "Idan Miara, Ben Avrahami"
+__author_email__ = "idan@miara.com"
+__license__ = "MIT"
+__url__ = r"https://github.com/talos-gis/gdalos"
+__description__ = "a simple gdal translate/warp/addo python wrapper for raster batch processing"
