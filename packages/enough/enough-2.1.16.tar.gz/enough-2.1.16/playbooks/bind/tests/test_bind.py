063222b4c5076f4b31ce866729e93c799d52b5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:44 +0000

    Add package enough/2.1.16/enough-2.1.16.tar.gz

---
+++ packages/enough/enough-2.1.16.tar.gz/enough-2.1.16/playbooks/bind/tests/test_bind.py
@@ -0,0 +1,26 @@
+testinfra_hosts = ['ansible://icinga-host']
+
+
+def test_bind(host):
+    domain = host.run("hostname -d").stdout.strip()
+    bind_host = host.get_host('ansible://bind-host',
+                              ansible_inventory=host.backend.ansible_inventory)
+    address = bind_host.ansible.get_variables()['ansible_host']
+    for h in ('ns1', 'bind', 'bind-host'):
+        cmd = host.run("getent hosts {}.{}".format(h, domain))
+        print(cmd.stdout)
+        print(cmd.stderr)
+        assert 0 == cmd.rc
+        assert address in cmd.stdout.strip()
+        assert h + "." + domain in cmd.stdout.strip()
+        # try also with shortnames
+        cmd = host.run("getent hosts {}".format(h))
+        assert 0 == cmd.rc
+        assert address in cmd.stdout.strip()
+        assert h + "." + domain in cmd.stdout.strip()
+
+
+def test_recursion(host):
+    cmd = host.run("getent hosts fsf.org")
+    assert 0 == cmd.rc
+    assert 'fsf.org' in cmd.stdout.strip()
