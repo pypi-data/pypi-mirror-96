f8d04e8ff5673ae8628fd1499262721ee655ebc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package picoapi/1.0.3/picoapi-1.0.3.tar.gz

---
+++ packages/picoapi/picoapi-1.0.3.tar.gz/picoapi-1.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import os
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="picoapi",
+    version=os.getenv("RELEASE_VERSION", "0.0.1"),
+    author="Patrick Coffey",
+    author_email="patrickcoffey91@gmail.com",
+    description="An opinionated wrapper around FastAPI with custom microservice registration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schlerp/picoapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pydantic",
+        "fastapi",
+        "requests",
+    ],
+    python_requires=">=3.6",
+)
