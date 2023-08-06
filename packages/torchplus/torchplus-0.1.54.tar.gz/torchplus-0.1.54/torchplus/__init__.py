ab7fab2249364eacdbb453ed15b1e406847a304b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:05 +0000

    Add package torchplus/0.1.54/torchplus-0.1.54.tar.gz

---
+++ packages/torchplus/torchplus-0.1.54.tar.gz/torchplus-0.1.54/torchplus/__init__.py
@@ -0,0 +1,18 @@
+#! python3.8 -u
+#  -*- coding: utf-8 -*-
+
+##############################
+## Project PyCTLib
+## Package torchplus
+##############################
+
+from .tensor import *
+from .tensorfunc import *
+from . import nn
+# import nn
+import torch as basic_torch
+from . import _jit_internal
+from .device import *
+
+distributed = basic_torch.distributed
+autograd = basic_torch.autograd
