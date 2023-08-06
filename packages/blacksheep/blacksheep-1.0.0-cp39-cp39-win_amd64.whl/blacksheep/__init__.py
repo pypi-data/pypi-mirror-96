d5bc63a4925047bec37d84f905b703271e020d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:54 +0000

    Add package blacksheep/1.0.0/blacksheep-1.0.0-cp39-cp39-win_amd64.whl

---
+++ packages/blacksheep/blacksheep-1.0.0-cp39-cp39-win_amd64.whl/blacksheep/__init__.py
@@ -0,0 +1,36 @@
+__author__ = "Roberto Prevato <roberto.prevato@gmail.com>"
+
+from .url import URL, InvalidURL
+from .headers import Header, Headers
+from .exceptions import HTTPException
+from .contents import (
+    Content,
+    StreamedContent,
+    JsonContent,
+    FormContent,
+    FormPart,
+    TextContent,
+    HtmlContent,
+    MultiPartFormData,
+    parse_www_form,
+)
+from .cookies import (
+    Cookie,
+    CookieSameSiteMode,
+    datetime_from_cookie_format,
+    datetime_to_cookie_format,
+    parse_cookie,
+)
+from .messages import Request, Response
+
+
+class HttpMethod:
+    GET = "GET"
+    HEAD = "HEAD"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    TRACE = "TRACE"
+    OPTIONS = "OPTIONS"
+    CONNECT = "CONNECT"
+    PATCH = "PATCH"
