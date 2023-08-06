0697e3d7f8a0e749b2f8d2a70f59a77c3dd9d1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:32 +0000

    Add package ambio/0.3.2/ambio-0.3.2-py3-none-any.whl

---
+++ packages/ambio/ambio-0.3.2-py3-none-any.whl/ambio/distance.py
@@ -0,0 +1,48 @@
+def hammingDistance(string1, string2):
+    if len(string1) != len(string2):
+        raise ValueError("The two given strings are not the same length.")
+
+    dist = 0
+    for i in range(len(string1)):
+        if string1[i] != string2[i]:
+            dist += 1
+
+    return dist
+
+
+def generateEditDistanceTable(string1, string2):
+    tab = [[0 for i in range(len(string1) + 1)]
+           for j in range(len(string2) + 1)]
+
+    string1 = "-" + string1
+    string2 = "-" + string2
+
+    for j in range(len(string2)):
+
+        for i in range(len(string1)):
+
+            choose = []
+
+            if j > 0:
+                choose.append(tab[j-1][i] + 1)
+
+            if i > 0:
+                choose.append(tab[j][i-1] + 1)
+
+            if j > 0 and i > 0:
+                if string1[i] == string2[j]:
+                    choose.append(tab[j-1][i-1])
+                else:
+                    choose.append(tab[j-1][i-1] + 1)
+
+            if len(choose) > 0:
+                tab[j][i] = min(choose)
+
+    return tab
+
+
+def editDistance(string1, string2):
+
+    tab = generateEditDistanceTable(string1, string2)
+
+    return tab[-1][-1]
