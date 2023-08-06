fb84cde182a505d981928a167377b771b1bfd517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:03 +0000

    Add package gm/3.0.134/gm-3.0.134-cp27-cp27m-win32.whl

---
+++ packages/gm/gm-3.0.134-cp27-cp27m-win32.whl/gm-3.0.134.data/purelib/gm/pb/history_pb2.pyi
@@ -0,0 +1,10 @@
+from typing import Any
+
+DESCRIPTOR: Any
+GetCurrentTicksReq: Any
+GetHistoryTicksReq: Any
+GetHistoryBarsReq: Any
+GetHistoryTicksNReq: Any
+GetHistoryBarsNReq: Any
+GetBenchmarkReturnReq: Any
+GetBenchmarkReturnRsp: Any
