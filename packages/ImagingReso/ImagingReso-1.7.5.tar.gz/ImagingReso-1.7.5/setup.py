26effa98fcc71ce57a232d3636b66b70ea949f24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:15 +0000

    Add package ImagingReso/1.7.5/ImagingReso-1.7.5.tar.gz

---
+++ packages/ImagingReso/ImagingReso-1.7.5.tar.gz/ImagingReso-1.7.5/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import codecs
+
+
+def read_file(filename):
+    """
+    Read a utf8 encoded text file and return its contents.
+    """
+    with codecs.open(filename, 'r', 'utf8') as f:
+        return f.read()
+
+
+setup(
+    name="ImagingReso",
+    version="1.7.5",
+    author="Yuxuan Zhang, Jean Bilheux",
+    author_email="zhangy6@ornl.gov, bilheuxjm@ornl.gov",
+    packages=find_packages(exclude=['tests', 'notebooks']),
+    package_data={'ImagingReso': ['reference_data/_data_for_unittest/*', 'reference_data/Bonded_H/*']},
+    include_package_data=True,
+    test_suite='tests',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'periodictable==1.5.2',
+        'scipy',
+        'matplotlib',
+        'six',
+        'plotly',
+    ],
+    dependency_links=[
+    ],
+    description="tool for resonance neutron imaging",
+    long_description=read_file('README.rst'),
+    license='BSD',
+    keywords=['neutron', 'resonance', 'imaging'],
+    url="https://github.com/ornlneutronimaging/ImagingReso.git",
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Natural Language :: English'],
+)
+
+# End of file
