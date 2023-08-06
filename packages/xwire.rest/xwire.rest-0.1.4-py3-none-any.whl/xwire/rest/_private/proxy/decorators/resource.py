8b4b4f21252451a381d09d213580f2ae01c7d277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:20 +0000

    Add package xwire.rest/0.1.4/xwire.rest-0.1.4-py3-none-any.whl

---
+++ packages/xwire.rest/xwire.rest-0.1.4-py3-none-any.whl/xwire/rest/_private/proxy/decorators/resource.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Type
+from xwire.common import decorator_utils
+
+T = TypeVar('T')
+
+
+def resource(resource_path: str):
+    def decorator(cls: Type[T]) -> Type[T]:
+        decorator_utils.set_decorator_metadata(cls, resource, {
+            'resource_path': resource_path,
+            'cookies': None
+        })
+        return cls
+    return decorator
+
