88245d4d2e78a318b87dd1f597e7b1c4dd00df07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:34 +0000

    Add package flask-mysql-connector/1.1.0/flask_mysql_connector-1.1.0.tar.gz

---
+++ packages/flask-mysql-connector/flask_mysql_connector-1.1.0.tar.gz/flask_mysql_connector-1.1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='flask_mysql_connector',
+    version='1.1.0',
+    url='https://github.com/brandenc40/flask_mysql_connector',
+    license='MIT',
+    author='Branden Colen',
+    author_email='brandencolen@gmail.com',
+    description='Easy to use MySQL client for Flask apps.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['flask_mysql_connector'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'mysql-connector-python',
+        'flask',
+        'pandas'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
+)
