a73382b7adee384361b480c0d660035c0bf7ea6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:43 +0000

    Add package marvelapp-cli/0.1.3/marvelapp-cli-0.1.3.tar.gz

---
+++ packages/marvelapp-cli/marvelapp-cli-0.1.3.tar.gz/marvelapp-cli-0.1.3/marvelcli/marvelcli.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import click
+import json
+import datetime
+
+import utils
+from user import user_commands
+from workspace import workspace_commands
+from project import project_commands
+from folder import folder_commands
+from group import group_commands
+
+@click.group()
+def marvelcli(args=None):
+	"""A CLI wrapper for Marvelapp
+
+	The wrapper will enable you to interact with your Marvel
+	account from the comfort of your command line.
+
+	You will need an access token to use the CLI, generate a
+	new one or find the one you already have set up here:
+	https://marvelapp.com/oauth/devtoken
+
+	You can export your token by running this command from the
+	same dir you installed the CLI:
+	`export MARVEL_CLI_TOKEN='your access token'`
+
+	"""
+
+# User
+marvelcli.add_command(user_commands.about_user)
+
+# Workspace
+marvelcli.add_command(workspace_commands.get_billing_info)
+marvelcli.add_command(workspace_commands.remove_users_from_workspace)
+
+# Project
+marvelcli.add_command(project_commands.delete_project)
+marvelcli.add_command(project_commands.create_project)
+marvelcli.add_command(project_commands.add_groups_to_project)
+marvelcli.add_command(project_commands.remove_groups_from_project)
+marvelcli.add_command(project_commands.add_collabs_to_project)
+marvelcli.add_command(project_commands.remove_collabs_from_project)
+marvelcli.add_command(project_commands.get_personal_projects)
+
+# Group
+marvelcli.add_command(group_commands.delete_groups)
+marvelcli.add_command(group_commands.create_group)
+marvelcli.add_command(group_commands.add_members_to_group)
+marvelcli.add_command(group_commands.remove_members_from_group)
+marvelcli.add_command(group_commands.update_group_name)
+
+# Folder - limited by the API
+marvelcli.add_command(folder_commands.create_folder)
+
+if __name__ == "__main__":
+	marvelcli()
---
+++ packages/marvelapp-cli/marvelapp-cli-0.1.3.tar.gz/marvelapp-cli-0.1.3/marvelcli/user/user_commands.py
@@ -0,0 +1,25 @@
+import click
+import datetime
+
+import utils
+from user import user_queries
+
+@click.option('-a', '--auth', help='Auth your request')
+@click.command()
+def about_user(auth: str):
+	"""User statistics"""
+	query = user_queries.about_user_query
+	r, json_data = utils.make_request(auth, query)
+
+	if r.status_code != 200:
+		click.echo("\nWe could not retrieve the data at this time, possibly due to a ratelimit. Try again in a few minutes\n")
+	else:
+		user = json_data['data']['user']
+		last_active = user['lastActiveAt']
+		last_active = datetime.datetime(int(last_active[0:4]), int(last_active[5:7]), int(last_active[8:10]), 0, 0)
+
+		click.echo(utils.colour.BOLD + '\nYour Account:' + utils.colour.END)
+		click.echo('Email: %s' % user['email'])
+		click.echo('Last Active: %s' % last_active)
+		click.echo('Status: %s' % user['status'])
+		click.echo('Username: %s \n' % user['username'])
