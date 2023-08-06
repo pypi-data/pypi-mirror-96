25149ae77f89ed7ddf6dc35af0e66ea3746f5904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:33 +0000

    Add package superfilter/0.2.0/superfilter-0.2.0.tar.gz

---
+++ packages/superfilter/superfilter-0.2.0.tar.gz/superfilter-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='superfilter',
+    version='0.2.0',
+    author='Perzan',
+    packages=["superfilter"],
+    author_email='PerzanDevelopment@gmail.com',
+    install_requires=["argskwargs<2.0"]
+)
