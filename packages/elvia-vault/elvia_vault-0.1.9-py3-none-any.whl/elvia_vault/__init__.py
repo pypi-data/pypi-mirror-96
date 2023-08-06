65d5355a8544af96d8f9a2566caf6887fba542e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package elvia-vault/0.1.9/elvia_vault-0.1.9-py3-none-any.whl

---
+++ packages/elvia-vault/elvia_vault-0.1.9-py3-none-any.whl/elvia_vault/__init__.py
@@ -0,0 +1,5 @@
+# This is so that these 2 classes can be imported directly from the package root
+from elvia_vault.vault_client import VaultClient, EnvVarNames
+
+PACKAGE_NAME = 'elvia-vault'
+__version__ = '0.1.9'
