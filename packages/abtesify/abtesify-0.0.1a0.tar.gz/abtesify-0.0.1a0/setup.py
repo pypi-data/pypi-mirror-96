74a0d440ba36e74685c8e5990d87b5afaf69c25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:29 +0000

    Add package abtesify/0.0.1a0/abtesify-0.0.1a0.tar.gz

---
+++ packages/abtesify/abtesify-0.0.1a0.tar.gz/abtesify-0.0.1a0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="abtesify",  # Replace with your own username
+    version="0.0.1-a0",
+    author="Fangnikoue Evarist",
+    author_email="malevae@gmail.com",
+    description="A python program to send notification about the project update and new release.",
+    url="https://github.com/eirtscience/abtesify.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['bin/abtesify'],
+    install_requires=[
+        'slackclient',
+        'yagmail',
+        'pyaml'
+    ],
+    python_requires='>=3.5',
+)
