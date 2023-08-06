0d378f971c105babc90be533024cc704fba2a52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:36 +0000

    Add package WolframAlpha.py/1.1.9/WolframAlpha.py-1.1.9.tar.gz

---
+++ packages/WolframAlpha.py/WolframAlpha.py-1.1.9.tar.gz/WolframAlpha.py-1.1.9/wolfram/AsyncApp.py
@@ -0,0 +1,59 @@
+import aiohttp, asyncio, requests, json, aiofiles
+
+from .errors import *
+
+def fix_format(string):
+    keys = {
+        " ": "+",
+        "/": "\/",
+        "'": "\'",
+        '"': '\"',
+        "+": "%2B"
+    }
+
+    for key in keys:
+        string.replace(key, keys[key])
+    return string
+
+class AsyncApp:
+    def __init__(self, appID):
+        self.id = appID
+        self.SIMPLE_BASE = "https://api.wolframalpha.com/v1/simple"
+        self.FULL_BASE = "https://api.wolframalpha.com/v2/query"
+        self.SHORT_BASE = "http://api.wolframalpha.com/v1/result"
+        self.CONV_BASE = "http://api.wolframalpha.com/v1/conversation.jsp"
+
+    async def create_request(self, BASE, **kwargs):
+        url = BASE + "?" + '&'.join(f"{i}={kwargs[i]}" for i in kwargs) + "&appid=" + self.id
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as request:
+                return await request.text()
+    
+    async def create_simple_request(self, **kwargs):
+        async with aiohttp.ClientSession() as session:
+            url = self.SIMPLE_BASE + "?" + '&'.join(f"{i}={kwargs[i]}" for i in kwargs) + "&appid=" + self.id
+            async with session.get(url) as request:
+                return await request.read()
+
+    async def simple(self, query, fp:str="wolfram_content"):
+        query = fix_format(query)
+        _data = await self.create_simple_request(i=query)
+
+        f = await aiofiles.open(f'{fp}.png', mode='wb+')
+        await f.write(_data)
+        await f.close()
+
+    async def full(self, query:lambda arg:fix_format(arg)):
+        query = fix_format(query)
+        _data = await self.create_request(self.FULL_BASE, input=fix_format(query), output="json")
+        return json.loads(_data)
+
+    async def short(self, query:lambda arg:fix_format(arg)):
+        query = fix_format(query)
+        return await self.create_request(self.SHORT_BASE, i=query)
+
+    async def talk(self, query:lambda arg:fix_format(arg)):
+        query = fix_format(query)
+        _data = await self.create_request(self.CONV_BASE, i=query, s=5)
+        return json.loads(_data)
