11f50af975aba468ed243a3bf84cb08a3bf62cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:08 +0000

    Add package asf-search/0.1.1/asf_search-0.1.1.tar.gz

---
+++ packages/asf-search/asf_search-0.1.1.tar.gz/asf_search-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="asf_search",
+    version="0.1.1",
+    author="Alaska Satellite Facility Discovery Team",
+    author_email="uaf-asf-discovery@alaska.edu",
+    description="Python wrapper for ASF's SearchAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asfadmin/Discovery-asf_search.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    #test_suite='???',
+    #tests_require=['???'],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests==2.25.1'
+    ]
+)
