ba634775dc97d7f3bc00cbd708d691a485068158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:13 +0000

    Add package apyscript/0.2.10/apyscript-0.2.10-py3-none-any.whl

---
+++ packages/apyscript/apyscript-0.2.10-py3-none-any.whl/apyscript/display/graphics_clear_interface.py
@@ -0,0 +1,32 @@
+"""Class implementation for graphics clear method related interface.
+"""
+
+
+from typing import List
+from typing import Optional
+
+from apyscript.display.display_object import DisplayObject
+from apyscript.type.int import Int
+from apyscript.type.number import Number
+
+
+class GraphicsClearInterface:
+
+    _fill_color: Optional[str] = None
+    _fill_alpha: Number = Number(1.0)
+    _line_color: Optional[str] = None
+    _line_thickness: Int = Int(1)
+    _line_alpha: Optional[float] = None
+    _childs: List[DisplayObject]
+
+    def clear(self) -> None:
+        """
+        Clear all graphics and reset fill and line settings.
+        """
+        self._fill_color = None
+        self._fill_alpha = Number(1.0)
+        self._line_color = None
+        self._line_thickness = Int(1)
+        self._line_alpha = None
+        while self._childs:
+            self._childs[0].remove_from_parent()
