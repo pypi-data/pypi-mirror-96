97fa945b1bcbe71725bcc0bb1ac04c831d541118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:15 +0000

    Add package pyarmor/6.6.2/pyarmor-6.6.2.zip

---
+++ packages/pyarmor/pyarmor-6.6.2.zip/pyarmor-6.6.2/README.md
@@ -0,0 +1,93 @@
+# PyArmor
+
+* [Homepage](https://pyarmor.dashingsoft.com) ([中文版网站](https://pyarmor.dashingsoft.com/index-zh.html))
+* [Documentation](https://pyarmor.readthedocs.io/en/latest/)([中文版](https://pyarmor.readthedocs.io/zh/latest/))
+
+PyArmor is a command line tool used to obfuscate python scripts, bind
+obfuscated scripts to fixed machine or expire obfuscated scripts. It
+protects Python scripts by the following ways:
+
+* Obfuscate code object to protect constants and literal strings.
+* Obfuscate co_code of each function (code object) in runtime.
+* Clear f_locals of frame as soon as code object completed execution.
+* Verify the license file of obfuscated scripts while running it.
+
+Also refer to [The Security of PyArmor](https://pyarmor.readthedocs.io/en/latest/security.html)
+
+## Support Platforms
+
+- Python 2.7 and Python3
+- Prebuilt Platform: win32, win_amd64, linux_i386, linux_x86_64, macosx_x86_64
+- Embedded Platform: Raspberry Pi, Banana Pi, Orange Pi, TS-4600 / TS-7600 and more
+
+Refer to [support platforms](https://pyarmor.readthedocs.io/en/latest/platforms.html)
+
+## Quick Start
+
+Installation
+
+    pip install pyarmor
+
+Obfuscate scripts
+
+    pyarmor obfuscate foo.py
+
+Run obfuscated scripts
+
+    python dist/foo.py
+
+Pack obfuscated scripts into one bundle
+
+    pip install pyinstaller
+    pyarmor pack foo.py
+
+Obfuscate scripts with an expired license
+
+    pyarmor licenses --expired 2018-12-31 r001
+    pyarmor obfuscate --with-license licenses/r001/license.lic foo.py
+
+There is also a web-ui package [pyarmor-webui](https://github.com/dashingsoft/pyarmor-webui)
+
+    pip install pyarmor-webui
+
+Start webui, open web page in browser ([snapshots](https://github.com/dashingsoft/pyarmor-webui/tree/master/snapshots))
+
+    pyarmor-webui
+
+More usage, refer to
+
+* [Examples](https://pyarmor.readthedocs.io/en/latest/examples.html)
+* [Using PyArmor](https://pyarmor.readthedocs.io/en/latest/usage.html)
+* [Advanced Usage](https://pyarmor.readthedocs.io/en/latest/advanced.html)
+* [Man Page](https://pyarmor.readthedocs.io/en/latest/man.html)
+* [Sample Shell Scripts](src/examples/README.md)
+
+## License & Purchase
+
+PyArmor is published as shareware, free trial version never expires, but there are
+some limitations:
+
+* The maximum size of code object is about 32768 bytes in trial version
+* All the trial version uses same public capsule other than private capsule
+* In trial version the module could not be obfuscated by advanced mode
+  if there are more than about 30 functions (code objects) in this module.
+* ...
+
+For details, refer to [PyArmor License](https://pyarmor.readthedocs.io/en/latest/license.html).
+
+## [Change Logs](docs/change-logs.rst)
+
+It describes the fixed issues, new features, incompatible issues in different
+versions.
+
+It's recommended to read this carefully before upgrading pyarmor.
+
+## [Report issuses](https://github.com/dashingsoft/pyarmor/issues)
+
+If there is any question, first check these [questions and
+solutions](https://pyarmor.readthedocs.io/en/latest/questions.html), it may help
+you solve the problem quickly.
+
+If there is no solution, for technical issue, click here to [report an
+issue](https://github.com/dashingsoft/pyarmor/issues) according to the issue
+template, for business and security issue send email to <jondy.zhao@gmail.com>.
---
+++ packages/pyarmor/pyarmor-6.6.2.zip/pyarmor-6.6.2/setup.py
@@ -0,0 +1,120 @@
+# Always prefer setuptools over distutils
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+from sys import argv
+
+from src.config import version
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'src', 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+pyarmor_data_files = [
+    'LICENSE', 'LICENSE-ZH',
+    'pyshield.key', 'pyshield.lic', 'public.key',
+    'product.key', 'license.tri', 'README.rst',
+    'protect_code.pt', 'protect_code2.pt', 'public_capsule.zip',
+    'plugins/README.md', 'plugins/check_ntp_time.py',
+    'examples/README.md', 'examples/README-ZH.md',
+    'examples/*.sh', 'examples/*.bat',
+    'examples/simple/*.py', 'examples/testmod/*.py',
+    'examples/testpkg/*.py', 'examples/testpkg/mypkg/*.py',
+    'examples/pybench/*.py', 'examples/pybench/package/*.py',
+    'examples/py2exe/*.py', 'examples/cx_Freeze/*.py',
+    'examples/helloworld/*.py',
+]
+
+platform_data_files = [
+    'platforms/index.json',
+    'platforms/windows/x86/_pytransform.dll',
+    'platforms/windows/x86_64/_pytransform.dll',
+    'platforms/linux/x86/_pytransform.so',
+    'platforms/linux/x86_64/_pytransform.so',
+    'platforms/darwin/x86_64/_pytransform.dylib',
+]
+
+if argv[1] == 'bdist_wheel':
+    for opt in argv[1:]:
+        if opt.startswith('--plat-name'):
+            name = opt.split('=')[1]
+            name = 'macosx_x86_64' if name.startswith('macosx_') else \
+                'linux_x86_64' if name == 'manylinux1_x86_64' else \
+                name
+            for i in range(len(platform_data_files)):
+                if platform_data_files[i].find(name) > -1:
+                    platform_data_files[i] = \
+                        platform_data_files[i].split('/')[-1]
+                    break
+            break
+
+setup(
+    name='pyarmor',
+
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    version=version,
+    description='A tool used to obfuscate python scripts, bind obfuscated' \
+                ' scripts to fixed machine or expire obfuscated scripts.',
+    long_description=long_description,
+
+    license='Free To Use But Restricted',
+
+    url='https://github.com/dashingsoft/pyarmor',
+    author='Jondy Zhao',
+    author_email='jondy.zhao@gmail.com',
+
+    # For a list of valid classifiers, see
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Utilities',
+        'Topic :: Security',
+        'Topic :: System :: Software Distribution',
+
+        # Pick your license as you wish
+        'License :: Free To Use But Restricted',
+
+        # Support platforms
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords='protect obfuscate encrypt obfuscation distribute',
+
+    packages=['pyarmor', 'pyarmor.polyfills', 'pyarmor.helper'],
+    package_dir={'pyarmor': 'src'},
+    package_data={
+        'pyarmor': pyarmor_data_files + platform_data_files,
+    },
+
+    entry_points={
+        'console_scripts': [
+            'pyarmor=pyarmor.pyarmor:main_entry',
+        ],
+    },
+)
