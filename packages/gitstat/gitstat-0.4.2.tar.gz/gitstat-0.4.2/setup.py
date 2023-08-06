f126be88d6163eeb1dd0b5b544dfd4bfaaeef208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:54 +0000

    Add package gitstat/0.4.2/gitstat-0.4.2.tar.gz

---
+++ packages/gitstat/gitstat-0.4.2.tar.gz/gitstat-0.4.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name='gitstat',
+    version='0.4.2',
+    packages=find_packages(),
+    install_requires=['Click',
+                      'PyInquirer',
+                      'rich',
+                      ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': ['gitstat=gitstat.gittools:cli']
+    },
+)
