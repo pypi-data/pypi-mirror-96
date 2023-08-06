c386c7acb2b17f954f25dad31f3eea2acd6563a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:50 +0000

    Add package youtube-searcher/0.1.5/youtube_searcher-0.1.5.tar.gz

---
+++ packages/youtube-searcher/youtube_searcher-0.1.5.tar.gz/youtube_searcher-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='youtube_searcher',
+    version='0.1.5',
+    packages=['youtube_searcher'],
+    url='https://github.com/HelloChatterbox/youtube_searcher',
+    license='Apache',
+    author='jarbasAI',
+    install_requires=["bs4", "requests", "requests_cache"],
+    author_email='jarbasai@mailfence.com',
+    description='search youtube'
+)
