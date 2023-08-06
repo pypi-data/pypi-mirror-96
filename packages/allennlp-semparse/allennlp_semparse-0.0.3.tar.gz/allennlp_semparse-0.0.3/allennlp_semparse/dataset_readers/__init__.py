33ebe37cce85024b7efa67b288cbfd3befbd8917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:19 +0000

    Add package allennlp-semparse/0.0.3/allennlp_semparse-0.0.3.tar.gz

---
+++ packages/allennlp-semparse/allennlp_semparse-0.0.3.tar.gz/allennlp_semparse-0.0.3/allennlp_semparse/dataset_readers/__init__.py
@@ -0,0 +1,7 @@
+from allennlp_semparse.dataset_readers.atis import AtisDatasetReader
+from allennlp_semparse.dataset_readers.grammar_based_text2sql import (
+    GrammarBasedText2SqlDatasetReader,
+)
+from allennlp_semparse.dataset_readers.nlvr import NlvrDatasetReader
+from allennlp_semparse.dataset_readers.template_text2sql import TemplateText2SqlDatasetReader
+from allennlp_semparse.dataset_readers.wikitables import WikiTablesDatasetReader
