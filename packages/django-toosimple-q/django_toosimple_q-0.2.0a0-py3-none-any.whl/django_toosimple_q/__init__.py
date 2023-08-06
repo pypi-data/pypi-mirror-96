865202503cfa2d01c88ad6d7b4cfd29ca319c8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:30 +0000

    Add package django-toosimple-q/0.2.0a0/django_toosimple_q-0.2.0a0-py3-none-any.whl

---
+++ packages/django-toosimple-q/django_toosimple_q-0.2.0a0-py3-none-any.whl/django_toosimple_q/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "v0.2.0a0"  # DO NOT CHANGE THIS LINE - it will be replaced by CI workflow
+default_app_config = "django_toosimple_q.apps.DjangoToosimpleQConfig"
---
+++ packages/django-toosimple-q/django_toosimple_q-0.2.0a0-py3-none-any.whl/django_toosimple_q/decorators.py
@@ -0,0 +1,70 @@
+from django.core.exceptions import ImproperlyConfigured
+from django.utils import timezone
+
+from .registry import schedules, tasks
+
+
+def register_task(
+    name=None, queue="default", priority=0, unique=False, retries=0, retry_delay=0
+):
+    """Attaches ._task_name attribute, the .queue() method and adds the callable to the tasks registry"""
+
+    def inner(func):
+        if name:
+            func._task_name = name
+        else:
+            func._task_name = func.__globals__["__name__"] + "." + func.__qualname__
+
+        def enqueue(*args_, **kwargs_):
+            from .models import Task
+
+            if unique:
+                existing_tasks = Task.objects.filter(
+                    function=func._task_name,
+                    args=args_,
+                    kwargs=kwargs_,
+                    queue=queue,
+                )
+                # If already queued, we don't do anything
+                if existing_tasks.filter(state=Task.QUEUED).exists():
+                    return False
+                # If there's a sleeping task, we set it's due date to now
+                if existing_tasks.filter(state=Task.SLEEPING).exists():
+                    task = existing_tasks.first()
+                    task.due = timezone.now()
+                    task.save()
+                    return False
+
+            return Task.objects.create(
+                function=func._task_name,
+                args=args_,
+                kwargs=kwargs_,
+                queue=queue,
+                priority=priority,
+                retries=retries,
+                retry_delay=retry_delay,
+            )
+
+        func.queue = enqueue
+        tasks[func._task_name] = func
+        return func
+
+    return inner
+
+
+def schedule(**kwargs):
+    """Adds the task to the schedules registry"""
+
+    def inner(func):
+        if not hasattr(func, "_task_name"):
+            raise ImproperlyConfigured(
+                "Only registered tasks can be scheduled."
+                " Are you sure you registered your callable with the @register_task() decorator ?"
+            )
+        schedule_name = kwargs.get("name", func._task_name)
+        kwargs["name"] = schedule_name
+        kwargs["function"] = func._task_name
+        schedules[schedule_name] = kwargs
+        return func
+
+    return inner
