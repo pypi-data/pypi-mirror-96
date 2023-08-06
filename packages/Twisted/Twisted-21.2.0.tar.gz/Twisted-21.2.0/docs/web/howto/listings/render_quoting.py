bb3628c33978cc5e9e56930aa1b95728f948d216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:00 +0000

    Add package Twisted/21.2.0/Twisted-21.2.0.tar.gz

---
+++ packages/Twisted/Twisted-21.2.0.tar.gz/Twisted-21.2.0/docs/web/howto/listings/render_quoting.py
@@ -0,0 +1,9 @@
+from twisted.web.template import flattenString
+from quoting_element import ExampleElement
+
+
+def renderDone(output):
+    print(output)
+
+
+flattenString(None, ExampleElement()).addCallback(renderDone)
---
+++ packages/Twisted/Twisted-21.2.0.tar.gz/Twisted-21.2.0/docs/web/howto/listings/render_transparent.py
@@ -0,0 +1,9 @@
+from twisted.web.template import flattenString
+from transparent_element import ExampleElement
+
+
+def renderDone(output):
+    print(output)
+
+
+flattenString(None, ExampleElement()).addCallback(renderDone)
