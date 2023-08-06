7cb3b15c58b007869071464a8f93cff270c7fe49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:43 +0000

    Add package omero-metadata/0.8.0/omero_metadata-0.8.0-py3-none-any.whl

---
+++ packages/omero-metadata/omero_metadata-0.8.0-py3-none-any.whl/omero/plugins/metadata.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+   Metadata plugin
+
+   Copyright 2015 Glencoe Software, Inc. All rights reserved.
+   Use is subject to license terms supplied in LICENSE.txt
+
+"""
+
+import sys
+from omero.cli import CLI
+from omero_metadata.cli import MetadataControl, HELP
+try:
+    register("metadata", MetadataControl, HELP)
+except NameError:
+    if __name__ == "__main__":
+        cli = CLI()
+        cli.register("metadata", MetadataControl, HELP)
+        cli.invoke(sys.argv[1:])
