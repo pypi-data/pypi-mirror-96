021d7ff0fbc2193ccbc4bd891c590ef841a227e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package rt-thread-studio/2.4/rt-thread-studio-2.4.tar.gz

---
+++ packages/rt-thread-studio/rt-thread-studio-2.4.tar.gz/rt-thread-studio-2.4/rt_thread_studio/rt_thread_studio_tools.py
@@ -0,0 +1,46 @@
+import click
+from rt_thread_studio.board_support_package_generator.bsp_generator.generate import Generator
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option(
+    '--type',
+    "-t",
+    envvar="type",
+    required=False,
+    default="bsp",
+    type=click.STRING,
+    help="specify the package type",
+)
+def create(type):
+    """: a command to create a package"""
+    # TODO :add project update feature
+    if str(type).lower().strip()=="bsp":
+        gen=Generator()
+        gen.generate_bsp()
+        pass
+    else:
+        print("please specify one supported package type")
+
+
+@cli.command("create_bsp")
+@click.option(
+    '--json',
+    "-j",
+    envvar="json",
+    required=True,
+    default="",
+    type=click.STRING,
+    help="specify json input",
+)
+def create_bsp_from_json(json):
+    """: a command to create a package"""
+    # TODO :add project update feature
+    generator1 = Generator()
+    generator1.generate_bsp_from_json(json)
+
+if __name__ == '__main__':
+    cli()
