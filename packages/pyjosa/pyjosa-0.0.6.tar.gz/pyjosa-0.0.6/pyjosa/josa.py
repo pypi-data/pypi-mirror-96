aab03d0e239e0282cecdd32e12783e7dfaae79b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package pyjosa/0.0.6/pyjosa-0.0.6.tar.gz

---
+++ packages/pyjosa/pyjosa-0.0.6.tar.gz/pyjosa-0.0.6/pyjosa/josa.py
@@ -0,0 +1,33 @@
+from pyjosa.jonsung import has_jongsung
+
+class Josa:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def get_josa(string, josa) -> str:
+        if (josa == '을') or (josa =='를'):
+            return '을' if has_jongsung(string) else '를'
+        elif (josa == '은') or (josa =='는'):
+            return '은' if has_jongsung(string) else '는'
+        elif (josa == '이') or (josa =='가'):
+            return '이' if has_jongsung(string) else '가'
+        else:
+            raise Exception("아직 구현중임")
+
+
+    @staticmethod
+    def get_full_string(string, josa) -> str:
+        pass
+
+# TODO: make main function as class's static method? or not a class's static method just a function.
+
+
+if __name__ =="__main__":
+    s = '오리'
+    s2 = '철수'
+    
+    print(f'{s}{Josa.get_josa(s, "이")}') # 오리가 
+    print(f'{s2}{Josa.get_josa(s2, "을")}') # 철수를
+    # 사용법이 너무 어려워 보이는데?
+    # 더 나은 사용법 제공을 위해 고민하기.
