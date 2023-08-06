05096f16a05f6be3cbbb59bcfdbcb1680c171356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package sphinxmark/0.2.1/sphinxmark-0.2.1.tar.gz

---
+++ packages/sphinxmark/sphinxmark-0.2.1.tar.gz/sphinxmark-0.2.1/setup.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+# coding: utf-8
+"""sphinxmark setup file."""
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sphinxmark',
+    version='0.2.1',
+    description='A Sphinx extension that enables watermarks for HTML output.',
+    long_description=long_description,
+    url='https://github.com/kallimachos/sphinxmark',
+    author='Brian Moss',
+    author_email='kallimachos@gmail.com',
+    license='Apache 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Documentation :: Sphinx',
+        'Framework :: Sphinx :: Extension',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+    keywords='sphinx documentation watermark',
+
+    packages=['sphinxmark'],
+
+    package_data={
+        'sphinxmark': ['watermark-draft.png', 'border.tpl',
+                       'watermark.tpl', 'arial.ttf'],
+    },
+
+    install_requires=[
+        'bottle',
+        'Pillow',
+        'sphinx',
+    ],
+)
