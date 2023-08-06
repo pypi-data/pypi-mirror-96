d8718c3c503d3ee214d19ddb0292fc63467f4653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:47 +0000

    Add package nessvec/0.0.1/nessvec-0.0.1.tar.gz

---
+++ packages/nessvec/nessvec-0.0.1.tar.gz/nessvec-0.0.1/src/nessvec/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+
+from nessvec import util
+from nessvec import nessvectors_glove as glove  # noqa
+# from nessvec import nessvectors as word2vec  # noqa
