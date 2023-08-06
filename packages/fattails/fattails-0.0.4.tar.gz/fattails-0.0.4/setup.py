09d2b7e205e088ff0b122abadf7d2ec8e3b3ce9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package fattails/0.0.4/fattails-0.0.4.tar.gz

---
+++ packages/fattails/fattails-0.0.4.tar.gz/fattails-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fattails", # Replace with your own username
+    version="0.0.4",
+    author="FergM",
+    description="A package for fat-tailed statistics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FergM/fattails/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas>=1.2.0',
+        'matplotlib>=3.3.3'
+    ],
+    python_requires='>=3.7',
+)
