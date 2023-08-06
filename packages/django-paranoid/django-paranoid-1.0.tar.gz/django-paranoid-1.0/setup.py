ba78da2fb1ba0cf36f7a7e5a6dc00bd74776f6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package django-paranoid/1.0/django-paranoid-1.0.tar.gz

---
+++ packages/django-paranoid/django-paranoid-1.0.tar.gz/django-paranoid-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+excluded = ['manage.py, tests/settings.py']
+
+setup(name="django-paranoid",
+      version="1.0",
+      description="this library adds 'created_at', 'updated_at' and 'delete_at'  fields like a rail apps in django,"
+                  " also added soft delete method",
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      author="Carlos Ganoza Plasencia",
+      author_email="cganozap@gmail.com",
+      url="https://github.com/drneox/django-paranoid",
+      license="MIT",
+      packages=["django_paranoid"],
+      keywords="django created_at updated_at deleted_at fields models django-admin soft delete softdelete",
+      )
