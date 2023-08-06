76ffa2b2264d936c7227de7cd39db6bb8eeab6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:15 +0000

    Add package voicemailbox/0.1.3/voicemailbox-0.1.3-py3-none-any.whl

---
+++ packages/voicemailbox/voicemailbox-0.1.3-py3-none-any.whl/voicemailbox/__init__.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+from voicemailbox import logging
+
+
+__version__ = '0.1.3'
+
+# Activate logs
+logger = logging.Logging().logger
+
+# Define constants
+STATIC_FILES_PATH = Path(__file__).parent / "static"
+NEW_MAIL_SONG_PATH = STATIC_FILES_PATH / "songs" / "new_mail.mp3"
+FONTAWESOME_PATH = (
+        STATIC_FILES_PATH / "fonts" / "Font Awesome 5 Free-Solid-900.otf")
+SLIDER_BORDER_PATH = STATIC_FILES_PATH / "images" / "blank_background.png"
+SLIDER_CURSOR_PATH = STATIC_FILES_PATH / "images" / "circle-solid-24.png"
