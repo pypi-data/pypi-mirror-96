fdf7eea4f066d107925d39e3073d210f864987ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:30 +0000

    Add package numerai-cli/0.2.6/numerai-cli-0.2.6.tar.gz

---
+++ packages/numerai-cli/numerai-cli-0.2.6.tar.gz/numerai-cli-0.2.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+# read the contents of the README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='numerai-cli',
+      version='0.2.6',
+      description='A library for helping to deploy a Numer.ai compute node',
+      url='https://github.com/numerai/numerai-cli',
+      author='Numer.ai',
+      author_email='contact@numer.ai',
+      license='MIT',
+      packages=['numerai_compute'],
+      include_package_data=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      install_requires=[
+          "click>=7",
+          "boto3",
+          "numerapi>=2.2.4",
+          "colorama",
+          "requests",
+      ],
+      entry_points={
+          'console_scripts': ['numerai=numerai_compute.cli:main'],
+      })
