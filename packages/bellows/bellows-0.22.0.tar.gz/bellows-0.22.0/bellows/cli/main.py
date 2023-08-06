dbcd5b1ddf3f2ce8f447724e2618b453aee9999e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:47 +0000

    Add package bellows/0.22.0/bellows-0.22.0.tar.gz

---
+++ packages/bellows/bellows-0.22.0.tar.gz/bellows-0.22.0/bellows/cli/main.py
@@ -0,0 +1,20 @@
+import logging
+
+import click
+import click_log
+
+from . import opts
+
+
+@click.group()
+@click_log.simple_verbosity_option(logging.getLogger(), default="WARNING")
+@opts.device
+@opts.baudrate
+@click.pass_context
+def main(ctx, device, baudrate):
+    ctx.obj = {"device": device, "baudrate": baudrate}
+    click_log.basic_config()
+
+
+if __name__ == "__main__":
+    main()
