7943b0265bb4bef91dbf62fc7cb8ed7aef2e033d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:34 +0000

    Add package clldutils/3.7.0/clldutils-3.7.0.tar.gz

---
+++ packages/clldutils/clldutils-3.7.0.tar.gz/clldutils-3.7.0/README.md
@@ -0,0 +1,13 @@
+# clldutils
+
+Utilities for clld apps without the overhead of requiring pyramid, rdflib et al.
+
+[![Build Status](https://github.com/clld/clldutils/workflows/tests/badge.svg)](https://github.com/clld/clldutils/actions?query=workflow%3Atests)
+[![codecov.io](https://codecov.io/github/clld/clldutils/coverage.svg?branch=master)](https://codecov.io/github/clld/clldutils?branch=master)
+[![Requirements Status](https://requires.io/github/clld/clldutils/requirements.svg?branch=master)](https://requires.io/github/clld/clldutils/requirements/?branch=master)
+[![PyPI](https://img.shields.io/pypi/v/clldutils.svg)](https://pypi.python.org/pypi/clldutils)
+
+
+## Manipulating "Standard Format" (SFM, a.k.a. toolbox) files
+
+- [`clldutils.sfm`](clldutils/sfm.py)
---
+++ packages/clldutils/clldutils-3.7.0.tar.gz/clldutils-3.7.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clldutils',
+    version='3.7.0',
+    description='Utilities for clld apps',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author='Robert Forkel',
+    author_email='forkel@shh.mpg.de',
+    url='https://github.com/clld/clldutils',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    install_requires=[
+        'python-dateutil',
+        'tabulate>=0.7.7',
+        'colorlog',
+        'attrs>=18.1.0',
+        'csvw>=1.0',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'pytest>=5.4',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+    license="Apache 2.0",
+    zip_safe=False,
+    keywords='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
