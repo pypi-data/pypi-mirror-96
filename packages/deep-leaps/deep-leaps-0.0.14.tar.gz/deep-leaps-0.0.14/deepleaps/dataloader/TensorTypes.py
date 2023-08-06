6eb20ed3de7c07060a376afc9817e00b430227ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:23 +0000

    Add package deep-leaps/0.0.14/deep-leaps-0.0.14.tar.gz

---
+++ packages/deep-leaps/deep-leaps-0.0.14.tar.gz/deep-leaps-0.0.14/deepleaps/dataloader/TensorTypes.py
@@ -0,0 +1,33 @@
+import torch
+import scipy.misc as misc
+
+class TensorType:
+    def __str__(self):
+        return self.__class__.__name__
+
+    def getSample(self, sample):
+        raise NotImplementedError
+
+class ANY(TensorType):
+    pass
+
+class IMAGE(TensorType):
+    def image_loader(self, path):
+        return misc.imread(path)/255.
+
+    def image_saver(self, path, data):
+        return misc.imsave(path, data)
+
+    def getSample(self, sample):
+        return self.image_loader(sample)
+
+class GRAYSCALE_IMAGE(IMAGE):
+    def image_loader(self, path):
+        return misc.imread(path)
+
+    def getSample(self, sample):
+        return self.image_loader(sample)
+
+class DISPARITY(TensorType):
+    pass
+
