a5dcf0c2e14b43e85eb2886365bc8ca321a5a48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package threefive/2.2.67/threefive-2.2.67-py3-none-any.whl

---
+++ packages/threefive/threefive-2.2.67-py3-none-any.whl/threefive/decode.py
@@ -0,0 +1,82 @@
+"""
+decode.py
+
+contains the all purpose
+threefive.decode() function
+"""
+
+import sys
+
+from .cue import Cue
+from .stream import Stream
+
+# Maximum size for a SCTE35 cue.
+_MAX_CUE_SIZE = 4096
+
+
+def _read_stdin():
+    """
+    handles piped in data
+    """
+    try:
+        Stream(sys.stdin.buffer).decode()
+    except:
+        try:
+            stuff = sys.stdin.buffer.read()
+            cue = Cue(stuff)
+            cue.decode()
+            cue.show()
+        except:
+            pass
+
+
+def _read_stuff(stuff):
+    """
+    reads filename or a string
+    """
+    try:
+        with open(stuff, "rb") as tsdata:
+            tsd = tsdata.read(_MAX_CUE_SIZE)
+            cue = Cue(tsd)
+            cue.decode()
+            cue.show()
+    except Exception:
+        pass
+    try:
+        with open(stuff, "rb") as tsdata:
+            s = Stream(tsdata)
+            s.decode()
+    except Exception:
+        pass
+
+    try:
+        cue = Cue(stuff)
+        cue.decode()
+        cue.show()
+    except Exception:
+        pass
+
+
+def decode(stuff=None):
+
+    """
+    All purpose SCTE 35 decoder function
+
+    # for a mpegts video
+
+        import threefive
+        threefive.decode('/path/to/mpegts')
+
+    # for a base64 encoded string
+
+        import threefive
+        Bee64='/DAvAAAAAAAA///wBQb+dGKQoAAZAhdDVUVJSAAAjn+fCAgAAAAALKChijUCAKnMZ1g='
+        threefive.decode(Bee64)
+
+    stuff can be a filename or encoded string.
+    if stuff is not set, reads from stdin.
+    """
+    if stuff in [None, sys.stdin.buffer]:
+        _read_stdin()
+    else:
+        _read_stuff(stuff)
