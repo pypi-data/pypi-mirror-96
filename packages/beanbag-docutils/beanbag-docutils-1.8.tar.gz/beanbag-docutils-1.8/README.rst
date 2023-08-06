0c8bae63cbcc23de174441d7349e689e98553bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:21 +0000

    Add package beanbag-docutils/1.8/beanbag-docutils-1.8.tar.gz

---
+++ packages/beanbag-docutils/beanbag-docutils-1.8.tar.gz/beanbag-docutils-1.8/README.rst
@@ -0,0 +1,33 @@
+=====================
+Beanbag Doc Utilities
+=====================
+
+This is a collection of utilities to help with generating documentation for
+Beanbag-related products, including:
+
+* `Review Board`_ - Our widely-used open source code review product.
+* RBCommons_ - Our Review Board SaaS.
+* Djblets_ - A set of utilities and infrastructure for Django-based projects.
+* RBTools_ - Command line tools for Review Board and RBCommons.
+
+
+.. _Review Board: https://www.reviewboard.org/
+.. _RBCommons: https://www.rbcommons.com/
+.. _Djblets: https://github.com/djblets/djblets/
+.. _RBTools: https://github.com/reviewboard/rbtools/
+
+
+Getting Started
+===============
+
+To install the package, run:
+
+.. code-block:: shell
+
+   $ pip install beanbag-docutils
+
+
+See the documentation_ for usage information.
+
+
+.. _documentation: https://beanbag-docutils.readthedocs.io/
---
+++ packages/beanbag-docutils/beanbag-docutils-1.8.tar.gz/beanbag-docutils-1.8/beanbag_docutils/sphinx/ext/django_utils.py
@@ -0,0 +1,76 @@
+"""Sphinx extension to add useful Django-related functionality.
+
+This adds some improvements when working with Django-based classes in autodocs,
+and when referencing Django documentation.
+
+Localized strings using :py:func:`~django.utils.translation.ugettext_lazy` will
+be turned into actual strings for the documentation, which is useful for forms
+and models.
+
+Roles are also added for common cross-references.
+
+
+Setup
+=====
+
+To use this, you just need to add the extension in :file:`conf.py`::
+
+    extensions = [
+        ...
+        'beanbag_docutils.sphinx.ext.django_utils',
+        ...
+    ]
+
+
+Roles
+=====
+
+.. rst:role:: setting
+
+   Creates and links to references for Django settings (those that live in
+   ``django.conf.settings``).
+
+   For example:
+
+   .. code-block:: rst
+
+       .. setting:: MY_SETTING
+
+       Settings go here.
+
+       And then to reference it: :setting:`MY_SETTING`.
+"""
+
+from __future__ import unicode_literals
+
+import six
+from django.utils.functional import Promise
+
+
+def _repr_promise(promise):
+    """Return a sane representation of a lazy localized string.
+
+    If the promise is a result of ugettext_lazy(), it will be converted into
+    a Unicode string before generating a representation.
+    """
+    if hasattr(promise, '_proxy____text_cast'):
+        return '_(%s)' % repr(six.text_type(promise))
+
+    return super(promise.__class__, promise).__repr__(promise)
+
+
+def setup(app):
+    """Set up the Sphinx extension.
+
+    This registers cross-references and fixes up the lazily-localized strings
+    for display.
+
+    Args:
+        app (sphinx.application.Sphinx):
+            The Sphinx application to listen to events on.
+    """
+    Promise.__repr__ = _repr_promise
+
+    app.add_crossref_type(directivename=str('setting'),
+                          rolename=str('setting'),
+                          indextemplate=str('pair: %s; setting'))
