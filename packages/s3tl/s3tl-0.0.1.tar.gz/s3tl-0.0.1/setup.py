eb5d8a5ab0f498673590442a7b64336c9f8685cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:47 +0000

    Add package s3tl/0.0.1/s3tl-0.0.1.tar.gz

---
+++ packages/s3tl/s3tl-0.0.1.tar.gz/s3tl-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="s3tl",
+    version="0.0.1",
+    description="Framework for building ETLs against AWS S3",
+    author="Tyler Lovely",
+    author_email="tyler.n.lovely@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.6, <4",
+    url="https://github.com/tlovely/s3tl",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    install_requires=[
+        "aiofiles>=0.6.0,<1",
+        "aiobotocore>=1.2.1,<2",
+        "toolz",
+    ],
+)
