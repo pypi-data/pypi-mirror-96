93d7aca81ac6ec1a2b9ca30b87dbc5bdf5e713d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:32 +0000

    Add package delaunay-triangulation/1.0.3/delaunay-triangulation-1.0.3.tar.gz

---
+++ packages/delaunay-triangulation/delaunay-triangulation-1.0.3.tar.gz/delaunay-triangulation-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="delaunay-triangulation",
+    version="1.0.3",
+    author="sleoh",
+    author_email="simon.henkel@gmx.de",
+    description="A lightweight collection of helper classes and methods to create a delaunay triangulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://delaunay.sleoh.de",
+    project_urls={
+        "Source Files": "https://gitlab.com/sleoh/delaunay-triangulation"
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
