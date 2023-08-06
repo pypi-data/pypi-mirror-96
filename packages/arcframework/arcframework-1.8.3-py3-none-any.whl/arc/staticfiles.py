877aff72da3a3a59fea8d28896457f5196973ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:28 +0000

    Add package arcframework/1.8.3/arcframework-1.8.3-py3-none-any.whl

---
+++ packages/arcframework/arcframework-1.8.3-py3-none-any.whl/arc/staticfiles.py
@@ -0,0 +1,15 @@
+from starlette.responses import FileResponse
+import os
+
+
+class EmptyWSGI:
+    def __init__(self):
+        ...
+
+
+class StaticFile:
+    def __init__(self, directory):
+        self.directory = directory
+    
+    async def __call__(self, path: str):
+        return FileResponse(path, media_type="text/css")
