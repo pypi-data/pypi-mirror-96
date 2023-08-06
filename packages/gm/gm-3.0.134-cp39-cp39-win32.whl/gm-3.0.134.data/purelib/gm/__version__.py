6fb9ffde2c0c6efa2ae0e6fdc961b5b895f9c835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package gm/3.0.134/gm-3.0.134-cp39-cp39-win32.whl

---
+++ packages/gm/gm-3.0.134-cp39-cp39-win32.whl/gm-3.0.134.data/purelib/gm/__version__.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import unicode_literals, print_function, absolute_import
+
+__title__ = 'gm'
+__description__ = '掘金量化 掘金3 sdk'
+__url__ = 'https://www.myquant.cn/'
+__version__ = '3.0.134'
+# 增加算法单
+__dev_version__ = '3.0.134'
+__author__ = 'myquant_dev'
+__author_email__ = 'support@myquant.cn'
+__license__ = 'Apache 2.0'
---
+++ packages/gm/gm-3.0.134-cp39-cp39-win32.whl/gm-3.0.134.data/purelib/gm/pb/performance_pb2.pyi
@@ -0,0 +1,7 @@
+from typing import Any
+
+DESCRIPTOR: Any
+Indicator: Any
+Indicators: Any
+IndicatorDuration: Any
+IndicatorDurations: Any
