64532e5ad1f8e4f0d4f8a3092f74f2c2f5c32768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:04 +0000

    Add package randan/0.2.1/randan-0.2.1-py3-none-any.whl

---
+++ packages/randan/randan-0.2.1-py3-none-any.whl/randan/__init__.py
@@ -0,0 +1,9 @@
+from . import (
+    descriptive_statistics,
+    bivariate_association, 
+    tree, 
+    comparison_of_central_tendency,
+    regression,
+    clustering,
+    dimension_reduction
+)
