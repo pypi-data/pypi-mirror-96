09f85ebb2c00778a3f5947c85e324e9643e4e75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:26 +0000

    Add package ScuffedAPI/0.0.4/ScuffedAPI-0.0.4-py3-none-any.whl

---
+++ packages/ScuffedAPI/ScuffedAPI-0.0.4-py3-none-any.whl/ScuffedAPI/__init__.py
@@ -0,0 +1,12 @@
+import requests
+name = 'ScuffedAPI'
+
+Scuffed_API = 'http://scuffedapi.xyz/api/'
+
+# Get any cosmetic, not checking cosmetic type.
+
+def getPlaylist(search):
+    url = Scuffed_API + 'api/playlists?search=' + search
+    apir = requests.get(url).json()
+    ans = response.json()["id"]
+    return ans
