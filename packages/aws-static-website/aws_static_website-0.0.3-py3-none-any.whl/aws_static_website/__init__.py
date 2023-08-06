8538a034a87ae5fef738c90e394e5906d034b406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package aws-static-website/0.0.3/aws_static_website-0.0.3-py3-none-any.whl

---
+++ packages/aws-static-website/aws_static_website-0.0.3-py3-none-any.whl/aws_static_website/__init__.py
@@ -0,0 +1,20 @@
+"""AWS static website
+
+This package contains the modules for deploying a simple AWS static website.
+
+It is part of the educational repositories (https://github.com/pandle/materials)
+to learn how to write stardard code and common uses of the TDD.
+
+Package contents one class for the website deployment.
+It extended the core.Stack class of aws_cdk package.
+
+    >>> import aws_static_website
+    >>> help(aws_static_website)
+    >>> import aws_static_website.website_stack as WebsiteStack
+    >>> help(WebsiteStack)
+
+# license MIT
+# support https://github.com/bilardi/aws-static-website/issues
+"""
+__version__ = '0.0.3'
+__author__ = 'Alessandra Bilardi'
