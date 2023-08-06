c836e75646d5e66d3c62a4a63b3c4b6bfbe7a28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:46 +0000

    Add package Wspeaker/1.0.0/Wspeaker-1.0.0.tar.gz

---
+++ packages/Wspeaker/Wspeaker-1.0.0.tar.gz/Wspeaker-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'Wspeaker',
+    version = '1.0.0',
+    py_modules = ['Wspeaker'],
+    author = 'WSSeieiei',        
+    author_email = 'dcfdj@qq.com',
+    description = 'A module can speak words'
+    )
