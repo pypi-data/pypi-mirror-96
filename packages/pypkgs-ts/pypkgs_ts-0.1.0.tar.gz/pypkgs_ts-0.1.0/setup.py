8a42691791236b8fd68afb6aa73754d600a2d9dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:08 +0000

    Add package pypkgs-ts/0.1.0/pypkgs_ts-0.1.0.tar.gz

---
+++ packages/pypkgs-ts/pypkgs_ts-0.1.0.tar.gz/pypkgs_ts-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypkgs_ts']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pypkgs-ts',
+    'version': '0.1.0',
+    'description': 'Python package that eases the pain of concatenating Pandas categoricals!',
+    'long_description': None,
+    'author': 'Tanmay Sharma',
+    'author_email': 'tanmaysharma19@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
