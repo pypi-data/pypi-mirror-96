13c09aee26e6014a3f9acfdd3d5a12e49165d1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package automl-pn/0.1/automl_pn-0.1.tar.gz

---
+++ packages/automl-pn/automl_pn-0.1.tar.gz/automl_pn-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='automl_pn',
+      version='0.1',
+      description='Automated binary classification',
+      packages=['automl_pn'],
+      author_email='maximponomarev92@gmail.com',
+      zip_safe=False)
