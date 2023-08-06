56cffa2d72a82ee9859727b962c756004fd7241e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:35 +0000

    Add package namemcpy/1.4.3/namemcpy-1.4.3.tar.gz

---
+++ packages/namemcpy/namemcpy-1.4.3.tar.gz/namemcpy-1.4.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="namemcpy",
+    version="1.4.3",
+    license="MIT",
+    author="Luke Lass",
+    author_email="ants.uk.us@gmail.com",
+    description="Api Wrapper for namemc",
+    long_description="Api Wrapper for https://namemc.com/ This moudule is to make it easier to use namemcs api.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/lasscat/namemcpy",
+    packages=setuptools.find_packages(),
+    install_requires = ["requests", "bs4"],
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
