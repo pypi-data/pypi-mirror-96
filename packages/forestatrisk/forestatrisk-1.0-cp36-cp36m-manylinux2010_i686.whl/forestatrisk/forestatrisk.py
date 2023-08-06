433a8d23eb8ecc15e2a7b07672ba06ebe4f82b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:14 +0000

    Add package forestatrisk/1.0/forestatrisk-1.0-cp36-cp36m-manylinux2010_i686.whl

---
+++ packages/forestatrisk/forestatrisk-1.0-cp36-cp36m-manylinux2010_i686.whl/forestatrisk/forestatrisk.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ==============================================================================
+# author          :Ghislain Vieilledent
+# email           :ghislain.vieilledent@cirad.fr, ghislainv@gmail.com
+# web             :https://ecology.ghislainv.fr
+# python_version  :>=2.7
+# license         :GPLv3
+# ==============================================================================
+
+from __future__ import print_function  # Python 3 compatibility
+
+__version__ = "1.0"
+
+
+def main():
+    """forestatrisk.forestatrisk: provides entry point main().
+
+    Running ``forestatrisk`` in the terminal prints ForestAtRisk
+    description and version. Can be used to check that the
+    ``forestatrisk`` Python package can be correctly imported.
+
+    """
+    print("# forestatrisk version {}.".format(__version__))
+    return None
+
+# End
