897c7d8c4cc75a7d95defbf815d300ae70e59133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:43 +0000

    Add package anonjail/1.6.7/anonjail-1.6.7.tar.gz

---
+++ packages/anonjail/anonjail-1.6.7.tar.gz/anonjail-1.6.7/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from setuptools import setup
+from setuptools.command.install import install as anoninstall
+from os import path
+
+VERSION = "1.6.7"
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file("README.md", "rst")
+except:
+    long_description = ""
+               
+setup(
+    name = 'anonjail',
+    version=VERSION,
+    description="Control firejail and tor desktop integration.",
+    long_description=long_description,
+    url="https://boards.420chan.org/",
+    license="GPLv2+",   
+    py_modules=["anonjail"],
+    install_requires=["click"],
+    entry_points={"console_scripts": ["anonjail=anonjail:cli"]},
+    scripts=["scripts/anonjail-install.bash"],
+    author="K",
+    author_email="anon@anon.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security"
+    ],
+    keywords="firejail and tor sandbox desktop integration",
+)
