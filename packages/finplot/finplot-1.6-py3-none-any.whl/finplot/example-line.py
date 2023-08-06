e27f57ca2afd99139128051e66175905abc9b207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:15 +0000

    Add package finplot/1.6/finplot-1.6-py3-none-any.whl

---
+++ packages/finplot/finplot-1.6-py3-none-any.whl/finplot/example-line.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import finplot as fplt
+import numpy as np
+import pandas as pd
+
+
+dates = pd.date_range('01:00', '01:00:01.200', freq='1ms')
+prices = pd.Series(np.random.random(len(dates))).rolling(30).mean() + 4
+fplt.plot(dates, prices, width=3)
+line = fplt.add_line((dates[100], 4.4), (dates[1100], 4.6), color='#9900ff', interactive=True)
+## fplt.remove_line(line)
+text = fplt.add_text((dates[500], 4.6), "I'm here alright!", color='#bb7700')
+## fplt.remove_text(text)
+
+def save():
+    fplt.screenshot(open('screenshot.png', 'wb'))
+fplt.timer_callback(save, 0.5, single_shot=True) # wait some until we're rendered
+
+fplt.show()
