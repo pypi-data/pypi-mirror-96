9df8571475a8d654836bbd3970c5ef97034129be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:13 +0000

    Add package scipion-em-sidesplitter/3.0.6/scipion-em-sidesplitter-3.0.6.tar.gz

---
+++ packages/scipion-em-sidesplitter/scipion-em-sidesplitter-3.0.6.tar.gz/scipion-em-sidesplitter-3.0.6/README.rst
@@ -0,0 +1,86 @@
+===================
+SIDESPLITTER plugin
+===================
+
+This plugin provide wrappers around `SIDESPLITTER <https://github.com/StructuralBiology-ICLMedicine/SIDESPLITTER>`_ program for mitigating local overfitting.
+
+.. image:: https://img.shields.io/pypi/v/scipion-em-sidesplitter.svg
+        :target: https://pypi.python.org/pypi/scipion-em-sidesplitter
+        :alt: PyPI release
+
+.. image:: https://img.shields.io/pypi/l/scipion-em-sidesplitter.svg
+        :target: https://pypi.python.org/pypi/scipion-em-sidesplitter
+        :alt: License
+
+.. image:: https://img.shields.io/pypi/pyversions/scipion-em-sidesplitter.svg
+        :target: https://pypi.python.org/pypi/scipion-em-sidesplitter
+        :alt: Supported Python versions
+
+.. image:: https://img.shields.io/sonar/quality_gate/scipion-em_scipion-em-sidesplitter?server=https%3A%2F%2Fsonarcloud.io
+        :target: https://sonarcloud.io/dashboard?id=scipion-em_scipion-em-sidesplitter
+        :alt: SonarCloud quality gate
+
+.. image:: https://img.shields.io/pypi/dm/scipion-em-sidesplitter
+        :target: https://pypi.python.org/pypi/scipion-em-sidesplitter
+        :alt: Downloads
+
+
++--------------+----------------+
+| prod: |prod| | devel: |devel| |
++--------------+----------------+
+
+.. |prod| image:: http://scipion-test.cnb.csic.es:9980/badges/sidesplitter_prod.svg
+.. |devel| image:: http://scipion-test.cnb.csic.es:9980/badges/sidesplitter_devel.svg
+
+
+Installation
+------------
+
+You will need to use `3.0 <https://github.com/I2PC/scipion/releases/tag/V3.0.0>`_ version of Scipion to be able to run these protocols. To install the plugin, you have two options:
+
+a) Stable version
+
+.. code-block::
+
+    scipion installp -p scipion-em-sidesplitter
+
+b) Developer's version
+
+    * download repository
+
+    .. code-block::
+
+        git clone https://github.com/scipion-em/scipion-em-sidesplitter.git
+
+    * install
+
+    .. code-block::
+
+        scipion installp -p path_to_scipion-em-sidesplitter --devel
+
+SIDESPLITTER sources will be installed automatically with the plugin, but you can also link an existing installation.
+
+    * Default installation path assumed is ``software/em/sidesplitter-1.2``, if you want to change it, set *SIDESPLITTER_HOME* in ``scipion.conf`` file pointing to the folder where the SIDESPLITTER is installed.
+
+To check the installation, simply run one of the following Scipion tests:
+
+.. code-block::
+
+   scipion test sidesplitter.tests.test_protocol_sidesplitter.TestSideSplitter
+
+A complete list of tests can also be seen by executing ``scipion test --show --grep sidesplitter``
+
+Supported versions
+------------------
+
+1.2
+
+Protocols
+---------
+
+* local filter
+
+References
+----------
+
+1. Kailash Ramlaul, Colin M. Palmer and Christopher H. S. Aylett (2020). Mitigating Local Over-fitting During Single Particle Reconstruction with SIDESPLITTER. https://www.biorxiv.org/content/10.1101/2019.12.12.874081v2
---
+++ packages/scipion-em-sidesplitter/scipion-em-sidesplitter-3.0.6.tar.gz/scipion-em-sidesplitter-3.0.6/sidesplitter/protocols/protocol_sidesplitter.py
@@ -0,0 +1,168 @@
+# **************************************************************************
+# *
+# * Authors:     Grigory Sharov (gsharov@mrc-lmb.cam.ac.uk)
+# *
+# * MRC Laboratory of Molecular Biology (MRC-LMB)
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import os
+
+import pyworkflow.protocol.params as params
+from pwem.protocols import ProtAnalysis3D
+from pwem.objects import Volume
+from pwem.emlib.image import ImageHandler
+
+from sidesplitter import Plugin
+from ..convert import convertMask
+
+
+class ProtSideSplitter(ProtAnalysis3D):
+    """
+    Protocol for mitigating local over-fitting by filtering.
+     
+    Find more information at https://github.com/StructuralBiology-ICLMedicine/SIDESPLITTER
+    """
+    _label = 'local filter'
+
+    def _createFilenameTemplates(self):
+        """ Centralize how files are called. """
+        myDict = {'half1': self._getExtraPath("half1_unfil.mrc"),
+                  'half2': self._getExtraPath("half2_unfil.mrc"),
+                  'mask': self._getExtraPath("mask.mrc"),
+                  'outHalf1Fn': self._getExtraPath('half1_unfil_sidesplitter.mrc'),
+                  'outHalf2Fn': self._getExtraPath('half1_unfil_sidesplitter.mrc'),
+                  }
+
+        self._updateFilenamesDict(myDict)
+
+    # --------------------------- DEFINE param functions ----------------------
+
+    def _defineParams(self, form):
+        form.addSection(label='Input')
+        form.addParam('protRefine', params.PointerParam,
+                      important=True,
+                      pointerClass="ProtRefine3D",
+                      label='Select a previous refinement protocol',
+                      help='Select any previous refinement protocol to get the '
+                           '3D half maps. Note that the refinement protocol '
+                           'must use gold-standard method.')
+        form.addParam('mask', params.PointerParam,
+                      allowsNull=True,
+                      pointerClass="VolumeMask",
+                      label='Volume mask',
+                      help="Provide the mask used in 3D refinement.")
+        form.addParam('doSNRWeighting', params.BooleanParam,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      default=False,
+                      label='Use SNR-weighted spectrum',
+                      help='Outputs the SNR weighted spectrum rather '
+                           'than matching input spectrum / grey-scale.')
+
+        form.addParallelSection(threads=1, mpi=0)
+
+    # --------------------------- INSERT steps functions ----------------------
+    
+    def _insertAllSteps(self):
+        self._createFilenameTemplates()
+        self._insertFunctionStep('convertInputStep')
+        self._insertFunctionStep('runSideSplitterStep')
+        self._insertFunctionStep('createOutputStep')
+
+    # --------------------------- STEPS functions -----------------------------
+    
+    def convertInputStep(self):
+        """ Convert input half-maps to mrc as expected by SIDESPLITTER."""
+        protRef = self.protRefine.get()
+        outVol = protRef.outputVolume
+        dim = outVol.getXDim()
+        vols = outVol.getHalfMaps().split(',')
+        ih = ImageHandler()
+
+        if self.mask.hasValue():
+            convertMask(self.mask.get(), self._getFileName('mask'), newDim=dim)
+
+        for vol, key in zip(vols, ['half1', 'half2']):
+            ih.convert(vol, self._getFileName(key))
+
+    def runSideSplitterStep(self):
+        """ Call SIDESPLITTER with the appropriate parameters. """
+        args = self._getArgs()
+        param = ' '.join(['%s %s' % (k, str(v)) for k, v in args.items()])
+        program = Plugin.getProgram()
+        cmd = 'export OMP_NUM_THREADS=%d; ' % self.numberOfThreads.get()
+        cmd += program
+
+        self.runJob(cmd, param, env=Plugin.getEnviron(),
+                    cwd=self._getExtraPath(),
+                    numberOfThreads=1)
+
+    def createOutputStep(self):
+        inputVol = self.protRefine.get().outputVolume
+        ps = inputVol.getSamplingRate()
+
+        vol = Volume()
+        vol.setSamplingRate(ps)
+        vol.setObjLabel('Filtered half-map 1')
+        vol.setFileName(self._getFileName('outHalf1Fn'))
+
+        vol2 = Volume()
+        vol2.setSamplingRate(ps)
+        vol2.setObjLabel('Filtered half-map 2')
+        vol2.setFileName(self._getFileName('outHalf2Fn'))
+
+        outputs = {'outputVolume1': vol,
+                   'outputVolume2': vol2}
+        self._defineOutputs(**outputs)
+        self._defineSourceRelation(inputVol, vol)
+        self._defineSourceRelation(inputVol, vol2)
+
+    # --------------------------- INFO functions ------------------------------
+    
+    def _summary(self):
+        summary = []
+
+        if hasattr(self, 'outputVolume1'):
+            summary.append("Created locally filtered half-maps.")
+        else:
+            summary.append("Output is not ready")
+
+        return summary
+    
+    def _validate(self):
+        errors = []
+
+        return errors
+    
+    # --------------------------- UTILS functions -----------------------------
+ 
+    def _getArgs(self):
+        """ Prepare the args dictionary."""
+        args = {'--v1': os.path.basename(self._getFileName('half1')),
+                '--v2': os.path.basename(self._getFileName('half2'))}
+
+        if self.mask.hasValue():
+            args['--mask'] = os.path.basename(self._getFileName('mask'))
+
+        if self.doSNRWeighting:
+            args['--spectrum'] = ' '
+
+        return args
