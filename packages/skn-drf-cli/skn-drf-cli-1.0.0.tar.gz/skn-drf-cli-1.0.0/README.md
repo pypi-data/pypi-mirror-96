2b4e8d5b87e20dea34e23ccd882bdec6af6d2e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:56 +0000

    Add package skn-drf-cli/1.0.0/skn-drf-cli-1.0.0.tar.gz

---
+++ packages/skn-drf-cli/skn-drf-cli-1.0.0.tar.gz/skn-drf-cli-1.0.0/README.md
@@ -0,0 +1,30 @@
+# Django Rest Framework CLI
+
+A usefull cli tool that creates and manages a Django Rest Framework project.
+
+## Contents
+
+-   [Installation](#Installation)
+-   [Commands](#commands)
+-   [License](#license)
+
+## Installation
+
+You can install this package by using pip:
+```
+pip install skn-drf-cli
+```
+
+## Commands
+
+### Init
+
+You can initialize a new starter project using:
+```
+drf-cli init your-project-name
+```
+This will create a starter project and you can build on top of that.
+
+## License
+
+Everything inside this repository is [MIT licensed](https://github.com/shywn-mrk/drf-cli/blob/main/LICENSE).
---
+++ packages/skn-drf-cli/skn-drf-cli-1.0.0.tar.gz/skn-drf-cli-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'click>=7.1,<7.2'
+]
+
+setuptools.setup(
+    name="skn-drf-cli",
+    version="1.0.0",
+    author="Shayan Karimi",
+    author_email="shayankarimi0078@gmail.com",
+    description="Django Rest Framework command-line tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shywn-mrk/drf-cli",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points= {
+        "console_scripts": [
+            "drf-cli = drf_cli.__main__:cli"
+        ]
+    }
+)
