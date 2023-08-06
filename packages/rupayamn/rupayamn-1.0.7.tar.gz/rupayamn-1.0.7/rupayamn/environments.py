d9ebedfda43aeeb668779ed06cdf195c3af12ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:36 +0000

    Add package rupayamn/1.0.7/rupayamn-1.0.7.tar.gz

---
+++ packages/rupayamn/rupayamn-1.0.7.tar.gz/rupayamn-1.0.7/rupayamn/environments.py
@@ -0,0 +1,57 @@
+environments = {
+    'mainnet': {
+        'rupaya': {
+            'BOOTNODES': (
+                'enode://576b502ff97838abf14433608bcdba5b36a708e465bc576e6c226'
+                'bfe088d110fd267a08a48965540f46fd0a99dc88a44f3d18ac48e94a08e4f'
+                'cac3a2378d799f@62.171.162.181:10707,enode://6be3ebc8e1305c60c'
+                '885ac35813179e58500ea6e2b6b2319c631cfde2c9fb7511c11ce65d5b73a'
+                '7113ad39c12035082c8c42ed5d33fb584a4db2bed35ac6ae23@62.171.167'
+                '.83:10707,enode://6b06a7b114e4bc3cf76e72d8ad7ad2b241a1afacd65'
+                '3e6a5080e128e75ffc821f6fcf90a0b5956f76c9435d1ed3ac2da7efc88e7'
+                'c8dd053663a1d2cb991e5741@161.97.93.239:10707'
+            ),
+            'NETSTATS_HOST': 'wss://stats.rupx.io',
+            'NETSTATS_PORT': '443',
+            'NETWORK_ID': '499',
+            'WS_SECRET': (
+                'imperial-snowmen-applauded'
+            )
+        },
+    },
+    'testnet': {
+        'rupaya': {
+            'BOOTNODES': (
+                'enode://ba966140e161ad416a7bd7c75dc695e0a41232723e2b19cbbf651'
+                '883ef5e8f2528801b17b9d63152814d219a58a4fcc3e3c877486e64057523'
+                'f6714092348efa@195.154.150.210:30301'
+            ),
+            'NETSTATS_HOST': 'stats.testnet.rupx.io',
+            'NETSTATS_PORT': '443',
+            'NETWORK_ID': '89',
+            'WS_SECRET': (
+                '9tlu4EymcTrEzaqWpSxh3KSa926au8'
+            )
+        },
+    },
+    'devnet': {
+        'rupaya': {
+            'BOOTNODES': (
+                'enode://5bec42d41c9eb291c1d20c9ac92bd9c86a4954189b6592b0833e5'
+                'c28e389b59e3992efed119a2782d9b95ba7aa78e7f71813067cd6734fadff'
+                '322f7dd6fc3b3c@104.248.99.234:30301,enode://89028bc15e9dda643'
+                'bc4b9a1a6352896dd3bce7411543b0b160a9eb95093ddbe1f5eda5999e38a'
+                '4874bfa6a00fb3526cc2fb9b4feb2a3f7cc80ef8016e05c493@104.248.99'
+                '.235:30301,enode://ea8f1eb1a2a695960bfa6df52094c635e173c65e5f'
+                'c120501672c0d21900d826d6c1c5a07d64ad36509ec5e7306d7a2c3398398'
+                'f34f3e279b91c487c2b3a9537@104.248.99.233:30301'
+            ),
+            'NETSTATS_HOST': 'stats.devnet.rupx.io',
+            'NETSTATS_PORT': '443',
+            'NETWORK_ID': '90',
+            'WS_SECRET': (
+                'torn-fcc-caper-drool-jelly-zip-din-fraud-rater-darn'
+            )
+        },
+    }
+}
