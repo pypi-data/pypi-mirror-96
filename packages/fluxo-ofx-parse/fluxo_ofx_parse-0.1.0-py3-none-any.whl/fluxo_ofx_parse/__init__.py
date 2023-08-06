5188edabdf7ac85b530328334e9231ccdc869049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:49 +0000

    Add package fluxo-ofx-parse/0.1.0/fluxo_ofx_parse-0.1.0-py3-none-any.whl

---
+++ packages/fluxo-ofx-parse/fluxo_ofx_parse-0.1.0-py3-none-any.whl/fluxo_ofx_parse/__init__.py
@@ -0,0 +1,11 @@
+from .ofxparse import (  # noqa F401
+    OfxParser,  # noqa F401
+    AccountType,  # noqa F401
+    Account,  # noqa F401
+    Statement,  # noqa F401
+    Transaction,  # noqa F401
+)  # noqa F401
+from .ofxerrors import *  # noqa F401
+from .ofxprinter import OfxPrinter  # noqa F401
+
+__version__ = "0.1.0"
