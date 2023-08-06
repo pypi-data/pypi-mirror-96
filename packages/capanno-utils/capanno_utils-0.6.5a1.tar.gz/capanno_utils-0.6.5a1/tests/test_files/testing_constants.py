79ec00a0bcc2438ac3edd170c93537437720620d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:50 +0000

    Add package capanno-utils/0.6.5a1/capanno_utils-0.6.5a1.tar.gz

---
+++ packages/capanno-utils/capanno_utils-0.6.5a1.tar.gz/capanno_utils-0.6.5a1/tests/test_files/testing_constants.py
@@ -0,0 +1,43 @@
+
+
+test_tool_parent_metadata_kwargs_1 = dict([
+            ('name', 'Test Parent Tool'),
+            ('softwareVersion', '1.test'),
+            ('identifier', 'TL_000000.00'),
+            ('featureList', ['subtool1', 'subtool2']),
+            ('metadataStatus', 'Released'),
+            ('description', 'Test parent 1'),
+            ('codeRepository', None),
+            ('license', None),
+            ('WebSite', None),
+            ('contactPoint', None),
+            ('publication', None),
+            ('keywords', None),
+            ('alternateName', None),
+            ('creator', None),
+            ('programmingLanguage', None),
+            ('datePublished', None),
+            ('downloadURL', None),
+            ('extra', None)
+])
+
+test_tool_parent_metadata_kwargs_2 = dict([
+            ('name', 'Test Parent Tool 2'),
+            ('softwareVersion', '1.test'),
+            ('identifier', 'TL_000000.00'),  # Repeated identifier
+            ('featureList', ['subtool1', 'subtool2']),
+            ('metadataStatus', 'Released'),
+            ('description', 'Test parent 1'),
+            ('codeRepository', None),
+            ('license', None),
+            ('WebSite', None),
+            ('contactPoint', None),
+            ('publication', None),
+            ('keywords', None),
+            ('alternateName', None),
+            ('creator', None),
+            ('programmingLanguage', None),
+            ('datePublished', None),
+            ('downloadURL', None),
+            ('extra', None)
+])
