4ab7e9f2f1a264548b77c0961120277bd593c1e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:33 +0000

    Add package lamp-core/2021.3.1/LAMP_core-2021.3.1.tar.gz

---
+++ packages/lamp-core/LAMP_core-2021.3.1.tar.gz/LAMP_core-2021.3.1/setup.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+"""
+    LAMP Platform
+
+    The LAMP Platform API.
+
+    The version of the OpenAPI document: 1.0.0
+    Contact: team@digitalpsych.org
+"""
+
+from setuptools import setup, find_packages
+
+__version__ = "2021.3.1"
+
+setup(
+    name="LAMP_core",
+    version=__version__,
+    description="LAMP Platform",
+    author="Division of Digital Psychiatry at Beth Israel Deaconess Medical Center.",
+    author_email="team@digitalpsych.org",
+    url="https://docs.lamp.digital/",
+    keywords=["LAMP Platform"],
+    install_requires=[
+      "urllib3 >= 1.15",
+      "six >= 1.10",
+      "certifi",
+      "python-dateutil",
+      "nulltype",
+      "numpy",
+      "pandas",
+    ],
+    extras_require={':python_version <= "2.7"': ['future']},
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description="""\
+    The LAMP Platform API.
+    """
+)
