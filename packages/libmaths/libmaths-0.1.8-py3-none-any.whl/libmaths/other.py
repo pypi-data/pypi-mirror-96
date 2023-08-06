6636eab2fc29c6d814ea7d3e3ef336cd87711d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:08 +0000

    Add package libmaths/0.1.8/libmaths-0.1.8-py3-none-any.whl

---
+++ packages/libmaths/libmaths-0.1.8-py3-none-any.whl/libmaths/other.py
@@ -0,0 +1,55 @@
+#Author : Vinay Venkatesh
+#Date : 2/20/2021
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def sigmoid(x):
+
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+
+  return np.maximum(0, x)
+
+def isPrime(*integers):
+  for value in integers:
+    if value > 1:
+    
+      for i in range(2, int((value ** 0.5))+1):
+    
+        if (value % i) == 0:
+          output = False
+          print(value, output)
+          break
+      else:
+        output = True
+        print(value, output)
+
+def isSquare(*integers):
+ 
+  #if x >= 0,
+  for value in integers: 
+    if value >= 0:
+      sr = math.sqrt(value)
+          
+      #return boolean T/F
+      if sr**2 == value and sr.is_integer():
+        output = True
+        print(value, output)
+      else:
+        output = False
+        print(value, output)
+
+def divisor(x):
+
+  count = 0
+
+  for i in range(1, x+1):
+    
+    if x % i == 0:
+      
+      count += 1
+
+  return(count)
