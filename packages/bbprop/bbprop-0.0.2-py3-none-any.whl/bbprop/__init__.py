7b4567b1bd9a1f5d9f822b979c4ef978d9169908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:01 +0000

    Add package bbprop/0.0.2/bbprop-0.0.2-py3-none-any.whl

---
+++ packages/bbprop/bbprop-0.0.2-py3-none-any.whl/bbprop/__init__.py
@@ -0,0 +1,12 @@
+"""difference of beta-binomial proportions
+
+Functions
+---------
+bbprop_cdf
+    cumulative distribution function for difference of beta-binomial proportions
+bbprop_test
+    hypothesis test for difference of beta-binomial proportions
+"""
+
+from bbprop.bbprop import bbprop_cdf
+from bbprop.bbprop import bbprop_test
