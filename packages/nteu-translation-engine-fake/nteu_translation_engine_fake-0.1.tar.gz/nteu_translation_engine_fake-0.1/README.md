4327398cc9f300265bddff60275b3b8186eaad56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:19 +0000

    Add package nteu-translation-engine-fake/0.1/nteu_translation_engine_fake-0.1.tar.gz

---
+++ packages/nteu-translation-engine-fake/nteu_translation_engine_fake-0.1.tar.gz/nteu_translation_engine_fake-0.1/README.md
@@ -0,0 +1,12 @@
+# nteu_translation_engine_fake
+
+
+```python
+from nteu_translation_engine_fake.nteu_translation_engine_fake import NTEUTranslationEngineFake
+
+
+NTEUTranslationEngineFake.run(
+    host='0.0.0.0',
+    port=5000
+)
+```
---
+++ packages/nteu-translation-engine-fake/nteu_translation_engine_fake-0.1.tar.gz/nteu_translation_engine_fake-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nteu_translation_engine_fake',
+    version='0.1',
+    description='NTEU engine',
+    url='https://github.com/Pangeamt/nteu_translation_engine_fake',
+    author='PangeaMT',
+    author_email='a.cerda@pangeanic.es',
+    license='MIT',
+    packages=['nteu_translation_engine_fake'],
+    zip_safe=False
+)
