8daebb078f84d7eaf11564328f4d34fbe145dab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:05 +0000

    Add package mgr/0.0.4/mgr-0.0.4.tar.gz

---
+++ packages/mgr/mgr-0.0.4.tar.gz/mgr-0.0.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+setup(
+  name = 'mgr',
+  packages = ['mgr'],
+  version = '0.0.4',
+  description = 'administration of modules and plugins ',
+  author = 'Flavio Lionel Rita',
+  author_email = 'flaviolrita@hotmail.com',
+  url = 'https://github.com/FlavioLionelRita/py-mgr', # use the URL to the github repo
+  download_url = 'https://github.com/FlavioLionelRita/py-mgr/tarball/0.0.5',
+  keywords = ['manager', 'plugin'],
+  classifiers = [],
+)
