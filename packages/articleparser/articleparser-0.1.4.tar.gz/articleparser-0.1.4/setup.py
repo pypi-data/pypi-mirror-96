48dd8ad0ceb4912544bf5155a90e591d7336e5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:07 +0000

    Add package articleparser/0.1.4/articleparser-0.1.4.tar.gz

---
+++ packages/articleparser/articleparser-0.1.4.tar.gz/articleparser-0.1.4/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+from articleparser.version import __version__
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="articleparser",
+    version=__version__,
+    author="Sean Lo",
+    author_email="seanlo96@hotmail.com",
+    description="Extracts structured data from web articles.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sean-lo/articleparser",
+    packages=setuptools.find_packages(
+        exclude=["output"],
+    ),
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires=">=3.8",
+)
+
+# To publish a new version on PyPI,
+# 1) check that your version number (in version.py) has been incremented
+# 2) check your requirements in requirements.txt have been updated
+# 3) verify that you are in a virtual environment with twine installed
+# 4) run "py setup.py sdist bdist_wheel"
+# 5) run "twine upload dist/*"
