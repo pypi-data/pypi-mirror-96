4232065198f3c6722ef88a17cb17460542be7ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:45 +0000

    Add package footil/0.24.0/footil-0.24.0.tar.gz

---
+++ packages/footil/footil-0.24.0.tar.gz/footil-0.24.0/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+from footil import __version__
+
+setup(
+    name='footil',
+    version=__version__,
+    packages=['footil', 'footil.tools'],
+    license='LGPLv3',
+    url='https://github.com/focusate/footil',
+    description="Various Python helpers for other projects",
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'yattag',
+        'python-dateutil',
+        'verboselogs',
+        'semver',
+        'natsort',
+    ],
+    maintainer='Andrius Laukavičius',
+    maintainer_email='dev@focusate.eu',
+    python_requires='>=3.5',
+    classifiers=[
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+    ],
+)
