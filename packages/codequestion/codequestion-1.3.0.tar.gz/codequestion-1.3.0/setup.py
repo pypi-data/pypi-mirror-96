690efcf1d7a036cece851419362b4cd79f138fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:17 +0000

    Add package codequestion/1.3.0/codequestion-1.3.0.tar.gz

---
+++ packages/codequestion/codequestion-1.3.0.tar.gz/codequestion-1.3.0/setup.py
@@ -0,0 +1,43 @@
+# pylint: disable = C0111
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    DESCRIPTION = f.read()
+
+setup(name="codequestion",
+      version="1.3.0",
+      author="NeuML",
+      description="Ask coding questions directly from the terminal",
+      long_description=DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url="https://github.com/neuml/codequestion",
+      project_urls={
+          "Documentation": "https://github.com/neuml/codequestion",
+          "Issue Tracker": "https://github.com/neuml/codequestion/issues",
+          "Source Code": "https://github.com/neuml/codequestion",
+      },
+      download_url="https://pypi.org/project/codequestion/",
+      license="Apache 2.0: http://www.apache.org/licenses/LICENSE-2.0",
+      packages=["codequestion"],
+      package_dir={"": "src/python/"},
+      keywords="python search embedding machine-learning",
+      python_requires=">=3.6",
+      entry_points={
+          "console_scripts": [
+              "codequestion = codequestion.shell:main",
+          ],
+      },
+      install_requires=[
+          "html2markdown>=0.1.7",
+          "mdv>=1.7.4",
+          "tqdm==4.48.0",
+          "txtai>=2.0.0"
+      ],
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "Topic :: Software Development",
+          "Topic :: Text Processing :: Indexing",
+          "Topic :: Utilities"
+      ])
