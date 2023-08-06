240108a36513ba66cfe64e20639538ce99973f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:49 +0000

    Add package zhinst/21.2.13366/zhinst-21.2.13366-cp36-cp36m-macosx_10_11_x86_64.whl

---
+++ packages/zhinst/zhinst-21.2.13366-cp36-cp36m-macosx_10_11_x86_64.whl/zhinst-21.2.13366.data/purelib/zhinst/examples/shfqa/__init__.py
@@ -0,0 +1,11 @@
+"""
+Zurich Instruments LabOne Python API Examples for the UHFQA Quantum Analyzer.
+"""
+
+import zhinst.examples.shfqa.example_pulsed_resonator
+import zhinst.examples.shfqa.example_resonator
+import zhinst.examples.shfqa.helper_resonator
+
+__all__ = ["example_pulsed_resonator", "example_resonator", "helper_resonator"]
+
+del zhinst
