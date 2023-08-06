a6d72adaa5b12a1a86c2736538c30f44e5f30835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:03 +0000

    Add package kili/2.38.0/kili-2.38.0-py2-none-any.whl

---
+++ packages/kili/kili-2.38.0-py2-none-any.whl/kili/mutations/label/__init__.py
@@ -0,0 +1,162 @@
+from json import dumps
+from typing import List
+
+from ...helpers import Compatible, format_result
+from .queries import (GQL_APPEND_TO_LABELS, GQL_CREATE_HONEYPOT,
+                      GQL_CREATE_PREDICTIONS,
+                      GQL_UPDATE_PROPERTIES_IN_LABEL)
+
+
+class MutationsLabel:
+
+    def __init__(self, auth):
+        """
+        Initializes the subclass
+
+        Parameters
+        ----------
+        - auth : KiliAuth object
+        """
+        self.auth = auth
+
+    @Compatible(['v1', 'v2'])
+    def create_predictions(self, project_id: str, external_id_array: List[str], model_name_array: List[str], json_response_array: List[dict]):
+        """
+        Create predictions for some assets
+
+        Parameters
+        ----------
+        - project_id : str
+        - external_id_array : list of str
+            The external identifiers of the assets for which we want to add predictions
+        - model_name_array : list of str
+            In case you want to precise from which model the label originated
+        - json_response_array : list of dict
+            The predictions are given here. An example can be found here, for a polygon
+            ```
+            {
+                "JOB_0": {
+                    "annotations": [
+                        {
+                            "categories": [{ "name": "OBJECT_B", "confidence": 100 }],
+                            "boundingPoly": [
+                                {
+                                    "normalizedVertices": [
+                                        { "x": 0.07, "y": 0.88 },
+                                        { "x": 0.07, "y": 0.32 },
+                                        { "x": 0.94, "y": 0.32 },
+                                        { "x": 0.94, "y": 0.88 }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+            ```
+
+        Returns
+        -------
+        - a result object which indicates if the mutation was successful, or an error message else.
+        """
+        assert len(external_id_array) == len(
+            json_response_array), "IDs list and predictions list should have the same length"
+        assert len(external_id_array) == len(
+            model_name_array), "IDs list and model names list should have the same length"
+        variables = {
+            'projectID': project_id,
+            'externalIDArray': external_id_array,
+            'modelNameArray': model_name_array,
+            'jsonResponseArray': [dumps(elem) for elem in json_response_array]
+        }
+        result = self.auth.client.execute(GQL_CREATE_PREDICTIONS, variables)
+        return format_result('data', result)
+
+    @Compatible(['v1', 'v2'])
+    def append_to_labels(self, author_id: str, json_response: dict, label_asset_id: str, label_type: str, seconds_to_label: int, skipped: bool = False):
+        """
+        Append a label to labels
+
+        Parameters
+        ----------
+        - author_id : str
+            Email of the author of the label
+        - json_response : dict
+            Label is given here
+        - label_asset_id : str
+            Identifier of the asset
+        - label_type : str
+            Can be one of {'AUTOSAVE', 'DEFAULT', 'PREDICTION', 'REVIEW'}
+        - seconds_to_label : int
+        - skipped : bool, optional (default = False)
+
+        Returns
+        -------
+        - a result object which indicates if the mutation was successful, or an error message else.
+        """
+        variables = {
+            'authorID': author_id,
+            'jsonResponse': dumps(json_response),
+            'labelAssetID': label_asset_id,
+            'labelType': label_type,
+            'secondsToLabel': seconds_to_label,
+            'skipped': skipped
+        }
+        result = self.auth.client.execute(GQL_APPEND_TO_LABELS, variables)
+        return format_result('data', result)
+
+    @Compatible(['v1', 'v2'])
+    def update_properties_in_label(self, label_id: str, seconds_to_label: int = None, model_name: str = None, json_response: dict = None):
+        """
+        Update properties of a label
+
+        Parameters
+        ----------
+        - label_id : str
+            Identifier of the label
+        - seconds_to_label : int, optional (default = None)
+        - model_name : str, optional (default = None)
+        - json_response : dict, optional (default = None)
+            The label is given here
+
+        Returns
+        -------
+        - a result object which indicates if the mutation was successful, or an error message else.
+        """
+        formatted_json_response = None if json_response is None else dumps(
+            json_response)
+        variables = {
+            'labelID': label_id,
+            'secondsToLabel': seconds_to_label,
+            'modelName': model_name,
+            'jsonResponse': formatted_json_response
+        }
+        result = self.auth.client.execute(
+            GQL_UPDATE_PROPERTIES_IN_LABEL, variables)
+        return format_result('data', result)
+
+    @Compatible(['v1', 'v2'])
+    def create_honeypot(self, asset_id: str, json_response: dict):
+        """
+        Create honeypot for an asset. 
+
+        Uses the json_response given to create a "REVIEW" label. This allows to compute a `honeypotMark`,
+        which measures how similar are other labels compared to this one.
+
+        Parameters
+        ----------
+        - asset_id : str
+            Identifier of the asset
+        - json_response : dict
+            The honeypot label of the asset
+
+        Returns
+        -------
+        - a result object which indicates if the mutation was successful, or an error message else.
+        """
+        variables = {
+            'assetID': asset_id,
+            'jsonResponse': dumps(json_response)
+        }
+        result = self.auth.client.execute(GQL_CREATE_HONEYPOT, variables)
+        return format_result('data', result)
---
+++ packages/kili/kili-2.38.0-py2-none-any.whl/kili/queries/organization/__init__.py
@@ -0,0 +1,86 @@
+import warnings
+
+from ...helpers import Compatible, deprecate, format_result, fragment_builder
+from .queries import gql_organizations, GQL_ORGANIZATIONS_COUNT
+from ...types import Organization
+
+
+class QueriesOrganization:
+
+    def __init__(self, auth):
+        """
+        Initializes the subclass
+
+        Parameters
+        ----------
+        - auth : KiliAuth object
+        """
+        self.auth = auth
+
+    @Compatible(['v1', 'v2'])
+    def organizations(self, email: str = None, organization_id: str = None, fields: list = ['id', 'name'], first: int = 100, skip: int = 0):
+        """
+        Get organizations
+
+        Returns users whose email / organization id correspond to the given ones.
+
+        Parameters
+        ----------
+        - email : str, optional (default = None)
+        - organization_id : str, optional (default = None)
+        - fields : list of string, optional (default = ['id', 'name'])
+            All the fields to request among the possible fields for the organizations, default for None are the non-calculated fields)
+            Possible fields : see https://cloud.kili-technology.com/docs/python-graphql-api/graphql-api/#organization
+        - first : int, optional (default = 100)
+            Maximum number of organizations to return
+        - skip : int, optional (default = 0)
+            Number of skipped organizations (they are ordered by creation date)
+
+        Returns
+        -------
+        - a result object which contains the query if it was successful, or an error message else.
+        """
+        variables = {
+            'first': first,
+            'skip': skip,
+            'where': {
+                'id': organization_id,
+                'user': {
+                    'email': email,
+                }
+            }
+        }
+        GQL_ORGANIZATIONS = gql_organizations(
+            fragment_builder(fields, Organization))
+        result = self.auth.client.execute(GQL_ORGANIZATIONS, variables)
+        return format_result('data', result)
+    
+    @Compatible(['v2'])
+    def count_organizations(self, email: str = None, organization_id: str = None):
+        """
+        Count organizations
+
+        Returns users whose email / organization id correspond to the given ones.
+
+        Parameters
+        ----------
+        - email : str, optional (default = None)
+        - organization_id : str, optional (default = None)
+        - fields : list of string, optional (default = ['id', 'name'])
+            All the fields to request among the possible fields for the organizations, default for None are the non-calculated fields)
+            Possible fields : see https://cloud.kili-technology.com/docs/python-graphql-api/graphql-api/#organization
+
+        Returns
+        -------
+        - a result object which contains the query if it was successful, or an error message else.
+        """
+        variables = {
+            'where': {
+                'id': organization_id,
+                'user': {
+                    'email': email,
+                }
+            }
+        }
+        result = self.auth.client.execute(GQL_ORGANIZATIONS_COUNT, variables)
+        return format_result('data', result)
