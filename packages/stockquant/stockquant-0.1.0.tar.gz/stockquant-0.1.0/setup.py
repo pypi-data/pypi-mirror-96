a2cac4f4da237c5e4825ece91e1aab9493c7ce63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:17 +0000

    Add package stockquant/0.1.0/stockquant-0.1.0.tar.gz

---
+++ packages/stockquant/stockquant-0.1.0.tar.gz/stockquant-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name="stockquant",
+    version="0.1.0",
+    packages=[
+        "stockquant",
+        "stockquant/source",
+        "stockquant/utils"
+    ],
+    platforms="any",
+    description="Professional quant framework",
+    url="https://github.com/Gary-Hertel/StockQuant",
+    author="Gary-Hertel",
+    author_email="hertelquant@foxmail.com",
+    license="MIT",
+    keywords=[
+        "stocklquant", "quant", "framework"
+    ],
+    install_requires=[
+        "numpy",
+        "requests",
+        "concurrent-log-handler",
+        "colorlog",
+        "pandas",
+        "matplotlib",
+        "tushare",
+        "baostock",
+        "finplot",
+        "easytrader"
+    ]
+)
