4c06e55a13ff125bf2d623296c1a0145219aea60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package b2bapi/1.1.0/b2bapi-1.1.0.tar.gz

---
+++ packages/b2bapi/b2bapi-1.1.0.tar.gz/b2bapi-1.1.0/b2bapi/wrapper.py
@@ -0,0 +1,37 @@
+import requests
+from time import sleep
+from .meme import Meme
+from .madlib import Madlib
+from .speedtext import Speedtext
+from .word import Word
+
+
+class BytesToBits:
+
+    def __init__(self) -> None:
+        self.base_url = 'https://api.bytestobits.dev'
+        return
+
+    def get_word(self) -> Word:
+        "Returns a random word from the API"
+        ret = requests.get(f'{self.base_url}/word/').json()
+        ret = Word(ret)
+        return ret
+
+    def get_speedtext(self) -> Speedtext:
+        "Returns a random paragraph from the API"
+        ret = requests.get(f'{self.base_url}/speedtext2/').json()
+        ret = Speedtext(ret)
+        return ret
+
+    def get_meme(self) -> Meme:
+        "Returns a random meme from a random subreddit through the API"
+        ret = requests.get(f'{self.base_url}/meme/').json()
+        ret = Meme(ret['title'], ret['url'], ret['link'], ret['subreddit'])
+        return ret
+
+    def get_madlib(self, cls =  Madlib): # return type is Any, needs a madlibs ABC for typing 
+        "Returns a randomadlib from the API"
+        ret = requests.get(f'{self.base_url}/madlibs/')
+        ret = cls(ret.text)
+        return ret
