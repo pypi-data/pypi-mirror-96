31bcfc9e1e8ae9b592cd44f746d9341917da8c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:39 +0000

    Add package scrapy-pyppeteer/0.0.15/scrapy-pyppeteer-0.0.15.tar.gz

---
+++ packages/scrapy-pyppeteer/scrapy-pyppeteer-0.0.15.tar.gz/scrapy-pyppeteer-0.0.15/scrapy_pyppeteer/page.py
@@ -0,0 +1,28 @@
+class PageCoroutine:
+    """
+    Represents a coroutine to be awaited on a Pyppeteer page,
+    such as "click", "screenshot" or "evaluate"
+    """
+
+    def __init__(self, method: str, *args, **kwargs) -> None:
+        self.method = method
+        self.args = args
+        self.kwargs = kwargs
+        self.result = None
+
+    def __str__(self):
+        return "<%s for method '%s'>" % (self.__class__.__name__, self.method)
+
+    __repr__ = __str__
+
+
+class NavigationPageCoroutine(PageCoroutine):
+    """
+    Same as PageCoroutine, but it waits for a navigation event. Use this when you know
+    a coroutine will trigger a navigation event, for instance when clicking on a link.
+
+    This forces a Page.waitForNavigation() call wrapped in asyncio.gather, as recommended in
+    https://miyakogi.github.io/pyppeteer/reference.html#pyppeteer.page.Page.click
+    """
+
+    pass
---
+++ packages/scrapy-pyppeteer/scrapy-pyppeteer-0.0.15.tar.gz/scrapy-pyppeteer-0.0.15/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+from scrapy_pyppeteer import __version__
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="scrapy-pyppeteer",
+    version=__version__,
+    license="BSD",
+    description="Pyppeteer integration for Scrapy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Eugenio Lacuesta",
+    author_email="eugenio.lacuesta@gmail.com",
+    url="https://github.com/elacuesta/scrapy-pyppeteer",
+    packages=["scrapy_pyppeteer"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Scrapy",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    install_requires=[
+        "scrapy>=2.0,!=2.4.0",
+        "pyppeteer>=0.0.23",
+    ],
+)
