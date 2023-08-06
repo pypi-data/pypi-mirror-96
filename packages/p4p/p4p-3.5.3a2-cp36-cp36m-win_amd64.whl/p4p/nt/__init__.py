9d5d97e3c69480562cbf334690e7328afa68a210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:17 +0000

    Add package p4p/3.5.3a2/p4p-3.5.3a2-cp36-cp36m-win_amd64.whl

---
+++ packages/p4p/p4p-3.5.3a2-cp36-cp36m-win_amd64.whl/p4p/nt/__init__.py
@@ -0,0 +1,290 @@
+
+import logging
+import sys
+_log = logging.getLogger(__name__)
+
+try:
+    from itertools import izip
+except ImportError:
+    izip = zip
+
+import time
+from collections import OrderedDict
+from operator import itemgetter
+from ..wrapper import Type, Value
+from .common import timeStamp, alarm
+from .scalar import NTScalar
+from .ndarray import NTNDArray
+from .enum import NTEnum
+
+__all__ = [
+    'NTScalar',
+    'NTEnum',
+    'NTMultiChannel',
+    'NTTable',
+    'NTNDArray',
+    'defaultNT',
+]
+
+_default_nt = {
+    "epics:nt/NTScalar:1.0": NTScalar,
+    "epics:nt/NTScalarArray:1.0": NTScalar,
+    "epics:nt/NTEnum:1.0": NTEnum,
+    "epics:nt/NTNDArray:1.0": NTNDArray,
+}
+
+def defaultNT():
+    """Returns a copy of the default NT helper mappings.
+
+    :since: 3.1.0
+    """
+    return _default_nt.copy()
+
+class UnwrapOnly(object):
+    def __init__(self, unwrap):
+        self.unwrap = unwrap
+    def __call__(self):
+        return self # we are state-less
+    def wrap(self, V):
+        return V
+
+def buildNT(nt=None, unwrap=None):
+    if unwrap is False or nt is False:
+        return ClientUnwrapper({}) # disable use of wrappers
+
+    if unwrap is not None:
+        # legacy
+        ret = {} # ignore new style
+        for ID,fn in (unwrap or {}).items():
+            ret[ID] = UnwrapOnly(fn)
+
+    else:
+        ret = dict(_default_nt)
+        ret.update(nt or {})
+
+    return ClientUnwrapper(ret)
+
+class ClientUnwrapper(object):
+    def __init__(self, nt=None):
+        self.nt = nt
+        self.id = None
+        self._wrap = self._unwrap = lambda x:x
+        self._assign = self._default_assign
+    def wrap(self, val):
+        """Pack a arbitrary python object into a Value
+        """
+        return self._wrap(val)
+    def unwrap(self, val):
+        """Unpack a Value as some other python type
+        """
+        if val.getID()!=self.id:
+            self._update(val)
+        return self._unwrap(val)
+
+    def assign(self, V, value):
+        if V.getID()!=self.id:
+            self._update(V)
+        self._assign(V, value)
+
+    def _update(self, val):
+        # type change
+        nt = self.nt.get(val.getID())
+        if nt is not None:
+            nt = nt() # instancate
+            self._wrap, self._unwrap = nt.wrap, nt.unwrap
+            self._assign = nt.assign
+            self.id = val.getID()
+        else:
+            # reset
+            self._wrap = self._unwrap = lambda x:x
+            self._assign = self._default_assign
+
+    def _default_assign(self, V, value):
+        V.value = value # assume NTScalar-like
+
+    def __repr__(self):
+        return '%s(%s)'%(self.__class__.__name__, repr(self.nt))
+    __str__ = __repr__
+
+class NTMultiChannel(object):
+
+    """Describes a structure holding the equivalent of a number of NTScalar
+    """
+    Value = Value
+
+    @staticmethod
+    def buildType(valtype, extra=[]):
+        """Build a Type
+
+        :param str valtype: A type code to be used with the 'value' field.  Must be an array
+        :param list extra: A list of tuples describing additional non-standard fields
+        :returns: A :py:class:`Type`
+        """
+        assert valtype[:1] == 'a', 'valtype must be an array'
+        return Type(id="epics:nt/NTMultiChannel:1.0",
+                    spec=[
+                    ('value', valtype),
+                    ('channelName', 'as'),
+                    ('descriptor', 's'),
+                    ('alarm', alarm),
+                    ('timeStamp', timeStamp),
+                    ('severity', 'ai'),
+                    ('status', 'ai'),
+                    ('message', 'as'),
+                    ('secondsPastEpoch', 'al'),
+                    ('nanoseconds', 'ai'),
+                    ('userTag', 'ai'),
+                    ('isConnected', 'a?'),
+                    ] + extra)
+
+
+class NTTable(object):
+
+    """A generic table
+
+    >>> table = NTTable.buildType(columns=[
+        ('columnA', 'ai'),
+        ('columnB', 'as'),
+    ])
+    """
+    Value = Value
+
+    @staticmethod
+    def buildType(columns=[], extra=[]):
+        """Build a table
+
+        :param list columns: List of column names and types. eg [('colA', 'd')]
+        :param list extra: A list of tuples describing additional non-standard fields
+        :returns: A :py:class:`Type`
+        """
+        return Type(id="epics:nt/NTTable:1.0",
+                    spec=[
+                    ('labels', 'as'),
+                    ('value', ('S', None, columns)),
+                    ('descriptor', 's'),
+                    ('alarm', alarm),
+                    ('timeStamp', timeStamp),
+                    ] + extra)
+
+    def __init__(self, columns=[], extra=[]):
+        self.labels = []
+        C = []
+        for col, type in columns:
+            if type[0] == 'a':
+                raise ValueError("NTTable column types may not be array")
+            C.append((col, 'a' + type))
+            self.labels.append(col)
+        self.type = self.buildType(C, extra=extra)
+
+    def wrap(self, values):
+        """Pack an iterable of dict into a Value
+
+        >>> T=NTTable([('A', 'ai'), ('B', 'as')])
+        >>> V = T.wrap([
+            {'A':42, 'B':'one'},
+            {'A':43, 'B':'two'},
+        ])
+        """
+        if isinstance(values, Value):
+            return values
+        cols = dict([(L, []) for L in self.labels])
+        try:
+            # unzip list of dict
+            for V in values:
+                for L in self.labels:
+                    try:
+                        cols[L].append(V[L])
+                    except (IndexError, KeyError):
+                        pass
+            # allow omit empty columns
+            for L in self.labels:
+                V = cols[L]
+                if len(V) == 0:
+                    del cols[L]
+
+            try:
+                return self.Value(self.type, {
+                    'labels': self.labels,
+                    'value': cols,
+                })
+            except:
+                _log.error("Failed to encode '%s' with %s", cols, self.labels)
+                raise
+        except:
+            _log.exception("Failed to wrap: %s", values)
+            raise
+
+    @staticmethod
+    def unwrap(value):
+        """Iterate an NTTable
+
+        :returns: An iterator yielding an OrderedDict for each column
+        """
+        ret = []
+
+        # build lists of column names, and value
+        lbl, cols = [], []
+        for cname, cval in value.value.items():
+            lbl.append(cname)
+            cols.append(cval)
+
+        # zip together column arrays to iterate over rows
+        for rval in izip(*cols):
+            # zip together column names and row values
+            ret.append(OrderedDict(zip(lbl, rval)))
+
+        return ret
+
+class NTURI(object):
+
+    @staticmethod
+    def buildType(args):
+        """Build NTURI
+
+        :param list args: A list of tuples of query argument name and PVD type code.
+
+        >>> I = NTURI([
+            ('arg_a', 'I'),
+            ('arg_two', 's'),
+        ])
+        """
+        try:
+            return Type(id="epics:nt/NTURI:1.0", spec=[
+                ('scheme', 's'),
+                ('authority', 's'),
+                ('path', 's'),
+                ('query', ('S', None, args)),
+            ])
+        except Exception as e:
+            raise ValueError('Unable to build NTURI compatible type from %s' % args)
+
+    def __init__(self, args):
+        self._args = list(args)
+        self.type = self.buildType(args)
+
+    def wrap(self, path, args=(), kws={}, scheme='', authority=''):
+        """Wrap argument values (tuple/list with optional dict) into Value
+
+        :param str path: The PV name to which this call is made
+        :param tuple args: Ordered arguments
+        :param dict kws: Keyword arguments
+        :rtype: Value
+        """
+        # build dict of argument name+value
+        AV = {}
+        AV.update([A for A in kws.items() if A[1] is not None])
+        AV.update([(N, V) for (N, _T), V in zip(self._args, args)])
+
+        # list of argument name+type tuples for which a value was provided
+        AT = [A for A in self._args if A[0] in AV]
+
+        T = self.buildType(AT)
+        try:
+            return Value(T, {
+                'scheme': scheme,
+                'authority': authority,
+                'path': path,
+                'query': AV,
+            })
+        except Exception as e:
+            raise ValueError('Unable to initialize NTURI %s from %s using %s' % (AT, AV, self._args))
---
+++ packages/p4p/p4p-3.5.3a2-cp36-cp36m-win_amd64.whl/p4p/server/__init__.py
@@ -0,0 +1,221 @@
+import sys
+import logging
+import warnings
+import re
+import time
+import uuid
+
+from weakref import WeakSet
+
+from .._p4p import (Server as _Server,
+                    installProvider,
+                    removeProvider,
+                    clearProviders,
+                    StaticProvider as _StaticProvider,
+                    DynamicProvider as _DynamicProvider,
+                    ServerOperation,
+                    )
+
+if sys.version_info >= (3, 0):
+    unicode = str
+
+_log = logging.getLogger(__name__)
+
+__all__ = (
+    'Server',
+        'installProvider',
+        'removeProvider',
+        'StaticProvider',
+        'DynamicProvider',
+        'ServerOperation',
+)
+
+class Server(object):
+
+    """Server(conf=None, useenv=True, providers=[""])
+
+    :param providers: A list of provider names or instances.  See below.
+    :param dict conf: Configuration keys for the server.  Uses same names as environment variables (aka. EPICS_PVAS_*)
+    :param bool useenv: Whether to use process environment in addition to provided config.
+    :param bool isolate: If True, override conf= and useenv= to select a configuration suitable for isolated testing.
+                         eg. listening only on localhost with a randomly chosen port number.  Use `conf()` to determine
+                         which port is being used.
+
+    Run a PVAccess server serving Channels from the listed providers.
+    The server is running after construction, until stop(). ::
+
+        S = Server(providers=["example"])
+        # do something else
+        S.stop()
+
+    As a convenience, a Server may be used as a context manager to automatically `stop()`. ::
+
+        with Server(providers=["example"]) as S:
+            # do something else
+
+    When configuring a Server, conf keys provided to the constructor have the same name as the environment variables.
+    If both are given, then the provided conf dict is used.
+
+    Call Server.conf() to see a list of valid server (EPICS_PVAS_*) key names and the actual values.
+
+    The providers list must be a list of name strings (cf. installProvider()),
+    a list of `StaticProvider` or `DynamicProvider` instances, or dict "{'pv:name':`SharedPV`}" to implicitly creat a `StaticProvider`.
+    """
+
+    def __init__(self, providers, isolate=False, **kws):
+        self.__keep_alive = [] # ick...
+
+        if isinstance(providers, (bytes, unicode)):
+            providers = providers.split() # split on space
+            warnings.warn("Server providers list should be a list", DeprecationWarning)
+
+        Ps = []
+        for provider in providers:
+            if isinstance(provider, (bytes, unicode)):
+                if not re.match(r'^[^ \t\n\r]+$', provider):
+                    raise ValueError("Invalid provider name: '%s'"%provider)
+                Ps.append(provider)
+
+            elif isinstance(provider, (_StaticProvider, _DynamicProvider)):
+                Ps.append(provider)
+
+            elif hasattr(provider, 'items'):
+                P = StaticProvider()
+                for name, pv in provider.items():
+                    P.add(name, pv)
+                Ps.append(P)
+                # Normally user code is responsible for keeping the StaticProvider alive.
+                # Not possible in this case though.
+                self.__keep_alive.append(P)
+
+            else:
+                raise ValueError("providers=[] must be a list of string, SharedPV, or dict.  Not %s"%provider)
+
+        if isolate:
+            assert 'useenv' not in kws and 'conf' not in kws, kws
+            kws['useenv'] = False
+            kws['conf'] = {
+                'EPICS_PVAS_INTF_ADDR_LIST': '127.0.0.1',
+                'EPICS_PVA_ADDR_LIST': '127.0.0.1',
+                'EPICS_PVA_AUTO_ADDR_LIST': '0',
+                'EPICS_PVA_SERVER_PORT': '0',
+                'EPICS_PVA_BROADCAST_PORT': '0',
+            }
+        _log.debug("Starting Server isolated=%s, %s", isolate, kws)
+        self._S = _Server(providers=Ps, **kws)
+
+        _all_servers.add(self._S)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, A, B, C):
+        self.stop()
+
+    def conf(self):
+        """Return a dict() with the effective configuration this server is using.
+
+        Suitable to pass to another Server to duplicate this configuration,
+        or to a client Context to allow it to connect to this server. ::
+
+            with Server(providers=["..."], isolate=True) as S:
+                with p4p.client.thread.Context('pva', conf=S.conf(), useenv=False) as C:
+                    print(C.get("pv:name"))
+        """
+        return self._S.conf()
+
+    def stop(self):
+        """Force server to stop serving, and close connections to existing clients.
+        """
+        _log.debug("Stopping Server")
+        self._S.stop()
+        self.__keep_alive = []
+
+    @classmethod
+    def forever(klass, *args, **kws):
+        """Create a server and block the calling thread until KeyboardInterrupt.
+        Shorthand for: ::
+
+            with Server(*args, **kws):
+                try;
+                    time.sleep(99999999)
+                except KeyboardInterrupt:
+                    pass
+        """
+        with klass(*args, **kws):
+            _log.info("Running server")
+            try:
+                while True:
+                    time.sleep(100)
+            except KeyboardInterrupt:
+                pass
+            finally:
+                _log.info("Stopping server")
+
+
+class StaticProvider(_StaticProvider):
+
+    """A channel provider which servers from a clearly defined list of names.
+    This list may change at any time.
+
+    :param str name: Provider name.  Must be unique within the local context in which it is used.
+                     None, the default, will choose an appropriate value.
+    """
+    def __init__(self, name=None):
+        if name is None:
+            # Caller doesn't care.  Pick something unique w/o spaces
+            name = str(uuid.uuid4())
+        super(StaticProvider, self).__init__(name)
+
+class DynamicProvider(_DynamicProvider):
+
+    """A channel provider which does not maintain a list of provided channel names.
+
+       The following example shows a simple case, in fact so simple that StaticProvider
+       is a better fit. ::
+
+            class DynHandler(object):
+                def __init__(self):
+                    self.pv = SharedPV()
+                def testChannel(self, name): # return True, False, or DynamicProvider.NotYet
+                    return name=="blah"
+                def makeChannel(self, name, peer):
+                    assert name=="blah"
+                    return self.pv
+            provider = DynamicProvider("arbitrary", DynHandler())
+            server = Server(providers=[provider])
+    """
+
+    # Return from Handler.testChannel() to prevent caching of negative result.
+    # Use when testChannel('name') might shortly return True
+    NotYet = b'nocache'
+
+    def __init__(self, name, handler):
+        _DynamicProvider.__init__(self, name, self._WrapHandler(handler))
+
+    class _WrapHandler(object):
+
+        "Wrapper around user Handler which logs exception"
+
+        def __init__(self, real):
+            self._real = real
+
+        def testChannel(self, name):
+            try:
+                return self._real.testChannel(name)
+            except:
+                _log.exception("Unexpected")
+
+        def makeChannel(self, name, peer):
+            try:
+                return self._real.makeChannel(name, peer)
+            except:
+                _log.exception("Unexpected")
+
+_all_servers = WeakSet()
+
+def _cleanup_servers():
+    _log.debug("Stopping all Server instances")
+    servers = list(_all_servers)
+    for srv in servers:
+        srv.stop()
