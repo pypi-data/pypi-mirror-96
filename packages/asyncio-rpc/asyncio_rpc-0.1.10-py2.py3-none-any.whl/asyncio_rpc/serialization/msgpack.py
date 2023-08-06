5954aa8eba1d2c9cd8789059f9f47aa98dfd0231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:21 +0000

    Add package asyncio-rpc/0.1.10/asyncio_rpc-0.1.10-py2.py3-none-any.whl

---
+++ packages/asyncio-rpc/asyncio_rpc-0.1.10-py2.py3-none-any.whl/asyncio_rpc/serialization/msgpack.py
@@ -0,0 +1,263 @@
+import dataclasses
+import msgpack
+import numpy as np
+from abc import ABC, abstractmethod
+from io import BytesIO
+from datetime import datetime
+from lz4.frame import compress as lz4_compress, decompress as lz4_decompress
+from typing import Any
+
+# Maximum byte lengths for str/ext
+MAX_STR_LEN = 2147483647
+MAX_EXT_LEN = 2147483647
+
+
+# Internal registry
+# TODO: figure out if it is ok to do
+# this on the module...
+REGISTRY = {'obj_types': {},
+            'ext_types': {},
+            'serializables': {}}
+
+
+def register(obj_def):
+    """
+    Register dataclasses or custom handlers in the registry.
+
+    For example obj_def and required methods, see NumpyArray below
+    """
+    if dataclasses.is_dataclass(obj_def):
+        # Handle dataclasses, every dataclass needs to be registered
+        # via register.
+        class_name = obj_def.__name__
+        REGISTRY['serializables'][class_name] = obj_def
+        REGISTRY['obj_types'][obj_def] = DataclassHandler
+
+        # Register the DataclassHandler if not done already
+        if DataclassHandler.ext_type not in REGISTRY['ext_types']:
+            REGISTRY['ext_types'][
+                DataclassHandler.ext_type] = DataclassHandler
+    else:
+        # Assume the obj_def has obj_type and ext_type, as can be
+        # seen below.
+        assert hasattr(obj_def, 'obj_type') and hasattr(obj_def, 'ext_type')
+        REGISTRY['obj_types'][obj_def.obj_type] = obj_def
+        REGISTRY['ext_types'][obj_def.ext_type] = obj_def
+
+
+class AbstractHandler(ABC):
+    ext_type: int = None  # Unique number
+    obj_type: Any = None  # Unique object type
+
+    @classmethod
+    @abstractmethod
+    def packb(cls, instance: Any) -> bytes:
+        """
+        Pack the instance into bytes
+        """
+
+    @classmethod
+    @abstractmethod
+    def unpackb(cls, data: bytes) -> Any:
+        """
+        Unpack the data back into an instance
+        """
+
+
+class NumpyArrayHandler(AbstractHandler):
+    """
+    Use np.save and np.load to serialize/deserialize
+    numpy array's.
+    """
+    ext_type = 1
+    obj_type = np.ndarray
+
+    # Note:
+    # More generic approach, but a bit slower than
+    # packing it as a list/tuple with (dtype, shape, bytes)
+    @classmethod
+    def packb(cls, array: np.ndarray) -> bytes:
+        buf = BytesIO()
+        np.save(buf, array)
+        buf.seek(0)
+        return buf.read()
+
+    @classmethod
+    def unpackb(cls, data: bytes) -> np.ndarray:
+        buf = BytesIO(data)
+        buf.seek(0)
+        return np.load(buf)
+
+
+class NumpyStructuredArrayHandler(NumpyArrayHandler):
+    ext_type = 2
+    obj_type = np.void  # = the type of structured array's...
+
+
+class DatetimeHandler:
+    """
+    Serialize datetime instances as timestamps.
+    """
+    ext_type = 3
+    obj_type = datetime
+
+    @classmethod
+    def packb(cls, dt: datetime) -> bytes:
+        return b'%f' % dt.timestamp()
+
+    @classmethod
+    def unpackb(cls, data: bytes) -> datetime:
+        return datetime.fromtimestamp(float(data))
+
+
+class DataclassHandler:
+    """
+    Serialize dataclasses by serializing the .__dict__
+    of dataclasses. This allows recursively serialization for example:
+    dataclasses in dataclasses or Numpy array's in dataclasses.
+    """
+    ext_type = 4
+
+    @classmethod
+    def packb(cls, obj) -> bytes:
+        dataclass_name = obj.__class__.__name__
+        if isinstance(dataclass_name, str):
+            dataclass_name = dataclass_name
+
+        # Recursively process dataclasses of the dataclass,
+        # serialize as tuple(dataclass_name, __dict__)
+        return dumpb(
+            (dataclass_name, obj.__dict__),
+            do_compress=False)
+
+    @classmethod
+    def unpackb(cls, data):
+        # Recursively process the contents of the dataclass
+        classname, data = loadb(
+            data, do_decompress=False, raw=False)
+        # Return registered class or Serializable (as default)
+        assert classname in REGISTRY['serializables'], \
+            f'class {classname} not yet registered'
+        klass = REGISTRY['serializables'][classname]
+        return klass(**data)
+
+
+class SliceHandler:
+    """
+    Serialize slices
+    """
+    ext_type = 5
+    obj_type = slice
+
+    @classmethod
+    def packb(cls, obj) -> bytes:
+        return dumpb((obj.start, obj.stop, obj.step))
+
+    @classmethod
+    def unpackb(cls, data):
+        return slice(*loadb(data))
+
+
+class NumpyInt32Handler(AbstractHandler):
+    """
+    Serialize np.int32
+    """
+
+    ext_type = 6
+    obj_type = np.int32
+
+    @classmethod
+    def packb(cls, data: np.int32) -> bytes:
+        return data.tobytes()
+
+    @classmethod
+    def unpackb(cls, data: bytes) -> np.int32:
+        return np.frombuffer(data, dtype=np.int32)[0]
+
+
+class NumpyInt64Handler(AbstractHandler):
+    """
+    Serialize np.int64
+    """
+
+    ext_type = 7
+    obj_type = np.int64
+
+    @classmethod
+    def packb(cls, data: np.int64) -> bytes:
+        return data.tobytes()
+
+    @classmethod
+    def unpackb(cls, data: bytes) -> np.int64:
+        return np.frombuffer(data, dtype=np.int64)[0]
+
+
+# Register custom handlers
+register(NumpyArrayHandler)
+register(NumpyStructuredArrayHandler)
+register(DatetimeHandler)
+register(SliceHandler)
+register(NumpyInt32Handler)
+register(NumpyInt64Handler)
+
+
+def default(obj: Any):
+    """
+    Serialize (dumpb) hook for obj types that msgpack does not
+    process out of the box.
+    """
+    if type(obj) in REGISTRY['obj_types']:
+        # If the type is in the registry, use the
+        # handler to serialize the obj
+        handler = REGISTRY['obj_types'][type(obj)]
+        return msgpack.ExtType(
+            handler.ext_type, handler.packb(obj))
+
+    raise TypeError("Unknown type: %r" % (obj,))
+
+
+def ext_hook(ext_type: int, bytes_data: bytes):
+    """
+    Deserialize (loadb) hook for ext_types that are
+    not default in msgpack.
+
+    ext_types are user defined numbers for special
+    deserialization handling.
+    """
+    if ext_type in REGISTRY['ext_types']:
+        # If the ext_type is in the registry, use the
+        # handler to deserialize the bytes_data
+        handler = REGISTRY['ext_types'][ext_type]
+        return handler.unpackb(bytes_data)
+
+    raise TypeError("Unknown ext_type: %r" % (ext_type,))  # pragma: no cover
+
+
+def do_nothing(x):
+    return x
+
+
+def dumpb(instance: Any, do_compress=True, compress_func=lz4_compress,
+          use_bin_type=True):
+    """
+    Dump/pack instance with msgpack to bytes
+    """
+    if not do_compress:
+        compress_func = do_nothing
+    return compress_func(msgpack.packb(
+        instance, default=default, use_bin_type=use_bin_type))
+
+
+def loadb(packed: bytes, do_decompress=True, decompress_func=lz4_decompress,
+          raw=False):
+    """
+    Load/unpack bytes back to instance
+    """
+    if packed is None:
+        return None
+    if not do_decompress:
+        decompress_func = do_nothing
+    return msgpack.unpackb(
+        decompress_func(packed), ext_hook=ext_hook,
+        max_ext_len=MAX_EXT_LEN,
+        max_str_len=MAX_STR_LEN, raw=raw)
---
+++ packages/asyncio-rpc/asyncio_rpc-0.1.10-py2.py3-none-any.whl/asyncio_rpc/server.py
@@ -0,0 +1,293 @@
+import asyncio
+import logging
+from typing import List
+from asyncio_rpc.models import (
+    RPCStack, RPCResult, RPCException, RPCCall,
+    RPCSubStack, RPCUnSubStack)
+from asyncio_rpc.commlayers.base import AbstractRPCCommLayer
+from asyncio_rpc.pubsub import Publisher
+logger = logging.getLogger("asyncio-rpc-server")
+
+
+class NamespaceError(Exception):
+    """
+    Exception raised when a namespace unknown or
+    already present.
+    """
+
+
+class RPCServer(object):
+    """
+    Remote procedure server class. Allows to register executors
+    by namespace and execute RPC calls from a RPCClient
+    """
+    def __init__(self, rpc_commlayer: AbstractRPCCommLayer = None):
+        assert isinstance(rpc_commlayer, AbstractRPCCommLayer)
+        self.queue = asyncio.Queue()
+        self._alive = True
+
+        # Allow multiple executors to be registered by
+        # namespace
+        self.registry = {}
+        self.publishers = {}
+        self.rpc_commlayer = rpc_commlayer
+        logger.debug("Initialized RPCServer")
+
+    def register_models(self, models):
+        """
+        Register all given models to the rpc_commlayer serialization
+
+        Intended usage is to register dataclasses
+        """
+        for model in models:
+            self.rpc_commlayer.serialization.register(model)
+
+    def register(self, executor):
+        """
+        Register an executor for a namespace, the namespace
+        should be unique and is used to route RPC calls from
+        the client to the correct executor in the registry of
+        the RPCServer.
+        """
+        assert hasattr(executor, 'namespace')
+
+        if executor.namespace in self.registry:
+            raise NamespaceError("Namespace already exists")
+
+        # Register executor for this namespaces
+        self.registry[executor.namespace] = executor
+
+    async def rpc_call(self, rpc_func_stack: RPCStack):
+        """
+        Incoming rpc_call, execute it via the registered
+        executor and return the result or exception
+        """
+        assert isinstance(rpc_func_stack, RPCStack)
+        logger.debug(
+            "Received RPC call rpc_func_stack %s, %s",
+            rpc_func_stack.uid, rpc_func_stack)
+
+        # Create a default result
+        result = RPCResult(
+            uid=rpc_func_stack.uid,
+            namespace=rpc_func_stack.namespace,
+            data=None)
+
+        if rpc_func_stack.namespace not in self.registry:
+            logger.debug(
+                "Unknown namespace for rpc_func_stack: %s", rpc_func_stack.uid
+            )
+            raise NamespaceError("Unknown namespace")
+
+        executor = self.registry[rpc_func_stack.namespace]
+
+        try:
+            # Wait for result from executor
+            logger.debug(
+                "Going to run executor for rpc_func_stack: %s",
+                rpc_func_stack.uid
+            )
+            result.data = await asyncio.wait_for(
+                executor.rpc_call(rpc_func_stack.stack),
+                timeout=rpc_func_stack.timeout)
+            logger.debug(
+                "Got result for rpc_func_stack: %s, %s",
+                rpc_func_stack.uid, result
+            )
+        except Exception as e:
+            # For now catch all exceptions here...
+            # TODO: debug mode with stacktrace
+            result = RPCException(
+                uid=rpc_func_stack.uid,
+                namespace=rpc_func_stack.namespace,
+                classname=e.__class__.__name__,
+                exc_args=e.args)
+            logger.debug(
+                "Exception occurred for rpc_funct_stack: %s, %s",
+                rpc_func_stack.uid, e
+            )
+
+        return result
+
+    async def subscribe_call(self, rpc_sub_stack: RPCSubStack):
+        assert isinstance(rpc_sub_stack, RPCSubStack)
+        if rpc_sub_stack.namespace not in self.registry:
+            raise NamespaceError("Unknown namespace")
+
+        executor = self.registry[rpc_sub_stack.namespace]
+        if not hasattr(executor, 'subscribe_call'):
+            raise NotImplementedError(
+                f"Executor for namespace: {rpc_sub_stack.namespace} has "
+                f"no subscribe_call function")
+
+        publisher = Publisher(self, rpc_sub_stack)
+        self.publishers[rpc_sub_stack.uid] = publisher
+
+        # Create task for this publisher
+        asyncio.create_task(
+            executor.subscribe_call(publisher))
+
+    async def _on_rpc_event(
+            self, rpc_func_stack: RPCStack, channel: bytes = None):
+        """
+        Callback function sent to rpc_commlayer, is called
+        when a RPCStack is received by the rpc_commlayer subscription
+        """
+        await self.queue.put((rpc_func_stack, channel))
+
+    async def _process_queue(self):
+        """
+        Background queue processing function, processes
+        the internal self.queue until b'END' is received
+
+
+        if on_rpc_message has been set, it will be called
+        whenever a RPCMessage is popped from the queue.
+        """
+
+        while self._alive:
+            item = await self.queue.get()
+
+            if item == b'END':
+                break
+
+            rpc_func_stack, channel = item
+
+            assert isinstance(rpc_func_stack, RPCStack)
+
+            logger.debug(
+                "Processing rpcstack %s, %s",
+                rpc_func_stack.uid, rpc_func_stack
+            )
+
+            if isinstance(rpc_func_stack, RPCSubStack):
+                try:
+                    # Process rpc_func_call_stack
+                    await self.subscribe_call(rpc_func_stack)
+                except Exception as e:
+                    # Log everything that is not an
+                    # instance of RPCException
+                    if not isinstance(e, RPCException):
+                        logger.exception(e)
+                    result = RPCException(
+                        uid=rpc_func_stack.uid,
+                        namespace=rpc_func_stack.namespace,
+                        classname=e.__class__.__name__,
+                        exc_args=e.args)
+                    # Publish exception
+                    await self.rpc_commlayer.publish(
+                        result, channel=rpc_func_stack.respond_to)
+            elif isinstance(rpc_func_stack, RPCUnSubStack):
+                publisher = self.publishers.pop(rpc_func_stack.uid, None)
+                if publisher is not None:
+                    publisher.set_is_active(False)
+            else:
+                try:
+                    # Process rpc_func_call_stack
+                    result = await self.rpc_call(rpc_func_stack)
+
+                    logger.debug(
+                        "Publishing result for %s, %s",
+                        rpc_func_stack.uid, rpc_func_stack)
+                    # Publish result of rpc call
+                    await self.rpc_commlayer.publish(
+                        result, channel=rpc_func_stack.respond_to)
+                    logger.debug("Publishing done for %s", rpc_func_stack.uid)
+                except Exception as e:
+                    logger.debug(
+                        "Error occured for %s: %s", rpc_func_stack.uid, e)
+                    # Log everything that is not an
+                    # instance of RPCException
+                    if not isinstance(e, RPCException):
+                        logger.exception(e)
+
+                    result = RPCException(
+                        uid=rpc_func_stack.uid,
+                        namespace=rpc_func_stack.namespace,
+                        classname=e.__class__.__name__,
+                        exc_args=e.args)
+                    # Try to publish error
+                    await self.rpc_commlayer.publish(
+                        result, channel=rpc_func_stack.respond_to)
+
+    async def serve(self):
+        """
+        Main entry point for RPCServer.
+
+        Starts RPCServer background processing, blocks
+        until self.rpc_commlayer.unsubscribe() is called.
+        """
+        task_args_map = {
+            self.rpc_commlayer.subscribe: [self._on_rpc_event],
+            self._process_queue: []
+        }
+
+        # create the main tasks
+        main_tasks = {
+            asyncio.ensure_future(coro(*args)): (coro, args)
+            for coro, args in task_args_map.items()
+        }
+
+        running = set(main_tasks.keys())
+
+        except_cnt = -1
+
+        while running:
+            except_cnt += 1
+            finished, running = await asyncio.wait(
+                running, return_when=asyncio.FIRST_EXCEPTION
+            )
+            for task in finished:
+                if task.exception():
+                    logger.exception(task.exception())
+                    task.print_stack()
+                    coro, args = main_tasks[task]
+                    new_task = asyncio.ensure_future(coro(*args))
+                    main_tasks[new_task] = (coro, args)
+                    running.add(new_task)
+
+
+class DefaultExecutor:
+    """
+    Default executor implementation, override if necessary
+    """
+
+    def __init__(self, namespace, instance):
+        assert namespace is not None
+        assert instance is not None
+        self.namespace = namespace
+        self.instance = instance
+
+    async def subscribe_call(self, publisher: Publisher):
+        """
+        Use the Publisher to publish results to the client
+        """
+        # if publisher.is_active:
+        #     await publisher.publish(b'blaat')
+        pass
+
+    async def rpc_call(self, stack: List[RPCCall] = []):
+        """
+        Process incoming rpc call stack.
+        The stack can contain multiple chained function calls for example:
+            node.filter(id=1).reproject_to('4326').data
+        """
+
+        resource = self.instance
+
+        for rpc_func_call in stack:
+            assert isinstance(rpc_func_call, RPCCall)
+
+            # Try to get the function/property from self.instance
+            instance_attr = getattr(resource, rpc_func_call.func_name)
+
+            if callable(instance_attr):
+                # Function
+                resource = instance_attr(
+                    *rpc_func_call.func_args,
+                    **rpc_func_call.func_kwargs)
+            else:
+                # Asume property
+                resource = instance_attr
+
+        return resource
