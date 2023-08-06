6ee3aa2327fa5ca05d4fb5c8b352c027d590887b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:05 +0000

    Add package ipydatagrid/1.0.0/ipydatagrid-1.0.0.tar.gz

---
+++ packages/ipydatagrid/ipydatagrid-1.0.0.tar.gz/ipydatagrid-1.0.0/ipydatagrid/__init__.py
@@ -0,0 +1,20 @@
+# Copyright (c) Bloomberg.
+# Distributed under the terms of the Modified BSD License.
+
+from ._version import __version__  # noqa
+from .datagrid import DataGrid, SelectionHelper  # noqa
+
+
+def _jupyter_nbextension_paths():
+    return [
+        {
+            "section": "notebook",
+            "src": "nbextension",
+            "dest": "ipydatagrid",
+            "require": "ipydatagrid/extension",
+        }
+    ]
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "ipydatagrid"}]
