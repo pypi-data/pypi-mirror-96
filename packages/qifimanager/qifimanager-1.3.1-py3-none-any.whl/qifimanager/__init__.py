07798d3e4b2465f8c528f1868a6f55c62cda8557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:18 +0000

    Add package qifimanager/1.3.1/qifimanager-1.3.1-py3-none-any.whl

---
+++ packages/qifimanager/qifimanager-1.3.1-py3-none-any.whl/qifimanager/__init__.py
@@ -0,0 +1,9 @@
+__version__ = '1.3.1'
+__author__ = 'yutiansut'
+
+from qifimanager.manager import QA_QIFIMANAGER, QA_QIFISMANAGER
+from qifimanager.qifiwebserver import QAQIFI_Handler, QAQIFIS_Handler
+from QAWebServer.QA_Web import start_server
+
+def run_server():
+    start_server([(r"/qifi", QAQIFI_Handler), (r"/qifis", QAQIFIS_Handler)], '0.0.0.0', 8019)
