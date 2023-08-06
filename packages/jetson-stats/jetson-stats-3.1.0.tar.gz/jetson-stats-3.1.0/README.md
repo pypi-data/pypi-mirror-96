b4c13d14dd399bf115b18899819f517eb372185d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:28 +0000

    Add package jetson-stats/3.1.0/jetson-stats-3.1.0.tar.gz

---
+++ packages/jetson-stats/jetson-stats-3.1.0.tar.gz/jetson-stats-3.1.0/README.md
@@ -0,0 +1,135 @@
+# Jetson stats
+[![PyPI - Downloads](https://img.shields.io/pypi/dw/jetson-stats.svg)](https://pypistats.org/packages/jetson-stats) [![PyPI version](https://badge.fury.io/py/jetson-stats.svg)](https://badge.fury.io/py/jetson-stats) [![PyPI - Python Version](https://img.shields.io/pypi/pyversions/jetson-stats.svg)](https://www.python.org/) [![PyPI - Format](https://img.shields.io/pypi/format/jetson-stats.svg)](https://pypi.org/project/jetson-stats/) [![CI & CD](https://github.com/rbonghi/jetson_stats/workflows/CI%20&%20CD/badge.svg)](https://github.com/rbonghi/jetson_stats/actions?query=workflow%3A%22CI+%26+CD%22)
+
+**jetson-stats** is a package for **monitoring** and **control** your [NVIDIA Jetson][NVIDIA Jetson] [Xavier NX, Nano, AGX Xavier, TX1, TX2] Works with all NVIDIA Jetson ecosystem.
+
+[:sparkling_heart: **Sponsor** jetson-stats](https://github.com/sponsors/rbonghi)
+
+When you install jetson-stats are included:
+- [Jetson stats](#jetson-stats)
+- [Install](#install)
+  - [Virtual environment](#virtual-environment)
+- [jtop](#jtop)
+  - [Pages](#pages)
+  - [Controls](#controls)
+- [jetson_config](#jetson_config)
+- [jetson_release](#jetson_release)
+- [jetson_swap](#jetson_swap)
+- [jetson variables](#jetson-variables)
+
+Read the [Wiki](https://github.com/rbonghi/jetson_stat/wiki) for more detailed information or read the package [documentation](https://rbonghi.github.io/jetson_stats).
+
+# Install
+
+```console
+sudo -H pip install -U jetson-stats
+```
+**🚀 That's it! 🚀** 
+
+_PS: Don't forget to **reboot** your board_
+
+**You can run jtop in your python script [read here][library]**
+
+## Virtual environment
+
+If you need to install in a virtual environment like *virtualenv*, you **must** install before in your host **and after** in your environment, like:
+```
+virtualenv venv
+source venv/bin/activate
+pip install -U jetson-stats
+```
+
+# [jtop][jtop] 
+It is a system monitoring utility that runs on the terminal and see and **control** realtime the status of your [NVIDIA Jetson][NVIDIA Jetson]. CPU, RAM, GPU status and frequency and other...
+
+The prompt interface will be show like this image, **now clickable!**:
+![jtop](https://github.com/rbonghi/jetson_stats/wiki/images/jtop.gif)
+
+You can run the jtop simple using a simple command `jtop`
+
+YES! Sudo is **not** more required!
+```console
+nvidia@jetson-xavier-nx:~/$ jtop
+```
+
+Other options are available with `-h` option:
+```console
+nvidia@jetson-xavier-nx:~/$ jtop -h
+usage: jtop [-h] [--no-warnings] [--restore] [--loop] [-r REFRESH] [-p PAGE]
+            [-v]
+
+jtop is system monitoring utility and runs on terminal
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --no-warnings         Do not show warnings (default: False)
+  --restore             Reset Jetson configuration (default: False)
+  --loop                Automatically switch page every 5s (default: False)
+  -r REFRESH, --refresh REFRESH
+                        refresh interval (default: 500)
+  -p PAGE, --page PAGE  Open fix page (default: 1)
+  -v, --version         show program's version number and exit
+```
+You can change page using _left_, _right_ arrow or _TAB_ to change page.
+## Pages
+**jtop** have four different pages to control your NVIDIA Jetson:
+1. **ALL** Are collected all information about your board: CPUs status, Memory, *GPU*, disk, fan and all status about jetson_clocks, NVPmodel and other
+2. **GPU** A real time GPU history about your NVIDIA Jetson
+3. **CPU** A real time CPU plot of NVIDIA Jetson
+4. **MEM** A real time Memory chart and swap monitor
+5. **CTRL** Enable/Disable **jetson_clocks**, **nvpmodel** or **fan** directly from here
+6. **INFO** All information about libraries, CUDA, Serial Number, interfaces, ...
+## Controls
+To control the your NVIDIA Jetson are available this keyboard commands:
+
+In page **4 MEM**:
+* **c** Clear cache
+* **s** Enable/Disable extra swap
+* **+** and **-** Increase and decrease swap size
+
+In page **5 CTRL**:
+* **a** Start/Stop jetson_clocks service (Note: jetson_clocks start only after 60s from up time)
+* **e** Enable/Disable jetson_clocks on board boot
+* **+** and **-** Increase and decrease the NVPmodel
+* **f** Manual/jetson_clocks mode for your fan
+* **p** and **m** Increase and decrease the Fan speed
+
+# [jetson_config][jetson_config]
+
+Check _jetson-stats_ **health**, enable/disable **desktop**, enable/disable **jetson_clocks**, improve the performance of your **wifi** are available only in one click using **jetson_config**
+
+![jetson_config](https://github.com/rbonghi/jetson_stats/wiki/images/jetson_config.png)
+# [jetson_release][jetson_release]
+The command show the status and all information about your [NVIDIA Jetson][NVIDIA Jetson]
+
+![jtop](https://github.com/rbonghi/jetson_stats/wiki/images/jetso_release.png)
+# [jetson_swap][jetson_swap]
+Simple manager to switch on and switch off a swapfile in your jetson.
+
+```console
+nvidia@jetson-nano:~/$ sudo jetson_swap -h
+usage: createSwapFile [[[-d directory ] [-s size] -a] | [-h] | [--off]]
+  -d | --dir    <directoryname> Directory to place swapfile
+  -n | --name   <swapname> Name swap file
+  -s | --size   <gigabytes>
+  -a | --auto   Enable swap on boot in /etc/fstab 
+  -t | --status Check if the swap is currently active
+  --off         Switch off the swap
+  -h | --help   This message
+```
+
+# [jetson variables][jetson_variables]
+When you install jetson-stats in your bash will be available a list of new environment variables to know which which hardware version is available are you working, which Jetpack is installed and other variable show below
+
+![jtop](https://github.com/rbonghi/jetson_stats/wiki/images/jetson_env.png)
+
+[library]: https://github.com/rbonghi/jetson_stats/wiki/library
+[jtop]: https://github.com/rbonghi/jetson_stats/wiki/jtop
+[jetson_config]: https://github.com/rbonghi/jetson_stats/wiki/jetson_config
+[jetson_swap]: https://github.com/rbonghi/jetson_stats/wiki/jetson_swap
+[jetson_variables]: https://github.com/rbonghi/jetson_stats/wiki/jetson_variables
+[jetson_release]: https://github.com/rbonghi/jetson_stats/wiki/jetson_release
+[jetson_performance]: https://github.com/rbonghi/jetson_stats/wiki/jetson_performance
+[jetson_docker]: https://github.com/rbonghi/jetson_stats/wiki/jetson_docker
+[NVIDIA]: https://www.nvidia.com/
+[NVIDIA Jetson]: http://www.nvidia.com/object/embedded-systems-dev-kits-modules.html
---
+++ packages/jetson-stats/jetson-stats-3.1.0.tar.gz/jetson-stats-3.1.0/setup.py
@@ -0,0 +1,242 @@
+# -*- coding: UTF-8 -*-
+# This file is part of the jetson_stats package (https://github.com/rbonghi/jetson_stats or http://rnext.it).
+# Copyright (c) 2019 Raffaello Bonghi.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+# Reference:
+# 1. https://packaging.python.org
+# 2. https://julien.danjou.info/starting-your-first-python-project/
+# 3. https://medium.com/@trstringer/the-easy-and-nice-way-to-do-cli-apps-in-python-5d9964dc950d
+# 4. https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
+# 5. https://python-packaging.readthedocs.io
+# 6. https://github.com/pypa/sampleproject
+# 7. https://pypi.org/classifiers/
+# 8. https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from jtop.service import import_jetson_variables
+# io.open is needed for projects that support Python 2.7
+# It ensures open() defaults to text mode with universal newlines,
+# and accepts an argument to specify the text encoding
+# Python 3 only projects can skip this import
+from io import open
+# Launch command
+import subprocess as sp
+import shlex
+import os
+from shutil import copyfile
+import sys
+import re
+import logging
+
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+log = logging.getLogger()
+
+
+# https://stackoverflow.com/questions/1871549/determine-if-python-is-running-inside-virtualenv
+def is_virtualenv():
+    if hasattr(sys, 'real_prefix'):
+        return True
+    if hasattr(sys, 'base_prefix'):
+        return sys.prefix != sys.base_prefix
+    return False
+
+
+def is_superuser():
+    return os.getuid() == 0
+
+
+def list_scripts():
+    JETSONS = import_jetson_variables()
+    # Load scripts to install
+    scripts = ['scripts/jetson_swap', 'scripts/jetson_release', 'scripts/jetson_config']
+    # If jetpack lower than 32 install also jetson_docker
+    l4t_release = JETSONS['JETSON_L4T_RELEASE']
+    if l4t_release.isdigit():
+        if int(l4t_release) < 32:
+            scripts += ['scripts/jetson_docker']
+    return scripts
+
+
+def list_services():
+    return ["services/{file}".format(file=f) for f in os.listdir("services") if os.path.isfile(os.path.join("services", f))]
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+project_homepage = "https://github.com/rbonghi/jetson_stats"
+documentation_homepage = "https://rbonghi.github.io/jetson_stats"
+
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Load version package
+with open(os.path.join(here, "jtop", "__init__.py")) as fp:
+    VERSION = (
+        re.compile(r""".*__version__ = ["'](.*?)['"]""", re.S).match(fp.read()).group(1)
+    )
+# Store version package
+version = VERSION
+
+
+def install_services(copy=False):
+    """
+    This function install all services in a proper folder and setup the deamons
+    """
+    print("System prefix {prefix}".format(prefix=sys.prefix))
+    # Make jetson stats folder
+    root = sys.prefix + "/local/jetson_stats/"
+    if not os.path.exists(root):
+        os.makedirs(root)
+    # Copy all files
+    for f_service in list_services():
+        folder, _ = os.path.split(__file__)
+        path = root + os.path.basename(f_service)
+        # remove if exist file
+        if os.path.exists(path):
+            os.remove(path)
+        # Copy or link file
+        if copy:
+            type_service = "Copying"
+            copyfile(folder + "/" + f_service, path)
+        else:
+            type_service = "Linking"
+            os.symlink(folder + "/" + f_service, path)
+        # Prompt message
+        print("{type} {file} -> {path}".format(type=type_service, file=os.path.basename(f_service), path=path))
+
+
+def pre_installer(installer, obj, copy):
+    js_is_active = os.system('systemctl is-active --quiet jetson_stats') == 0
+    # Run the uninstaller before to copy all scripts
+    if not is_virtualenv():
+        if is_superuser():
+            sp.call(shlex.split('./scripts/jetson_config --uninstall'))
+            # Install services (linking only for develop)
+            install_services(copy=copy)
+        else:
+            print("----------------------------------------")
+            print("Install on your host using superuser permission, like:")
+            print("sudo -H pip install -U jetson-stats")
+            sys.exit(1)
+    else:
+        if is_superuser():
+            print("Skip uninstall on virtual environment")
+        elif not js_is_active:
+            print("----------------------------------------")
+            print("Please, before install in your virtual environment, install jetson-stats on your host with superuser permission, like:")
+            print("sudo -H pip install -U jetson-stats")
+            sys.exit(1)
+    # Run the default installation script
+    installer.run(obj)
+    # Run the restart all services before to close the installer
+    if not is_virtualenv() and is_superuser():
+        sp.call(shlex.split('./scripts/jetson_config --install'))
+    else:
+        print("Skip install on virtual environment")
+
+
+class PostInstallCommand(install):
+    """Installation mode."""
+    def run(self):
+        # Run the uninstaller before to copy all scripts
+        pre_installer(install, self, True)
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        # Run the uninstaller before to copy all scripts
+        # Install services (linking)
+        pre_installer(develop, self, False)
+
+
+# Configuration setup module
+setup(
+    name="jetson-stats",
+    version=version,
+    author="Raffaello Bonghi",
+    author_email="raffaello@rnext.it",
+    description="Interactive system-monitor and process viewer for all NVIDIA Jetson [Xavier NX, Nano, AGX Xavier, TX1, TX2]",
+    license='AGPL-3.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=project_homepage,
+    download_url=(project_homepage + "/archive/master.zip"),
+    project_urls={
+        "How To": documentation_homepage,
+        "Examples": (project_homepage + "/tree/master/examples"),
+        "Bug Reports": (project_homepage + "/issues"),
+        "Source": (project_homepage + "/tree/master")
+    },
+    packages=find_packages(exclude=['examples', 'scripts', 'tests', 'jtop.tests']),  # Required
+    # Load jetson_variables
+    package_data={"jtop": ["jetson_variables", "jetson_libraries"]},
+    # Define research keywords
+    keywords=("jetson_stats jtop python system-monitor docker \
+               nvidia Jetson XavierNX Nano Xavier TX2 TX1 process viewer"
+              ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        # Audience and topics
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Software Development :: Debuggers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: System :: Hardware",
+        "Topic :: System :: Logging",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Operating System",
+        "Topic :: System :: Operating System Kernels",
+        "Topic :: System :: Shells",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Terminals",
+        # License
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        # Programming and Operative system
+        "Programming Language :: Unix Shell",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux"],
+    # Requisites
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+    platforms=["linux", "linux2", "darwin"],
+    # Zip safe configuration
+    # https://setuptools.readthedocs.io/en/latest/setuptools.html#setting-the-zip-safe-flag
+    zip_safe=False,
+    # Add jetson_variables in /opt/jetson_stats
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
+    data_files=[('jetson_stats', list_services())],
+    # Install extra scripts
+    scripts=list_scripts(),
+    cmdclass={'develop': PostDevelopCommand,
+              'install': PostInstallCommand},
+    # The following provide a command called `jtop`
+    entry_points={'console_scripts': ['jtop=jtop.__main__:main']},
+)
+# EOF
