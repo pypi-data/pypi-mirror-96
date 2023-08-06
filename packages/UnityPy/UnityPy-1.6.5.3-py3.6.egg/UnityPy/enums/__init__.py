2293a6a97e0a1d4e6764cd7ef1b0e4aaf4a277a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:20 +0000

    Add package UnityPy/1.6.5.3/UnityPy-1.6.5.3-py3.6.egg

---
+++ packages/UnityPy/UnityPy-1.6.5.3-py3.6.egg/UnityPy/enums/__init__.py
@@ -0,0 +1,7 @@
+from .Audio import AudioType, AudioCompressionFormat, AUDIO_TYPE_EXTEMSION
+from .BuildTarget import BuildTarget
+from .ClassIDType import ClassIDType
+from .FileType import FileType
+from .TextureFormat import TextureFormat
+from .SpriteMeshType import SpriteMeshType
+from .GfxPrimitiveType import GfxPrimitiveType
