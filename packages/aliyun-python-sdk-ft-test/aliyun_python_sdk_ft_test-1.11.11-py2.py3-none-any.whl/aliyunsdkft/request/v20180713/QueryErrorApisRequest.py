440ff7e8d8778553b3ad6b3c0fd35bddc6e4a8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package aliyun-python-sdk-ft-test/1.11.11/aliyun_python_sdk_ft_test-1.11.11-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-ft-test/aliyun_python_sdk_ft_test-1.11.11-py2.py3-none-any.whl/aliyunsdkft/request/v20180713/QueryErrorApisRequest.py
@@ -0,0 +1,68 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkft.endpoint import endpoint_data
+
+class QueryErrorApisRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Ft', '2018-07-13', 'QueryErrorApis')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_BeginDate(self):
+		return self.get_query_params().get('BeginDate')
+
+	def set_BeginDate(self,BeginDate):
+		self.add_query_param('BeginDate',BeginDate)
+
+	def get_EndDate(self):
+		return self.get_query_params().get('EndDate')
+
+	def set_EndDate(self,EndDate):
+		self.add_query_param('EndDate',EndDate)
+
+	def get_ProductName(self):
+		return self.get_query_params().get('ProductName')
+
+	def set_ProductName(self,ProductName):
+		self.add_query_param('ProductName',ProductName)
+
+	def get_BucUid(self):
+		return self.get_query_params().get('BucUid')
+
+	def set_BucUid(self,BucUid):
+		self.add_query_param('BucUid',BucUid)
+
+	def get_BucName(self):
+		return self.get_query_params().get('BucName')
+
+	def set_BucName(self,BucName):
+		self.add_query_param('BucName',BucName)
+
+	def get_BucEmpId(self):
+		return self.get_query_params().get('BucEmpId')
+
+	def set_BucEmpId(self,BucEmpId):
+		self.add_query_param('BucEmpId',BucEmpId)
