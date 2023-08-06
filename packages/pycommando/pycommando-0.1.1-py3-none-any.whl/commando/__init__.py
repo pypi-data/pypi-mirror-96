703fbf26a3840f09d3b80b9fe9f952bd0da72b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:42 +0000

    Add package pycommando/0.1.1/pycommando-0.1.1-py3-none-any.whl

---
+++ packages/pycommando/pycommando-0.1.1-py3-none-any.whl/commando/__init__.py
@@ -0,0 +1,7 @@
+from .commando import Commando as _Command
+
+__version__ = '0.1.1'
+
+__all__ = ["commando"]
+
+commando = _Command()
---
+++ packages/pycommando/pycommando-0.1.1-py3-none-any.whl/commando/commando.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+class Commando:
+
+    def __init__(self):
+        self.__commands = []
+
+    def add(self, cmd):
+        self.__commands.append(cmd)
+
+    def list(self):
+        return self.__commands
+
+    def execute(self):
+        commands = self.__commands
+        for cmd in commands:
+
+            # 呼び出し可能かどうかで条件分岐
+            if callable(cmd):
+                # 返り値は特に受け取らない
+                cmd()
+            else:
+                subprocess.run(cmd, shell=True)
