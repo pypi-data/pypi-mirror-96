3c7fdfafa284d1b42d3cdca3e57f194510957704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:15 +0000

    Add package py-everything/1.0.2/py_everything-1.0.2-py3-none-any.whl

---
+++ packages/py-everything/py_everything-1.0.2-py3-none-any.whl/py_utils/__init__.py
@@ -0,0 +1,22 @@
+import os, subprocess, shutil
+from py_utils_exceptions import pycacheNotFoundError, installModulesFailedError
+
+def hello_world():
+    string = 'Hello, World!'
+    return string
+
+def print_no_newline(*args):
+    for arg in args:
+        print(arg, end=" ")
+
+def clearPycache(path):
+    if not shutil.rmtree(os.path.join(path, "__pycache__")):
+        raise pycacheNotFoundError("The '__pycache__' folder was not found in the given directory. Check if it exists and try again!")
+
+def install_modules(*args):
+    for module in args:
+        command = f"python -m pip install {module}"
+        if subprocess.run(command):
+            return True
+        else:
+            raise installModulesFailedError
