cfb691b5c8671b4cdad8b36381a7f277feb31a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:48 +0000

    Add package pymspyt/0.1.2/pymspyt-0.1.2.tar.gz

---
+++ packages/pymspyt/pymspyt-0.1.2.tar.gz/pymspyt-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='pymspyt',
+    version='0.1.2',
+    description='Module CLI to install YouTube Video/Audio',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    entry_points={
+        'console_scripts': [
+            'mspyt = pymspyt.main:main',
+        ]
+    },     #{'console_scripts': ['mspyt = pymspyt.main:main']},
+    license='MIT',
+    author='Siva Pranav Mandadi',
+    author_email='msivapranav@gmail.com',
+    packages=find_packages(),
+    install_requires=['pytube']
+)
