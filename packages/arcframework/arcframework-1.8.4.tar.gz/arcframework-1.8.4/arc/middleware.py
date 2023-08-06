d6a8a7e77a6e040ac2f72ddf9d0c9986dff34fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:30 +0000

    Add package arcframework/1.8.4/arcframework-1.8.4.tar.gz

---
+++ packages/arcframework/arcframework-1.8.4.tar.gz/arcframework-1.8.4/arc/middleware.py
@@ -0,0 +1,32 @@
+from starlette.requests import Request
+
+class Middleware:
+    def __init__(self, app):
+        self.app = app
+        self.test = "YAY"
+    
+    async def __call__(self, scope, receive, send):
+        request = Request(scope=scope, receive=receive)
+        
+        response = self.app.handle_request(request)
+        
+        await response(scope, receive, send)
+
+    def add(self, middleware_cls):
+        self.app = middleware_cls(self.app)
+
+    def process_request(self, req):
+        pass
+
+    def process_response(self, req, res):
+        pass
+
+    def handle_request(self, request):
+        self.process_request(request)
+
+        response = self.app.handle_request(request)
+        self.process_response(request, response)
+
+        return response
+
+
