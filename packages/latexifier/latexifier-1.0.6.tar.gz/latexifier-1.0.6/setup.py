07022bebf9fb5f71e1a2b23b6d6229075edd8d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:48 +0000

    Add package latexifier/1.0.6/latexifier-1.0.6.tar.gz

---
+++ packages/latexifier/latexifier-1.0.6.tar.gz/latexifier-1.0.6/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latexifier",
+    version="1.0.6",
+    author="Guillaume Garrigos",
+    author_email="guillaume.garrigos@lpsm.paris",
+    license="MIT",
+    description="A package to convert python objects into latex strings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Guillaume-Garrigos/latexify",
+    packages=['latexifier', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'sympy',
+        'numbers',
+        'fractions',
+        'mpmath',
+        'warnings',
+        'copy',
+        'math',
+        'collections',
+        'functools',
+    ],
+    include_package_data=True,
+)
