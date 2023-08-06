50bff7da24f491331dd6e930d233e90011cbce75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:55 +0000

    Add package BlueTools/0.1/BlueTools-0.1.tar.gz

---
+++ packages/BlueTools/BlueTools-0.1.tar.gz/BlueTools-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BlueTools',
+      version='0.1',
+      description='Tools for working with Blue Files',
+      packages=['BlueTools'],
+      zip_safe=False)
