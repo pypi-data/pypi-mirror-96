322da66c9bd2459ad2e01526946afc6423e8913f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:23 +0000

    Add package bx-python/0.8.3/bx_python-0.8.3-cp27-cp27m-macosx_10_9_x86_64.whl

---
+++ packages/bx-python/bx_python-0.8.3-cp27-cp27m-macosx_10_9_x86_64.whl/bx_python-0.8.3.data/scripts/nib_length.py
@@ -0,0 +1,15 @@
+#!python
+
+"""
+Print the number of bases in a nib file.
+
+usage: %prog nib_file
+"""
+from __future__ import print_function
+
+import sys
+
+from bx.seq import nib as seq_nib
+
+nib = seq_nib.NibFile( file( sys.argv[1] ) )
+print(nib.length)
