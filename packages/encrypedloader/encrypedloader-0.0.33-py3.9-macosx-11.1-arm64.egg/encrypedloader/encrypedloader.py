d6c1ef424af3f48d3225ce4b1a865faeed5ad9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:50 +0000

    Add package encrypedloader/0.0.33/encrypedloader-0.0.33-py3.9-macosx-11.1-arm64.egg

---
+++ packages/encrypedloader/encrypedloader-0.0.33-py3.9-macosx-11.1-arm64.egg/encrypedloader/encrypedloader.py
@@ -0,0 +1,27 @@
+import click
+from loader import encrypt_file, gen_key
+
+@click.group()
+def cmd():
+    pass
+
+
+@cmd.command()
+@click.argument('key')
+@click.argument('fp')
+def encrypt(key, fp):
+    encrypt_file(fp, key)
+    click.echo('encrypted!')
+
+
+@cmd.command()
+@click.argument('expire')
+def genkey(expire):
+    gen_key(int(expire))
+
+
+cli = click.CommandCollection(sources=[cmd,])
+
+
+def main():
+    cli()
