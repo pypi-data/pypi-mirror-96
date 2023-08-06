e2352fa3b9a4bcdfa02f6ec7c41de5267d250c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:15 +0000

    Add package bgajjela/0.0.1/bgajjela-0.0.1.tar.gz

---
+++ packages/bgajjela/bgajjela-0.0.1.tar.gz/bgajjela-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bgajjela", # Replace with your own username
+    version="0.0.1",
+    author="BhavithaReddyGajjela",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
