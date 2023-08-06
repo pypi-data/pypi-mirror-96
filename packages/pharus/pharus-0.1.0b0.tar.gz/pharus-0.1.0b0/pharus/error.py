5d739d0d9b91d381034414c2c67dceae0a700c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:50 +0000

    Add package pharus/0.1.0b0/pharus-0.1.0b0.tar.gz

---
+++ packages/pharus/pharus-0.1.0b0.tar.gz/pharus-0.1.0b0/pharus/error.py
@@ -0,0 +1,13 @@
+"""Error class library."""
+
+
+class UnsupportedTableType(Exception):
+    pass
+
+
+class InvalidRestriction(Exception):
+    pass
+
+
+class InvalidDeleteRequest(Exception):
+    pass
