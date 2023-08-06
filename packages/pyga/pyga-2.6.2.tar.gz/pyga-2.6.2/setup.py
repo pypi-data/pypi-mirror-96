d0bc5953835a0fc1987d8e05bdfe631e36cddae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:04 +0000

    Add package pyga/2.6.2/pyga-2.6.2.tar.gz

---
+++ packages/pyga/pyga-2.6.2.tar.gz/pyga-2.6.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+
+import os
+from setuptools import setup, find_packages
+from pyga.requests import __version__, __license__
+
+
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_desc = open(README).read() + '\n\n'
+
+setup(name='pyga',
+      version=__version__,
+      license=__license__,
+      author='Arun K.R.',
+      author_email='the1.arun@gmail.com',
+      url='http://kra3.github.com/py-ga-mob/',
+      description='Server side implementation of Google Analytics in Python.',
+      long_description=long_desc,
+      keywords='Google analytics  mobile server side',
+      requires=[
+          'six',
+      ],
+      install_requires=['setuptools', 'six'],
+      packages=find_packages(),
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Web Environment',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+      ],
+      test_suite='tests',
+      tests_require=['mock',]
+    )
