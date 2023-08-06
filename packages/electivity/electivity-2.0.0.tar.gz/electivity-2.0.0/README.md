26d8f4d73b041c5151fa5ebad31af989a894b20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:51 +0000

    Add package electivity/2.0.0/electivity-2.0.0.tar.gz

---
+++ packages/electivity/electivity-2.0.0.tar.gz/electivity-2.0.0/README.md
@@ -0,0 +1,58 @@
+[![DOI](https://zenodo.org/badge/342729167.svg)](https://zenodo.org/badge/latestdoi/342729167)
+
+# electivity
+
+Ecological electivity and forage indices
+
+## Description
+
+A compact scientific package for calculating electivity and forage preference indices including Ivlev E, Jacobs D, and Vanderploeg and Scavia E\*.
+
+Designed to work seamlessly with Pandas dataframes.
+
+## Installation
+
+```
+pip install electivity
+```
+
+## Usage
+
+Every `electivity` [function](https://github.com/aazuspan/electivity#Functions-and-indices) takes two parameters—a list of available resources and a list of consumed resources—and returns an equal-length Pandas Series of electivity values calculated element-wise. The easiest way to work with `electivity` is using Pandas dataframes, but any list-like data input will work. If a list output is desired, the output can be cast using `list( ... )`.
+
+### Example
+
+```python
+import pandas as pd
+import electivity
+
+# Build a dataframe of resource data
+data = pd.DataFrame({"available": [10, 10, 10], "consumed": [10, 3, 0]})
+
+# Calculate Ivlev electivity and assign it to a new column
+data = data.assign(E=electivity.ivlev_electivity(data.available, data.consumed))
+```
+
+### Functions and indices
+
+| Function               | Algorithm                                                     |
+| ---------------------- | ------------------------------------------------------------- |
+| ivlev_forage_ratio     | Ivlev Forage Ratio E' (Ivlev 1961)                            |
+| ivlev_electivity       | Ivlev Electivity E (Ivlev 1961)                               |
+| jacobs_electivity      | Jacobs' Electivity D (Jacobs 1974)                            |
+| jacobs_forage_ratio    | Jacobs' Forage Ratio Q (Jacobs 1974)                          |
+| strauss_linear         | Strauss' Linear Index L (Strauss 1979)                        |
+| chessons_alpha         | Chesson's Alpha α (Chesson 1978)                              |
+| relativized_electivity | Relativized Electivity Index E\* (Vanderploeg & Scavia, 1979) |
+
+## Citation
+
+Zuspan, A. 2021. electivity: Ecological electivity and forage indices, v1.0.0, Zenodo, doi:10.5281/zenodo.4567591
+
+## References
+
+- Chesson, J. 1978. Measuring preference in selective predation. Ecology 59:211-215.
+- Ivlev, V. S. 1961. Experimental ecology of the feeding of fishes. Yale Univ. Press, New Haven.
+- Jacobs, J. 1974. Quantitative measurement of food selection. Oecologia (Berl) 14:413-417.
+- Strauss, R. E. 1979. Reliability estimates for Ivlev's electivity index, the forage ratio, and a proposed linear index of food selection. Trans Am Fish Soc 108: 344-352.
+- Vanderploeg H. A. and Scavia D. 1979. Calculation and use of selectivity coefficients of feeding: zooplankton grazing. Ecol Modelling 7:135-149.
---
+++ packages/electivity/electivity-2.0.0.tar.gz/electivity-2.0.0/electivity/electivity.py
@@ -0,0 +1,125 @@
+import pandas as pd
+
+
+def _normalize(available, consumed):
+    """
+    Convert lists of available and consumed items into normalized r and p series.
+    """
+    if len(available) != len(consumed):
+        raise ValueError(
+            "Available and consumed must be equal length list-like objects.")
+
+    available = pd.Series(available)
+    consumed = pd.Series(consumed)
+
+    r = consumed.divide(consumed.sum())
+    p = available.divide(available.sum())
+
+    return r, p
+
+
+def ivlev_forage_ratio(available, consumed):
+    """
+    Calculate Ivlev forage ratio E' (Ivlev 1961).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    E = r.divide(p)
+
+    return E
+
+
+def ivlev_electivity(available, consumed):
+    """
+    Calculate Ivlev electivity E (Ivlev 1961).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    E = r.subtract(p).divide(r.add(p))
+
+    return E
+
+
+def jacobs_electivity(available, consumed):
+    """
+    Calculate Jacobs electivity D (Jacobs 1974).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    D = r.subtract(p).divide(r.add(p).subtract(r.multiply(p.multiply(2))))
+
+    return D
+
+
+def jacobs_forage_ratio(available, consumed):
+    """
+    Calculate Jacobs modified forage ratio Q (Jacobs 1974).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    Q = r.multiply(p.multiply(-1).add(1)
+                   ).divide(p.multiply(r.multiply(-1).add(1)))
+
+    return Q
+
+
+def strauss_linear(available, consumed):
+    """
+    Calculate Strauss' Linear Index L (Strauss 1979).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    L = r.subtract(p)
+
+    return L
+
+
+def chessons_alpha(available, consumed):
+    """
+    Calculate Chesson's alpha a (Chesson 1978) or Vanderploeg and Scavia's Selectivity W.
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    r, p = _normalize(available, consumed)
+
+    a = r.divide(p).divide(r.divide(p).sum())
+
+    return a
+
+
+def relativized_electivity(available, consumed):
+    """
+    Calculate Vanderploeg and Scavia's Relativized Electivity E* (Vanderploeg and Scavia 1979).
+
+    :param list: available A list or list-like object of elements representing available resources.
+    :param list: consumed A list of list-like object of elements representing consumed resources.
+    :return pd.Series: A list of electivity values corresponding to each element of the input lists.
+    """
+    W = pd.Series(chessons_alpha(available, consumed))
+    n = len(available)
+
+    E = W.subtract(1/n).divide(W.add(1/n))
+
+    return E
