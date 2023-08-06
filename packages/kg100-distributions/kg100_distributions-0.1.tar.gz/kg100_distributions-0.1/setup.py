3f6f553a05272c1b4d156cda74c35f2a9ca0b37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package kg100-distributions/0.1/kg100_distributions-0.1.tar.gz

---
+++ packages/kg100-distributions/kg100_distributions-0.1.tar.gz/kg100_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kg100_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Krishna Goel',
+      author_email = 'kgoel100@gmail.com',
+      zip_safe=False)
