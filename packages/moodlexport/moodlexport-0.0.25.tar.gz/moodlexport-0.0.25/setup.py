c2cae36a9ec73e98ad79b75558d018300239a86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:52 +0000

    Add package moodlexport/0.0.25/moodlexport-0.0.25.tar.gz

---
+++ packages/moodlexport/moodlexport-0.0.25.tar.gz/moodlexport-0.0.25/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="moodlexport",
+    version="0.0.25",
+    author="Guillaume Garrigos",
+    author_email="guillaume.garrigos@lpsm.paris",
+    license="MIT",
+    description="A package to export test questions into Moodle from python or latex",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Guillaume-Garrigos/moodlexport",
+    packages=['moodlexport', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+    'texsoup == 0.2.1',
+    'xmltodict >= 0.12.0',
+    'numpy >= 1.18.1',
+    'beautifulsoup4 >= 4.8.2'
+    ],
+    include_package_data=True,
+)
