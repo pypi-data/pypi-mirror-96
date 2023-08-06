3a61fea448afef4ea8d0c7e0ac4e3b2a50107348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:50 +0000

    Add package drstorage/0.2.1/drstorage-0.2.1-py2.py3-none-any.whl

---
+++ packages/drstorage/drstorage-0.2.1-py2.py3-none-any.whl/drstorage/cli/cli.py
@@ -0,0 +1,32 @@
+import logging
+import click
+
+from ..version import __version__
+from .. import models
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+
+
+@click.group(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(version=__version__)
+def drstorage():
+    pass
+
+
+@drstorage.command()
+@click.argument("bytestream", default="-", type=click.File("rb"))
+@click.option(
+    "--model",
+    "-m",
+    "model_name",
+    help="The model to use for parsing",
+    type=click.Choice(models.__all__),
+    default=models.__all__[0],
+)
+def parse(bytestream, model_name):
+    model = getattr(models, model_name)
+    line = bytestream.read(model.size)
+    while line:
+        click.echo(str(model.parse(line)).replace("\n", ""))
+        line = bytestream.read(model.size)
