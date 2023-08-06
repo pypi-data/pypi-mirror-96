63c5b6d5e771c37c615f9cb488223035081e93f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package lined/0.0.22/lined-0.0.22.tar.gz

---
+++ packages/lined/lined-0.0.22.tar.gz/lined-0.0.22/lined/__init__.py
@@ -0,0 +1,19 @@
+from lined.base import Line, Pipeline, mk_multi_func, LayeredPipeline
+from lined.simple import Pipe
+from lined.tools import (
+    keys_extractor,
+    items,
+    iterize,
+    iterate,
+    tail_io,
+    map_star,
+    singularize_arg_input,  # old alias of map_star,
+    with_cursor,
+    BufferStats,
+    mk_filter,
+    side_call,
+    print_and_pass_on,
+)
+from lined.util import (
+    partial_plus
+)
