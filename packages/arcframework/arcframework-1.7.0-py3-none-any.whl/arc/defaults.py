033dcc8e717a760b72326956e15e0841bea55c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:52 +0000

    Add package arcframework/1.7.0/arcframework-1.7.0-py3-none-any.whl

---
+++ packages/arcframework/arcframework-1.7.0-py3-none-any.whl/arc/defaults.py
@@ -0,0 +1,36 @@
+from arc.middleware import Middleware
+from arc.errors import AppException
+import os
+from jinja2 import Environment, FileSystemLoader
+from jinja2.loaders import FileSystemLoader
+from starlette.responses import PlainTextResponse
+
+
+class DefaultMiddleware(Middleware):
+    def process_request(self, req):
+        print(f"\n[REQUEST][{req.method}] {req.url}")
+
+    def process_response(self, req, res):
+        print(f"\n[RESPONSE] {req.url}")
+
+
+class DefaultExceptionHandler:
+    def __init__(self, app):
+        self.app = app
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.path = os.path.join(self.dir_path, "selfpages")
+        self.errors_env = Environment(
+            loader=FileSystemLoader(os.path.join(self.dir_path, "selfpages")))
+
+    def handle_error(self, error):
+        return PlainTextResponse("ERROR: \n", error)
+        
+        
+        # response.body = self.errors_env.get_template(
+        #     "error-500.html").render({"error": error}).encode()
+
+    def handle_404(self):
+        return PlainTextResponse("ERROR 404")
+
+        # response.body = self.errors_env.get_template(
+        #     "error-404.html").render().encode()
