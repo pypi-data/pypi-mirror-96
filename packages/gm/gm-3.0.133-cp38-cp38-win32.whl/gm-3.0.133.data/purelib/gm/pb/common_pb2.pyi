03dfaa0c89478f053b7bbfb937a2c32193671aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package gm/3.0.133/gm-3.0.133-cp38-cp38-win32.whl

---
+++ packages/gm/gm-3.0.133-cp38-cp38-win32.whl/gm-3.0.133.data/purelib/gm/pb/common_pb2.pyi
@@ -0,0 +1,11 @@
+from typing import Any
+
+DESCRIPTOR: Any
+Property: Any
+Filter: Any
+Error: Any
+ConnectionAddress: Any
+ConnectionStatus: Any
+Log: Any
+Logs: Any
+Heartbeat: Any
---
+++ packages/gm/gm-3.0.133-cp38-cp38-win32.whl/gm-3.0.133.data/purelib/gm/pb/rtconf_service_pb2.pyi
@@ -0,0 +1,6 @@
+from typing import Any
+
+DESCRIPTOR: Any
+GetParametersReq: Any
+DelParametersReq: Any
+GetSymbolsReq: Any
