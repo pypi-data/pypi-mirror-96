fa0fe506529ddd9902f3be642f24282abdf91932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package countrynames/1.8.0/countrynames-1.8.0.tar.gz

---
+++ packages/countrynames/countrynames-1.8.0.tar.gz/countrynames-1.8.0/setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup, find_packages
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(path, "README.md"), "r") as f:
+    readme = f.read()
+
+setup(
+    name="countrynames",
+    version="1.8.0",
+    description="A library to map country names to ISO codes.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="names countries iso country",
+    author="Friedrich Lindenberg",
+    author_email="friedrich@pudo.org",
+    url="http://github.com/occrp/countrynames",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "test"]),
+    namespace_packages=[],
+    package_data={"": ["countrynames/data.yaml", "countrynames/py.typed"]},
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="nose.collector",
+    install_requires=["normality", "python-Levenshtein", "pyyaml"],
+    extras_require={
+        "dev": ["mypy", "wheel", "twine", "nose"],
+    },
+    tests_require=[],
+    entry_points={},
+)
