7c25f97003e5e93c099c6f3429acfdfa6cc3dfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:06 +0000

    Add package dothttp-req/0.0.5/dothttp_req-0.0.5-py3-manylinux1-any.whl

---
+++ packages/dothttp-req/dothttp_req-0.0.5-py3-manylinux1-any.whl/dothttp/dsl_jsonparser.py
@@ -0,0 +1,23 @@
+from typing import Dict, List, Union
+
+
+def json_or_array_to_json(model) -> Union[Dict, List]:
+    if array := model.array:
+        return [jsonmodel_to_json(value) for value in array.values]
+    elif json_object := model.object:
+        return {member.key: jsonmodel_to_json(member.value) for member in json_object.members}
+
+
+def jsonmodel_to_json(model):
+    if str_value := model.str:
+        return str_value.value
+    elif flt := model.flt:
+        return flt.value
+    elif bl := model.bl:
+        return bl.value
+    elif json_object := model.object:
+        return {member.key: jsonmodel_to_json(member.value) for member in json_object.members}
+    elif array := model.array:
+        return [jsonmodel_to_json(value) for value in array.values]
+    elif model == 'null':
+        return None
