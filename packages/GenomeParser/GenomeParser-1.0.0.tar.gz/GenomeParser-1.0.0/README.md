efb244ee15630cda8679949fc7c5bf81222a74ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:31 +0000

    Add package GenomeParser/1.0.0/GenomeParser-1.0.0.tar.gz

---
+++ packages/GenomeParser/GenomeParser-1.0.0.tar.gz/GenomeParser-1.0.0/README.md
@@ -0,0 +1,6 @@
+# GenomeParser
+
+Biblioteca para Parseamento de arquivos
+de notação de genomas e exomas.
+Abrange leitura dos principais tipos
+de arquivos, fastq, gtf, etc.
---
+++ packages/GenomeParser/GenomeParser-1.0.0.tar.gz/GenomeParser-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='GenomeParser',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Descrição curta do meu pacote',
+    long_description='Longa descrição do meu pacote',
+    author='bfunicheli',
+    author_email='osvaldobreno99@gmail.com',
+    url='https://github.com/BrenoOsvaldoFunicheli/GenomeParser.git',
+    install_requires=['', ''],
+    license='MIT',
+    keywords=['dev', 'web'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
