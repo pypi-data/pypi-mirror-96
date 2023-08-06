9fde47198cfd6a9e04877884648a06ac5923a7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:41 +0000

    Add package steam-nb-api/0.3.17/steam_nb_api-0.3.17.tar.gz

---
+++ packages/steam-nb-api/steam_nb_api-0.3.17.tar.gz/steam_nb_api-0.3.17/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="steam_nb_api",
+    version="0.3.17", #Original: 0.3.17, test: 0.3.106
+    author="TE-MPE",
+    author_email="steam@cern.ch",
+    description="A package with an API for STEAM.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.cern.ch/steam/steam-notebook-api",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
