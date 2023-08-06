e58b2e4577d1df3cef2408fc338939f67bf60ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:16 +0000

    Add package onnx-simplifier/0.3.3/onnx-simplifier-0.3.3.tar.gz

---
+++ packages/onnx-simplifier/onnx-simplifier-0.3.3.tar.gz/onnx-simplifier-0.3.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages  # type: ignore
+
+setup(
+    name='onnx-simplifier',
+    # The version will be updated automatically in CI
+    version='0.3.3',
+    description='Simplify your ONNX model',
+    author='daquexian',
+    author_email='daquexian566@gmail.com',
+    url='https://github.com/daquexian/onnx-simplifier',
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    license='Apache',
+    keywords='deep-learning ONNX',
+    install_requires=[
+        'onnx',
+        'onnxoptimizer >= 0.2.4',
+        'onnxruntime >= 1.6.0',
+        'protobuf >= 3.7.0'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+    ],
+    python_requires='>=3.5'
+)
