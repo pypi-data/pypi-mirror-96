952d2a6c56e7921649a47140b1bb30a14f584cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:51 +0000

    Add package sparsify/0.1.1/sparsify-0.1.1-py3-none-any.whl

---
+++ packages/sparsify/sparsify-0.1.1-py3-none-any.whl/sparsify/ui/precache-manifest.4378608b9b621dde1dc5abcb1b3f67fc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90f0c03e96b3a5a3057c172a096b909",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd90487814465a1c6c87",
+    "url": "/static/js/2.ede2fff4.chunk.js"
+  },
+  {
+    "revision": "7e9a8e733657b480b1ddfdb66d18d65a",
+    "url": "/static/js/2.ede2fff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998ff89c7fa5ae6a6d91",
+    "url": "/static/js/main.f931815c.chunk.js"
+  },
+  {
+    "revision": "d19b4aa8cd7535ef8951",
+    "url": "/static/js/runtime-main.c1b608db.js"
+  },
+  {
+    "revision": "7c3e0835ee106ea5732b481d7d418864",
+    "url": "/static/media/analyze.7c3e0835.svg"
+  },
+  {
+    "revision": "2c79b78bfbd582efe5d0a708de1e57b0",
+    "url": "/static/media/icon.2c79b78b.svg"
+  },
+  {
+    "revision": "819c3ff526c63f2b326e382f10a4bc5e",
+    "url": "/static/media/icon.819c3ff5.svg"
+  },
+  {
+    "revision": "aba82ccecec27f0ba5ec16911fa48579",
+    "url": "/static/media/icon.aba82cce.svg"
+  },
+  {
+    "revision": "b1f0d69f42bc9ca00acaacab6522b252",
+    "url": "/static/media/icon.b1f0d69f.svg"
+  },
+  {
+    "revision": "c1c9830b2d8b71f49cd1e32d5573dd59",
+    "url": "/static/media/icon.c1c9830b.svg"
+  },
+  {
+    "revision": "f1cd48149df5277c5f5cbbf0699c553f",
+    "url": "/static/media/icon.f1cd4814.svg"
+  },
+  {
+    "revision": "152d259616decc282b174b8db8f9b55f",
+    "url": "/static/media/integrate.152d2596.svg"
+  },
+  {
+    "revision": "fe6fc3787ec30447bd39c0df46d6d086",
+    "url": "/static/media/logo.fe6fc378.svg"
+  },
+  {
+    "revision": "aba82ccecec27f0ba5ec16911fa48579",
+    "url": "/static/media/model.aba82cce.svg"
+  },
+  {
+    "revision": "58851c1bc631b28c9e93d3231a8b1ad8",
+    "url": "/static/media/optimize.58851c1b.svg"
+  },
+  {
+    "revision": "f82adafaedcbdc1cc3a35891c00d4532",
+    "url": "/static/media/profile.f82adafa.svg"
+  },
+  {
+    "revision": "dd29f0f685f383900fbf93ce06cc3bdb",
+    "url": "/static/media/project.dd29f0f6.svg"
+  }
+]);
