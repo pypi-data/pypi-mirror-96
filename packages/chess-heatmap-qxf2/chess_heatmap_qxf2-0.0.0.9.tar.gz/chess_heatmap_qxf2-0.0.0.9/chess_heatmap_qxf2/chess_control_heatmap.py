dd5cd0b04fd8ce05d3c8ad2a3cf7da3d9ef3b4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:07 +0000

    Add package chess-heatmap-qxf2/0.0.0.9/chess_heatmap_qxf2-0.0.0.9.tar.gz

---
+++ packages/chess-heatmap-qxf2/chess_heatmap_qxf2-0.0.0.9.tar.gz/chess_heatmap_qxf2-0.0.0.9/chess_heatmap_qxf2/chess_control_heatmap.py
@@ -0,0 +1,21 @@
+"""
+This module produces a control heatmap for a chess game which shows which side
+controls which squares how many times per ply/move in a Chess board
+"""
+from .chess_util import ChessUtil
+from .chess_dask_cluster import ChessDaskCluster
+from .chess_image_generator import ChessImageGenerator
+
+import multiprocessing
+
+class ChessControlHeatmap:
+    "Class to generate the control heatmap based on the input PGN files"
+
+    def generate_heatmap_images(self):
+        "Fetches the input from files and starts to analyze the games"
+        game_list = ChessUtil.get_games_from_pgn_files()
+        dask_cluster = ChessDaskCluster()
+        gamedata_list = dask_cluster.analyse_games_in_cluster(game_list)
+        print("Done calculating heatmap, now generting gifs")
+        pool = multiprocessing.Pool()
+        pool.map(ChessImageGenerator.create_gif, gamedata_list)
