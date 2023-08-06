5778afe55ef81910808170a7c2d20402acc46f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:04 +0000

    Add package ETAES/0.0.43/ETAES-0.0.43.tar.gz

---
+++ packages/ETAES/ETAES-0.0.43.tar.gz/ETAES-0.0.43/ETAES/prototype/prototype/data/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+# @File     :__init__.py.py
+# @Time     :2021/2/24
+# @Author   :jiawei.li
+# @Software :PyCharm
+# @Desc     :None
---
+++ packages/ETAES/ETAES-0.0.43.tar.gz/ETAES-0.0.43/ETAES/utils/mapping.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*- 
+# @File     :mapping.py
+# @Time     :2021/1/26
+# @Author   :jiawei.li
+# @Software :PyCharm
+# @Desc     :None
+import os
+
+import importlib
+from ETAES.globals.mapItem import build_mapping_classes
+
+def build_mapping(modules):
+    mapping_dict = {}
+    for module in modules:
+        try:
+            all = module.__all__
+            for item in all:
+                mapping_dict[item] = getattr(module, item)
+        except:
+            pass
+    return mapping_dict
+
+def mapping(func_name, mapping_classes = build_mapping_classes()):
+    mapping_dict = build_mapping(mapping_classes)
+    return mapping_dict[func_name]
+
+def mapping_component(name):
+    return MAPPING_COMPONENTS[name]
