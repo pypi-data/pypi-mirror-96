54380cfb256a3a6428922e16ea4bba71ae8a1d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:02 +0000

    Add package couplet-composer/1.7.5/couplet_composer-1.7.5-py2-none-any.whl

---
+++ packages/couplet-composer/couplet_composer-1.7.5-py2-none-any.whl/couplet_composer/__version__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2019 Antti Kivi
+# Licensed under the MIT License
+
+"""
+The version data of Couplet Composer. The version number is given
+according to Semantic Versioning.
+"""
+
+import os
+
+
+def get_release_version():
+    """
+    Gives the current release version data of Couplet Composer
+    and, thus, returns three values: the major version number,
+    the minor version number, and the patch version number.
+    """
+    return 1, 7, 5
+
+
+_VERSION_SUFFIX = ""
+
+
+__version__ = "{}{}".format(
+    ".".join([str(n) for n in get_release_version()]),
+    _VERSION_SUFFIX
+)
+
+
+if __name__ == "__main__":
+    print(__version__)
