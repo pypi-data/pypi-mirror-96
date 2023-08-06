269b62077ac383c09332297a9a7f847ccce1d9a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:34 +0000

    Add package dbutils-phornee/0.1.3/dbutils_phornee-0.1.3-py3-none-any.whl

---
+++ packages/dbutils-phornee/dbutils_phornee-0.1.3-py3-none-any.whl/dbutils_phornee/db_conn.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+class DBOpenException(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class DBGetLockException(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class DBReleaseLockException(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class DBConn(ABC):
+
+    def __init__(self):
+        self.conn = None
+
+    @abstractmethod
+    def openConn(self, params, autocommit=True):
+        pass
+
+    def getConn(self):
+        return self.conn
+
+    def closeConn(self):
+        pass
+
+    @abstractmethod
+    def insert(self, table, params):
+        pass
+
+    def getLock(self, lockname):
+        raise
+
+    def releaseLock(self, lockname):
+        raise
+
