f39c982d37f54871b23e2fe71172936f811a4c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:02 +0000

    Add package testasdf/1.0/testasdf-1.0.tar.gz

---
+++ packages/testasdf/testasdf-1.0.tar.gz/testasdf-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='testasdf',
+    version='1.0',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages()
+)
