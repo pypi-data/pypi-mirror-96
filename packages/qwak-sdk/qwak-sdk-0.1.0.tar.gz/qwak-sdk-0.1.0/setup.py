665443e58e8343f3c6321951b8efed667edf3780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package qwak-sdk/0.1.0/qwak-sdk-0.1.0.tar.gz

---
+++ packages/qwak-sdk/qwak-sdk-0.1.0.tar.gz/qwak-sdk-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qwak-sdk", # Replace with your own username
+    version="0.1.0",
+    author="Qwak",
+    author_email="info@qwak.ai",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.qwak.ai/",
+    project_urls={
+        "Bug Tracker": "https://www.qwak.ai/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
+
