597987ca8e73d8c5f525d9232fa5f8fb04e25085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package resonATe/1.0.9/resonATe-1.0.9.tar.gz

---
+++ packages/resonATe/resonATe-1.0.9.tar.gz/resonATe-1.0.9/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from codecs import open
+
+from setuptools import find_packages, setup
+
+with open('README') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='resonATe',
+    version='1.0.9',
+    description='resonate data analysis package',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Alex Nunes',
+    include_package_data=True,
+    author_email='anunes@dal.ca',
+    url='https://gitlab.oceantrack.org/otndc/resonate',
+    download_url='https://gitlab.oceantrack.org/otndc/resonate',
+    license='GPLv2',
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
