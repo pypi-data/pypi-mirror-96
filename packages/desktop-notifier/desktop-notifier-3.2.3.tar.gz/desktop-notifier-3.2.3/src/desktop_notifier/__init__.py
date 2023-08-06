b15f06e22c3b1336128b91dd53e31400c66e48fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:01 +0000

    Add package desktop-notifier/3.2.3/desktop-notifier-3.2.3.tar.gz

---
+++ packages/desktop-notifier/desktop-notifier-3.2.3.tar.gz/desktop-notifier-3.2.3/src/desktop_notifier/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .main import DesktopNotifier, Button, ReplyField, Notification, Urgency
+
+
+__version__ = "3.2.3"
+__author__ = "Sam Schott"
+__url__ = "https://github.com/samschott/desktop-notifier"
