c3c3328c19790db7b187836be3dd5ee3106cdf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:46 +0000

    Add package fastlabel/0.3.1/fastlabel-0.3.1.tar.gz

---
+++ packages/fastlabel/fastlabel-0.3.1.tar.gz/fastlabel-0.3.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+with open("requirements.txt", encoding="utf-8") as file:
+    install_requires = file.read()
+
+setuptools.setup(
+    name="fastlabel",
+    version="0.3.1",
+    author="eisuke-ueta",
+    author_email="eisuke.ueta@fastlabel.ai",
+    description="The official Python SDK for FastLabel API, the Data Platform for AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    python_requires=">=3.7",
+    include_package_data=True,
+)
