253bb10124b9b486624123c3d8872c130725c91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:19 +0000

    Add package mygene/3.2.1/mygene-3.2.1.tar.gz

---
+++ packages/mygene/mygene-3.2.1.tar.gz/mygene-3.2.1/setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="mygene",
+    version="3.2.1",
+    author="Chunlei Wu, Cyrus Afrasiabi, Sebastien Lelong",
+    author_email="cwu@scripps.edu",
+    description="Python Client for MyGene.Info services.",
+    license="BSD",
+    keywords="biology gene annotation web service client api",
+    url="https://github.com/biothings/mygene.py",
+    packages=['mygene'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    install_requires=[
+        'biothings_client>=0.2.5'
+    ],
+)
