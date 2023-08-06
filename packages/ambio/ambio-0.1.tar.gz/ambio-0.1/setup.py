07b107e8916d9a669e47e48f3abe1c5e87bc468f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:29 +0000

    Add package ambio/0.1/ambio-0.1.tar.gz

---
+++ packages/ambio/ambio-0.1.tar.gz/ambio-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ambio',
+      version='0.1',
+      description='A lightweight Bioinformatics library',
+      url='https://github.com/albertomosconi/ambio',
+      author='Alberto Mosconi',
+      author_email="albertomaria.mosconi@gmail.com",
+      license='MIT',
+      packages=['ambio'],
+      zip_safe=False)
