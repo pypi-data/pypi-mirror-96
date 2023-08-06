e787ed1014e96a8898e7c3d6f3befae360cd315d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:37 +0000

    Add package incqueryserver-api-python-client/0.18.0.post1/incqueryserver-api-python-client-0.18.0.post1.tar.gz

---
+++ packages/incqueryserver-api-python-client/incqueryserver-api-python-client-0.18.0.post1.tar.gz/incqueryserver-api-python-client-0.18.0.post1/iqs_client/api/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from iqs_client.api.acquisition_api import AcquisitionApi
+from iqs_client.api.analysis_api import AnalysisApi
+from iqs_client.api.async_api import AsyncApi
+from iqs_client.api.demo_api import DemoApi
+from iqs_client.api.impact_analysis_api import ImpactAnalysisApi
+from iqs_client.api.in_memory_index_api import InMemoryIndexApi
+from iqs_client.api.integration_api import IntegrationApi
+from iqs_client.api.mms_repository_api import MmsRepositoryApi
+from iqs_client.api.persistent_index_api import PersistentIndexApi
+from iqs_client.api.queries_api import QueriesApi
+from iqs_client.api.query_execution_api import QueryExecutionApi
+from iqs_client.api.repository_api import RepositoryApi
+from iqs_client.api.server_management_api import ServerManagementApi
+from iqs_client.api.validation_api import ValidationApi
