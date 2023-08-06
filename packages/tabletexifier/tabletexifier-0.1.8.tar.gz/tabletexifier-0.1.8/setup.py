7b2e01ac575b9d00577e8a0543c86c2e20183c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:05 +0000

    Add package tabletexifier/0.1.8/tabletexifier-0.1.8.tar.gz

---
+++ packages/tabletexifier/tabletexifier-0.1.8.tar.gz/tabletexifier-0.1.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tabletexifier", 
+    version="0.1.8",
+    author="Kamuish",
+    author_email="andremiguel952@gmail.com",
+    description="Pretty formatted tables that can be exported to LaTeX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
