f92eea56f3f8eec140c987af801ff302a7550de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:37 +0000

    Add package sheraf/0.4.5/sheraf-0.4.5.tar.gz

---
+++ packages/sheraf/sheraf-0.4.5.tar.gz/sheraf-0.4.5/sheraf/__init__.py
@@ -0,0 +1,85 @@
+# pylint: disable=F,W
+# flake8: noqa
+from ZODB.DemoStorage import DemoStorage
+from ZODB.FileStorage import FileStorage
+from ZODB.MappingStorage import MappingStorage
+
+from .attributes import Attribute, BaseAttribute, set_read_memoization
+from .attributes.blobs import Blob, BlobAttribute
+from .attributes.collections import (
+    DictAttribute,
+    LargeDictAttribute,
+    LargeListAttribute,
+    ListAttribute,
+    SetAttribute,
+    SmallDictAttribute,
+    SmallListAttribute,
+)
+from .attributes.counter import CounterAttribute
+from .attributes.files import (
+    FileAttribute,
+    FileObject,
+    FilesGarbageCollector,
+    set_files_root_dir,
+)
+from .attributes.index import Index
+from .attributes.models import (
+    ModelAttribute,
+    InlineModelAttribute,
+    IndexedModelAttribute,
+)
+from .attributes.password import PasswordAttribute
+from .attributes.simples import (
+    BooleanAttribute,
+    DateAttribute,
+    DateTimeAttribute,
+    FloatAttribute,
+    IntegerAttribute,
+    SimpleAttribute,
+    StringAttribute,
+    StringUUIDAttribute,
+    TimeAttribute,
+    UUIDAttribute,
+)
+from .health import check_health, print_health
+from .constants import ASC, DESC
+from .databases import Database, connection
+from .exceptions import (
+    SherafException,
+    ObjectNotFoundException,
+    ModelObjectNotFoundException,
+    SameNameForTableException,
+    NotConnectedException,
+    InvalidFilterException,
+    InvalidIndexException,
+    InvalidOrderException,
+    QuerySetUnpackException,
+    EmptyQuerySetUnpackException,
+    PrimaryKeyException,
+    UniqueIndexException,
+)
+from .models import (
+    AttributeModel,
+    IntIndexedIntAttributesModel,
+    IntIndexedModel,
+    IntIndexedNamedAttributesModel,
+    IntOrderedNamedAttributesModel,
+    Model,
+    UUIDIndexedDatedNamedAttributesModel,
+    UUIDIndexedModel,
+    UUIDIndexedNamedAttributesModel,
+)
+from .models.attributes import (
+    DatedNamedAttributesModel,
+    IntAttributesModel,
+    NamedAttributesModel,
+)
+from .models.base import BaseModel
+from .models.indexation import (
+    BaseIndexedModel,
+    IndexedModel,
+)
+from .models.inline import InlineModel
+from .queryset import QuerySet
+from .transactions import attempt, commit
+from .version import __version__, __version_info__
---
+++ packages/sheraf/sheraf-0.4.5.tar.gz/sheraf-0.4.5/sheraf/models/inline.py
@@ -0,0 +1,82 @@
+from sheraf.attributes import Attribute
+
+from .attributes import NamedAttributesModel
+
+
+class InlineModel(NamedAttributesModel):
+    """
+    :class:`~sheraf.models.inline.InlineModel` behaves like a regular model, but
+    it is not indexed by itself. This has several consequences:
+    - The ``table`` attribute is not needed.
+    - The :func:`~sheraf.models.base.BaseModel.read` method is not available.
+    :class:`~sheraf.models.inline.InlineModel` aims to be used in combination
+    with :class:`~sheraf.attributes.models.InlineModelAttribute`.
+
+    >>> class Gun(sheraf.InlineModel):
+    ...     nb_bullets = sheraf.IntegerAttribute()
+    ...
+    >>> class Cowboy(sheraf.Model):
+    ...     table = "cowboy"
+    ...     name = sheraf.SimpleAttribute()
+    ...     gun = sheraf.InlineModelAttribute(Gun)
+    ...
+    >>> with sheraf.connection(commit=True):
+    ...     george = Cowboy.create(
+    ...         name="George Abitbol",
+    ...         gun=Gun.create(nb_bullets=5),
+    ...     )
+    ...     assert 5 == george.gun.nb_bullets
+
+    You can manage your own indexation by combining
+    :class:`~sheraf.attributes.models.InlineModelAttribute` with a collection
+    attribute, like :class:`~sheraf.attributes.collections.DictAttribute`.
+
+    >>> class Gun(sheraf.InlineModel):
+    ...     nb_bullets = sheraf.IntegerAttribute()
+    ...
+    >>> class Cowboy(sheraf.Model):
+    ...     table = "cowboy"
+    ...     name = sheraf.SimpleAttribute()
+    ...     guns = sheraf.LargeDictAttribute(
+    ...         sheraf.InlineModelAttribute(Gun)
+    ...     )
+    ...
+    >>> with sheraf.connection(commit=True):
+    ...     george = Cowboy.create(
+    ...         name="George Abitbol",
+    ...         guns = {
+    ...             "martha": Gun.create(nb_bullets=6),
+    ...             "gretta": Gun.create(nb_bullets=5),
+    ...         }
+    ...     )
+    ...     assert 6 == george.guns["martha"].nb_bullets
+
+    :class:`~sheraf.models.inline.InlineModel` can also be anonymous. To create
+    an anonymous model instance, just pass the attributes list as parameter of the
+    class.
+
+    >>> class Cowboy(sheraf.Model):
+    ...     table = "cowboy"
+    ...     name = sheraf.SimpleAttribute()
+    ...     horse = sheraf.InlineModelAttribute(
+    ...         sheraf.InlineModel(
+    ...             name=sheraf.SimpleAttribute()
+    ...         )
+    ...     )
+    ...
+    >>> with sheraf.connection(commit=True):
+    ...     george = Cowboy.create(horse=dict(name="Jolly Jumper"))
+    ...     george.horse.name
+    'Jolly Jumper'
+    """
+
+    def __init__(self, **kwargs):
+        if kwargs:
+            self.__class__.attributes = {}
+
+        super().__init__()
+
+        for k, v in kwargs.items():
+            if isinstance(v, Attribute):
+                v.attribute_name = k
+                self.attributes[k] = v
