b0b6a1b0a23f2f3eab7aceb2e625cf0f3f0bbb31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:34 +0000

    Add package pytorch-segmentation-models-trainer/0.1/pytorch_segmentation_models_trainer-0.1-py3-none-any.whl

---
+++ packages/pytorch-segmentation-models-trainer/pytorch_segmentation_models_trainer-0.1-py3-none-any.whl/pytorch_segmentation_models_trainer/predict.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ pytorch_segmentation_models_trainer
+                              -------------------
+        begin                : 2021-03-02
+        git sha              : $Format:%H$
+        copyright            : (C) 2021 by Philipe Borba - Cartographic Engineer
+                                                            @ Brazilian Army
+        email                : philipeborba at gmail dot com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ****
+"""
+import logging
+
+import hydra
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import Trainer
+
+from pytorch_segmentation_models_trainer.model_loader.model import Model
+
+logger = logging.getLogger(__name__)
+
+@hydra.main(config_path="conf", config_name="config")
+def predict(cfg: DictConfig) -> Trainer:
+    print("Not implemented yet.")
+    pass
+
+if __name__=="__main__":
+    predict()
