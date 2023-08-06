2fbcd1df5df356448add032fd3c5b3e70b1d19c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:19 +0000

    Add package boschshcpy/0.2.8.dev4/boschshcpy-0.2.8.dev4.tar.gz

---
+++ packages/boschshcpy/boschshcpy-0.2.8.dev4.tar.gz/boschshcpy-0.2.8.dev4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="boschshcpy",
+    version="0.2.8.dev4",
+    url="https://github.com/tschamm/boschshcpy",
+    author="Clemens-Alexander Brust, Thomas Schamm",
+    author_email="cabrust@pm.me, thomas@tschamm.de",
+    description="Bosch Smart Home Controller API Python Library",
+    license='bsd-3-clause',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['boschshcpy'],
+    package_dir={'boschshcpy': 'boschshcpy'},
+    package_data={'boschshcpy': ['tls_ca_chain.pem']},
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: BSD License"
+    ],
+    platform='any',
+    python_requires='>=3.7',
+    install_requires=['requests>=2.22', 'zeroconf>=0.28.0', 'getmac==0.8.2']
+)
