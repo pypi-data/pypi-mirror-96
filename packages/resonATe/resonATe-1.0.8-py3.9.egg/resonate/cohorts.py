4a181bb7355525f0ce9ded025d9a9c7c6d66d61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package resonATe/1.0.8/resonATe-1.0.8-py3.9.egg

---
+++ packages/resonATe/resonATe-1.0.8-py3.9.egg/resonate/cohorts.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+
+def cohort(compressed_df, interval_time=3600):
+    """
+    Creates a dataframe of cohorts using a compressed detection file
+
+    :param compressed_df: compressed dataframe
+    :param interval_time: cohort detection time interval (in seconds)
+    :return: cohort dataframe with the following columns
+
+        * anml_1
+        * anml_1_seq
+        * station
+        * anml_2
+        * anml_2_seq
+        * anml_2_arrive
+        * anml_2_depart
+        * anml_2_startunqdetecid
+        * anml_2_endunqdetecid
+        * anml_2_detcount
+
+    """
+
+    # Convert input int interval_time into a timedelta object
+    interval_time = pd.to_timedelta(interval_time, unit='s')
+
+    # Sort input compressed data file
+    cmps = compressed_df.sort_values(['catalognumber', 'seq_num'])
+
+    # Loop through rows in the compressed data file and choose
+    # cohorts if the times are within range
+    seen = []
+    final_set = []
+
+    for idx1, item1 in cmps.iterrows():
+        match = cmps[
+            (cmps.station == item1.station) &
+            (cmps.catalognumber != item1.catalognumber) &
+            (~cmps.index.to_series().apply(lambda x: (x, idx1)).isin(seen)) &
+            ((cmps.startdate > item1.startdate - interval_time) &
+                (cmps.startdate < item1.enddate + interval_time) |
+                (cmps.enddate > item1.startdate - interval_time) &
+                (cmps.enddate < item1.enddate + interval_time))]
+
+        if not match.empty:
+            match.insert(0, 'anml_1', item1.catalognumber)
+            match.insert(1, 'anml_1_seq', item1.seq_num)
+            final_set.extend(match[['anml_1', 'anml_1_seq', 'station',
+                                    'catalognumber', 'seq_num', 'startdate',
+                                    'enddate', 'startunqdetecid',
+                                    'endunqdetecid', 'total_count'
+                                    ]].values.tolist())
+            seen.extend([(idx1, i) for i in match.index.tolist()])
+
+    output_df = pd.DataFrame(final_set)
+    output_df.columns = ['anml_1',
+                         'anml_1_seq',
+                         'station',
+                         'anml_2',
+                         'anml_2_seq',
+                         'anml_2_arrive',
+                         'anml_2_depart',
+                         'anml_2_startunqdetecid',
+                         'anml_2_endunqdetecid',
+                         'anml_2_detcount']
+    return output_df
---
+++ packages/resonATe/resonATe-1.0.8-py3.9.egg/resonate/interval_data_tool.py
@@ -0,0 +1,67 @@
+import geopy
+import pandas as pd
+
+
+def interval_data(compressed_df, dist_matrix_df, station_radius_df=None):
+    """
+    Creates a detection interval file from a compressed detection, distance matrix and station detection radius DataFrames
+
+    :param compressed_df: compressed detection dataframe
+    :param dist_matrix_df: station distance matrix dataframe
+    :param station_radius: station distance radius dataframe
+    :return: interval detection Dataframe
+    """
+
+    # Create two dataframes from input compressed detections and decrement the second's seq_num
+    fst = compressed_df[
+        ['catalognumber', 'station', 'seq_num', 'total_count', 'startdate', 'enddate', 'endunqdetecid']].copy()
+    snd = compressed_df[
+        ['catalognumber', 'station', 'seq_num', 'total_count', 'startdate', 'enddate', 'endunqdetecid']].copy()
+    snd.seq_num -= 1
+
+    # Rename columns
+    fst.columns = ['catalognumber', 'from_station', 'seq_num', 'from_detcnt',
+                   'from_arrive', 'from_leave', 'unqdetid_from']
+    snd.columns = ['catalognumber', 'to_station', 'seq_num', 'to_detcnt',
+                   'to_arrive', 'to_leave', 'unqdetid_arrive']
+
+    # Merge the two DataFrames together linking catalognumber and seq_num
+    merged = pd.merge(fst, snd, how='left', on=['catalognumber', 'seq_num'])
+
+    # Create additional column placeholders
+    merged['intervaltime'] = None
+    merged['intervalseconds'] = None
+    merged['distance_m'] = None
+    merged['metres_per_second'] = None
+
+    # Loop through all rows linking distance matrices and calculating average speed between intervals
+    for idx, item in merged.iterrows():
+        # If any of the station pairs don't exist, skip processing current row
+        if not (pd.isnull(item['from_station']) or pd.isnull(item['to_station'])):
+            # Get station matrix distance (input in m)
+            matrix_distance_m = dist_matrix_df.loc[item['from_station'],
+                                                   item['to_station']]
+
+            # If matrix pair exists do distance calculations
+            if matrix_distance_m:
+                if isinstance(station_radius_df, pd.DataFrame):
+                    stn1_radius = station_radius_df.loc[item['from_station'], 'radius']
+                    stn2_radius = station_radius_df.loc[item['to_station'], 'radius']
+
+                    distance = max(geopy.distance.Distance(
+                        0).m, matrix_distance_m - stn1_radius.m - stn2_radius.m) * 1000
+                else:
+                    distance = max(geopy.distance.Distance(
+                        0).m, matrix_distance_m) * 1000
+
+                merged.loc[idx, 'distance_m'] = distance
+
+                time_interval = item['to_arrive'] - item['from_leave']
+                merged.loc[idx, 'intervaltime'] = time_interval
+                merged.loc[idx, 'intervalseconds'] = time_interval.total_seconds()
+
+                if time_interval.seconds != 0:
+                    merged.loc[idx, 'metres_per_second'] = distance / \
+                        time_interval.seconds
+
+    return merged
