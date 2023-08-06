4703b19a123eb9b31c4cdfb12248384cfc7a0c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:03 +0000

    Add package attachment-downloader/1.1.4/attachment-downloader-1.1.4.tar.gz

---
+++ packages/attachment-downloader/attachment-downloader-1.1.4.tar.gz/attachment-downloader-1.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='attachment-downloader',
+    version='1.1.4',
+    description='Simple tool for downloading email attachments for all emails in a given folder using an IMAP client.',
+    long_description=open('README.rst').read(),
+    author='James Ridgway',
+    url='https://github.com/jamesridgway/attachment-downloader',
+    license='MIT',
+    packages=['attachment_downloader'],
+    scripts=['bin/attachment-downloader'],
+    install_requires=["jinja2", "imbox"]
+)
