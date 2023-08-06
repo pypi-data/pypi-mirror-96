39740e89b1f007a773859fb0fef0a1ed7f877db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:40 +0000

    Add package easy-athena/0.0.3/easy_athena-0.0.3-py3-none-any.whl

---
+++ packages/easy-athena/easy_athena-0.0.3-py3-none-any.whl/tests/test_default.py
@@ -0,0 +1,5 @@
+import easy_athena
+
+def test_query():
+    result = easy_athena.EasyAthena(output_bucket_name='kmong-data-lake', output_prefix='97.trash/athena/query_output').query("""SELECT * FROM "history"."buyer_coupon_providers_20201201" limit 10;""")
+    assert len(result) == 10
