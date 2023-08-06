367118c694d2eae5c1514dd81154820229b9b533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:59 +0000

    Add package traxex/0.0.3/traxex-0.0.3.tar.gz

---
+++ packages/traxex/traxex-0.0.3.tar.gz/traxex-0.0.3/README.md
@@ -0,0 +1,21 @@
+# Traxex
+
+Quick schedule for competitive DotA 2 Tournaments.
+
+## Installation
+
+```
+pip install traxex
+```
+
+## Endpoints
+
+* ongoing
+* upcoming
+* ended
+
+# Usage example
+```py
+import traxex
+print(traxex.ongoing())
+```
---
+++ packages/traxex/traxex-0.0.3.tar.gz/traxex-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import re
+
+from setuptools import setup
+
+
+version = ''
+with open('traxex/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name='traxex',
+    author='sinkaroid',
+    author_email='anakmancasan@gmail.com',
+    version='0.0.3',
+    long_description=readme,
+    url='https://github.com/sinkaroid/traxex.py',
+    packages=['traxex'],
+    license='MIT',
+    description='Quick schedule for competitive DotA 2 Tournaments.',
+    include_package_data=True,
+    keywords = ['dota 2', 'wrapper', 'liquipedia'],
+    install_requires=requirements
+)
