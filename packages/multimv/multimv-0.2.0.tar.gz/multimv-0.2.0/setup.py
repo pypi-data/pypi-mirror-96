9274957d11c44d141350a15cfc4fe6cf43e5f1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:21 +0000

    Add package multimv/0.2.0/multimv-0.2.0.tar.gz

---
+++ packages/multimv/multimv-0.2.0.tar.gz/multimv-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multimv']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['multimv = multimv:main']}
+
+setup_kwargs = {
+    'name': 'multimv',
+    'version': '0.2.0',
+    'description': 'Multi mv via fixed string / regex / bash pattern substitutions',
+    'long_description': None,
+    'author': 'Summer',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
