6eee550fc3b2336ab04ebc3429e79907071d7674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:29 +0000

    Add package indeterminatebeam/v2.0.1/indeterminatebeam-v2.0.1.tar.gz

---
+++ packages/indeterminatebeam/indeterminatebeam-v2.0.1.tar.gz/indeterminatebeam-v2.0.1/indeterminatebeam/data_validation.py
@@ -0,0 +1,35 @@
+"""Module to aid with data validation"""
+
+
+def assert_positive_number(n, name):
+    """Assert that an input is a positive number."""
+    if type(n) not in [int, float]:
+        raise ValueError(
+            f"The value for '{name}' should be an integer or a float, not a {type(n)}.")
+    if n < 0:
+        raise ValueError(
+            f"The value for '{name}' should be >= 0, not {n}")
+
+
+def assert_strictly_positive_number(n, name):
+    """Assert that an input is a strictly positive number."""
+    if type(n) not in [int, float]:
+        raise ValueError(
+            f"The value for '{name}' should be an integer or a float, not a {type(n)}.")
+    if n < 0:
+        raise ValueError(
+            f"The value for '{name}' should be > 0, not {n}")
+
+
+def assert_number(n, name):
+    """Assert that an input is a number."""
+    if type(n) not in [int, float]:
+        raise ValueError(
+            f"The value for '{name}' should be an integer or a float, not a {type(n)}.")
+
+
+def assert_length(var, num, name):
+    """Assert that a tuple or list is of a specific length."""
+    if len(var) != num:
+        raise ValueError(
+            f"The length of '{name}' should be {num} not {len(var)}.")
