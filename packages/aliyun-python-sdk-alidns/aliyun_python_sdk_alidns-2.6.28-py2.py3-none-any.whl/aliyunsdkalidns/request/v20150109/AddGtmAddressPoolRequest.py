97954d816b32c8419b39b96a8ae0937f7eb8c2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package aliyun-python-sdk-alidns/2.6.28/aliyun_python_sdk_alidns-2.6.28-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-alidns/aliyun_python_sdk_alidns-2.6.28-py2.py3-none-any.whl/aliyunsdkalidns/request/v20150109/AddGtmAddressPoolRequest.py
@@ -0,0 +1,114 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+
+class AddGtmAddressPoolRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Alidns', '2015-01-09', 'AddGtmAddressPool','alidns')
+		self.set_method('POST')
+
+	def get_MonitorExtendInfo(self):
+		return self.get_query_params().get('MonitorExtendInfo')
+
+	def set_MonitorExtendInfo(self,MonitorExtendInfo):
+		self.add_query_param('MonitorExtendInfo',MonitorExtendInfo)
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_Timeout(self):
+		return self.get_query_params().get('Timeout')
+
+	def set_Timeout(self,Timeout):
+		self.add_query_param('Timeout',Timeout)
+
+	def get_MinAvailableAddrNum(self):
+		return self.get_query_params().get('MinAvailableAddrNum')
+
+	def set_MinAvailableAddrNum(self,MinAvailableAddrNum):
+		self.add_query_param('MinAvailableAddrNum',MinAvailableAddrNum)
+
+	def get_EvaluationCount(self):
+		return self.get_query_params().get('EvaluationCount')
+
+	def set_EvaluationCount(self,EvaluationCount):
+		self.add_query_param('EvaluationCount',EvaluationCount)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
+
+	def get_Addrs(self):
+		return self.get_query_params().get('Addr')
+
+	def set_Addrs(self, Addrs):
+		for depth1 in range(len(Addrs)):
+			if Addrs[depth1].get('Mode') is not None:
+				self.add_query_param('Addr.' + str(depth1 + 1) + '.Mode', Addrs[depth1].get('Mode'))
+			if Addrs[depth1].get('LbaWeight') is not None:
+				self.add_query_param('Addr.' + str(depth1 + 1) + '.LbaWeight', Addrs[depth1].get('LbaWeight'))
+			if Addrs[depth1].get('Value') is not None:
+				self.add_query_param('Addr.' + str(depth1 + 1) + '.Value', Addrs[depth1].get('Value'))
+
+	def get_MonitorStatus(self):
+		return self.get_query_params().get('MonitorStatus')
+
+	def set_MonitorStatus(self,MonitorStatus):
+		self.add_query_param('MonitorStatus',MonitorStatus)
+
+	def get_InstanceId(self):
+		return self.get_query_params().get('InstanceId')
+
+	def set_InstanceId(self,InstanceId):
+		self.add_query_param('InstanceId',InstanceId)
+
+	def get_Name(self):
+		return self.get_query_params().get('Name')
+
+	def set_Name(self,Name):
+		self.add_query_param('Name',Name)
+
+	def get_ProtocolType(self):
+		return self.get_query_params().get('ProtocolType')
+
+	def set_ProtocolType(self,ProtocolType):
+		self.add_query_param('ProtocolType',ProtocolType)
+
+	def get_Interval(self):
+		return self.get_query_params().get('Interval')
+
+	def set_Interval(self,Interval):
+		self.add_query_param('Interval',Interval)
+
+	def get_IspCityNodes(self):
+		return self.get_query_params().get('IspCityNode')
+
+	def set_IspCityNodes(self, IspCityNodes):
+		for depth1 in range(len(IspCityNodes)):
+			if IspCityNodes[depth1].get('CityCode') is not None:
+				self.add_query_param('IspCityNode.' + str(depth1 + 1) + '.CityCode', IspCityNodes[depth1].get('CityCode'))
+			if IspCityNodes[depth1].get('IspCode') is not None:
+				self.add_query_param('IspCityNode.' + str(depth1 + 1) + '.IspCode', IspCityNodes[depth1].get('IspCode'))
