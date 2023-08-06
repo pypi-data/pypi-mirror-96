6334d863399a3eb51b9dbae6b40ddd4c26c44e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:18 +0000

    Add package xiRT/1.2.3/xiRT-1.2.3-py3-none-any.whl

---
+++ packages/xiRT/xiRT-1.2.3-py3-none-any.whl/xirt/processing.py
@@ -0,0 +1,179 @@
+"""Module with higher level processing functions.
+
+Combines several sequence processing steps for convenience and sequence-
+unrelated processing functions.
+"""
+
+import numpy as np
+import pandas as pd
+from pyteomics import parser
+from tensorflow.keras import utils
+import logging
+from xirt import sequences as xs
+
+
+logger = logging.getLogger(__name__)
+
+
+def prepare_seqs(psms_df, seq_cols=["Peptide1", "Peptide2"]):
+    """Convert Peptide sequences to unified format.
+
+    This conversion simplifies the alphabet of the amino acids, removes special characters,
+    replaces digits to written numbers, removes n-terminal modifications, rewrites Xmod
+    modifications to modX format and splits the amino acid string into a list.
+
+    Args:
+        psms_df:  df, psm dataframe
+        seq_cols: list, list of columns names that contain the peptide sequences with
+        modifications
+
+    Returns:
+        df, sequence arrays
+
+    Information:
+        Performs the following steps:
+        - simplifie amino acid alphabet by replacing non-sandard aa
+        - remove special characters (brackets, _)
+        - replace numbers by written numbers (3 -> three)
+        - remove n-terminal mods since they are currently not supported
+        - rewrite peptide sequence to modX format (e.g. Mox -> oxM)
+        - parse a string sequence into a list of amino acids, adding termini
+    """
+    # for all sequence columns in the dataframe perform the processing
+    # if linear only a single column, if cross-linked two columns are processed
+    logger.info("Preparing peptide sequences for columns: {}".format(",".join(seq_cols)))
+
+    for seq_col in seq_cols:
+        # code if sequences are represented with N.SEQUENCE.C
+        if "." in psms_df.iloc[0][seq_col]:
+            psms_df["Seq_" + seq_col] = psms_df[seq_col].str.split(".").str[1]
+        else:
+            psms_df["Seq_" + seq_col] = psms_df[seq_col]
+
+        sequences = psms_df["Seq_" + seq_col]
+        sequences = sequences.apply(xs.simplify_alphabet)
+        sequences = sequences.apply(xs.remove_brackets_underscores)
+        sequences = sequences.apply(xs.replace_numbers)
+        sequences = sequences.apply(xs.remove_nterm_mod)
+        sequences = sequences.apply(xs.rewrite_modsequences)
+        sequences = sequences.apply(parser.parse, show_unmodified_termini=True)
+        psms_df["Seqar_" + seq_col] = sequences
+    return psms_df
+
+
+def featurize_sequences(psms_df, seq_cols=["Seqar_Peptide1", "Seqar_Peptide2"], max_length=-1):
+    """Generate a featureized version of sequences from a data frame.
+
+    The featureization is done via by retrieving all modifications, the relevant alphabet of amino
+    acids and then applying label encoding do the amino acid sequences.
+
+    :param psms_df: df, dataframe with identifications
+    :param seq_cols: list, list with column names
+    :param max_legnth: int, maximal length for peptides to be included as feature
+    :return:
+    """
+    # transform a list of amino acids to a feature dataframe
+    # if two sequence columns (crosslinks)
+
+    # define lambda function to get the alphabet in both cases
+    f = lambda x: xs.get_alphabet(x)
+
+    # get padding length
+    if max_length == -1:
+        max_length = psms_df[seq_cols].applymap(len).max().max()
+    logger.info("Setting max_length to: {}".format(max_length))
+
+    # get amino acid alphabet
+    if len(seq_cols) > 1:
+        alphabet = np.union1d(f(psms_df[seq_cols[0]].str.join(sep="").drop_duplicates()),
+                              f(psms_df[seq_cols[1]].str.join(sep="").drop_duplicates()))
+    else:
+        alphabet = f(psms_df[seq_cols[0]].str.join(sep="").drop_duplicates())
+
+    logger.info("alphabet: {}".format(alphabet))
+
+    # perform the label encoding + padding
+    encoded_s1, le = xs.label_encoding(psms_df[seq_cols[0]], max_length, alphabet=alphabet)
+    seq1_padded = generate_padded_df(encoded_s1, psms_df.index)
+
+    if len(seq_cols) > 1:
+        encoded_s2, _ = xs.label_encoding(psms_df[seq_cols[1]], max_length, alphabet=alphabet,
+                                          le=le)
+        seq2_padded = generate_padded_df(encoded_s2, psms_df.index)
+    else:
+        seq2_padded = pd.DataFrame()
+
+    return seq1_padded, seq2_padded, le
+
+
+def generate_padded_df(encoded_ar, index):
+    """Generate indexed dataframe from already label-encoded sequences.
+
+    Args:
+        encoded_ar:
+        index:
+
+    Returns:
+        dataframe, label encoded peptides as rows
+    """
+    seqs_padded = pd.DataFrame(encoded_ar, index=index)
+    seqs_padded.columns = ["rnn_{}".format(str(i).zfill(2)) for i in np.arange(len(encoded_ar[0]))]
+    return seqs_padded
+
+
+def fraction_encoding(psms_df, rt_methods):
+    """
+    Add the necessary fraction columns.
+
+    Following columns are added: prefix_0based, Actual_prefix and prefix_1hot, where
+    prefix is taken from the rt_methods argument.
+
+    Parameters:
+        psms_df: df, dataframe with peptide identifications and fraction columns
+        rt_methods: ar-like, list of columns that have the fraction encoding.
+
+    Return:
+        None
+    """
+    for col in rt_methods:
+        # encode the classes for the fractions 0-based
+        frac_unique = psms_df[col].drop_duplicates().sort_values()
+        # get 0-based numbers
+        frac_unique = pd.Series(np.arange(frac_unique.size), index=frac_unique)
+        # add new column to dataframe
+        psms_df[col + "_0based"] = frac_unique.loc[psms_df[col]].values
+
+        # special encoding
+        nclasses = len(frac_unique)
+        classes = {}
+        for class_i in np.arange(0, nclasses):
+            # version 1
+            # 1, 2, 3 -> [1, 0, 0], [1, 1, 0], [1, 1, 1]
+            # init_class_encoding = np.zeros(nclasses)
+            # init_class_encoding[:class_i+1] = 1
+            # classes[class_i] = init_class_encoding
+            # version 2
+            init_class_encoding = np.ones(nclasses)
+            init_class_encoding[class_i:] = 0
+            classes[class_i] = init_class_encoding
+
+        # make categorical
+        frac_categorical = utils.to_categorical(psms_df[col + "_0based"])
+
+        psms_df[col + "_1hot"] = list(frac_categorical)
+        psms_df[col + "_ordinal"] = psms_df[col + "_0based"].map(classes)
+
+
+def transform_RT(retention_times):
+    """
+    Return RT in minutes.
+
+    Args:
+        retention_times: ar-like, observed RT data
+
+    Returns:
+        ar-like, transformed retention times in minutes
+    """
+    if (retention_times > 720).sum() > 0:
+        retention_times = retention_times / 60.
+    return retention_times
---
+++ packages/xiRT/xiRT-1.2.3-py3-none-any.whl/xirt/sequences.py
@@ -0,0 +1,259 @@
+"""Module to process peptide sequences."""
+import re
+from collections import Counter
+import numpy as np
+from pyteomics import parser
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing import sequence as ts
+
+
+def simplify_alphabet(sequence):
+    """Replace ambiguous amino acids.
+
+    Some sequences are encoded with 'U', arbitrarily choose C as residue to
+    replace any U (Selenocystein).
+
+    Parameters:
+    ----------
+    sequence: string,
+              peptide sequences
+    """
+    return sequence.replace("U", "C")
+
+
+def remove_brackets_underscores(sequence):
+    """Remove all brackets (and underscores...) from protein sequences.
+
+    Needed for MaxQuant processing.
+
+    Parameters:
+    ----------
+    sequence: string,
+              peptide sequences
+    """
+    return re.sub(r"[\(\)\[\]_\-]", "", sequence)
+
+
+def replace_numbers(sequence):
+    """Replace digits to words (necessary for modX format to work.
+
+    :param sequence:
+    :return:
+    """
+    rep = {"1": "one",
+           "2": "two",
+           "3": "three",
+           "4": "four",
+           "5": "five",
+           "6": "six",
+           "7": "seven",
+           "8": "eight",
+           "9": "nine",
+           "0": "zero"}
+    pattern = re.compile(r"|".join(rep.keys()))
+    return pattern.sub(lambda m: rep[re.escape(m.group(0))], sequence)
+
+
+def remove_nterm_mod(sequence):
+    """Remove the nterminal modification.
+
+    Meant to be used for "ac" modifications in front of the sequence.
+    They are not currently supported and need to be removed.
+
+    :param sequence: str, peptide sequence
+    :return:
+    """
+    return re.sub(r'^([a-z]+)([A-Z])', r'\2', sequence, flags=re.MULTILINE)
+
+
+def rewrite_modsequences(sequence):
+    """Rewrite modified sequences to modX format.
+
+    Requires the input to be preprocessed such that no brackets are in the sequences.
+
+    Meant to be used via apply.
+
+    Example:
+    -------
+    sequence = "ELVIS"
+    sequence = "ELVISCcmASD"
+
+    :param sequence: str, peptide sequence
+    :return:
+    """
+    return re.sub(r"([A-Z])([^A-Z]+)", r'\2\1', sequence)
+
+
+def remove_lower_letters(sequence):
+    """Remove lower capital letters from the sequence.
+
+    :param sequence: str, peptide sequence
+    :return:
+    """
+    return re.sub(r"[a-z]", "", sequence)
+
+
+def to_unmodified_sequence(sequence):
+    """Remove lower capital letters, brackets from the sequence.
+
+    :param sequence: str, peptide sequence
+    :return:
+    """
+    return (re.sub(r"[^[A-Z]", "", sequence))
+
+
+def reorder_sequences(matches_df):
+    """Reorder peptide sequences by length.
+
+    Defining the longer peptide as alpha peptide and the shorter petpide as
+    beta peptide. Ties are resolved lexicographically.
+
+    Args:
+        matches_df: df, dataframe with peptide identifications.
+
+    Returns:
+        df, dataframe with the swapped cells and additional indicator column ('swapped')
+    """
+    # compile regex to match columns with 1/2 in the end of the string
+    # check if all pairwise columns are there
+    r = re.compile(r"\w+(?:1$|2$)")
+    # get matching columns
+    match_columns = list(filter(r.match, matches_df.columns))
+    # remove number
+    pairs_noidx = [i[:-1] for i in match_columns]
+    # count occurrences and check if pairs are there
+    counts = [(i, j) for i, j in Counter(pairs_noidx).items() if j == 2]
+    if len(counts) * 2 != len(pairs_noidx):
+        print(match_columns)
+        raise ValueError("Error! Automatic column matching could not find pairwise crosslink "
+                         "columns. Please make sure that you have a peptide1, peptide2 "
+                         "column name pattern for crosslinks. Columns must appear in pairs if there"
+                         " is a number in the end of the name.")
+
+    # order logic, last comparison checks lexigographically
+    is_longer = (matches_df["Peptide1"].apply(len) > matches_df["Peptide2"].apply(len)).values
+    is_shorter = (matches_df["Peptide1"].apply(len) < matches_df["Peptide2"].apply(len)).values
+    is_greater = (matches_df["Peptide1"] > matches_df["Peptide2"]).values
+
+    # create a copy of the dataframe
+    swapping_df = matches_df.copy()
+    swapped = np.ones(len(matches_df), dtype=bool)
+
+    # z_idx for 0-based index
+    # df_idx for pandas
+    for z_idx, df_idx in enumerate(matches_df.index):
+        if not is_shorter[z_idx] and is_greater[z_idx] or is_longer[z_idx]:
+            # for example: AC - AA, higher first, no swapping required
+            swapped[z_idx] = False
+        else:
+            # for example: AA > AC, other case, swap
+            swapped[z_idx] = True
+
+        if swapped[z_idx]:
+            for col in pairs_noidx:
+                swapping_df.at[df_idx, col + str(2)] = matches_df.iloc[z_idx][col + str(1)]
+                swapping_df.at[df_idx, col + str(1)] = matches_df.iloc[z_idx][col + str(2)]
+    swapping_df["swapped"] = swapped
+    return swapping_df
+
+
+def modify_cl_residues(matches_df, seq_in=["Peptide1", "Peptide2"], reduce_cl=False):
+    """
+    Change the cross-linked residues to modified residues.
+
+    Function uses the Seqar_*suf columns to compute the new peptides.
+
+    Args:
+        matches_df: df, dataframe with peptide identifications. Required columns
+        seq_in: ar-like, list of columns wiht peptide entries
+        reduce_cl: bool, if true crosslinked residues are reduced to X, else the linked residue
+        is kept (default: False). This is useful for transfer learning or promiscuous crosslinker
+        such as SDA.
+    Returns:
+        psms_df: df, dataframe with adapted sequences in-place
+    """
+    # increase the alphabet by distinguishing between crosslinked K and non-crosslinked K
+    # introduce a new prefix cl for each crosslinked residue
+    for seq_id, seq_i in enumerate(seq_in):
+        for idx, row in matches_df.iterrows():
+            if reduce_cl:
+                # reasign special crosslinker residue
+                residue = "X"
+            else:
+                try:
+                    residue = row["Seqar_" + seq_i][row["LinkPos" + str(seq_id + 1)]]
+                except IndexError:
+                    print(row)
+                    print("List index out of range. Check peptide sequence for unwanted characters")
+                    print(row["Seqar_" + seq_i])
+                    print(seq_id)
+                    print(row["LinkPos" + str(seq_id + 1)])
+
+            matches_df.at[idx, "Seqar_" + seq_i][row["LinkPos" + str(seq_id + 1)]] = "cl" + residue
+
+
+def get_mods(sequences):
+    """Retrieve modifciations from dataframe in the alphabet.
+
+    Parameters
+    ----------
+    sequences : ar-like
+        peptide sequences.
+
+    Returns
+    -------
+    List with modification strings.
+    """
+    return np.unique(re.findall("-OH|H-|[a-z0-9]+[A-Z]", " ".join(sequences)))
+
+
+def get_alphabet(sequences):
+    """Retrieve alphabet of amino acids with modifications.
+
+    Parameters
+    ----------
+    sequences : ar-like
+        peptide sequences.
+
+    Returns
+    -------
+    List with modification strings.
+    """
+    return np.unique(
+        re.findall("-OH|H-|[a-z0-9]+[A-Z]|[A-Z]", " ".join(sequences))
+    )
+
+
+def label_encoding(sequences, max_sequence_length, alphabet=[], le=None):
+    """Label encode a list of peptide sequences.
+
+    Parameters
+    ----------
+    sequences : ar-like
+        list of amino acid characters (n/c-term/modifications.
+    max_sequence_length : int
+        maximal sequence length (for padding).
+    alphabet : list, optional
+        list of the unique characters given in the sequences
+    le : TYPE, optional
+        label encoder instance, can be a prefitted model.
+
+    Returns
+    -------
+    X_encoded : TYPE
+        DESCRIPTION.
+    """
+    # if no alternative alphabet is given, use the defaults
+    if len(alphabet) == 0:
+        alphabet = parser.std_amino_acids
+
+    if not le:
+        # init encoder for the AA alphabet
+        le = LabelEncoder()
+        le.fit(alphabet)
+
+    # use an offset of +1 since shorter sequences will be padded with zeros
+    # to achieve equal sequence lengths
+    X_encoded = sequences.apply(le.transform) + 1
+    X_encoded = ts.pad_sequences(X_encoded, maxlen=max_sequence_length)
+    return X_encoded, le
