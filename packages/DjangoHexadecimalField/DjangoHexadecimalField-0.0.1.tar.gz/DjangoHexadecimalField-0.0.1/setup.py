e0f0948054fbaa990f5bfe3911972ab9f85ae9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:16 +0000

    Add package DjangoHexadecimalField/0.0.1/DjangoHexadecimalField-0.0.1.tar.gz

---
+++ packages/DjangoHexadecimalField/DjangoHexadecimalField-0.0.1.tar.gz/DjangoHexadecimalField-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="DjangoHexadecimalField",
+    version="0.0.1",
+    author="mathrithms",
+    author_email="hello@mathrithms.com",
+    description="Hexadecimal Field for Django",
+    url="https://github.com/mathrithms/Django-Hexadecimal-Field.git",
+    packages=['djangoHexadecimal'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
