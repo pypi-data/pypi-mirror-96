da7c545141b08d21bb6f3f22540ec2dbfbef7a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:21 +0000

    Add package ztools/2.3.6/ztools-2.3.6-py2.py3-none-any.whl

---
+++ packages/ztools/ztools-2.3.6-py2.py3-none-any.whl/ztools/tool/progressbar.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# ----------------------------------------------------------------------------------------------------
+# 类 progressbar
+# ----------------------------------------------------------------------------------------------------
+# 变更履历：
+# 2021-02-27 | Zou Mingzhe   | Ver0.3  | 1.删除 Version(self, isShow = False)
+#            |               |         | 2.修改 Percent(self, ...) 为 percent(self, ...)
+#            |               |         | 3.修改 Number(self, ...)  为 number(self, ...)
+# 2019-05-03 | Zou Mingzhe   | Ver0.2  | 1.增加 EOF(self, clear = False)
+#            |               |         | 2.修改 PercentProgressBar(self, ...) 为 Percent(self, ...)
+#            |               |         | 3.修改 NumberProgressBar(self, ...)  为 Number(self, ...)
+# 2018-09-05 | Zou Mingzhe   | Ver0.1  | 初始版本
+# ----------------------------------------------------------------------------------------------------
+# MAP：
+# 已测试 | percent(self, ...)           | 百分比进度条
+# 已测试 | number(self, ...)            | 数字进度条
+# 已测试 | EOF(self, ...)               | 结束符
+# ----------------------------------------------------------------------------------------------------
+import sys
+# ----------------------------------------------------------------------------------------------------
+class progressbar:
+    """
+    progressbar类提供了进度条字符串。
+    """
+    def __init__(self):
+        self.__version = "0.3"
+        self.__progress = 0
+        self.__maxlength = 0
+        self.__barlist = ['', '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█', '█']
+# ----------------------------------------------------------------------------------------------------
+    def percent(self, progress, title = None, show = False):
+        """
+        百分比进度条：
+        输入参数：progress, show = False
+        返回参数：string
+        说明：调用该方法将返回百分比进度条，百分比进度条 = 百分比 + 进度条。progress必须介于0至1之间。
+        """
+        progress = progress * 100
+        if(progress > 100):
+            progress = 100
+        elif(progress < 0):
+            progress = 0
+        self.__progress = progress
+        strbar = '█'*int(progress / 10) + self.__barlist[int(progress) % 10]
+        string = (str('%s:'%title) if title else '') + str('%.2f%% %s' % (progress, strbar))
+        self.__maxlength = max(len(string), self.__maxlength)
+        if(show):
+            sys.stdout.write(string + '\r')
+            sys.stdout.flush()
+        return string
+# ----------------------------------------------------------------------------------------------------
+    def number(self, done, sum, title = None, show = False):
+        """
+        数字进度条：
+        输入参数：done, sum, show = False
+        返回参数：string
+        说明：调用该方法将返回数字进度条，数字进度条 = done/sum + 进度条。
+        其中，done为已完成进度，sum为总进度。
+        """
+        if(sum <= 0):
+            sum = 1
+        if(done < 0):
+            done = 0
+        if(done > sum):
+            done = sum
+        progress = done * 100 / sum
+        if(progress > 100):
+            progress = 100
+        elif(progress < 0):
+            progress = 0
+        self.__progress = progress
+        strbar = '█'*int(progress / 10) + self.__barlist[int(progress) % 10]
+        string = (str('%s:'%title) if title else '') + str('%.2f%% (%d/%d) %s' % (progress, sum, done, strbar))
+        self.__maxlength = max(len(string), self.__maxlength)
+        if(show):
+            sys.stdout.write(string + '\r')
+            sys.stdout.flush()
+        return string
+# ----------------------------------------------------------------------------------------------------
+    def EOF(self, clear = False):
+        """
+        结束符：
+        输入参数：clear = False
+        返回参数：
+        说明：调用该方法将换行或清空屏幕进度条。
+        """
+        if(clear):
+            for n in range(self.__maxlength):
+                sys.stdout.write(' ')
+                sys.stdout.flush()
+            sys.stdout.write('\r')
+            sys.stdout.flush()
+        else:
+            sys.stdout.write('\n')
+            sys.stdout.flush()
+        self.__maxlength = 0
+# ----------------------------------------------------------------------------------------------------
---
+++ packages/ztools/ztools-2.3.6-py2.py3-none-any.whl/ztools/tool/timeout.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# ----------------------------------------------------------------------------------------------------
+# 类 timeout
+# ----------------------------------------------------------------------------------------------------
+# 变更履历：
+# 2018-10-04 | Zou Mingzhe   | Ver0.2  | 1.修改（Modified）：reset(self) return value
+#            |               |         |   from self.__start_time to 0
+#            |               |         | 2.更名（Rename  ）：start_time(self) -> get_start_time(self)
+# 2018-09-05 | Zou Mingzhe   | Ver0.1  | 初始版本
+# ----------------------------------------------------------------------------------------------------
+# MAP：
+# 已测试 | time(self)                   | 返回计时时间
+# 已测试 | reset(self)                  | 重置计时
+# 已测试 | timeout(self, ...)           | 超时判断
+# 已测试 | get_start_time(self)         | 返回开始计时时间
+# ----------------------------------------------------------------------------------------------------
+import time
+# ----------------------------------------------------------------------------------------------------
+class timeout:
+    """
+    timeout类提供了计时和超时判断功能，它通过调用系统time模块来获取时间。
+    """
+    def __init__(self):
+        self.__version = "0.2"
+        self.__start_time = None
+# ----------------------------------------------------------------------------------------------------
+    def time(self):
+        """
+        返回计时时间：
+        输入参数：无
+        返回参数：time.clock() - self.__start_time/0
+        说明：调用该方法将返回计时时间。
+        若self.__start_time != None，则返回time.clock() - self.__start_time。
+        若self.__start_time == None，则记录当前时间至self.__start_time，返回0。
+        """
+        if(self.__start_time != None):
+            return time.clock() - self.__start_time
+        else:
+            self.__start_time = time.clock()
+            return 0
+# ----------------------------------------------------------------------------------------------------
+    def reset(self):
+        """
+        重置计时：
+        输入参数：无
+        返回参数：0
+        说明：调用该方法将记录当前时间至self.__start_time，返回0。
+        """
+        self.__start_time = time.clock()
+        return 0
+# ----------------------------------------------------------------------------------------------------
+    def timeout(self, threshold):
+        """
+        超时判断：
+        输入参数：threshold
+        返回参数：True/False
+        说明：调用该方法将进行超时判断。
+        若self.__start_time != None，则将time.clock() - self.__start_time与threshold进行比较，
+        若time.clock() - self.__start_time > threshold，返回True，否则返回False。
+        若self.__start_time == None，则记录当前时间至self.__start_time，返回False。
+        """
+        if(self.__start_time != None):
+            if(time.clock() - self.__start_time > threshold):
+                return True
+            else:
+                return False
+        else:
+            self.__start_time = time.clock()
+            return False
+# ----------------------------------------------------------------------------------------------------
+    def get_start_time(self):
+        """
+        返回开始计时时间：
+        输入参数：无
+        返回参数：self.__start_time
+        说明：调用该方法将返回self.__start_time。
+        """
+        return self.__start_time
+# ----------------------------------------------------------------------------------------------------
