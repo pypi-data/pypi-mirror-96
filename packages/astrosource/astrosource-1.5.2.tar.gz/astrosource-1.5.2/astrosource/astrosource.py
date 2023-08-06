3fb54fbf073ee9a0aeba08d4d04685b9396a996c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:02 +0000

    Add package astrosource/1.5.2/astrosource-1.5.2.tar.gz

---
+++ packages/astrosource/astrosource-1.5.2.tar.gz/astrosource-1.5.2/astrosource/astrosource.py
@@ -0,0 +1,99 @@
+from pathlib import Path
+import logging
+import sys
+
+from astrosource.analyse import find_variable_stars, photometric_calculations, calibrated_photometry
+from astrosource.comparison import find_comparisons, find_comparisons_calibrated
+from astrosource.detrend import detrend_data
+from astrosource.eebls import plot_bls
+from astrosource.identify import find_stars, gather_files
+from astrosource.periodic import plot_with_period
+from astrosource.plots import make_plots, make_calibrated_plots, open_photometry_files, output_files, phased_plots
+from astrosource.utils import AstrosourceException, folder_setup, cleanup, setup_logger
+
+
+class TimeSeries:
+    def __init__(self, targets, indir, **kwargs):
+        self.targets = targets
+        self.indir = Path(indir)
+        filelist = kwargs.get('filelist', None)
+        self.format = kwargs.get('format', 'fz')
+        self.imgreject = kwargs.get('imgreject', 0.05)
+        self.periodupper = kwargs.get('periodupper', -99.9)
+        self.periodlower = kwargs.get('periodlower', 0.05)
+        self.periodtests = kwargs.get('periodtests', -99)
+        self.rejectbrighter = kwargs.get('rejectbrighter', 99)
+        self.rejectdimmer = kwargs.get('rejectdimmer', 99)
+        self.thresholdcounts = kwargs.get('thresholdcounts', 1000000)
+        self.hicounts = kwargs.get('hicounts', 1500000)
+        self.lowcounts = kwargs.get('lowcounts', 1000)
+        self.starreject = kwargs.get('starreject', 0.3)
+        self.nopanstarrs = kwargs.get('nopanstarrs', False)
+        self.nosdss = kwargs.get('nosdss', False)
+        self.closerejectd = kwargs.get('closerejectd', 5.0)
+        self.skipvarsearch = kwargs.get('skipvarsearch', False)
+        self.mincompstars = kwargs.get('mincompstars', 0.1)
+        verbose = kwargs.get('verbose', False)
+        bjd = kwargs.get('bjd', False)
+        self.paths = folder_setup(self.indir)
+        logger = setup_logger('astrosource', verbose)
+        self.files, self.filtercode = gather_files(self.paths, filelist=filelist, filetype=self.format, bjd=bjd)
+
+    def analyse(self, calib=True):
+        self.usedimages, self.stars = find_stars(targets=self.targets,
+                                                 paths=self.paths,
+                                                 fileList=self.files,
+                                                 mincompstars=self.mincompstars,
+                                                 imageFracReject=self.imgreject,
+                                                 starreject=self.starreject,
+                                                 hicounts=self.hicounts,
+                                                 lowcounts=self.lowcounts)
+        find_comparisons(self.targets, self.indir, self.usedimages, thresholdCounts=self.thresholdcounts)
+        # Check that it is a filter that can actually be calibrated - in the future I am considering calibrating w against V to give a 'rough V' calibration, but not for now.
+        self.calibrated = False
+        if calib and self.filtercode in ['B', 'V', 'up', 'gp', 'rp', 'ip', 'zs']:
+            try:
+                find_comparisons_calibrated(targets=self.targets,
+                                            filterCode=self.filtercode,
+                                            paths=self.paths,
+                                            nopanstarrs=self.nopanstarrs,
+                                            nosdss=self.nosdss,
+                                            closerejectd=self.closerejectd)
+                self.calibrated = True
+            except AstrosourceException as e:
+                sys.stdout.write(f'🛑 {e}')
+        elif calib:
+            sys.stdout.write(f'⚠️ filter {self.filtercode} not supported for calibration')
+
+    def find_variables(self):
+        find_variable_stars(targets=self.targets, parentPath=self.paths['parent'])
+
+    def photometry(self, filesave=False):
+        data = photometric_calculations(targets=self.targets, paths=self.paths, filesave=filesave)
+        self.output(mode='diff', data=data)
+        if self.calibrated:
+            self.data = calibrated_photometry(paths=self.paths, photometrydata=data)
+            self.output(mode='calib', data=self.data)
+        else:
+            self.data = data
+
+    def plot(self, detrend=False, eebls=False, period=False, phaseShift=0.0, filesave=False):
+        if not hasattr(self, 'data'):
+            self.data = open_photometry_files(self.paths['outcatPath'])
+        make_plots(filterCode=self.filtercode, paths=self.paths, photometrydata=self.data)
+        if self.calibrated:
+            make_calibrated_plots(filterCode=self.filtercode, paths=self.paths, photometrydata=self.data)
+        if detrend:
+            detrend_data(filterCode=self.filtercode, paths=self.paths)
+        if period:
+            self.period = plot_with_period(filterCode=self.filtercode, paths=self.paths, minperiod=self.periodlower, maxperiod=self.periodupper, periodsteps=self.periodtests)
+            if self.calibrated:
+                phased_plots(filterCode=self.filtercode, paths=self.paths, targets=self.targets, period=self.period, phaseShift=phaseShift)
+        if eebls:
+            plot_bls(paths=self.paths)
+
+    def output(self, mode, data):
+        output_files(paths=self.paths, photometrydata=data, mode=mode)
+
+    def clean(self):
+        cleanup(parentPath=self.paths['parent'])
---
+++ packages/astrosource/astrosource-1.5.2.tar.gz/astrosource-1.5.2/astrosource/periodic.py
@@ -0,0 +1,423 @@
+from numpy import asarray, savetxt, std, max, min, genfromtxt, load
+import numpy as np
+import sys
+import os
+import platform
+import glob
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from pathlib import Path
+
+import logging
+
+from astrosource.utils import photometry_files_to_array, AstrosourceException
+
+logger = logging.getLogger('astrosource')
+
+#########################################
+
+def sortByPhase (phases, fluxes):
+    phaseIndices = asarray(phases).argsort()
+    sortedPhases = []
+    sortedFluxes = []
+    for i in range(0,len(phases)):
+        sortedPhases.append(phases[phaseIndices[i]])
+        sortedFluxes.append(fluxes[phaseIndices[i]])
+
+    return (sortedPhases, sortedFluxes)
+
+#########################################
+
+def normalize(fluxes):
+
+    normalizedFluxes = []
+
+    for flux in fluxes:
+        normalizedFlux = (flux - min(fluxes)) / (max(fluxes) - min(fluxes))
+        normalizedFluxes.append(normalizedFlux)
+
+    return(normalizedFluxes)
+
+#########################################
+
+def getPhases(julian_dates, fluxes, period):
+
+    phases = []
+
+    for jd in julian_dates:
+        phases.append((jd / period) % 1)
+
+    (sortedPhases, sortedFluxes) = sortByPhase(phases, fluxes)
+
+    return(sortedPhases, sortedFluxes)
+
+#########################################
+
+# Find the value in array2 corresponding to the minimum value in array1.
+
+def find_minimum(array1, array2):
+    value_to_return = 0.0
+
+    minimum = min(array1)
+
+    for i in range(0, len(array1)):
+        if (array1[i] == minimum):
+          value_to_return = array2[i]
+
+    return (value_to_return, minimum)
+
+#########################################
+
+def sum_distances (sortedPhases, sortedNormalizedFluxes):
+
+    distanceSum = 0.0
+
+    for i in range(0, (len(sortedPhases) - 1)):
+        fluxdiff = sortedNormalizedFluxes[i + 1] - sortedNormalizedFluxes[i]
+        phasediff = sortedPhases[i + 1] - sortedPhases[i]
+
+        distanceSum = distanceSum + (((fluxdiff ** 2) + (phasediff ** 2)) ** 0.5)
+
+    return(distanceSum)
+
+#########################################
+
+def sum_stdevs (sortedPhases, sortedNormalizedFluxes, numBins):
+
+    stdevSum = 0.0
+
+    for i in range (0, numBins):
+        fluxes_inrange = []
+        minIndex = (float(i) / float(numBins)) * float(len(sortedPhases))
+        maxIndex = (float(i + 1) / float(numBins)) * float(len(sortedPhases))
+
+        for j in range (0, len(sortedPhases)):
+            if (j >= minIndex and j < maxIndex):
+                fluxes_inrange.append(sortedNormalizedFluxes[j])
+
+        stdev_of_bin_i = std(fluxes_inrange)
+        stdevSum = stdevSum + stdev_of_bin_i
+
+    return(stdevSum)
+
+#########################################
+
+def phase_dispersion_minimization(varData, periodsteps, minperiod, maxperiod, numBins, periodPath, variableName):
+    #numBins = 10
+    #minperiod=0.2
+    #maxperiod=1
+    #periodsteps=10000
+    periodguess_array = []
+    distance_results = []
+    stdev_results = []
+
+    (julian_dates, fluxes) = (varData[:,0],varData[:,1])
+    normalizedFluxes = normalize(fluxes)
+
+    for r in range(periodsteps):
+        periodguess = minperiod + (r * ((maxperiod-minperiod)/periodsteps))
+        (sortedPhases, sortedNormalizedFluxes) = getPhases(julian_dates, normalizedFluxes, periodguess)
+
+        distance_sum = sum_distances(sortedPhases, sortedNormalizedFluxes)
+        stdev_sum = sum_stdevs(sortedPhases, sortedNormalizedFluxes, numBins)
+
+        periodguess_array.append(periodguess)
+        distance_results.append(distance_sum)
+        stdev_results.append(stdev_sum)
+
+    periodTrialMatrix=[]
+    for r in range(periodsteps):
+        periodTrialMatrix.append([periodguess_array[r],distance_results[r],stdev_results[r]])
+    periodTrialMatrix=np.asarray(periodTrialMatrix)
+    np.savetxt(periodPath / f"{variableName}_Trials.csv", periodTrialMatrix, delimiter=",", fmt='%0.8f')
+
+    (distance_minperiod, distance_min) = find_minimum(distance_results, periodguess_array)
+    (stdev_minperiod, stdev_min) = find_minimum(stdev_results, periodguess_array)
+
+    pdm = {}
+    pdm["periodguess_array"] = periodguess_array
+    pdm["distance_results"] = distance_results
+    pdm["distance_minperiod"] = distance_minperiod
+    pdm["stdev_results"] = stdev_results
+    pdm["stdev_minperiod"] = stdev_minperiod
+
+    # Estimating the error
+    # stdev method
+    # Get deviation to the left
+    totalRange=np.max(stdev_results) - np.min(stdev_results)
+    
+    if np.isnan(pdm["stdev_results"][0]):
+        logger.info("Not enough datapoint coverage to undertake a Phase-Dispersion Minimization routine.")
+    else:
+        for q in range(len(periodguess_array)):
+            if periodguess_array[q]==pdm["stdev_minperiod"]:
+                beginIndex=q
+                beginValue=stdev_results[q]
+
+        currentperiod=stdev_minperiod
+        stepper=0
+        thresholdvalue=beginValue+(0.5*totalRange)
+        while True:
+            if stdev_results[beginIndex-stepper] > thresholdvalue:
+                lefthandP=periodguess_array[beginIndex-stepper]
+                break
+            stepper=stepper+1
+    
+        stepper=0
+        thresholdvalue=beginValue+(0.5*totalRange)
+    
+        while True:
+            if beginIndex+stepper+1 == periodsteps:
+                righthandP=periodguess_array[beginIndex+stepper]
+                logger.debug("Warning: Peak period for stdev method too close to top of range")
+                break
+            if stdev_results[beginIndex+stepper] > thresholdvalue:
+                righthandP=periodguess_array[beginIndex+stepper]
+                break
+            stepper=stepper+1
+    
+    
+        #print ("Stdev method error: " + str((righthandP - lefthandP)/2))
+        pdm["stdev_error"] = (righthandP - lefthandP)/2
+
+
+    # Estimating the error
+    # stdev method
+    # Get deviation to the left
+    totalRange=np.max(distance_results) - np.min(distance_results)
+    for q in range(len(periodguess_array)):
+        if periodguess_array[q]==pdm["distance_minperiod"]:
+            beginIndex=q
+            beginValue=distance_results[q]
+    #print (beginIndex)
+    #print (beginValue)
+    currentperiod=distance_minperiod
+    stepper=0
+    thresholdvalue=beginValue+(0.5*totalRange)
+    while True:
+        #print (beginIndex-stepper)
+        #print (stdev_results[beginIndex-stepper])
+        if distance_results[beginIndex-stepper] > thresholdvalue:
+            #print ("LEFTHAND PERIOD!")
+            #print (periodguess_array[beginIndex-stepper])
+            lefthandP=periodguess_array[beginIndex-stepper]
+            #print (distance_results)
+            break
+        stepper=stepper+1
+
+    stepper=0
+    thresholdvalue=beginValue+(0.5*totalRange)
+    while True:
+        if beginIndex+stepper+1 == periodsteps:
+            righthandP=periodguess_array[beginIndex+stepper]
+            logger.debug("Warning: Peak period for distance method too close to top of range")
+            break
+        #print (beginIndex+stepper)
+        #print (stdev_results[beginIndex+stepper])
+        if distance_results[beginIndex+stepper] > thresholdvalue:
+            #print ("RIGHTHAND PERIOD!")
+            #print (periodguess_array[beginIndex+stepper])
+            righthandP=periodguess_array[beginIndex+stepper]
+            #print (distance_results)
+            break
+        stepper=stepper+1
+
+    #print ("Distance method error: " + str((righthandP - lefthandP)/2))
+    pdm["distance_error"] = (righthandP - lefthandP)/2
+
+    return (pdm)
+
+#########################################
+
+def plot_with_period(paths, filterCode, numBins = 10, minperiod=0.2, maxperiod=1.2, periodsteps=10000):
+    
+    if minperiod==-99.9:
+            minperiod=0.05
+    
+    if maxperiod==-99.9:
+        # Load in list of used files
+        fileList = []
+        with open(paths['parent'] / "usedImages.txt", "r") as f:
+            for line in f:
+                fileList.append(line.strip())
+        
+        dateList=[]
+        for file in fileList:
+            dateList.append(file.split('_')[2].replace('d','.'))
+        dateList=np.asarray(dateList,dtype=float)
+        maxperiod=(np.max(dateList)-np.min(dateList))/3 # At least three periods should fit into the dataset.
+    
+    if periodsteps == -99:
+        periodsteps=int(maxperiod/0.001)
+        if periodsteps < 10000:
+            periodsteps = 10000
+        
+        if periodsteps > 1000000:
+            periodsteps = 1000000
+    
+    
+    logger.info("Minimum Period Tested  : " +str(minperiod))
+    logger.info("Maximum Period Tested  : " +str(maxperiod))
+    logger.info("Number of Period Trials: " +str(periodsteps))
+        
+
+    
+    
+    trialRange=[minperiod, maxperiod]
+
+    # Get list of phot files
+    periodPath = paths['periods']
+    if not periodPath.exists():
+        os.makedirs(periodPath)
+
+    logger.debug("Filter Set: " + filterCode)
+
+    fileList = paths['outcatPath'].glob('*_diffExcel.csv')
+    with open(paths['parent'] / "periodEstimates.txt", "w") as f:
+        f.write("Period Estimates \n\n")
+
+    # Load in the files
+    for file in fileList:
+        logger.debug(file)
+        variableName=file.stem.split('_')[0]
+        #logger.debug(str(outcatPath).replace('//',''))
+        logger.debug("Variable Name: {}".format(variableName))
+        varData = genfromtxt(file, dtype=float, delimiter=',')
+        calibFile = file.parent / "{}{}".format(file.stem.replace('diff','calib'), file.suffix)
+        logger.debug(calibFile)
+        if calibFile.exists():
+            calibData=genfromtxt(calibFile, dtype=float, delimiter=',')
+
+        #logger.debug(minDate)
+
+        pdm=phase_dispersion_minimization(varData, periodsteps, minperiod, maxperiod, numBins, periodPath, variableName)
+
+        plt.figure(figsize=(15, 5))
+
+        logger.debug("Distance Method Estimate (days): " + str(pdm["distance_minperiod"]))
+        logger.debug("Distance method error: " + str(pdm["distance_error"]))
+        # phaseTest=(varData[:,0] / (pdm["distance_minperiod"])) % 1
+        phaseTest=(varData[:,0] / (pdm["distance_minperiod"])) % 1
+        with open(paths['parent'] / "periodEstimates.txt", "a+") as f:
+            f.write("Variable : "+str(variableName) +"\n")
+            f.write("Distance Method Estimate (days): " + str(pdm["distance_minperiod"])+"\n")
+            f.write("Distance method error: " + str(pdm["distance_error"])+"\n")
+
+
+        plt.plot(pdm["periodguess_array"], pdm["distance_results"])
+        plt.gca().invert_yaxis()
+        plt.title("Range {0} d  Steps: {1}".format(trialRange, periodsteps))
+        plt.xlabel(r"Trial Period")
+        plt.ylabel(r"Likelihood of Period")
+        plt.savefig(periodPath / f"{variableName}_StringLikelihoodPlot.png")
+        plt.clf()
+
+        plt.plot(phaseTest, varData[:,1], 'bo', linestyle='None')
+        plt.plot(phaseTest+1, varData[:,1], 'ro', linestyle='None')
+        plt.errorbar(phaseTest, varData[:,1], yerr=varData[:,2], linestyle='None')
+        plt.errorbar(phaseTest+1, varData[:,1], yerr=varData[:,2], linestyle='None')
+        plt.gca().invert_yaxis()
+        plt.title("Period: {0} d  Steps: {1}".format(pdm["distance_minperiod"], periodsteps))
+        plt.xlabel(r"Phase ($\phi$)")
+        plt.ylabel(f"Differential {filterCode} Magnitude")
+        plt.savefig(periodPath / f"{variableName}_StringTestPeriodPlot.png")
+        plt.clf()
+
+        if calibFile.exists():
+            phaseTestCalib=(calibData[:,0] / (pdm["distance_minperiod"])) % 1
+            plt.plot(phaseTestCalib, calibData[:,1], 'bo', linestyle='None')
+            plt.plot(phaseTestCalib+1, calibData[:,1], 'ro', linestyle='None')
+            plt.errorbar(phaseTestCalib, calibData[:,1], yerr=varData[:,2], linestyle='None')
+            plt.errorbar(phaseTestCalib+1, calibData[:,1], yerr=varData[:,2], linestyle='None')
+            plt.gca().invert_yaxis()
+            plt.title("Period: {0} d  Steps: {1}".format(pdm["distance_minperiod"], periodsteps))
+            plt.xlabel(r"Phase ($\phi$)")
+            plt.ylabel(f"Calibrated {filterCode} Magnitude")
+            plt.savefig(periodPath / f"{variableName}_StringTestPeriodPlot_Calibrated.png")
+            plt.clf()
+
+            tempPeriodCatOut=[]
+            for g in range(len(calibData[:,0])):
+                tempPeriodCatOut.append([(calibData[g,0]/(pdm["distance_minperiod"]) % 1), calibData[g,1], calibData[g,2]])
+            tempPeriodCatOut=asarray(tempPeriodCatOut)
+            savetxt(periodPath / f"{variableName}_String_PhasedCalibMags.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+
+
+        tempPeriodCatOut=[]
+        for g in range(len(phaseTest)):
+            tempPeriodCatOut.append([phaseTest[g],varData[g,1]])
+        tempPeriodCatOut=asarray(tempPeriodCatOut)
+        savetxt(periodPath / f"{variableName}_StringTrial.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+
+        tempPeriodCatOut=[]
+        for g in range(len(varData[:,0])):
+            tempPeriodCatOut.append([(varData[g,0]/(pdm["distance_minperiod"]) % 1), varData[g,1], varData[g,2]])
+        tempPeriodCatOut=asarray(tempPeriodCatOut)
+        savetxt(periodPath / f"{variableName}_String_PhasedDiffMags.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+
+        if np.isnan(pdm["stdev_results"][0]):
+            logger.info("No PDM results due to lack of datapoint coverage")
+        else:
+            logger.debug("PDM Method Estimate (days): "+ str(pdm["stdev_minperiod"]))
+            #logger.debug(pdm["stdev_minperiod"])
+            phaseTest=(varData[:,0] / (pdm["stdev_minperiod"])) % 1
+            logger.debug("PDM method error: " + str(pdm["stdev_error"]))
+
+            with open(paths['parent'] / "periodEstimates.txt", "a+") as f:
+                f.write("PDM Method Estimate (days): "+ str(pdm["stdev_minperiod"])+"\n")
+                f.write("PDM method error: " + str(pdm["stdev_error"])+"\n\n")
+
+
+        plt.plot(pdm["periodguess_array"], pdm["stdev_results"])
+        plt.gca().invert_yaxis()
+        plt.title("Range {0} d  Steps: {1}".format(trialRange, periodsteps))
+        plt.xlabel(r"Trial Period")
+        plt.ylabel(r"Likelihood of Period")
+        plt.savefig(periodPath / f"{variableName}_PDMLikelihoodPlot.png")
+
+        plt.clf()
+
+
+        plt.plot(phaseTest, varData[:,1], 'bo', linestyle='None')
+        plt.plot(phaseTest+1, varData[:,1], 'ro', linestyle='None')
+        plt.errorbar(phaseTest, varData[:,1], yerr=varData[:,2], linestyle='None')
+        plt.errorbar(phaseTest+1, varData[:,1], yerr=varData[:,2], linestyle='None')
+        plt.gca().invert_yaxis()
+        plt.title("Period: {0} d  Steps: {1}".format(pdm["stdev_minperiod"], periodsteps))
+        plt.xlabel(r"Phase ($\phi$)")
+        plt.ylabel(r"Differential " + str(filterCode) + " Magnitude")
+        plt.savefig(periodPath / f"{variableName}_PDMTestPeriodPlot.png")
+        plt.clf()
+
+        if calibFile.exists():
+            phaseTestCalib=(calibData[:,0] / (pdm["stdev_minperiod"])) % 1
+            plt.plot(phaseTestCalib, calibData[:,1], 'bo', linestyle='None')
+            plt.plot(phaseTestCalib+1, calibData[:,1], 'ro', linestyle='None')
+            plt.errorbar(phaseTestCalib, calibData[:,1], yerr=varData[:,2], linestyle='None')
+            plt.errorbar(phaseTestCalib+1, calibData[:,1], yerr=varData[:,2], linestyle='None')
+            plt.gca().invert_yaxis()
+            plt.title("Period: {0} d  Steps: {1}".format(pdm["stdev_minperiod"], periodsteps))
+            plt.xlabel(r"Phase ($\phi$)")
+            plt.ylabel(r"Calibrated " + str(filterCode) + " Magnitude")
+            plt.savefig(periodPath / f"{variableName}_PDMTestPeriodPlot_Calibrated.png")
+            plt.clf()
+
+            tempPeriodCatOut=[]
+            for g in range(len(calibData[:,0])):
+                tempPeriodCatOut.append([(calibData[g,0]/(pdm["stdev_minperiod"])) % 1, calibData[g,1], calibData[g,2]])
+            tempPeriodCatOut=asarray(tempPeriodCatOut)
+            savetxt(periodPath / f"{variableName}_PDM_PhasedCalibMags.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+
+        tempPeriodCatOut=[]
+        for g in range(len(phaseTest)):
+            tempPeriodCatOut.append([phaseTest[g],varData[g,1]])
+        tempPeriodCatOut=asarray(tempPeriodCatOut)
+        savetxt(periodPath / f"{variableName}_PDMTrial.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+
+        tempPeriodCatOut=[]
+        for g in range(len(varData[:,0])):
+            tempPeriodCatOut.append([(varData[g,0]/(pdm["stdev_minperiod"])) % 1, varData[g,1], varData[g,2]])
+        tempPeriodCatOut=asarray(tempPeriodCatOut)
+        savetxt(periodPath / f"{variableName}_PDM_PhaseddiffMags.csv", tempPeriodCatOut, delimiter=",", fmt='%0.8f')
+    return pdm["distance_minperiod"]
