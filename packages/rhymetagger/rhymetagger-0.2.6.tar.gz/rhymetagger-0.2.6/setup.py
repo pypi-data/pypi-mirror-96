acf1779d1e652a63ff2f015428fdac0c748dfee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:14 +0000

    Add package rhymetagger/0.2.6/rhymetagger-0.2.6.tar.gz

---
+++ packages/rhymetagger/rhymetagger-0.2.6.tar.gz/rhymetagger-0.2.6/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+setup(
+  name = 'rhymetagger',       
+  include_package_data = True,  
+  packages = ['rhymetagger'],   
+  version = '0.2.6',      
+  license='MIT',        
+  description = 'A simple collocation-driven recognition of rhymes',   
+  author = 'Petr Plechac',                   
+  author_email = 'plechac@ucl.cas.cz',      
+  url = 'https://github.com/versotym/rhymetagger',
+  download_url = 'https://github.com/versotym/rhymeTagger/archive/v0.2.tar.gz',
+  keywords = ['poetry', 'rhyme', 'versification'],
+  package_data={'rhymetagger': ['models/*.json']},
+  data_files=[('models', ['models/cs.json', 'models/en.json', 'models/de.json', 'models/nl.json', 'models/es.json', 'models/ru.json'])],
+  install_requires=[           
+          'ujson',
+          'nltk',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',      
+    'Topic :: Text Processing :: Linguistic',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.1',      
+    'Programming Language :: Python :: 3.2',      
+    'Programming Language :: Python :: 3.3',      
+    'Programming Language :: Python :: 3.4',      
+    'Programming Language :: Python :: 3.5',      
+    'Programming Language :: Python :: 3.6',      
+    'Programming Language :: Python :: 3.7',      
+    'Programming Language :: Python :: 3.8',      
+    'Programming Language :: Python :: 3.9',      
+    'Programming Language :: Python :: 3.10',      
+  ],
+)
