5cd999a21d24f47e305e0fe20b07cac72956d7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:38 +0000

    Add package pymortafix/0.1.0/pymortafix-0.1.0.tar.gz

---
+++ packages/pymortafix/pymortafix-0.1.0.tar.gz/pymortafix-0.1.0/README.md
@@ -0,0 +1,5 @@
+# Setup
+You can find the package, [here](https://pypi.org/project/pymortafix/).
+```
+pip3 install pymortafix
+```
---
+++ packages/pymortafix/pymortafix-0.1.0.tar.gz/pymortafix-0.1.0/pymortafix/utils.py
@@ -0,0 +1,24 @@
+from re import findall, match, sub
+
+from colorifix.colorifix import erase
+
+
+def multisub(sub_dict, string):
+    """Infinite sub in one iteration # sub_dict: {what_to_sub:substitution}"""
+    rgx = "|".join(f"({s})" for s in sub_dict.keys())
+    return sub(rgx, lambda m: sub_dict.get(m.group()), string)
+
+
+def strict_input(text, wrong_text=None, choices=None, regex=None, flush=False):
+    """Get user input with some requirements"""
+    inp = input(text)
+    if flush:
+        erase(len(findall(r"\n", text)) + 1)
+    while (choices and inp not in choices) and (regex and not match(regex, inp)):
+        if wrong_text:
+            inp = input(wrong_text)
+        else:
+            inp = input(text)
+        if flush:
+            erase(len(findall(r"\n", wrong_text or text)) + 1)
+    return inp
