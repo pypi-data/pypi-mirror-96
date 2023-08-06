51a63bd8390658342b11f8742d8e3bc5536d1296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:23 +0000

    Add package metaserver-core/0.0.1/metaserver-core-0.0.1.tar.gz

---
+++ packages/metaserver-core/metaserver-core-0.0.1.tar.gz/metaserver-core-0.0.1/metaserver/__init__.py
@@ -0,0 +1,15 @@
+import logging
+
+__all__ = ("DESCRIPTION", "NAME", "VERSION")
+
+NAME = "metaserver-core"
+TITLE = "MetaServer Core package"
+DESCRIPTION = """The core part for all game metaservers."""
+
+URL = "https://pypi.org/project/metaserver-core/"
+TEAM = "Mint Team"
+
+# https://www.python.org/dev/peps/pep-0440/
+VERSION = "0.0.1"
+
+logging.critical("This package was loaded from external pypi repository!")
---
+++ packages/metaserver-core/metaserver-core-0.0.1.tar.gz/metaserver-core-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metaserver-core",
+    packages = ['metaserver'],
+    version="0.0.1",
+    description="The core part for all game metaservers.",
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/metaserver-core/",
+    author="Mint",
+    author_email="pasaranax@gmail.com",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+    ],
+)
