f30133b3da6b54d6c77d9369530b58456a6cdee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:43 +0000

    Add package opentelemetry-azure-monitor-exporter/0.1a2/opentelemetry_azure_monitor_exporter-0.1a2-py3-none-any.whl

---
+++ packages/opentelemetry-azure-monitor-exporter/opentelemetry_azure_monitor_exporter-0.1a2-py3-none-any.whl/azure_monitor/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+from azure_monitor.trace import AzureMonitorSpanExporter
+from azure_monitor.version import __version__  # noqa
+
+__all__ = ["AzureMonitorSpanExporter"]
