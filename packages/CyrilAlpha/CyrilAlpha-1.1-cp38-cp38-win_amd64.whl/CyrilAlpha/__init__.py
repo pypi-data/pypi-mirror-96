9c1c57169b8d79e187066c330bb9a11bee4e68c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:14 +0000

    Add package CyrilAlpha/1.1/CyrilAlpha-1.1-cp38-cp38-win_amd64.whl

---
+++ packages/CyrilAlpha/CyrilAlpha-1.1-cp38-cp38-win_amd64.whl/CyrilAlpha/__init__.py
@@ -0,0 +1,7 @@
+from CyrilAlpha.Module.Cyril_module import backtest, multi_backtest
+from CyrilAlpha.Module.Cyril_tradingday import TradingDay
+TD = TradingDay()
+
+
+if __name__ == '__main__':
+    print(dir())
