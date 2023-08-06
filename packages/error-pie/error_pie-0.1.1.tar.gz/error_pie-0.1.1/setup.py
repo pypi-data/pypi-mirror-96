b508692368b3f9375940e2098df21ae6a1232ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:48 +0000

    Add package error-pie/0.1.1/error_pie-0.1.1.tar.gz

---
+++ packages/error-pie/error_pie-0.1.1.tar.gz/error_pie-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['error_pie']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'error-pie',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'CharlesAverill',
+    'author_email': 'charlesaverill20@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
