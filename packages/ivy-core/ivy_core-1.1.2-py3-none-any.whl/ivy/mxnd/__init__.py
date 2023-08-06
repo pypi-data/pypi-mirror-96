9877508725d7e414923c6bc2556ab39812486555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:43 +0000

    Add package ivy-core/1.1.2/ivy_core-1.1.2-py3-none-any.whl

---
+++ packages/ivy-core/ivy_core-1.1.2-py3-none-any.whl/ivy/mxnd/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import ivy
+
+from .core import *
+from . import nn
+from .nn import *
+
+# noinspection PyUnresolvedReferences
+use = ivy.framework_handler.ContextManager(sys.modules[__name__])
