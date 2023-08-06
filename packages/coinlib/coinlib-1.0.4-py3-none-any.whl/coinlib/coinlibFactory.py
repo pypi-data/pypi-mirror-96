87483c3db894cce2acce61f649742b87435253a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:25 +0000

    Add package coinlib/1.0.4/coinlib-1.0.4-py3-none-any.whl

---
+++ packages/coinlib/coinlib-1.0.4-py3-none-any.whl/coinlib/coinlibFactory.py
@@ -0,0 +1,45 @@
+from helper import is_in_ipynb
+from helper import pip_install_or_ignore
+pip_install_or_ignore("websocket", "websocket-client")
+pip_install_or_ignore("plotly", "plotly")
+pip_install_or_ignore("simplejson", "simplejson")
+pip_install_or_ignore("asyncio", "asyncio")
+pip_install_or_ignore("grpc", "grpcio-tools")
+pip_install_or_ignore("matplotlib", "matplotlib")
+pip_install_or_ignore("autobahn", "autobahn")
+pip_install_or_ignore("zlib", "zlib")
+pip_install_or_ignore("pandas", "pandas")
+pip_install_or_ignore("timeit", "timeit")
+pip_install_or_ignore("dateutil", "python-dateutil")
+pip_install_or_ignore("chipmunkdb", "chipmunkdb-python-client")
+
+import StatisticsMethodLoader
+import StatisticsRuleLoader
+import ChartsLoader
+from DataWorker import WorkerJobListener
+from Registrar import Registrar
+import asyncio
+from helper import log
+
+registrar = Registrar()
+
+def run_main_function():
+    registrar.statsRuleFactory = StatisticsRuleLoader.StatisticsRuleFactory()
+    registrar.statsMethodFactory = StatisticsMethodLoader.StatisticsMethodFactory()
+    registrar.chartsFactory = ChartsLoader.ChartsFactory()
+
+    workerJobs = WorkerJobListener()
+    workerJobs.start()
+
+    log.info("Starting Coinlib Factory")
+
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
+
+if __name__ == '__main__':
+    run_main_function()
+elif is_in_ipynb:
+    run_main_function()
