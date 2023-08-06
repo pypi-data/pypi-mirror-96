0a9dae50c1b20d6a42ca4149e427f9dd06b770b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:49 +0000

    Add package pyoinformatics/0.2.5/pyoinformatics-0.2.5-py3-none-any.whl

---
+++ packages/pyoinformatics/pyoinformatics-0.2.5-py3-none-any.whl/tests/test_align.py
@@ -0,0 +1,36 @@
+from pyoinformatics.align import lcs, format_matrix
+from pyoinformatics.seq import Seq
+
+
+def test_lcs():
+    assert lcs(Seq("AACCTTGG"), Seq("ACACTGTGA")) == Seq("AACTTG")
+
+
+def test_format_matrix():
+    Seq1 = Seq("AACCTTGG")
+    Seq2 = Seq("ACACTGTGA")
+
+    M = [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 2, 2, 2, 2, 2, 2],
+        [1, 2, 2, 2, 2, 2, 2, 2],
+        [1, 2, 3, 3, 3, 3, 3, 3],
+        [1, 2, 3, 3, 4, 4, 4, 4],
+        [1, 2, 3, 3, 4, 4, 5, 5],
+        [1, 2, 3, 3, 4, 5, 5, 5],
+        [1, 2, 3, 3, 4, 5, 6, 6],
+        [1, 2, 3, 3, 4, 5, 6, 6],
+    ]
+
+    assert format_matrix(Seq1, Seq2, M) == [
+        "  A A C C T T G G",
+        "A 1 1 1 1 1 1 1 1",
+        "C 1 1 2 2 2 2 2 2",
+        "A 1 2 2 2 2 2 2 2",
+        "C 1 2 3 3 3 3 3 3",
+        "T 1 2 3 3 4 4 4 4",
+        "G 1 2 3 3 4 4 5 5",
+        "T 1 2 3 3 4 5 5 5",
+        "G 1 2 3 3 4 5 6 6",
+        "A 1 2 3 3 4 5 6 6",
+    ]
