ea43280fa6417f061abdedfb2c284a4f0a03ff09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:07 +0000

    Add package memepack-builder/0.2.3/memepack_builder-0.2.3.tar.gz

---
+++ packages/memepack-builder/memepack_builder-0.2.3.tar.gz/memepack_builder-0.2.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="memepack_builder",
+    version="0.2.3",
+    author="MysticNebula70",
+    author_email="mysticnebula70@teahou.se",
+    description="A tool for packaging Memefied Chinese resource pack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Teahouse-Studios/memepack-builder",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.9'
+)
