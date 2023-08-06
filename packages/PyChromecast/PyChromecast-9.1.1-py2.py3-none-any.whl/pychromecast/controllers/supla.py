54fc08149556cc3aa0051c5c1cccd36fda0a6038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:01 +0000

    Add package PyChromecast/9.1.1/PyChromecast-9.1.1-py2.py3-none-any.whl

---
+++ packages/PyChromecast/PyChromecast-9.1.1-py2.py3-none-any.whl/pychromecast/controllers/supla.py
@@ -0,0 +1,45 @@
+"""
+Controller to interface with Supla.
+"""
+import logging
+
+from . import BaseController
+from ..config import APP_SUPLA
+
+APP_NAMESPACE = "urn:x-cast:fi.ruutu.chromecast"
+
+
+# pylint: disable=too-many-instance-attributes
+class SuplaController(BaseController):
+    """ Controller to interact with Supla namespace. """
+
+    def __init__(self):
+        super().__init__(APP_NAMESPACE, APP_SUPLA)
+
+        self.logger = logging.getLogger(__name__)
+
+    def play_media(self, media_id, is_live=False):
+        """
+        Play Supla media
+        """
+        msg = {
+            "type": "load",
+            "mediaId": media_id,
+            "currentTime": 0,
+            "isLive": is_live,
+            "isAtLiveMoment": False,
+            "bookToken": "",
+            "sample": True,
+            "fw_site": "Supla",
+            "Sanoma_adkv": "",
+            "prerollAdsPlayed": True,
+            "supla": True,
+            "nextInSequenceList": 0,
+            "playbackRate": 1,
+            "env": "prod",
+        }
+        self.send_message(msg, inc_session_id=True)
+
+    def quick_play(self, media_id=None, is_live=False, **kwargs):
+        """ Quick Play """
+        self.play_media(media_id, is_live=is_live, **kwargs)
