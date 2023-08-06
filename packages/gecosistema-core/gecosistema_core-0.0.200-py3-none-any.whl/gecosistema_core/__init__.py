24daf71db27b57f026bf4be801dad2216b1a849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:07 +0000

    Add package gecosistema-core/0.0.200/gecosistema_core-0.0.200-py3-none-any.whl

---
+++ packages/gecosistema-core/gecosistema_core-0.0.200-py3-none-any.whl/gecosistema_core/__init__.py
@@ -0,0 +1,36 @@
+# -----------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2018 Luzzi Valerio for Gecosistema S.r.l.
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# Name:        execution
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:     27/07/2018
+# -----------------------------------------------------------------------------
+from .platform import *
+from .filesystem import *
+from .compression import *
+from .strings import *
+from .execution import *
+from .datatypes import *
+from .stime import *
+from .http import *
+from .xml_utils import *
+#from .excel import *
+from .exceptions import *
+
+from .maths import *
---
+++ packages/gecosistema-core/gecosistema_core-0.0.200-py3-none-any.whl/gecosistema_core/exceptions.py
@@ -0,0 +1,32 @@
+# -------------------------------------------------------------------------------
+# Licence:
+# Copyright (c) 2012-2018 Luzzi Valerio 
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+#
+# Name:        exceptions.py
+# Purpose:
+#
+# Author:      Luzzi Valerio
+#
+# Created:     29/08/2018
+# -------------------------------------------------------------------------------
+import inspect
+
+def manage(ex):
+    """
+    manage
+    """
+    caller_name = inspect.stack()[1][3]
+    print("Exception on <%s>:%s"%(caller_name,ex))
