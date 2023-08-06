d4745b281a864675fb4500e275ce81ac61785dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:12 +0000

    Add package sevenbridges-python/2.2.1/sevenbridges_python-2.2.1-py3-none-any.whl

---
+++ packages/sevenbridges-python/sevenbridges_python-2.2.1-py3-none-any.whl/sevenbridges/models/endpoints.py
@@ -0,0 +1,45 @@
+import logging
+
+from sevenbridges.meta.resource import Resource
+from sevenbridges.meta.fields import HrefField
+
+logger = logging.getLogger(__name__)
+
+
+class Endpoints(Resource):
+    """
+    Central resource for managing Endpoints.
+    """
+    _URL = {
+        'get': '/'
+    }
+
+    rate_limit_url = HrefField()
+    user_url = HrefField()
+    users_url = HrefField()
+    billing_url = HrefField()
+    projects_url = HrefField()
+    files_url = HrefField()
+    tasks_url = HrefField()
+    apps_url = HrefField()
+    action_url = HrefField()
+    upload_url = HrefField()
+
+    @classmethod
+    def get(cls, api=None, **kwargs):
+        """
+        Get api links.
+        :param api: Api instance.
+        :return: Endpoints object.
+        """
+        api = api if api else cls._API
+        extra = {
+            'resource': cls.__name__,
+            'query': {}
+        }
+        logger.info('Getting resources', extra=extra)
+        endpoints = api.get(url=cls._URL['get']).json()
+        return Endpoints(api=api, **endpoints)
+
+    def __str__(self):
+        return '<Endpoints>'
