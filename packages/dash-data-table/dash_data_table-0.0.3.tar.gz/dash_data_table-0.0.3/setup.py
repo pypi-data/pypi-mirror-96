c58a1f9589a1ed1b6b20abe4e80e71ec3b851ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:59 +0000

    Add package dash-data-table/0.0.3/dash_data_table-0.0.3.tar.gz

---
+++ packages/dash-data-table/dash_data_table-0.0.3.tar.gz/dash_data_table-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dash_data_table']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dash>=1.19.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dash-data-table',
+    'version': '0.0.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Jakob Jul Elben',
+    'author_email': 'elbenjakobjul@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
