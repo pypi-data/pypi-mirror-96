afaadf0533eddcd809db0583396714d32e18f684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:11 +0000

    Add package reprypt/2.0.3/reprypt-2.0.3.tar.gz

---
+++ packages/reprypt/reprypt-2.0.3.tar.gz/reprypt-2.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+requires = []
+long_description = open("readme.md").read()
+
+
+setup(
+    name='reprypt',
+    version='2.0.3',
+    description='Encryption Module',
+    url='https://github.com/tasuren/reprypt',
+    author='tasuren',
+    author_email='tasuren5@gmail.com',
+    license='MIT',
+    keywords='encrypt decrypt',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=[
+        "reprypt"
+    ],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
