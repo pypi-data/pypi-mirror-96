a77c15e584d622f155fc6123414b8732bdc7779d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:54 +0000

    Add package mggp/0.0.1/mggp-0.0.1.tar.gz

---
+++ packages/mggp/mggp-0.0.1.tar.gz/mggp-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mggp", # Replace with your own username
+    version="0.0.1",
+    author="Henrique Castro",
+    author_email="henriquec.castro@outlook.com",
+    description="Multi Gene Genetic Programming toolbox for System Identification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=[],
+    scripts=["mggp.py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['deap'],
+)
