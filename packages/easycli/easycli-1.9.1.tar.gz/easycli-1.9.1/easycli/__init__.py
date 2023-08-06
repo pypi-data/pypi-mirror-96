2329fb06accc693e6fbba3600a9d467ce66428cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:39 +0000

    Add package easycli/1.9.1/easycli-1.9.1.tar.gz

---
+++ packages/easycli/easycli-1.9.1.tar.gz/easycli-1.9.1/easycli/__init__.py
@@ -0,0 +1,8 @@
+"""Uncomplicated OOP command line argument parser."""
+from .argument import Argument, Mutex
+from .command import Command, SubCommand
+from .root import Root
+from .progressbar import ProgressBar
+
+
+__version__ = '1.9.1'
---
+++ packages/easycli/easycli-1.9.1.tar.gz/easycli-1.9.1/easycli/root.py
@@ -0,0 +1,80 @@
+import sys
+import argparse
+from os import path
+
+from .command import Command
+
+
+class Root(Command):
+    """base class for the CLI entry point.
+
+    .. code-block::
+
+       import sys
+
+       from easycli import Root, ...
+
+
+       class Git(Root):
+           __help__ = 'git help message'
+           __completion__ = True
+           __arguments__ = [
+               Pull,
+               Push,
+               Commit,
+               ...
+           ]
+
+       if __name__ == '__main__':
+           sys.exit(Git.quickstart())
+
+    """
+
+    __completion__ = None
+    """If True, a completion sub-command with two subsub-commands: install and
+    uninstall will be added to ``self.__arguments__`` collection.
+    """
+
+    def __init__(self):
+        if self.__completion__:
+            from .completion import Completion
+            self.__arguments__.append(Completion)
+
+        super().__init__()
+
+        if self.__completion__:
+            import argcomplete
+            argcomplete.autocomplete(self._parser)
+
+    @classmethod
+    def _create_parser(self):
+        return argparse.ArgumentParser(
+            prog=path.basename(self.__command__ or sys.argv[0]),
+            description=self.__help__
+        )
+
+    def _parse_arguments(self, argv):
+        return self._parser.parse_args(argv)
+
+    def _execute_subcommand(self, args):
+        return args.func(args)
+
+    def main(self, argv=None):
+        """Call this function as the main entry point for your cli application.
+
+        :param argv: If not given, :attr:`sys.argv` will be used.
+        :return: exit status
+        """
+        args = self._parse_arguments(argv)
+
+        if hasattr(args, 'func'):
+            status = self._execute_subcommand(args)
+        else:
+            status = self(args)
+
+        return status or 0
+
+    @classmethod
+    def quickstart(cls, argv=None):
+        """Shorthand for ``Root().main()``."""
+        return cls().main(argv)
