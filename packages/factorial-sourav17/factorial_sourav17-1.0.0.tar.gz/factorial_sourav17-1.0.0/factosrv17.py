f60a5ba4bf4df5ad5c03f3bc62a46439b4dc134b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:05 +0000

    Add package factorial-sourav17/1.0.0/factorial_sourav17-1.0.0.tar.gz

---
+++ packages/factorial-sourav17/factorial_sourav17-1.0.0.tar.gz/factorial_sourav17-1.0.0/factosrv17.py
@@ -0,0 +1,5 @@
+def facto(n):
+	if n == 0:
+		return 1
+	else:
+		return n * facto(n-1)
---
+++ packages/factorial-sourav17/factorial_sourav17-1.0.0.tar.gz/factorial_sourav17-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name = 'factorial_sourav17',
+        version = '1.0.0',
+        py_modules = ['factosrv17'],
+        author = 'Sourav Bhattacharya',
+        author_mail = 'bhsourav17@gmail.com',
+        description = 'factorial function'
+     )
