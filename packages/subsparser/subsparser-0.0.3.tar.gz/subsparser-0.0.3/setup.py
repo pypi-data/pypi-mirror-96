83b44b089defb6f4dd9f56c7cc0311bb1dc5796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:06 +0000

    Add package subsparser/0.0.3/subsparser-0.0.3.tar.gz

---
+++ packages/subsparser/subsparser-0.0.3.tar.gz/subsparser-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name="subsparser",
+    version="0.0.3",
+    description="subtitle parser for srt and text",
+    author="Dharmveer Baloda",
+    packages=["subsparser"],
+    install_requires=[]
+)
