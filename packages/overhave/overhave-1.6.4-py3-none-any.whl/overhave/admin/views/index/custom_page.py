fdeccb6f4680718716951abfbf680d2566c0b6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:30 +0000

    Add package overhave/1.6.4/overhave-1.6.4-py3-none-any.whl

---
+++ packages/overhave/overhave-1.6.4-py3-none-any.whl/overhave/admin/views/index/custom_page.py
@@ -0,0 +1,42 @@
+import typing
+from pathlib import Path
+
+from flask_admin.form import BaseForm
+from flask_admin.model.widgets import XEditableWidget
+from markupsafe import Markup
+from wtforms import Field
+
+
+class CustomPageWidget(XEditableWidget):
+    """ Customising widget.
+
+    Widget content is filled with content from specified
+    ``self.template_path```.
+    """
+
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
+        self.template_path: typing.Optional[Path] = None
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, field: Field, **kwargs: typing.Any) -> Markup:
+        if self.template_path is not None:
+            with self.template_path.open("r") as template:
+                return Markup(template.read())
+        return Markup("This is Overhave info page.")
+
+
+class CustomPageField(Field):
+    """ Field for custom page widget. """
+
+    widget = CustomPageWidget()
+
+
+class CustomPageForm(BaseForm):
+    """ Form for custom page. """
+
+    type = CustomPageField()
+
+    def __init__(self, template_path: typing.Optional[Path]):
+        super().__init__()
+        self.type.widget.template_path = template_path
+        self.type.label = None
