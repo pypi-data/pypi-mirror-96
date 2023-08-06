17edd979dfab7def99691a509589102730c7f7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:23 +0000

    Add package prie/0.0.1/prie-0.0.1.tar.gz

---
+++ packages/prie/prie-0.0.1.tar.gz/prie-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='prie',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/JoaoRodrigues/pie',
+    author='Joao Rodrigues',
+    author_email='j.p.g.l.m.rodrigues@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
