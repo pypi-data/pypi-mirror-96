73aec413c870d67d0fc6d6c4a4425fd62a246a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:52 +0000

    Add package sharkvalidator/0.1.12/sharkvalidator-0.1.12.tar.gz

---
+++ packages/sharkvalidator/sharkvalidator-0.1.12.tar.gz/sharkvalidator-0.1.12/setup.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2021 SMHI, Swedish Meteorological and Hydrological Institute 
+# License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
+"""
+Created on 2021-02-18 13:52
+
+@author: johannes
+
+"""
+import os
+import setuptools
+
+
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
+
+NAME = 'sharkvalidator'
+README = open('READMEpypi.rst', 'r').read()
+
+setuptools.setup(
+    name=NAME,
+    version="0.1.12",
+    author="SMHI - NODC",
+    author_email="shark@smhi.se",
+    description="Validate data delivery at the Swedish NODC",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sharksmhi/sharkvalidator",
+    packages=setuptools.find_packages(),
+    package_data={'sharkvalidator': [
+        os.path.join('etc', '*.xlsx'),
+        os.path.join('etc', 'readers', '*.yaml'),
+        os.path.join('etc', 'validators', '*.yaml'),
+        os.path.join('etc', 'writers', '*.yaml'),
+    ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
