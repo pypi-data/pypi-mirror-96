97dafb562f9a768c1e15e690773f34222c23420a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:30 +0000

    Add package taky/0.6/taky-0.6.tar.gz

---
+++ packages/taky/taky-0.6.tar.gz/taky-0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='taky',
+    python_requires='>=3.6',
+    use_scm_version=True,
+    author="Tim K",
+    author_email="tpkuester@gmail.com",
+    setup_requires=['setuptools_scm'],
+    install_requires=['lxml', 'dateutils', 'Flask', 'pyopenssl', 'gunicorn'],
+    description='A simple TAK server and COT router',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tkuester/taky',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Topic :: Communications",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ],
+    entry_points={
+        'console_scripts': [
+            'taky = taky.cot.__main__:main',
+            'taky_dps = taky.dps.__main__:main',
+            'takyctl = taky.__main__:main',
+        ]
+    }
+)
