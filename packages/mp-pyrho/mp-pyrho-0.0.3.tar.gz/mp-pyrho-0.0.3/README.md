d0974b7304d9d6ca4a20102664f275b7583318ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:52 +0000

    Add package mp-pyrho/0.0.3/mp-pyrho-0.0.3.tar.gz

---
+++ packages/mp-pyrho/mp-pyrho-0.0.3.tar.gz/mp-pyrho-0.0.3/README.md
@@ -0,0 +1,26 @@
+# mp-pyrho
+
+[![image](https://img.shields.io/pypi/v/src.svg)](https://pypi.python.org/pypi/src)
+
+[![image](https://img.shields.io/travis/jmmshn/src.svg)](https://travis-ci.com/jmmshn/src)
+
+[![Documentation Status](https://readthedocs.org/projects/src/badge/?version=latest)](https://src.readthedocs.io/en/latest/?badge=latest)
+
+[![Updates](https://pyup.io/repos/github/jmmshn/src/shield.svg)](https://pyup.io/repos/github/jmmshn/src/)
+
+Python Boilerplate contains all the boilerplate you need to create a
+Python package.
+
+-   Free software: BSD license
+-   Documentation: <https://src.readthedocs.io>.
+
+## Features
+
+-   TODO
+
+## Credits
+
+This package was created with
+[Cookiecutter](https://github.com/audreyr/cookiecutter) and the
+[audreyr/cookiecutter-pypackage](https://github.com/audreyr/cookiecutter-pypackage)
+project template.
---
+++ packages/mp-pyrho/mp-pyrho-0.0.3.tar.gz/mp-pyrho-0.0.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3.7', ]
+
+setup(
+    author="Jimmy-Xuan Shen",
+    author_email='jmmshn@gmail.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type = "text/markdown",
+    include_package_data=True,
+    name='mp-pyrho',
+    packages=find_packages(include=['src', 'src.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/jmmshn/src',
+    version='0.0.3',
+    zip_safe=False,
+)
