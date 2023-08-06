93218715fa4fe1094f3a34908cae1affeee12493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package microt-preprocessing/0.0.13/microt_preprocessing-0.0.13.tar.gz

---
+++ packages/microt-preprocessing/microt_preprocessing-0.0.13.tar.gz/microt_preprocessing-0.0.13/time_study_preprocessing_main/preprocess_scripts/phone/GPS/main.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+from os import path, makedirs
+
+from ...utils.validate_dates import *
+from ...utils.validate_hours import validate_hours
+from .parse import *
+import warnings
+# import zipFile
+import pyminizip
+
+warnings.filterwarnings("ignore")
+
+area = "data"
+device = "phone"
+file_shortname = "GPS"
+
+# since the battery file name is mixed with irregular numbers, file name matching is needed.
+name_pattern = "GPS"
+
+
+def reg_exp_matching(hour_folder_path, name_pattern):
+    file_list = listdir(hour_folder_path)
+    matched_name = ""
+    for file_str in file_list:
+        if file_str.startswith(name_pattern):
+            matched_name = file_str
+            break
+    return matched_name
+
+
+def pre_process(microT_root_path, intermediate_file_save_path, p_id, decrypt_password, date):
+    participant_folder_path = microT_root_path
+    area_folder_path = participant_folder_path + sep + area
+    hours_with_target_file_list = {}  # included in participant stats report
+    # step 2: iterate through all hour folders for each date
+    if path.exists(area_folder_path):
+        date_folder_path = area_folder_path + sep + date
+        if path.exists(date_folder_path):
+            file_save_date_path = intermediate_file_save_path + sep + "intermediate_file" + sep + p_id + sep + date
+            if not path.exists(file_save_date_path):
+                makedirs(file_save_date_path)
+            day_file_name = device + "_" + file_shortname + "_clean_" + date + ".csv"
+            day_file_save_path = file_save_date_path + sep + day_file_name
+            day_zip_file_save_path = day_file_save_path + '.zip'
+            if not path.exists(day_zip_file_save_path):
+                df_participant = pd.DataFrame()
+                # check hourly folder
+                validated_hour_list, HAVE_ALL_HOURS = validate_hours(date_folder_path)
+                if len(validated_hour_list) == 0:
+                    print("Cannot find hour folders in {} data".format(date))
+                # iterate through hour folders
+                hours_with_target_file = len(validated_hour_list)  # included in participant stats report
+                for hour in validated_hour_list:
+                    hour_folder_path = date_folder_path + sep + hour
+                    if not path.exists(hour_folder_path):
+                        continue
+                    # step 2.1: read target hourly file
+                    target_file_matched = reg_exp_matching(hour_folder_path, name_pattern)
+                    if len(target_file_matched) > 0:
+                        target_file_path = hour_folder_path + sep + target_file_matched
+                        try:
+                            df_hour_raw = pd.read_csv(target_file_path, header=None)
+                        except:
+                            print("Empty csv file", target_file_path)
+                            continue
+                    else:
+                        hours_with_target_file -= 1
+                        continue
+
+                    # step 2.2: parse target hourly file
+                    if df_hour_raw.shape[0] != 0:
+                        df_hour_parsed = parse_raw_df(df_hour_raw)
+                        # step 2.4: concatenate hourly file in day level
+                        if df_hour_parsed.shape[0] != 0:
+                            df_participant = pd.concat([df_participant, df_hour_parsed])
+
+                hours_with_target_file_list[date] = hours_with_target_file
+                if hours_with_target_file != 0:
+                    # step 3:  write out day file for participant
+                    df_participant.to_csv(day_file_save_path, index=False)
+                    pyminizip.compress(day_file_save_path, None, day_zip_file_save_path, decrypt_password, 0)
+                    os.remove(day_file_save_path)
+
+
+if __name__ == "__main__":
+    microT_root_path = r"E:\data\wocket\Wockets-win32-x64\resources\app\src\srv\MICROT"
+    intermediate_file_save_path = r"C:\Users\jixin\Desktop\temp"
+    p_id_list = ["aditya4_internal@timestudy_com"]
+    date_start = "2020-01-01"
+    date_end = "2020-07-01"
+
+    pre_process(microT_root_path, intermediate_file_save_path, p_id_list, date_start, date_end)
