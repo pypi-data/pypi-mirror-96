6fbf7f8037a0ba0d4c9f278d2c460f2337040181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:13 +0000

    Add package az-iranian-bank-gateways/1.6.5/az-iranian-bank-gateways-1.6.5.tar.gz

---
+++ packages/az-iranian-bank-gateways/az-iranian-bank-gateways-1.6.5.tar.gz/az-iranian-bank-gateways-1.6.5/azbankgateways/views.py
@@ -0,0 +1,40 @@
+import logging
+from urllib.parse import unquote
+
+from django.shortcuts import render
+
+from azbankgateways.bankfactories import BankFactory
+from azbankgateways.exceptions import BankGatewayUnclear, BankGatewayStateInvalid
+
+
+def callback_view(request):
+    bank_type = request.GET.get('bank_type', None)
+    identifier = request.GET.get('identifier', None)
+    if not bank_type:
+        logging.critical("Bank type is required. but it doesnt send.")
+        raise BankGatewayUnclear("Bank type is required")
+
+    factory = BankFactory()
+    bank = factory.create(bank_type, identifier=identifier)
+    try:
+        bank.verify_from_gateway(request)
+    except BankGatewayStateInvalid:
+        pass
+    return bank.redirect_client_callback()
+
+
+def go_to_bank_gateway(request):
+    context = {
+        'params': {}
+    }
+    for key, value in request.GET.items():
+        if key == 'url' or key == 'method':
+            context[key] = unquote(value)
+        else:
+            context['params'][key] = unquote(value)
+
+    return render(
+        request,
+        'azbankgateways/redirect_to_bank.html',
+        context=context
+    )
