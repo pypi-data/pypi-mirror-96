d516858eb771ed5617f50b34d736ec42727515e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:53 +0000

    Add package regugcb/0.4/regugcb-0.4.tar.gz

---
+++ packages/regugcb/regugcb-0.4.tar.gz/regugcb-0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="regugcb",
+    version="0.4",
+    author="Francis B. Lavoie",
+    author_email="francis.b.lavoie@usherbrooke.ca",
+    description="Functions for Regulation GCB",
+    long_description="Functions for Regulation GCB",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ),
+)
