7b5e4d0eaa1a55184cfcc547e61c7c9eaecc995b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:53 +0000

    Add package lsdviztools/0.4.4/lsdviztools-0.4.4-py2.py3-none-any.whl

---
+++ packages/lsdviztools/lsdviztools-0.4.4-py2.py3-none-any.whl/lsdviztools/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for lsdviztools."""
+
+__author__ = """Simon Marius Mudd"""
+__email__ = 'simon.m.mudd@ed.ac.uk'
+__version__ = '0.4.4'
+__url__ = 'https://github.com/LSDtopotools/lsdviztools'
+__license__ = 'MIT'
+
+
+__all__ = ['lsdbasemaptools', 'lsdmapfigure','lsdmapwrappers','lsdplottingtools','scripts']
