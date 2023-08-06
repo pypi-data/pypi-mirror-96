307e533f8f7001ec6aa975bfb4330263bf8bfee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:04 +0000

    Add package ambio/0.4.3/ambio-0.4.3-py3-none-any.whl

---
+++ packages/ambio/ambio-0.4.3-py3-none-any.whl/ambio/__init__.py
@@ -0,0 +1,12 @@
+"""
+Ambio
+=====
+A light-weight bioinformatics library written in Python. 
+It contains a variety of algorithms and functions that deal with strings.
+
+Available subpackages
+---------------------
+algs
+    Features the most famous and useful algorithms
+    used for matching and analizing DNA sequences.
+"""
