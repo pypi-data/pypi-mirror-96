fe110360558a4d90893c90ae397f48286811abf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:13 +0000

    Add package dotted-notation/0.4.4/dotted-notation-0.4.4.tar.gz

---
+++ packages/dotted-notation/dotted-notation-0.4.4.tar.gz/dotted-notation-0.4.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "rt") as f:
+    long_description = f.read()
+
+with open('LICENSE', 'rt') as f:
+    license = f.read()
+
+
+setuptools.setup(
+    name="dotted-notation",
+    version="0.4.4",
+    author="Frey Waid",
+    author_email="logophage1@gmail.com",
+    description="Dotted notation parser with pattern matching",
+    license=license,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/freywaid/dotted",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pyparsing>=2.4,<3',],
+)
