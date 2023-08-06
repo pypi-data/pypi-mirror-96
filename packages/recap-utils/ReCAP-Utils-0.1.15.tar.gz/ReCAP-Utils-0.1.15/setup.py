4ec82579fca436dd9a47c296927aa959ee8eb9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:23 +0000

    Add package recap-utils/0.1.15/ReCAP-Utils-0.1.15.tar.gz

---
+++ packages/recap-utils/ReCAP-Utils-0.1.15.tar.gz/ReCAP-Utils-0.1.15/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['recap_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click-pathlib>=2020.3,<2021.0',
+ 'click>=7.0,<8.0',
+ 'deepl-pro>=0.1.4,<0.2.0',
+ 'recap-argument-graph>=0.1.37,<0.2.0',
+ 'tomlkit>=0.7,<0.8']
+
+entry_points = \
+{'console_scripts': ['recap-utils = recap_utils.app:cli']}
+
+setup_kwargs = {
+    'name': 'recap-utils',
+    'version': '0.1.15',
+    'description': '',
+    'long_description': None,
+    'author': 'Mirko Lenz',
+    'author_email': 'info@mirko-lenz.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
