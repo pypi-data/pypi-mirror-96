94146c0e77751d133a4bad187dfcf8443e83a953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:59 +0000

    Add package insipher/0.0.1/insipher-0.0.1.tar.gz

---
+++ packages/insipher/insipher-0.0.1.tar.gz/insipher-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='insipher',
+      version='0.0.1',
+      description='Insipher Python SDK',
+      packages=['insipher'],
+      author="Insipher",
+      author_email='info@insipher.com',
+      license='MIT',
+      url='https://www.insipher.com',
+      install_requires=[
+            'uvicorn==0.13.4',
+            'mlflow==1.13.1',
+            'fastapi==0.63.0',
+            'minio==7.0.2',
+            'pyyaml==5.4.1',
+      ],
+      project_urls={
+        "Source Code": "https://github.com/insipher/insipher-python",
+      },
+      zip_safe=False)
