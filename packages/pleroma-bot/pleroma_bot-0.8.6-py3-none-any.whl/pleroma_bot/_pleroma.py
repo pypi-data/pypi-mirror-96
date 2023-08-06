eae24401f7df3e661beb7a485a04460c46e04441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package pleroma-bot/0.8.6/pleroma_bot-0.8.6-py3-none-any.whl

---
+++ packages/pleroma-bot/pleroma_bot-0.8.6-py3-none-any.whl/pleroma_bot/_pleroma.py
@@ -0,0 +1,245 @@
+import os
+from json.decoder import JSONDecodeError
+
+import re
+import json
+import shutil
+import requests
+import mimetypes
+from datetime import datetime, timedelta
+
+# Try to import libmagic
+# if it fails just use mimetypes
+try:
+    import magic
+except ImportError:
+    magic = None
+
+from . import logger
+from .i18n import _
+from ._utils import random_string, guess_type
+
+
+def get_date_last_pleroma_post(self):
+    """Gathers last post from the user in Pleroma and returns the date
+    of creation.
+
+    :returns: Date of last Pleroma post in '%Y-%m-%dT%H:%M:%SZ' format
+    """
+    pleroma_posts_url = (
+        f"{self.pleroma_base_url}/api/v1/accounts/"
+        f"{self.pleroma_username}/statuses"
+    )
+    response = requests.get(pleroma_posts_url, headers=self.header_pleroma)
+    if not response.ok:
+        response.raise_for_status()
+    posts = json.loads(response.text)
+    self.posts = posts
+    if posts:
+        date_pleroma = posts[0]["created_at"]
+    else:
+        self.posts = "none_found"
+        logger.warning(
+            _("No posts were found in the target Fediverse account")
+        )
+        if self.first_time:
+            date_pleroma = self.force_date()
+        else:
+            date_pleroma = datetime.strftime(
+                datetime.now() - timedelta(days=2), "%Y-%m-%dT%H:%M:%SZ"
+            )
+
+    return date_pleroma
+
+
+def post_pleroma(self, tweet: tuple, poll: dict, sensitive: bool) -> str:
+    """Post the given text to the Pleroma instance associated with the
+    User object
+
+    :param tweet: Tuple containing tweet_id, tweet_text. The ID will be used to
+    link to the Twitter status if 'signature' is True and to find related media
+    tweet_text is the literal text to use when creating the post.
+    :type tweet: tuple
+    :param poll: dict of poll if attached to tweet
+    :type poll: dict
+    :param sensitive: if tweet is possibly sensitive or not
+    :type sensitive: bool
+    :returns: id of post
+    :rtype: str
+    """
+    # TODO: transform twitter links to nitter links, if self.nitter
+    #  'true' in resolved shortened urls
+    pleroma_post_url = f"{self.pleroma_base_url}/api/v1/statuses"
+    pleroma_media_url = f"{self.pleroma_base_url}/api/v1/media"
+
+    tweet_id = tweet[0]
+    tweet_text = tweet[1]
+    tweet_folder = os.path.join(self.tweets_temp_path, tweet_id)
+    media_files = os.listdir(tweet_folder)
+    media_ids = []
+    if self.media_upload:
+        for file in media_files:
+            media_file = open(os.path.join(tweet_folder, file), "rb")
+            file_size = os.stat(os.path.join(tweet_folder, file)).st_size
+            mime_type = guess_type(os.path.join(tweet_folder, file))
+            timestamp = str(datetime.now().timestamp())
+            file_name = (
+                f"pleromapyupload_"
+                f"{timestamp}"
+                f"_"
+                f"{random_string(10)}"
+                f"{mimetypes.guess_extension(mime_type)}"
+            )
+            file_description = (file_name, media_file, mime_type)
+            files = {"file": file_description}
+            response = requests.post(
+                pleroma_media_url, headers=self.header_pleroma, files=files
+            )
+            try:
+                if not response.ok:
+                    response.raise_for_status()
+            except requests.exceptions.HTTPError:
+                if response.status_code == 413:
+                    size_msg = _(
+                        "Exception occurred"
+                        "\nMedia size too large:"
+                        "\nFilename: {file}"
+                        "\nSize: {size}MB"
+                        "\nConsider increasing the attachment"
+                        "\n size limit of your instance"
+                    ).format(file=file, size=round(file_size / 1048576, 2))
+                    logger.error(size_msg)
+                    pass
+                else:
+                    response.raise_for_status()
+            try:
+                media_ids.append(json.loads(response.text)["id"])
+            except (KeyError, JSONDecodeError):
+                logger.warning(
+                    _("Error uploading media:\t{}").format(str(response.text))
+                )
+                pass
+
+    if self.signature:
+        signature = f"\n\n 🐦🔗: {self.twitter_url}/status/{tweet_id}"
+        tweet_text = f"{tweet_text} {signature}"
+
+    # config setting override tweet attr
+    if hasattr(self, "sensitive"):
+        sensitive = self.sensitive
+
+    data = {
+        "status": tweet_text,
+        "sensitive": str(sensitive).lower(),
+        "visibility": self.visibility,
+        "media_ids[]": media_ids,
+    }
+
+    if poll:
+        data.update(
+            {
+                "poll[options][]": poll["options"],
+                "poll[expires_in]": poll["expires_in"],
+            }
+        )
+
+    if hasattr(self, "rich_text"):
+        if self.rich_text:
+            data.update({"content_type": self.content_type})
+    response = requests.post(
+        pleroma_post_url, data, headers=self.header_pleroma
+    )
+    if not response.ok:
+        response.raise_for_status()
+    logger.info(_("Post in Pleroma:\t{}").format(str(response)))
+    post_id = json.loads(response.text)["id"]
+    return post_id
+
+
+def update_pleroma(self):
+    """Update the Pleroma user info with the one retrieved from Twitter
+    when the User object was instantiated.
+    This includes:
+
+    * Profile image (if exists)
+    * Banner image (if exists)
+    * Bio text
+    * Screen name
+    * Additional metadata fields
+
+    :returns: None
+    """
+    # Get the biggest resolution for the profile picture (400x400)
+    # instead of 'normal'
+    if self.profile_image_url:
+        profile_img_big = re.sub(r"normal", "400x400", self.profile_image_url)
+        response = requests.get(profile_img_big, stream=True)
+        if not response.ok:
+            response.raise_for_status()
+        response.raw.decode_content = True
+        with open(self.avatar_path, "wb") as outfile:
+            shutil.copyfileobj(response.raw, outfile)
+
+    if self.profile_banner_url:
+        response = requests.get(self.profile_banner_url, stream=True)
+        if not response.ok:
+            response.raise_for_status()
+        response.raw.decode_content = True
+        with open(self.header_path, "wb") as outfile:
+            shutil.copyfileobj(response.raw, outfile)
+
+    # Set it on Pleroma
+    cred_url = f"{self.pleroma_base_url}/api/v1/accounts/update_credentials"
+
+    # Construct fields
+    fields = []
+    for field_item in self.fields:
+        field = (field_item["name"], field_item["value"])
+        fields.append(field)
+    data = {"note": self.bio_text, "display_name": self.display_name}
+
+    if self.profile_image_url:
+        data.update({"avatar": self.avatar_path})
+
+    if self.profile_banner_url:
+        data.update({"header": self.header_path})
+
+    if len(fields) > 4:
+        raise Exception(
+            _(
+                "Total number of metadata fields cannot exceed 4."
+                "\nProvided: {}. Exiting..."
+            ).format(len(fields))
+        )
+    for idx, (field_name, field_value) in enumerate(fields):
+        data[f'fields_attributes["{str(idx)}"][name]'] = field_name
+        data[f'fields_attributes["{str(idx)}"][value]'] = field_value
+
+    files = {}
+    timestamp = str(datetime.now().timestamp())
+    if self.profile_image_url:
+        avatar = open(self.avatar_path, "rb")
+        avatar_mime_type = guess_type(self.avatar_path)
+        avatar_file_name = (
+            f"pleromapyupload_{timestamp}_"
+            f"{random_string(10)}"
+            f"{mimetypes.guess_extension(avatar_mime_type)}"
+        )
+        files.update({"avatar": (avatar_file_name, avatar, avatar_mime_type)})
+    if self.profile_banner_url:
+        header = open(self.header_path, "rb")
+        header_mime_type = guess_type(self.header_path)
+        header_file_name = (
+            f"pleromapyupload_{timestamp}_"
+            f"{random_string(10)}"
+            f"{mimetypes.guess_extension(header_mime_type)}"
+        )
+        files.update({"header": (header_file_name, header, header_mime_type)})
+
+    response = requests.patch(
+        cred_url, data, headers=self.header_pleroma, files=files
+    )
+    if not response.ok:
+        response.raise_for_status()
+    logger.info(_("Updating profile:\t {}").format(str(response)))
+    return
---
+++ packages/pleroma-bot/pleroma_bot-0.8.6-py3-none-any.whl/pleroma_bot/_twitter.py
@@ -0,0 +1,208 @@
+import json
+import requests
+
+
+from pleroma_bot.i18n import _
+from pleroma_bot._utils import spinner
+
+
+def _get_twitter_info(self):
+    """Updates User object attributes with current Twitter info
+
+    This includes:
+
+    * Bio text
+    * Profile image url
+    * Banner image url
+    * Screen name
+
+    :return: None
+    """
+    twitter_user_url = (
+        f"{self.twitter_base_url}"
+        f"/users/show.json?screen_name="
+        f"{self.twitter_username}"
+    )
+    response = requests.get(
+        twitter_user_url, headers=self.header_twitter, auth=self.auth
+    )
+    if not response.ok:
+        response.raise_for_status()
+    user_twitter = json.loads(response.text)
+    self.bio_text = f'{self.bio_text}{user_twitter["description"]}'
+    # Check if user has profile image
+    if "profile_image_url_https" in user_twitter.keys():
+        self.profile_image_url = user_twitter["profile_image_url_https"]
+    # Check if user has banner image
+    if "profile_banner_url" in user_twitter.keys():
+        base_banner_url = user_twitter["profile_banner_url"]
+        self.profile_banner_url = f"{base_banner_url}/1500x500"
+    self.display_name = user_twitter["name"]
+    return
+
+
+def _get_tweets(self, version: str, tweet_id=None, start_time=None):
+    """Gathers last 'max_tweets' tweets from the user and returns them
+    as an dict
+    :param version: Twitter API version to use to retrieve the tweets
+    :type version: string
+    :param tweet_id: Tweet ID to retrieve
+    :type tweet_id: int
+
+    :returns: last 'max_tweets' tweets
+    :rtype: dict
+    """
+    if version == "v1.1":
+        if tweet_id:
+            twitter_status_url = (
+                f"{self.twitter_base_url}/statuses/"
+                f"show.json?id={str(tweet_id)}"
+            )
+            response = requests.get(
+                twitter_status_url, headers=self.header_twitter, auth=self.auth
+            )
+            if not response.ok:
+                response.raise_for_status()
+            tweet = json.loads(response.text)
+            return tweet
+        else:
+            twitter_status_url = (
+                f"{self.twitter_base_url}"
+                f"/statuses/user_timeline.json?screen_name="
+                f"{self.twitter_username}"
+                f"&count={str(self.max_tweets)}&include_rts=true"
+            )
+            response = requests.get(
+                twitter_status_url, headers=self.header_twitter, auth=self.auth
+            )
+            if not response.ok:
+                response.raise_for_status()
+            tweets = json.loads(response.text)
+            return tweets
+    elif version == "v2":
+        tweets_v2 = self._get_tweets_v2(
+            tweet_id=tweet_id, start_time=start_time
+        )
+        return tweets_v2
+    else:
+        raise ValueError(_("API version not supported: {}").format(version))
+
+
+def _get_tweets_v2(
+    self,
+    start_time,
+    tweet_id=None,
+    next_token=None,
+    previous_token=None,
+    tweets_v2=None,
+):
+    # Tweet number must be between 10 and 100
+    if not (100 >= self.max_tweets > 10):
+        global _
+        error_msg = _(
+            "max_tweets must be between 10 and 100. max_tweets: {}"
+        ).format(self.max_tweets)
+        raise ValueError(error_msg)
+    params = {}
+    previous_token = next_token
+    if tweet_id:
+        url = f"{self.twitter_base_url_v2}/tweets/{tweet_id}"
+    else:
+        url = (
+            f"{self.twitter_base_url_v2}/users/by?"
+            f"usernames={self.twitter_username}"
+        )
+        response = requests.get(
+            url, headers=self.header_twitter, auth=self.auth
+        )
+        if not response.ok:
+            response.raise_for_status()
+        response = json.loads(response.text)
+        twitter_id = response["data"][0]["id"]
+
+        url = f"{self.twitter_base_url_v2}/users/{twitter_id}/tweets"
+        if next_token:
+            params.update({"pagination_token": next_token})
+
+        params.update(
+            {
+                "start_time": start_time,
+                "max_results": self.max_tweets,
+            }
+        )
+
+    params.update(
+        {
+            "poll.fields": "duration_minutes,end_datetime,id,options,"
+            "voting_status",
+            "media.fields": "duration_ms,height,media_key,"
+            "preview_image_url,type,url,width,"
+            "public_metrics",
+            "expansions": "attachments.poll_ids,"
+            "attachments.media_keys,author_id,"
+            "entities.mentions.username,geo.place_id,"
+            "in_reply_to_user_id,referenced_tweets.id,"
+            "referenced_tweets.id.author_id",
+            "tweet.fields": "attachments,author_id,"
+            "context_annotations,conversation_id,"
+            "created_at,entities,"
+            "geo,id,in_reply_to_user_id,lang,"
+            "public_metrics,"
+            "possibly_sensitive,referenced_tweets,"
+            "source,text,"
+            "withheld",
+        }
+    )
+
+    response = requests.get(
+        url, headers=self.header_twitter, params=params, auth=self.auth
+    )
+    if not response.ok:
+        response.raise_for_status()
+
+    if tweets_v2:
+        next_tweets = response.json()
+
+        includes = ["users", "tweets", "media", "polls"]
+        for include in includes:
+            try:
+                _ = tweets_v2["includes"][include]
+            except KeyError:
+                tweets_v2["includes"].update({include: []})
+            try:
+                _ = next_tweets["includes"][include]
+            except KeyError:
+                next_tweets["includes"].update({include: []})
+
+        for tweet in next_tweets["data"]:
+            tweets_v2["data"].append(tweet)
+        for user in next_tweets["includes"]["users"]:
+            tweets_v2["includes"]["users"].append(user)
+        for tweet_include in next_tweets["includes"]["tweets"]:
+            tweets_v2["includes"]["tweets"].append(tweet_include)
+        for media in next_tweets["includes"]["media"]:
+            tweets_v2["includes"]["media"].append(media)
+        for poll in next_tweets["includes"]["polls"]:
+            tweets_v2["includes"]["polls"].append(poll)
+        tweets_v2["meta"] = next_tweets["meta"]
+    else:
+        tweets_v2 = json.loads(response.text)
+
+    try:
+        next_token = response.json()["meta"]["next_token"]
+        if next_token and next_token != previous_token:
+            self._get_tweets_v2(
+                start_time=start_time,
+                tweets_v2=tweets_v2,
+                next_token=next_token,
+                previous_token=previous_token,
+            )
+    except KeyError:
+        pass
+
+    return tweets_v2
+
+
+@spinner(_("Gathering tweets... "))
+def get_tweets(self, start_time):
+    return self._get_tweets("v2", start_time=start_time)
