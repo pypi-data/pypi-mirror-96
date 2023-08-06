2909fdc3cc10b5d0bb00d9e91348bb8322e96d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:32 +0000

    Add package stringanalysis/0.5.2/stringanalysis-0.5.2-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/stringanalysis/stringanalysis-0.5.2-cp38-cp38-manylinux2010_x86_64.whl/stringanalysis/token_alignment.py
@@ -0,0 +1,4 @@
+from stringanalysis.stringanalysis import order_by_token_alignment_vectorized as raw  # type: ignore
+
+def order_by_token_alignment_vectorized(*args, **kwargs):
+    return raw(*args, **kwargs)
