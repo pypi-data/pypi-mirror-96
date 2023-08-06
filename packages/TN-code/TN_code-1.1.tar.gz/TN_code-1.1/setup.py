7b1fbf6a2fe8f91a1aea92f3d45a410b8d7db598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:19 +0000

    Add package TN-code/1.1/TN_code-1.1.tar.gz

---
+++ packages/TN-code/TN_code-1.1.tar.gz/TN_code-1.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name='TN_code',
+    version='1.1',
+    description='Code gebruikt bij de opleiding TN van de Haagse Hogeschool',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://hhs-tn.github.io',
+    license='MIT',
+    author='Derek Land',
+    author_email='d.d.land@hhs.nl',
+    packages=setuptools.find_packages(exclude=('voorbeelden'),),
+    install_requires=['numpy',
+                      'matplotlib',
+                      'pandas',
+                      'sympy',
+                      ],
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Natural Language :: Dutch',
+                 'Programming Language :: Python :: 3',
+                 'Topic :: Utilities',
+                 ],
+)
