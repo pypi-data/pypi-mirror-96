3bc657305bffb1fc4cf7605d4777bc47f252bd5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:01 +0000

    Add package windyquery/0.0.11/windyquery-0.0.11.tar.gz

---
+++ packages/windyquery/windyquery-0.0.11.tar.gz/windyquery-0.0.11/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="windyquery",
+    version="0.0.11",
+    author="windymile.it",
+    author_email="windymile.it@gmail.com",
+    description="A non-blocking PostgreSQL query builder using Asyncio",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bluerelay/windyquery",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'asyncpg==0.22.0',
+        'ply==3.11',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
