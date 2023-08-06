9b5eee94fda4255e7c0ed76b58d2b9d77810957d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:47 +0000

    Add package regru-cloudapi/1.1.2/regru_cloudapi-1.1.2.tar.gz

---
+++ packages/regru-cloudapi/regru_cloudapi-1.1.2.tar.gz/regru_cloudapi-1.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='regru_cloudapi',
+      version='1.1.2',
+      url='https://github.com/plvskiy/regru_cloudapi',
+      author='Matthew Polovskiy',
+      packages=find_packages(),
+      python_requires='>=3.6'
+)
