1e1c8116498cac42979aa4be80ab80b99bb06dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package poi-clustering/0.0.6/poi-clustering-0.0.6.tar.gz

---
+++ packages/poi-clustering/poi-clustering-0.0.6.tar.gz/poi-clustering-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='poi-clustering',
+    version='0.0.6',
+    description='Point-of-Interest clustering from GPS trajectories',
+    author='Woohyeok Choi',
+    author_email='woohyeok.choi@kaist.ac.kr',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/woohyeok-choi/poi-clustering',
+    packages=['poi'],
+    install_requires=[
+        'numpy',
+        'scikit-learn'
+    ],
+    zip_safe=False,
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+    ]
+)
