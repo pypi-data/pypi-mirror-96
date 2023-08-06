70b239097064d4495b7158f7c21776d0c2b6e659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:47 +0000

    Add package cldz-cli/0.1.0/cldz_cli-0.1.0.tar.gz

---
+++ packages/cldz-cli/cldz_cli-0.1.0.tar.gz/cldz_cli-0.1.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cldz_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyJWT>=1.7.0,<2.0.0',
+ 'coolname>=1.1.0,<2.0.0',
+ 'fire>=0.3.1,<0.4.0',
+ 'prettytable>=0.7.2,<0.8.0',
+ 'requests>=2.7.0,<3.0.0',
+ 'requirements-parser>=0.2.0,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['cldz = cldz_cli.cldz_cli:main']}
+
+setup_kwargs = {
+    'name': 'cldz-cli',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Rohan Kothapalli',
+    'author_email': 'rohan.kothapalli@clouderizer.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
