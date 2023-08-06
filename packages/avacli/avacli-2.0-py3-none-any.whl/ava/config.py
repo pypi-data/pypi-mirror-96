0ef6168a1588abfaa14528b53a27b350e3fe7cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package avacli/2.0/avacli-2.0-py3-none-any.whl

---
+++ packages/avacli/avacli-2.0-py3-none-any.whl/ava/config.py
@@ -0,0 +1,12 @@
+# Weather API
+WX_API_KEY = "<YOUR-API-KEY>"
+WX_LOCATION = "<YOUR-DEFAULT-LOCATION>"
+WX_METRIC_TEMP = "<celsius|fahrenheit|kelvin>"
+WX_METRIC_WIND = "<miles_hour|km_hour|knots>"
+
+# Github API
+GITHUB_USER = "<USERNAME>"
+GITHUB_PASS = "<PASSWORD>"
+
+# Tidy
+TIDY_ROOT = None  # "</path/to/root>"
