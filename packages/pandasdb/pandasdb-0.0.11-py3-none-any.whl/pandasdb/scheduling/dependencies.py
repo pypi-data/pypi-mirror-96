8bc7c0b1e6f081cd20b084660d66089b2286c8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:33 +0000

    Add package pandasdb/0.0.11/pandasdb-0.0.11-py3-none-any.whl

---
+++ packages/pandasdb/pandasdb-0.0.11-py3-none-any.whl/pandasdb/scheduling/dependencies.py
@@ -0,0 +1,28 @@
+from airflow.sensors.external_task import ExternalTaskSensor, ExternalTaskMarker
+
+
+def waiting_for(dag_id, parent_dag_ids, timeout=600, allowed_states=['success'], failed_states=['failed', 'skipped'],
+                mode="reschedule"):
+    tasks = []
+    for parent_id in parent_dag_ids:
+        tasks.append(ExternalTaskSensor(
+            task_id=f"{parent_id}.{dag_id}.dependency",
+            external_dag_id=parent_id,
+            external_task_id=f"{parent_id}.{dag_id}.dependence",
+            timeout=timeout,
+            allowed_states=allowed_states,
+            failed_states=failed_states,
+            mode=mode,
+        ))
+    return tasks
+
+
+def awaited_by(dag_id, child_dag_ids):
+    tasks = []
+    for child_id in child_dag_ids:
+        tasks.append(ExternalTaskMarker(
+            task_id=f"{dag_id}.{child_id}.dependency",
+            external_dag_id=child_id,
+            external_task_id=f"{dag_id}.{child_id}.dependency",
+        ))
+    return tasks
