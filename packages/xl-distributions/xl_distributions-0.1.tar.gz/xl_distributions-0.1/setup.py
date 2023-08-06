6034dfbaa9290763bb22ea1ace0af26eab54f2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:15 +0000

    Add package xl-distributions/0.1/xl_distributions-0.1.tar.gz

---
+++ packages/xl-distributions/xl_distributions-0.1.tar.gz/xl_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xl_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['xl_distributions'],
+      author = 'Xiaolin Huang',
+      author_email = '501667014@qq.com',
+      zip_safe=False)
