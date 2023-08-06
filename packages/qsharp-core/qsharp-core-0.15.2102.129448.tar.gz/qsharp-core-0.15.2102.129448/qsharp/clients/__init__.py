d4f3bf5e815d71d05d50646814210c3c9a653348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package qsharp-core/0.15.2102.129448/qsharp-core-0.15.2102.129448.tar.gz

---
+++ packages/qsharp-core/qsharp-core-0.15.2102.129448.tar.gz/qsharp-core-0.15.2102.129448/qsharp/clients/__init__.py
@@ -0,0 +1,66 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+##
+# __init__.py: Logic for launching and configuring Q# clients.
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+
+
+import os
+import sys
+import time
+import logging
+import jupyter_client
+from distutils.util import strtobool
+
+class IQSharpNotInstalledError(Exception):
+    pass
+
+class IQSharpNotAvailableError(Exception):
+    pass
+
+def _start_client():
+    logger = logging.getLogger(__name__)
+
+    client_name =  os.getenv("QSHARP_PY_CLIENT", "iqsharp")
+
+    if client_name == "iqsharp":
+        import qsharp.clients.iqsharp
+        client = qsharp.clients.iqsharp.IQSharpClient()
+    elif client_name == "mock":
+        import qsharp.clients.mock
+        client = qsharp.clients.mock.MockClient()
+
+    try:
+        client.start()
+    except jupyter_client.kernelspec.NoSuchKernel as ex:
+        message = "IQ# is not installed." + \
+            "\nPlease follow the instructions at https://aka.ms/qdk-install/python."
+        print(message)
+        raise IQSharpNotInstalledError(message)
+
+    # Check if the server is up and running:
+    server_ready = False
+    for idx_attempt in range(20):
+        try:
+            server_ready = client.is_ready()
+            if server_ready:
+                break
+            if idx_attempt == 0:
+                print("Preparing Q# environment...")
+            else:
+                print(".", end='', flush=True)
+            time.sleep(1)
+        except Exception as ex:
+            logger.debug('Exception while checking Q# environment.', exc_info=ex)
+            print("!", end='', flush=True)
+            time.sleep(1)
+    if not server_ready:
+        message = "Q# environment was not available in allocated time." + \
+            "\nPlease check the instructions at https://aka.ms/qdk-install/python."
+        print(message)
+        raise IQSharpNotAvailableError(message)
+
+    return client
---
+++ packages/qsharp-core/qsharp-core-0.15.2102.129448.tar.gz/qsharp-core-0.15.2102.129448/qsharp/tests/test_azure.py
@@ -0,0 +1,149 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+##
+# test_azure.py: Tests Azure Quantum functionality against a mock workspace.
+##
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+##
+
+## IMPORTS ##
+
+import pytest
+import qsharp
+from qsharp.azure import AzureError, AzureJob, AzureTarget
+from .utils import set_environment_variables
+
+## SETUP ##
+
+@pytest.fixture(scope="session", autouse=True)
+def session_setup():
+    set_environment_variables()
+
+## TESTS ##
+
+def test_empty_workspace():
+    """
+    Tests behavior of a mock workspace with no providers.
+    """
+    with pytest.raises(AzureError) as exception_info:
+        qsharp.azure.target()
+    assert exception_info.value.error_name == "NotConnected"
+
+    targets = qsharp.azure.connect(
+        storage="test",
+        subscription="test",
+        resourceGroup="test",
+        workspace="test"
+    )
+    assert targets == []
+
+    with pytest.raises(AzureError) as exception_info:
+        qsharp.azure.target("invalid.target")
+    assert exception_info.value.error_name == "InvalidTarget"
+
+    jobs = qsharp.azure.jobs()
+    assert jobs == []
+
+def test_workspace_create_with_parameters():
+    """
+    Tests behavior of a mock workspace with providers, using parameters to connect.
+    """
+    targets = qsharp.azure.connect(
+        storage="test",
+        subscription="test",
+        resourceGroup="test",
+        workspace="WorkspaceNameWithMockProviders"
+    )
+    assert isinstance(targets, list)
+    assert len(targets) > 0
+
+    _test_workspace_with_providers_after_connection()
+
+def test_workspace_create_with_resource_id():
+    """
+    Tests behavior of a mock workspace with providers, using resource ID to connect.
+    Also verifies case-insensitivity of resource ID parsing.
+    """
+    subscriptionId = "f846b2bd-d0e2-4a1d-8141-4c6944a9d387"
+    resourceGroupName = "test"
+    workspaceName = "WorkspaceNameWithMockProviders"
+    targets = qsharp.azure.connect(
+        resourceId=f"/subscriptions/{subscriptionId}/RESOurceGroups/{resourceGroupName}/providers/Microsoft.Quantum/Workspaces/{workspaceName}")
+    assert isinstance(targets, list)
+    assert len(targets) > 0
+
+    _test_workspace_with_providers_after_connection()
+    _test_workspace_job_execution()
+
+def test_workspace_create_with_resource_id_and_storage():
+    """
+    Tests behavior of a mock workspace with providers, using resource ID and storage connection string to connect.
+    """
+    subscriptionId = "f846b2bd-d0e2-4a1d-8141-4c6944a9d387"
+    resourceGroupName = "test"
+    workspaceName = "WorkspaceNameWithMockProviders"
+    storageAccountConnectionString = "test"
+    targets = qsharp.azure.connect(
+        resourceId=f"/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Quantum/Workspaces/{workspaceName}",
+        storage=storageAccountConnectionString)
+    assert isinstance(targets, list)
+    assert len(targets) > 0
+
+    _test_workspace_with_providers_after_connection()
+
+def _test_workspace_with_providers_after_connection():
+    with pytest.raises(AzureError) as exception_info:
+        qsharp.azure.target()
+    assert exception_info.value.error_name == "NoTarget"
+
+    targets = qsharp.azure.connect()
+    for target in targets:
+        active_target = qsharp.azure.target(target.id)
+        assert isinstance(active_target, AzureTarget)
+        assert active_target == target
+
+    # Submit a snippet operation without parameters
+    op = qsharp.compile("""
+        operation HelloQ() : Result
+        {
+            Message($"Hello from quantum world!"); 
+            return Zero;
+        }
+    """)
+
+    job = qsharp.azure.submit(op)
+    assert isinstance(job, AzureJob)
+
+    retrieved_job = qsharp.azure.status(job.id)
+    assert isinstance(retrieved_job, AzureJob)
+    assert job.id == retrieved_job.id
+
+def _test_workspace_job_execution():
+    # Execute a workspace operation with parameters
+    op = qsharp.QSharpCallable("Microsoft.Quantum.SanityTests.HelloAgain", None)
+
+    with pytest.raises(AzureError) as exception_info:
+        qsharp.azure.execute(op)
+    assert exception_info.value.error_name == "JobSubmissionFailed"
+
+    histogram = qsharp.azure.execute(op, count=3, name="test", timeout=3, poll=2)
+    assert isinstance(histogram, dict)
+
+    retrieved_histogram = qsharp.azure.output()
+    assert isinstance(retrieved_histogram, dict)
+    assert histogram == retrieved_histogram
+
+    # Check that the submitted job exists in the workspace
+    jobs = qsharp.azure.jobs()
+    assert isinstance(jobs, list)
+    assert len(jobs) == 1
+
+    # Check that job filtering works
+    jobs = qsharp.azure.jobs(jobs[0].id)
+    assert isinstance(jobs, list)
+    assert len(jobs) == 1
+
+    jobs = qsharp.azure.jobs("invalid")
+    assert isinstance(jobs, list)
+    assert len(jobs) == 0
