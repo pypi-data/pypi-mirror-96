1aadc9063e3f1abc84a6427dc79374122320ee13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:19 +0000

    Add package pytket/0.8.0/pytket-0.8.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/pytket/pytket-0.8.0-cp39-cp39-manylinux2014_x86_64.whl/pytket/pauli/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2019-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+"""This module gives basic Pauli data structures in tket."""
+
+from pytket._tket.pauli import *  # type: ignore
---
+++ packages/pytket/pytket-0.8.0-cp39-cp39-manylinux2014_x86_64.whl/pytket/predicates/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2019-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+"""Module exposing the predicates and tools needed for compiler passes."""
+
+from pytket._tket.predicates import *  # type: ignore
