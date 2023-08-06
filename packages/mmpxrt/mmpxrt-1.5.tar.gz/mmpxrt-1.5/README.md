0e2ae27086c32e7cec064a5f5f2c1b1b2b08339e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package mmpxrt/1.5/mmpxrt-1.5.tar.gz

---
+++ packages/mmpxrt/mmpxrt-1.5.tar.gz/mmpxrt-1.5/README.md
@@ -0,0 +1,10 @@
+# mmpxrt
+Raytracing code for x-ray spectrometers with (not only) mossaic crystals
+Written 2017-2020, Michal Smid
+
+This is a raytracing code for design and analysis of x-ray spectrometers. Useful tool for (rather) experienced physicist. For more info about distribution, installation, usage, etc. of this package, please see attached manual.
+
+All files related to the code are available at:
+https://gitlab.hzdr.de/smid55/mmpxrt
+
+The packaging was done by using this manual: https://packaging.python.org/tutorials/packaging-projects/
---
+++ packages/mmpxrt/mmpxrt-1.5.tar.gz/mmpxrt-1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mmpxrt",
+    version="1.5",
+    author="Michal Smid",
+    author_email="m.smid@hzdr.de",
+    description="Raytracing code for x-ray (mosaic) spectrometers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.hzdr.de/smid55/mmpxrt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
