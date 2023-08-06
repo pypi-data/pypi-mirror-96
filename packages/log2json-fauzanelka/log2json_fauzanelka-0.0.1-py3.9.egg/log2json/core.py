5b3b492ee4179ee9d3b96f2b731b8a5e34db2f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:07 +0000

    Add package log2json-fauzanelka/0.0.1/log2json_fauzanelka-0.0.1-py3.9.egg

---
+++ packages/log2json-fauzanelka/log2json_fauzanelka-0.0.1-py3.9.egg/log2json/core.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import argparse
+import shutil
+import sys
+import os
+from typing import List, Optional, Tuple, Union
+from log2json.status import ExitStatus
+from log2json.cli.definition import parser
+
+
+def main(args: List[Union[str, bytes]] = sys.argv) -> ExitStatus:
+	"""
+	main
+	"""
+
+	try:
+		parsed_args = parser.parse_args(
+			args=args
+		)
+	except KeyboardInterrupt:
+		exit_status = ExitStatus.ERROR_CTRL_C
+	except SystemExit:
+		exit_status = ExitStatus.ERROR
+	else:
+		try:
+			exit_status = program(
+				args=parsed_args
+			)
+		except KeyboardInterrupt:
+			exit_status = ExitStatus.ERROR_CTRL_C
+		except SystemExit:
+			exit_status = ExitStatus.ERROR
+		except Exception:
+			exit_status = ExitStatus.ERROR
+
+	return exit_status
+
+
+def program(args: argparse.Namespace) -> ExitStatus:
+	exit_status = ExitStatus.SUCCESS
+
+	args.logfile.pop(0)
+
+	if (len(args.logfile) > 1 and args.o):
+		parser.error(
+			"If multiple log file(s) provided. You cannot use output file flag. Use directory flag instead.")
+		exit_status = ExitStatus.ERROR
+
+	if args.t == "text":
+		if len(args.logfile) == 1:
+			input_path = os.path.abspath(args.logfile[0])
+
+			if (args.o):
+				output_path = os.path.abspath(args.o)
+
+				if (os.path.splitext(output_path)[1] != 'txt'):
+					output_path += '.txt'
+			else:
+				output_path = os.path.abspath(
+					os.path.join(
+						os.getcwd(),
+						os.path.splitext(args.logfile[0]) + '.txt'
+					)
+				)
+
+			shutil.copy2(input_path, output_path)
+		else:
+			if (args.d):
+				pass
+			else:
+				pass
+	elif args.t == "json":
+		if (len(args.logfile) == 1):
+			if (args.o):
+				pass
+			else:
+				pass
+		else:
+			if (args.d):
+				pass
+			else:
+				pass
+
+	return exit_status
