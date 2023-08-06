88ff90b6f96988e990578f3d9f3bdbac46dcfd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:31 +0000

    Add package Freetail-GNURadio/0.1.1/Freetail_GNURadio-0.1.1-py3-none-any.whl

---
+++ packages/Freetail-GNURadio/Freetail_GNURadio-0.1.1-py3-none-any.whl/freetail_gnuradio/__init__.py
@@ -0,0 +1,29 @@
+import os.path as osp
+
+with open(osp.join(osp.dirname(__file__), 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
+
+
+def install_gr_blocks():
+    """Installs Freetail blocks into GNU Radio Companion's path
+
+    Note: This will overwrite any existing configuration of local_blocks_path
+    """
+    import configparser
+    import os.path as osp
+    from gnuradio import gr
+
+    config_filename = osp.join(gr.userconf_path(), "config.conf")
+
+    config = configparser.ConfigParser()
+    config.read(config_filename)
+
+    if 'grc' not in config:
+        config['grc'] = {}
+
+    local_blocks_path = osp.join(osp.dirname(osp.abspath(__file__)), 'grc')
+
+    config['grc']['local_blocks_path'] = local_blocks_path
+    with open(config_filename, 'w') as config_file:
+        config.write(config_file)
+
