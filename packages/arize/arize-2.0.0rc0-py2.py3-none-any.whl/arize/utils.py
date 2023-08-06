35b3e950f5a07790ea206b45bf8d675bceb7a9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:52 +0000

    Add package arize/2.0.0rc0/arize-2.0.0rc0-py2.py3-none-any.whl

---
+++ packages/arize/arize-2.0.0rc0-py2.py3-none-any.whl/arize/utils.py
@@ -0,0 +1,40 @@
+import math
+import pandas as pd
+
+from arize import public_pb2 as public__pb2
+
+MAX_BYTES_PER_BULK_RECORD = 100000
+
+
+def num_chunks(records):
+    total_bytes = sum(r.ByteSize() for r in records)
+    num_of_bulk = math.ceil(total_bytes / MAX_BYTES_PER_BULK_RECORD)
+    return math.ceil(len(records) / num_of_bulk)
+
+
+def bundle_records(organization_key: str, model_id: str, model_version, records) -> {}:
+    recs_per_msg = num_chunks(records)
+    recs = {
+        (i, i + recs_per_msg): records[i : i + recs_per_msg]
+        for i in range(0, len(records), recs_per_msg)
+    }
+    for k, r in recs.items():
+        recs[k] = public__pb2.BulkRecord(
+            records=r,
+            organization_key=organization_key,
+            model_id=model_id,
+            model_version=model_version,
+        )
+    return recs
+
+
+def convert_element(value):
+    """ converts scalar or array to python native """
+    val = getattr(value, "tolist", lambda: value)()
+    # Check if it's a list since elements from pd indices are converted to a scalar
+    # whereas pd series/dataframe elements are converted to list of 1 with the native value
+    if isinstance(val, list):
+        val = val[0]
+    if pd.isna(val):
+        return None
+    return val
