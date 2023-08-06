6fa80fca7f426ad726b0acebd7ba429cca11f7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:39 +0000

    Add package HQChart/1.0.22/hqchart-1.0.22.tar.gz

---
+++ packages/HQChart/hqchart-1.0.22.tar.gz/hqchart-1.0.22/hqchart/tushare/hqchartpy2_tushare_config.py
@@ -0,0 +1,12 @@
+
+#############################################################################################
+# tushare配置信息
+#
+#
+
+class TushareConfig():
+    # hqchartpy2授权码
+    HQCHART_AUTHORIZATION_KEY=None
+
+    # tushare授权码
+    TUSHARE_AUTHORIZATION_KEY="836dcc340f026bd41b378d702d5e11950df18c1750b18ec18dc4ea09"
