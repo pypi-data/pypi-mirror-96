1f710e22a4c4f903fc0aee74ebbb30b1a0454a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:36 +0000

    Add package Dialogs/1.0/Dialogs-1.0.tar.gz

---
+++ packages/Dialogs/Dialogs-1.0.tar.gz/Dialogs-1.0/setup.py
@@ -0,0 +1,33 @@
+ #!/usr/bin/env python
+ # -*- coding: utf-8 -*-
+
+import os
+from distutils.core import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='Dialogs',
+      version='1.0',
+      license='BSD',
+      description='Handles natural language inputs and outputs on cognitive robots',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      author='Séverin Lemaignan',
+      author_email='severin.lemaignan@epfl.ch',
+      url='https://github.com/severin-lemaignan/dialogs',
+      requires=['pykb'],
+      package_dir = {'': 'src'},
+      packages=['dialogs', 'dialogs.interpretation', 'dialogs.parsing', 'dialogs.helpers', 'dialogs.verbalization'],
+      scripts=['scripts/dialogs', 'scripts/dialogs_test'],
+      data_files=[('share/dialogs', ['share/dialogs/' + f for f in os.listdir('share/dialogs')]),
+                  ('share/doc/dialogs', ['AUTHORS', 'LICENSE', 'TODO', 'README.md']),
+                  ('share/doc/dialogs', ['doc/' + f for f in os.listdir('doc/') if f != "demo"]),
+                  ('share/examples/dialogs', ['share/examples/dialogs/' + f for f in os.listdir('share/examples/dialogs')])]
+      )
