72c4b5fbccd7fa65484c7aa2fa3044e03ab691d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:59 +0000

    Add package whatsall/2.4/whatsall-2.4.tar.gz

---
+++ packages/whatsall/whatsall-2.4.tar.gz/whatsall-2.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+
+setup(
+  name = 'whatsall',         
+  packages = ['whatsall'],   
+  version = '2.4',      
+  license='MIT',        
+  description = 'Automate sending messages on whatsapp to unlimited users without even saving the number',
+  author = 'Divesh Lunker',             
+  author_email = 'diveshdj92@gmail.com',      
+  url = 'https://github.com/diveshlunker/whatsall',
+  download_url = 'https://github.com/diveshlunker/whatsall/archive/2.tar.gz',   
+  keywords = ['whatsapp', 'automated messaging', 'whatsapp web','whatspip'], 
+  install_requires=[ 
+          "selenium"
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
