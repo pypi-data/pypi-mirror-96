408105b7c52bf466395a3663b16a8c5fd44bb925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:18 +0000

    Add package xtrf-api/1.0.10/xtrf_api-1.0.10-py3-none-any.whl

---
+++ packages/xtrf-api/xtrf_api-1.0.10-py3-none-any.whl/xtrf_api/api/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from xtrf_api.api.browser_api import BrowserApi
+from xtrf_api.api.client_invoices_api import ClientInvoicesApi
+from xtrf_api.api.clients_api import ClientsApi
+from xtrf_api.api.currency_exchange_rates_api import CurrencyExchangeRatesApi
+from xtrf_api.api.dictionaries_api import DictionariesApi
+from xtrf_api.api.files_api import FilesApi
+from xtrf_api.api.jobs__smart_v2_api import JobsSmartV2Api
+from xtrf_api.api.license_api import LicenseApi
+from xtrf_api.api.macros_api import MacrosApi
+from xtrf_api.api.projects__classic_api import ProjectsClassicApi
+from xtrf_api.api.projects__smart_v2_api import ProjectsSmartV2Api
+from xtrf_api.api.quotes__classic_api import QuotesClassicApi
+from xtrf_api.api.quotes__smart_v2_api import QuotesSmartV2Api
+from xtrf_api.api.reports_api import ReportsApi
+from xtrf_api.api.subscription_api import SubscriptionApi
+from xtrf_api.api.tasks__classic_api import TasksClassicApi
+from xtrf_api.api.users_api import UsersApi
+from xtrf_api.api.vendor_invoices_api import VendorInvoicesApi
+from xtrf_api.api.vendors_api import VendorsApi
