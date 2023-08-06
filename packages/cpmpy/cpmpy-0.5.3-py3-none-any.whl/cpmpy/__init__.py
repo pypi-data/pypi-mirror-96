39ce7394cec998214429d3050652c936ba461be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:54 +0000

    Add package cpmpy/0.5.3/cpmpy-0.5.3-py3-none-any.whl

---
+++ packages/cpmpy/cpmpy-0.5.3-py3-none-any.whl/cpmpy/__init__.py
@@ -0,0 +1,13 @@
+# Tias Guns, 2019
+# current version
+#==============================================================================
+# VERSION = (0, 5, 2, "dev")
+
+# __version__ = "%d.%d.%d.%s" % VERSION if len(VERSION) == 4 else \
+#               "%d.%d.%d" % VERSION
+__version__ = "0.5.3"
+
+from .variables import *
+from .expressions import *
+from .globalconstraints import *
+from .model import *
