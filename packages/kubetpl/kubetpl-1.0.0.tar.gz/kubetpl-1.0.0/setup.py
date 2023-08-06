34702837e73950f8c02468cfb6b34d12a62935e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:10 +0000

    Add package kubetpl/1.0.0/kubetpl-1.0.0.tar.gz

---
+++ packages/kubetpl/kubetpl-1.0.0.tar.gz/kubetpl-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kubetpl",
+    version="1.0.0",
+    author="Grauer W01f",
+    author_email="grauerwf@gmail.com",
+    description="A templater for kubernetes resources",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grauerwf/kubtemplate",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'kubtemplate=kubtemplate.kubtemplate:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
