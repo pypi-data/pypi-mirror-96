5fb9b75ffe377cb2d53047163b7a7d241c67b7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:56 +0000

    Add package DnsBench/1.0.5/DnsBench-1.0.5.tar.gz

---
+++ packages/DnsBench/DnsBench-1.0.5.tar.gz/DnsBench-1.0.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+from glob import glob,iglob
+
+setup(
+    name='DnsBench',
+    version='1.0.5',
+    url='https://github.com/aravindha1234u/dnsbench',
+    license='MIT',
+    author='Aravindha Hariharan M, Kumaran S',
+    author_email='aravindha1234u@gmail.com, kumaransubramanian0007@gmail.com',
+    description='Multi-Threaded Python Application for Dns Benchmark',
+    long_description = ''.join(open('README.md', encoding='utf-8').readlines()),
+    long_description_content_type ='text/markdown',
+    packages=['DnsBench'],
+    include_package_data=True,
+    install_requires=['Eel', 'pyinstaller',"beautifulsoup4", "requests", "dnspython","pyautogui"],
+    python_requires='>=3.5',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+    ],
+    entry_points={
+        'console_scripts': [
+            'DnsBench=DnsBench.__main__:main',
+            'dnsbench=DnsBench.__main__:main'
+        ],
+    },
+)
