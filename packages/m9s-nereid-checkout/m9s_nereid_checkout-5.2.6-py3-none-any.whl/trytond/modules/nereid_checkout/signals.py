87b1bbc07d9bd5556c0a3131aafacf1fc2f8043a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:54 +0000

    Add package m9s-nereid-checkout/5.2.6/m9s_nereid_checkout-5.2.6-py3-none-any.whl

---
+++ packages/m9s-nereid-checkout/m9s_nereid_checkout-5.2.6-py3-none-any.whl/trytond/modules/nereid_checkout/signals.py
@@ -0,0 +1,7 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from flask.signals import _signals
+
+#: Address update signal
+#:  - This signal is triggered when a the address is updated.
+cart_address_updated = _signals.signal('address-updated')
