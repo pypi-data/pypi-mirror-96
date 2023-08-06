6aadebf58c98cdc3a3df393534f87cff52a0c381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:01 +0000

    Add package ipso-phen/0.7.106.539/ipso_phen-0.7.106.539-py3-none-any.whl

---
+++ packages/ipso-phen/ipso_phen-0.7.106.539-py3-none-any.whl/ipso_phen/ipapi/ipt/ipt_invert_mask.py
@@ -0,0 +1,72 @@
+from ipso_phen.ipapi.base.ipt_abstract import IptBase
+
+
+import os
+import logging
+
+logger = logging.getLogger(os.path.splitext(__name__)[-1].replace(".", ""))
+
+
+class IptInvertMask(IptBase):
+    def build_params(self):
+        self.add_enabled_checkbox()
+
+    def process_wrapper(self, **kwargs):
+        wrapper = self.init_wrapper(**kwargs)
+        if wrapper is None:
+            return False
+
+        res = False
+        try:
+            if self.get_value_of("enabled") == 1:
+                mask = self.get_mask()
+                if mask is None:
+                    logger.error("Failure Invert mask: mask must be initialized")
+                    return
+                self.result = 255 - mask
+
+                # Write your code here
+                wrapper.store_image(self.result, "inverted_mask")
+                res = True
+            else:
+                wrapper.store_image(wrapper.current_image, "current_image")
+                res = True
+        except Exception as e:
+            res = False
+            logger.error(f"Invert mask FAILED, exception: {repr(e)}")
+        else:
+            pass
+        finally:
+            return res
+
+    @property
+    def name(self):
+        return "Invert mask"
+
+    @property
+    def package(self):
+        return "TPMP"
+
+    @property
+    def is_wip(self):
+        return True
+
+    @property
+    def real_time(self):
+        return True
+
+    @property
+    def result_name(self):
+        return "mask"
+
+    @property
+    def output_kind(self):
+        return "mask"
+
+    @property
+    def use_case(self):
+        return ["Mask cleanup"]
+
+    @property
+    def description(self):
+        return """'Invert mask"""
