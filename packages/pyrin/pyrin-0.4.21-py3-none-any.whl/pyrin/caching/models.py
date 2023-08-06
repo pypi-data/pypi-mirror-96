989d887e02aa8de3fd59f4d087fd40fb46d65171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:08 +0000

    Add package pyrin/0.4.21/pyrin-0.4.21-py3-none-any.whl

---
+++ packages/pyrin/pyrin-0.4.21-py3-none-any.whl/pyrin/caching/models.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+caching models module.
+"""
+
+from sqlalchemy import BigInteger, Unicode, LargeBinary
+
+from pyrin.database.model.base import CoreEntity
+from pyrin.database.model.mixin import CreateHistoryMixin
+from pyrin.database.orm.sql.schema.base import CoreColumn
+from pyrin.database.orm.sql.schema.columns import AutoPKColumn
+
+
+class CacheItemBaseEntity(CoreEntity):
+    """
+    cache item base entity class.
+    """
+
+    _table = 'cache_item'
+
+    id = AutoPKColumn(name='id')
+
+
+class CacheItemEntity(CacheItemBaseEntity, CreateHistoryMixin):
+    """
+    cache item entity class.
+    """
+
+    _extend_existing = True
+
+    cache_name = CoreColumn(name='cache_name', type_=Unicode(length=25), nullable=False)
+    shard_name = CoreColumn(name='shard_name', type_=Unicode(25), nullable=True)
+    version = CoreColumn(name='version', type_=Unicode(length=20), nullable=False)
+    key = CoreColumn(name='key', type_=BigInteger, nullable=False)
+    item = CoreColumn(name='item', type_=LargeBinary, nullable=False)
