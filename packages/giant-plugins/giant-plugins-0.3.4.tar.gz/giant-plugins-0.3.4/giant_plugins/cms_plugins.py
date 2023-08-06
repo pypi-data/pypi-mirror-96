d482d6f37db0b8fdc51e243ab48db08c861f671e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:28 +0000

    Add package giant-plugins/0.3.4/giant-plugins-0.3.4.tar.gz

---
+++ packages/giant-plugins/giant-plugins-0.3.4.tar.gz/giant-plugins-0.3.4/giant_plugins/cms_plugins.py
@@ -0,0 +1,17 @@
+from django.utils.module_loading import import_string
+from cms.exceptions import PluginAlreadyRegistered
+
+import_list = [
+    "content_width_video",
+    "content_width_image",
+    "page_card",
+    "pullquote",
+    "rich_text",
+    "hero_image",
+]
+
+for plugin in import_list:
+    try:
+        import_string(f"giant_plugins.{plugin}.cms_plugins.__all__")
+    except PluginAlreadyRegistered:
+        pass
