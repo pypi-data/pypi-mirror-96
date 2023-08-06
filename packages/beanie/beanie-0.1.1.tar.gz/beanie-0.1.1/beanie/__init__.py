4f13ddc5f4352e0b6671dcf7c68d8452fc490790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package beanie/0.1.1/beanie-0.1.1.tar.gz

---
+++ packages/beanie/beanie-0.1.1.tar.gz/beanie-0.1.1/beanie/__init__.py
@@ -0,0 +1,6 @@
+from beanie.collections import Collection
+from beanie.cursor import Cursor
+from beanie.documents import Document
+
+__version__ = "0.1.1"
+__all__ = ["Document", "Collection", "Cursor"]
