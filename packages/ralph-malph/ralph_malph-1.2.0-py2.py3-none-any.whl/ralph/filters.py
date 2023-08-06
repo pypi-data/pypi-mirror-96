9188dfc98c11bc1d64d8112b1f1eb8cda0f8be32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:11 +0000

    Add package ralph-malph/1.2.0/ralph_malph-1.2.0-py2.py3-none-any.whl

---
+++ packages/ralph-malph/ralph_malph-1.2.0-py2.py3-none-any.whl/ralph/filters.py
@@ -0,0 +1,27 @@
+"""
+Ralph tracking logs filters.
+"""
+from .exceptions import EventKeyError
+
+
+def anonymous(event):
+    """Remove anonymous events.
+
+    Args:
+        event (dict): Event to filter.
+
+    Returns:
+        event (dict): When event is not anonymous.
+        None: Otherwise.
+
+    Raises:
+        EventKeyError: When the event does not contain the `username` key.
+
+    """
+
+    if "username" not in event:
+        raise EventKeyError("Cannot filter anonymous event without 'username' key.")
+    if not event.get("username", ""):
+        return None
+
+    return event
