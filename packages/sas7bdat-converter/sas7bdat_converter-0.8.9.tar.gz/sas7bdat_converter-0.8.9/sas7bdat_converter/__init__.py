515e7f729ba74233973d1a2e52b12f36c3835b62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package sas7bdat-converter/0.8.9/sas7bdat_converter-0.8.9.tar.gz

---
+++ packages/sas7bdat-converter/sas7bdat_converter-0.8.9.tar.gz/sas7bdat_converter-0.8.9/sas7bdat_converter/__init__.py
@@ -0,0 +1,18 @@
+from sas7bdat_converter.converter import (  # noqa: F401
+    batch_to_csv,
+    batch_to_excel,
+    batch_to_json,
+    batch_to_xml,
+    dir_to_csv,
+    dir_to_excel,
+    dir_to_json,
+    dir_to_xml,
+    to_csv,
+    to_dataframe,
+    to_excel,
+    to_json,
+    to_xml,
+)
+
+name = "sas7bdat_converter"
+__version__ = "0.8.9"
