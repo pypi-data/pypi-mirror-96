a469270766f96d923785ac39c5b80cd96e8cea6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:03 +0000

    Add package openremote-cli/0.2.2/openremote_cli-0.2.2-py3-none-any.whl

---
+++ packages/openremote-cli/openremote_cli-0.2.2-py3-none-any.whl/openremote_cli/config.py
@@ -0,0 +1,18 @@
+import os
+
+
+def initialize():
+    global DRY_RUN, LEVEL, TELEMETRY_URL, VERBOSE, PROFILE, BUCKET
+    VERBOSE = False
+    DRY_RUN = False
+    LEVEL = 'logging.ERROR'
+    TELEMETRY_URL = 'https://cli.developers.openremote.io/metrics'
+    try:
+        if os.environ['TELEMETRY_URL']:
+            TELEMETRY_URL = f"{os.environ['TELEMETRY_URL']}/metrics"
+    except:
+        pass
+
+    # AWS parameters
+    PROFILE = 'openremote-cli'
+    BUCKET = 'openremote-mvp-map-storage'
