e64135cb9fb2ce28f777a2e3b2dd7dc9cbbf130f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:10 +0000

    Add package hikyuu/1.1.8/hikyuu-1.1.8-cp38-none-win_amd64.whl

---
+++ packages/hikyuu/hikyuu-1.1.8-cp38-none-win_amd64.whl/hikyuu/gui/data/ImportPytdxToH5Task.py
@@ -0,0 +1,92 @@
+# coding:utf-8
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2010-2017 fasiondog/hikyuu
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import logging
+import sqlite3
+import mysql.connector
+from pytdx.hq import TdxHq_API
+from hikyuu.data.pytdx_to_h5 import import_data as h5_import_data
+from hikyuu.data.pytdx_to_mysql import import_data as mysql_import_data
+
+
+class ProgressBar:
+    def __init__(self, src):
+        self.src = src
+
+    def __call__(self, cur, total):
+        self.src.queue.put(
+            [self.src.task_name, self.src.market, self.src.ktype, (cur + 1) * 100 // total, 0]
+        )
+
+
+class ImportPytdxToH5:
+    def __init__(
+        self, queue, config, market, ktype, quotations, ip, port, dest_dir, start_datetime
+    ):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.task_name = 'IMPORT_KDATA'
+        self.queue = queue
+        self.config = config
+        self.market = market
+        self.ktype = ktype
+        self.quotations = quotations
+        self.ip = ip
+        self.port = port
+        self.dest_dir = dest_dir
+        self.startDatetime = start_datetime
+
+    def __call__(self):
+        if self.config.getboolean('hdf5', 'enable', fallback=True):
+            sqlite_file = "{}/stock.db".format(self.config['hdf5']['dir'])
+            connect = sqlite3.connect(sqlite_file, timeout=1800)
+            import_data = h5_import_data
+            self.logger.debug('use hdf5 import kdata')
+        else:
+            db_config = {
+                'user': self.config['mysql']['usr'],
+                'password': self.config['mysql']['pwd'],
+                'host': self.config['mysql']['host'],
+                'port': self.config['mysql']['port']
+            }
+            connect = mysql.connector.connect(**db_config)
+            import_data = mysql_import_data
+            self.logger.debug('use mysql import kdata')
+
+        count = 0
+        try:
+            progress = ProgressBar(self)
+            api = TdxHq_API()
+            api.connect(self.ip, self.port)
+            count = import_data(
+                connect, self.market, self.ktype, self.quotations, api, self.dest_dir,
+                self.startDatetime, progress
+            )
+        except Exception as e:
+            self.logger.error("ImportPytdxToH5Task failed! {}".format(e))
+            #self.queue.put([self.task_name, self.market, self.ktype, str(e), count])
+        finally:
+            connect.commit()
+            connect.close()
+
+        self.queue.put([self.task_name, self.market, self.ktype, None, count])
---
+++ packages/hikyuu/hikyuu-1.1.8-cp38-none-win_amd64.whl/hikyuu/util/mylog.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# cp936
+
+import logging
+import traceback
+import time
+import functools
+
+
+# 统计函数运行时间
+def spend_time(func):
+    @functools.wraps(func)
+    def wrappedFunc(*args, **kargs):
+        starttime = time.time()
+        try:
+            print("\nCalling: %s" % func.__name__)
+            return func(*args, **kargs)
+        finally:
+            endtime = time.time()
+            print("spend time: %.4fs, %.2fm" % (endtime - starttime, (endtime - starttime) / 60))
+
+    return wrappedFunc
+
+
+FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s [%(name)s::%(funcName)s]'
+logging.basicConfig(format=FORMAT, level=logging.INFO)
+
+hku_logger_name = 'hikyuu'
+hku_logger = logging.getLogger(hku_logger_name)
+
+
+def class_logger(cls, enable=False):
+    #logger = logging.getLogger("{}.{}".format(cls.__module__, cls.__name__))
+    logger = logging.getLogger("{}".format(cls.__name__))
+    if enable == 'debug':
+        logger.setLevel(logging.DEBUG)
+    elif enable == 'info':
+        logger.setLevel(logging.INFO)
+    cls._should_log_debug = logger.isEnabledFor(logging.DEBUG)
+    cls._should_log_info = logger.isEnabledFor(logging.INFO)
+    cls.logger = logger
+
+
+def add_class_logger_handler(handler, class_list, level=logging.INFO):
+    """为指定的类增加日志 handler，并设定级别
+
+    :param handler: logging handler
+    :param class_list: 类列表
+    :param level: 日志级别
+    """
+    for cls in class_list:
+        #logger = logging.getLogger("{}.{}".format(cls.__module__, cls.__name__))
+        logger = logging.getLogger("{}".format(cls.__name__))
+        logger.addHandler(handler)
+        logger.setLevel(level)
+
+
+def hku_debug(msg, logger=None):
+    st = traceback.extract_stack()[-2]
+    if logger:
+        logger.debug("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+    else:
+        hku_logger.debug("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+
+
+hku_trace = hku_debug
+
+
+def hku_info(msg, logger=None):
+    st = traceback.extract_stack()[-2]
+    if logger:
+        logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+    else:
+        hku_logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+
+
+def hku_warn(msg, logger=None):
+    st = traceback.extract_stack()[-2]
+    if logger:
+        logger.warning("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+    else:
+        hku_logger.warning("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+
+
+def hku_error(msg, logger=None):
+    st = traceback.extract_stack()[-2]
+    if logger:
+        logger.error("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+    else:
+        hku_logger.error("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+
+
+def hku_fatal(msg, logger=None):
+    st = traceback.extract_stack()[-2]
+    if logger:
+        logger.critical("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+    else:
+        hku_logger.critical("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+
+
+def hku_debug_if(exp, msg, logger=None, callback=None):
+    if exp:
+        st = traceback.extract_stack()[-2]
+        if logger:
+            logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        else:
+            hku_logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        if callback:
+            callback()
+
+
+hku_trace_if = hku_debug_if
+
+
+def hku_info_if(exp, msg, logger=None, callback=None):
+    if exp:
+        st = traceback.extract_stack()[-2]
+        if logger:
+            logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        else:
+            hku_logger.info("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        if callback:
+            callback()
+
+
+def hku_warn_if(exp, msg, logger=None, callback=None):
+    if exp:
+        st = traceback.extract_stack()[-2]
+        if logger:
+            logger.warning("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        else:
+            hku_logger.warning("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        if callback:
+            callback()
+
+
+def hku_error_if(exp, msg, logger=None, callback=None):
+    if exp:
+        st = traceback.extract_stack()[-2]
+        if logger:
+            logger.error("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        else:
+            hku_logger.error("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        if callback:
+            callback()
+
+
+def hku_fatal_if(exp, msg, logger=None, callback=None):
+    if exp:
+        st = traceback.extract_stack()[-2]
+        if logger:
+            logger.critical("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        else:
+            hku_logger.critical("{} [{}] ({}:{})".format(msg, st.name, st.filename, st.lineno))
+        if callback:
+            callback()
+
+
+# 跟踪函数运行
+def with_trace(level=logging.INFO):
+    def with_trace_wrap(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            hku_info('start run: %s' % func.__name__)
+            result = func(*args, **kwargs)
+            hku_info('completed: %s' % func.__name__)
+            return result
+
+        return wrapper
+
+    return with_trace_wrap
