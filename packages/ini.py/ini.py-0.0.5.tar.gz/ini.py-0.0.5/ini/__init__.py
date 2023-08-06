bb2324c1c3e0e92ead93cad5528bc0e7e47226c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:46 +0000

    Add package ini.py/0.0.5/ini.py-0.0.5.tar.gz

---
+++ packages/ini.py/ini.py-0.0.5.tar.gz/ini.py-0.0.5/ini/__init__.py
@@ -0,0 +1,43 @@
+def parse(text):
+    data = {}
+    section = ''
+
+    for line in text.split('\n'):
+        if line.startswith(';'):
+            continue
+
+        elif line.startswith('[') and line.endswith(']'):
+            line = line.replace('[', '')
+            line = line.replace(']', '')
+            
+            section = line.strip()
+            
+            if not data.get(section):
+                data[section] = {}
+
+        else:
+            if "=" in line:
+                key, value = line.split("=")[0], "=".join(line.split("=")[1:])              
+                key = key.replace("\"", "")
+                key = key.replace("\'", "")
+                key = key.strip()
+                
+                value = value.replace("\"", "")
+                value = value.replace("\'", "")
+                value = value.strip()
+
+                data[section][key] = value
+
+    return data
+
+def convert(data):
+    text = ""
+    for section in data:
+        text += "[%s]\n" % section
+
+        for key in data[section]:
+            text += "%s = %s\n" % (key, data[section][key])
+
+        text += "\n"
+
+    return text.strip()
