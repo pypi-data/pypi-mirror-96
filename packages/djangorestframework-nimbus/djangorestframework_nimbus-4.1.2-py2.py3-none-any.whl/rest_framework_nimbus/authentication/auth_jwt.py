c0a9a10d6ec6fa583080555ed55a5d09c985c535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:28 +0000

    Add package djangorestframework-nimbus/4.1.2/djangorestframework_nimbus-4.1.2-py2.py3-none-any.whl

---
+++ packages/djangorestframework-nimbus/djangorestframework_nimbus-4.1.2-py2.py3-none-any.whl/rest_framework_nimbus/authentication/auth_jwt.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import jwt
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_jwt.authentication import jwt_decode_handler
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication as JWTAuthentication
+from ..err_code import ErrCode, APIError
+
+
+class JSONWebTokenAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        jwt_value = self.get_jwt_value(request)
+        if not jwt_value:
+            raise APIError(ErrCode.ERR_JWT_EMPTY, status_code=401)
+        try:
+            payload = jwt_decode_handler(jwt_value)
+        except jwt.ExpiredSignature:
+            raise APIError(ErrCode.ERR_JWT_EXPIRED, status_code=401)
+        except jwt.DecodeError:
+            raise APIError(ErrCode.ERR_JWT_DECODE, status_code=401)
+        except jwt.InvalidTokenError:
+            raise APIError(ErrCode.ERR_JWT_INVALID, status_code=401)
+        user = self.authenticate_credentials(payload)
+        return user, jwt_value
+
