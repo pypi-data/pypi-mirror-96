d7e29d81a1f130be5c16e2723f46df0445c33ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:36 +0000

    Add package printtutorial-ef/0.0.1/printtutorial_ef-0.0.1.tar.gz

---
+++ packages/printtutorial-ef/printtutorial_ef-0.0.1.tar.gz/printtutorial_ef-0.0.1/printmepack/printmenow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  2 14:29:24 2021
+
+@author: eduard
+"""
+
+def print_HALLO():
+    print("Hello from print me now")
+    
+def print_HALLO_2():
+    print("Hello from print me now 2")    
