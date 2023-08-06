04546c446f14a5d7b4373d00fde75fec76e2bb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package gitflo/0.0.3a0/gitflo-0.0.3a0.tar.gz

---
+++ packages/gitflo/gitflo-0.0.3a0.tar.gz/gitflo-0.0.3a0/gf/utils.py
@@ -0,0 +1,43 @@
+import os
+import re
+import git
+import typer
+from git.exc import GitCommandError, InvalidGitRepositoryError
+
+
+def get_repo():
+    try:
+        ret = git.Repo()
+    except InvalidGitRepositoryError as e:
+        typer.secho(f'not a git repository: {e}', fg=typer.colors.RED)
+        os._exit(0)
+    else:
+        return ret
+    
+
+repo= get_repo()
+
+
+def merge_to():
+    pass
+
+
+def no_traceback(f):
+    """block out python traceback message"""
+    def _wapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except GitCommandError as e:
+            # typer.echo(e)
+            raise typer.Exit(e)
+    return _wapper
+
+
+def get_create_date(branch_name: str):
+    """Get branch creation time"""
+    txt = repo.git.reflog(branch_name, date='short')
+    try:
+        dates = re.findall('{[\d-]+}', txt)
+        return dates[-1].strip('{}')
+    except IndexError:
+        return ''
---
+++ packages/gitflo/gitflo-0.0.3a0.tar.gz/gitflo-0.0.3a0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gitflo", 
+    version="0.0.3a",
+    author="Be5yond",
+    author_email="beyond147896@126.com",
+    description="git-flow branch management with standard commit format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Be5yond/gf",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["gitpython", "typer", "prompt_toolkit"],
+    python_requires='>=3.6',
+    entry_points = {
+        'console_scripts': ['gf = gf.main:app']
+    }
+)
