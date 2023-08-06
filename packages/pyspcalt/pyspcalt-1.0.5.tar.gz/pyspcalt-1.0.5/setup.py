96f02d6f202e937c87a914b143540d64aa8697ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:10 +0000

    Add package pyspcalt/1.0.5/pyspcalt-1.0.5.tar.gz

---
+++ packages/pyspcalt/pyspcalt-1.0.5.tar.gz/pyspcalt-1.0.5/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2016  Carlos Henrique Silva <carlosqsilva@outlook.com>
+#
+# This library is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(name='pyspcalt',
+      version='1.0.5',
+      description='Statistical Process Control Charts Library for Humans',
+      long_description='Statistical Process Control Charts Library for Humans',
+      url='https://github.com/carlosqsilva/pyspc',
+      author='Carlos Silva',
+      author_email='carlosqsilva@outlook.com',
+      license='GPLv3',
+      packages=find_packages(),
+      package_dir={ "pyspcalt": "pyspcalt" },
+      install_requires=['pandas', 'matplotlib', 'numpy', 'scipy'],
+      zip_safe=False)
