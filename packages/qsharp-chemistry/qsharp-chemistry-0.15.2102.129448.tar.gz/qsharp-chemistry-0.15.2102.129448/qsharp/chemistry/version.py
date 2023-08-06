860b8d33dcd8e9a40238c8dc2c70f70870f5fb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:22 +0000

    Add package qsharp-chemistry/0.15.2102.129448/qsharp-chemistry-0.15.2102.129448.tar.gz

---
+++ packages/qsharp-chemistry/qsharp-chemistry-0.15.2102.129448.tar.gz/qsharp-chemistry-0.15.2102.129448/qsharp/chemistry/version.py
@@ -0,0 +1,9 @@
+# Auto-generated file, do not edit.
+##
+# version.py: Specifies the version of the qsharp-chemistry package.
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+__version__ = "0.15.2102.129448"
+_user_agent_extra = ""
