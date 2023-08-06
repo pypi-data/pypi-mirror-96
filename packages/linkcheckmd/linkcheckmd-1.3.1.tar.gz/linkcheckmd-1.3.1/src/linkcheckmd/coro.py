92149447931da52868cceaca04028fe0f165ea8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:13 +0000

    Add package linkcheckmd/1.3.1/linkcheckmd-1.3.1.tar.gz

---
+++ packages/linkcheckmd/linkcheckmd-1.3.1.tar.gz/linkcheckmd-1.3.1/src/linkcheckmd/coro.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+import re
+import typing as T
+from pathlib import Path
+import warnings
+import asyncio
+import itertools
+import logging
+
+import aiohttp
+
+from . import files
+
+# tuples, not lists
+
+EXC = (
+    aiohttp.client_exceptions.ClientConnectorError,
+    aiohttp.client_exceptions.ServerDisconnectedError,
+)
+OKE = asyncio.TimeoutError
+TIMEOUT = 10
+
+
+async def check_urls(
+    path: Path,
+    regex: str,
+    ext: str,
+    hdr: dict[str, str] = None,
+    method: str = "get",
+    recurse: bool = False,
+) -> list[tuple[str, str, T.Any]]:
+
+    glob = re.compile(regex)
+
+    tasks = [check_url(fn, glob, ext, hdr, method=method) for fn in files.get(path, ext, recurse)]
+
+    warnings.simplefilter("ignore")
+
+    urls = await asyncio.gather(*tasks)
+
+    warnings.resetwarnings()
+
+    # this is per aiohttp manual, when using HTTPS SSL sites, just before closing
+    # the event loop, do a 250ms sleep (not for each site)
+    await asyncio.sleep(0.250)
+
+    return list(itertools.chain(*urls))  # flatten list of lists
+
+
+async def check_url(
+    fn: Path, glob, ext: str, hdr: dict[str, str] = None, *, method: str = "get"
+) -> list[tuple[str, str, T.Any]]:
+
+    urls = glob.findall(fn.read_text(errors="ignore"))
+    logging.debug(fn.name, " ".join(urls))
+    bad: list[tuple[str, str, T.Any]] = []
+
+    timeout = aiohttp.ClientTimeout(total=TIMEOUT)
+
+    for url in urls:
+        if ext == ".md":
+            url = url[1:-1]
+        try:
+            # anti-crawling behavior doesn't like .head() method--.get() is slower but avoids lots of false positives
+            async with aiohttp.ClientSession(headers=hdr, timeout=timeout) as session:
+                if method == "get":
+                    async with session.get(url, allow_redirects=True) as response:
+                        code = response.status
+                elif method == "head":
+                    async with session.head(url, allow_redirects=True) as response:
+                        code = response.status
+                else:
+                    raise ValueError(f"Unknown retreive method {method}")
+        except OKE:
+            continue
+        except EXC as e:
+            bad.append((fn.name, url, e))  # e, not str(e)
+            print("\n", bad[-1])
+            continue
+
+        if code != 200:
+            bad.append((fn.name, url, code))
+            print("\n", bad[-1])
+        else:
+            logging.info(f"OK: {url:80s}")
+
+    return bad
---
+++ packages/linkcheckmd/linkcheckmd-1.3.1.tar.gz/linkcheckmd-1.3.1/src/linkcheckmd/sync.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+import requests
+import re
+import typing as T
+from pathlib import Path
+import warnings
+import urllib3
+import logging
+
+from . import files
+
+TIMEOUT = 10
+RETRYCODES = (400, 404, 405, 503)
+# multiple exceptions must be tuples, not lists in general
+OKE = requests.exceptions.TooManyRedirects  # FIXME: until full browswer like Arsenic implemented
+EXC = (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError)
+
+"""
+synchronous routines
+"""
+
+
+def check_urls(
+    path: Path,
+    regex: str,
+    ext: str = ".md",
+    hdr: dict[str, str] = None,
+    verifycert: bool = False,
+    recurse: bool = False,
+) -> list[tuple[str, str, T.Any]]:
+
+    bads: list[tuple[str, str, T.Any]] = []
+
+    glob = re.compile(regex)
+
+    warnings.simplefilter("ignore", urllib3.exceptions.InsecureRequestWarning)
+
+    with requests.Session() as sess:
+        if hdr:
+            sess.headers.update(hdr)
+            sess.max_redirects = 5
+        # %% loop
+        for fn in files.get(path, ext, recurse):
+            for bad in check_url(fn, glob, ext, sess, hdr, verifycert):
+                print("\n", bad)
+                bads.append(bad)
+
+    warnings.resetwarnings()
+
+    return bads
+
+
+def check_url(
+    fn: Path, glob, ext: str, sess, hdr: dict[str, str] = None, verifycert: bool = False
+) -> T.Iterable[tuple[str, str, T.Any]]:
+
+    urls = glob.findall(fn.read_text(errors="ignore"))
+
+    for url in urls:
+        if ext == ".md":
+            url = url[1:-1]
+        try:
+            R = sess.head(url, allow_redirects=True, timeout=TIMEOUT, verify=verifycert)
+            if R.status_code in RETRYCODES:
+                if retry(url, hdr, verifycert):
+                    continue
+                else:
+                    yield fn.name, url, R.status_code
+                    continue
+        except OKE:
+            continue
+        except EXC as e:
+            if retry(url, hdr, verifycert):
+                continue
+            yield fn.name, url, str(e)
+            continue
+
+        code = R.status_code
+        if code != 200:
+            yield fn.name, url, code
+        else:
+            logging.info(f"OK: {url:80s}")
+
+
+def retry(url: str, hdr: dict[str, str] = None, verifycert: bool = False) -> bool:
+    ok = False
+
+    try:
+        # anti-crawling behavior doesn't like .head() method--.get() is slower but avoids lots of false positives
+        with requests.get(
+            url, allow_redirects=True, timeout=TIMEOUT, verify=verifycert, headers=hdr, stream=True
+        ) as stream:
+            Rb = next(stream.iter_lines(80), None)
+            # if Rb is not None and 'html' in Rb.decode('utf8'):
+            if Rb and len(Rb) > 10:
+                ok = True
+    except EXC:
+        pass
+
+    return ok
