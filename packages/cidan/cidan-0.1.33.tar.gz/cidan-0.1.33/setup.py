9f994ed10122753c4806da3bf6ad9a74e51e065f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:18 +0000

    Add package cidan/0.1.33/cidan-0.1.33.tar.gz

---
+++ packages/cidan/cidan-0.1.33.tar.gz/cidan-0.1.33/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+try:
+    with open("README.md", 'r') as f:
+        long_description = f.read()
+
+except:
+    long_description = ""
+
+
+setup(
+    name='cidan',
+    version='0.1.33',
+    description='cidan-Calcium Imaging Data ANalysis',
+    license="MIT",
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+    author='Sam Schickler',
+    author_email='sschickl@ucsd.edu',
+    # url="http://www.foopackage.com/",
+    packages=find_packages(),
+    install_requires=["numpy", "QtPy", "QDarkStyle", "pybind11", "pyqtgraph==0.11.0rc0",
+                      "pyside2",
+                      "dask[complete]", "matplotlib", "scipy", "tiffile",
+                      "scikit-image", "pybind11", "hnswlib", "pillow",
+                      'tifffile', "zarr", "neurofinder", "sklearn", 'pandas', 'future',
+                      "peakutils", "requests"],
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'cidan = cidan.__main__:main',
+        ]
+    },
+)
