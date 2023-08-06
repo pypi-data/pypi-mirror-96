94f53e66c0864047ddd6191e3683fb3f32fa026b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:06 +0000

    Add package mypypackage/0.1.0/mypypackage-0.1.0.tar.gz

---
+++ packages/mypypackage/mypypackage-0.1.0.tar.gz/mypypackage-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mypypackage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'mypypackage',
+    'version': '0.1.0',
+    'description': 'This package was created for Assignment 1 DSCI 524',
+    'long_description': None,
+    'author': 'Nash Makhija',
+    'author_email': 'naurattan.makhija@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
