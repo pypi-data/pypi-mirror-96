90fbd8a55f4e768ae7401f21ccbe7f40900a2f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package datavita-python-sdk/0.0.5/datavita_python_sdk-0.0.5-py3-none-any.whl

---
+++ packages/datavita-python-sdk/datavita_python_sdk-0.0.5-py3-none-any.whl/datavita/fingraph/fingraph_apis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 hcyjs.com, Inc. All Rights Reserved
+
+"""
+    @Time : 2021-02-05 15:08
+    @Author : Yin Jian
+    @Version：V 0.1
+    @File : fingraph_apis.py
+    @desc :
+"""
+fingraph_apis_dict = {'get_fingraph_data_by_group_id': '{base_url}/api/v1/group/dataDict?groupId={group_id}'
+                      }
