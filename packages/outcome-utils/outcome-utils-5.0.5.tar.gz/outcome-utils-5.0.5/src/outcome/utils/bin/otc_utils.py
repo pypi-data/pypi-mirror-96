5f0960fd38ac7489904572fcd600d643cf7af347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:31 +0000

    Add package outcome-utils/5.0.5/outcome-utils-5.0.5.tar.gz

---
+++ packages/outcome-utils/outcome-utils-5.0.5.tar.gz/outcome-utils-5.0.5/src/outcome/utils/bin/otc_utils.py
@@ -0,0 +1,16 @@
+"""Utils CLI."""
+
+import click
+from outcome.utils.loader import load_obj
+
+
+@click.group()
+def main():
+    ...
+
+
+@main.command(help='List available feature flags')
+@click.option('--feature-set', default='outcome.utils:feature_set')
+def features(feature_set: str):
+    fs = load_obj(feature_set, default_obj='feature_set')
+    fs.display_features()
