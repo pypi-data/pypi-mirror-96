03ea95b15344cca3a220e8c1820a9673f30f61ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:26 +0000

    Add package maml/2021.2.27/maml-2021.2.27.tar.gz

---
+++ packages/maml/maml-2021.2.27.tar.gz/maml-2021.2.27/maml/__init__.py
@@ -0,0 +1,9 @@
+"""
+maml - materials machine learning
+"""
+
+__version__ = "2021.2.27"
+
+from .base import BaseDataSource  # noqa
+from .base import BaseDescriber  # noqa
+from .base import BaseModel, SKLModel, KerasModel, SequentialDescriber  # noqa
