bee918470f719387baa966ea9ab0d4708a796c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:49 +0000

    Add package distributions2-chrisrydahl/0.2/distributions2-chrisrydahl-0.2.tar.gz

---
+++ packages/distributions2-chrisrydahl/distributions2-chrisrydahl-0.2.tar.gz/distributions2-chrisrydahl-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions2-chrisrydahl',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
+
