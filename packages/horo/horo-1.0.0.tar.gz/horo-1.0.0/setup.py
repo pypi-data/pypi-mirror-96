220295be49a2858b75be6ee1c27fe2dbc465e0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:06 +0000

    Add package horo/1.0.0/horo-1.0.0.tar.gz

---
+++ packages/horo/horo-1.0.0.tar.gz/horo-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="horo",
+    packages=find_packages(include=["horo"]),
+    version="1.0.0",
+    description="A program that displays your horoscope right from the terminal",
+    author="George Munyoro",
+    license="MIT",
+)
+
