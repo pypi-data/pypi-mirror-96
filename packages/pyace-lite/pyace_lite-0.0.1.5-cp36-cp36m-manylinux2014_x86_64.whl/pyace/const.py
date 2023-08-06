b264e1bbf9e96155f11403ceb7871ca46d051c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:11 +0000

    Add package pyace-lite/0.0.1.5/pyace_lite-0.0.1.5-cp36-cp36m-manylinux2014_x86_64.whl

---
+++ packages/pyace-lite/pyace_lite-0.0.1.5-cp36-cp36m-manylinux2014_x86_64.whl/pyace/const.py
@@ -0,0 +1,67 @@
+# /*
+# * Atomic cluster expansion
+# *
+# * Copyright 2021  (c) Yury Lysogorskiy, Anton Bochkarev,
+# * Sarath Menon, Ralf Drautz
+# *
+# * Ruhr-University Bochum, Bochum, Germany
+# *
+# * See the LICENSE file.
+# * This FILENAME is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+#
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+#     * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# */
+
+
+FIT_NITER_KW = 'maxiter'
+FIT_OPTIONS_KW = 'options'
+BACKEND_BATCH_SIZE_KW = "batch_size"
+POTENTIAL_RANKMAX_KW = 'rankmax'
+BACKEND_NWORKERS_KW = "n_workers"
+BACKEND_PARALLEL_MODE_KW = "parallel_mode"
+POTENTIAL_METADATA_KW = "metadata"
+POTENTIAL_DELTASPLINEBINS_KW = "deltaSplineBins"
+POTENTIAL_RADCOEFFICIENTS_KW = "radcoefficients"
+POTENTIAL_RADPARAMETERS_KW = 'radparameters'
+POTENTIAL_RADBASE_KW = "radbase"
+POTENTIAL_NDENSITY_KW = "ndensity"
+POTENTIAL_NRADBASE_KW = 'nradbase'
+POTENTIAL_CUTOFF_FUNCTION_KW = 'NameOfCutoffFunction'
+POTENTIAL_DCUT_KW = "dcut"
+POTENTIAL_RCUT_KW = 'rcut'
+POTENTIAL_FS_PARAMETERS_KW = "fs_parameters"
+POTENTIAL_NPOT_KW = 'npot'
+POTENTIAL_LMAX_KW = 'lmax'
+POTENTIAL_NRADMAX_KW = 'nradmax'
+POTENTIAL_ELEMENT_KW = "element"
+POTENTIAL_BASISDF_KW = 'basisdf'
+POTENTIAL_INITIAL_POTENTIAL_KW = "initial_potential"
+
+ATOMIC_ENV_DF_COLUMN = "atomic_env"
+TP_ATOMS_DF_COLUMN = "tp_atoms"
+PYACE_EVAL = 'pyace'
+TENSORPOT_EVAL = 'tensorpot'
+ENERGYBASED_WEIGHTING_POLICY = "EnergyBasedWeightingPolicy"
+WEIGHTING_TYPE_KW = "type"
+FIT_WEIGHTING_KW = 'weighting'
+FIT_LOSS_KW = "loss"
+FIT_OPTIMIZER_KW = 'optimizer'
+FIT_OPTIMIZER_OPTIONS_KW = 'optimizer_options'
+FIT_FIT_CYCLES_KW = "fit_cycles"
+FIT_LADDER_STEP_KW = 'ladder_step'
+FIT_LADDER_TYPE_KW = 'ladder_type'
+FIT_NOISE_REL_SIGMA = "noise_relative_sigma"
+FIT_NOISE_ABS_SIGMA = "noise_absolute_sigma"
+
+BACKEND_EVALUATOR_KW = 'evaluator'
+INITIAL_POTENTIAL_YAML = "initial_potential.yaml"
+METADATA_USER_KW = 'user'
+METADATA_STARTTIME_KW = 'starttime'
