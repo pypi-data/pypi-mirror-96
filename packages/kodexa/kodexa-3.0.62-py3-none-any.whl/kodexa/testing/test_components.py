5f64cebfff3165b7168ec8a88eaf7c9ba3936d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package kodexa/3.0.62/kodexa-3.0.62-py3-none-any.whl

---
+++ packages/kodexa/kodexa-3.0.62-py3-none-any.whl/kodexa/testing/test_components.py
@@ -0,0 +1,36 @@
+from kodexa.assistant import Assistant, AssistantContext, AssistantResponse
+from kodexa.model import ContentEvent
+from kodexa.pipeline import Pipeline
+
+class TestAction:
+    """ """
+
+    def __init__(self, cheese: str = None):
+        self.cheese = cheese
+
+    def get_name(self):
+        """ """
+        return "Hello"
+
+
+class TestAssistant(Assistant):
+    """ """
+
+    def process_event(self, event: ContentEvent, context: AssistantContext = None) -> AssistantResponse:
+        """
+
+        Args:
+          event: ContentEvent:
+          context: AssistantContext:  (Default value = None)
+
+        Returns:
+
+        """
+        # This is just an example of an assistant
+        # basically we are just going to return a pipeline that
+        # adds a label to the document - creating a new version
+
+        pipeline = Pipeline()
+        pipeline.add_label('hello')
+
+        return AssistantResponse([pipeline])
