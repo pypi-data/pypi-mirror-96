e04fc628cd9c83c5c6fd82330ebe345a07f9cdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:48 +0000

    Add package twist-moe/1.0.5/twist_moe-1.0.5.tar.gz

---
+++ packages/twist-moe/twist_moe-1.0.5.tar.gz/twist_moe-1.0.5/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['twist_moe']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pycrypto==2.6.1', 'requests>=2.25.0,<3.0.0', 'tqdm>=4.53.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['twist = twist_moe.tui:main']}
+
+setup_kwargs = {
+    'name': 'twist-moe',
+    'version': '1.0.5',
+    'description': 'twist.moe client',
+    'long_description': None,
+    'author': 'witherornot',
+    'author_email': 'damemem@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
