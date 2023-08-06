e7e621433fca9110934aabad72fe973cbc2b18ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:36 +0000

    Add package Zeppi-ConvertX/0.1.4/Zeppi_ConvertX-0.1.4.tar.gz

---
+++ packages/Zeppi-ConvertX/Zeppi_ConvertX-0.1.4.tar.gz/Zeppi_ConvertX-0.1.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name = 'Zeppi_ConvertX',
+    version = '0.1.4',
+    keywords="zeppelin convert to python notebooks interpreter",
+    description="Convert your zeppelin notebooks to python",
+    long_description=readme,
+    author = "Harsha Karpurapu",
+    author_email='kbsriharsha@gmail.com',
+    url='https://github.com/mecsys/Zeppi_ConvertX',
+    packages = find_packages(exclude=['contrib']),
+    license="MIT",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'zeppi-convert = Zeppi_Convert.__main__:main'
+        ]
+    })
