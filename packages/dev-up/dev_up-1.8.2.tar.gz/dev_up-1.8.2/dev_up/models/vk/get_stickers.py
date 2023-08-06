11c80b79041d80aa51f8d393914ed29fd4947ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:57 +0000

    Add package dev-up/1.8.2/dev_up-1.8.2.tar.gz

---
+++ packages/dev-up/dev_up-1.8.2.tar.gz/dev_up-1.8.2/dev_up/models/vk/get_stickers.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+__all__ = (
+    'StickersPrice',
+    'Sticker',
+    'VkGetStickersResponse',
+    'VkGetStickers',
+)
+
+
+class StickersPrice(BaseModel):
+    votes: int
+    rub: int
+
+
+class Sticker(BaseModel):
+    id: int
+    pack_id: int
+    name: str
+    stickers_count: Optional[int] = None
+    author: Optional[str] = None
+    description: Optional[str] = None
+    photo: Optional[str] = None
+    price: Optional[StickersPrice] = None
+    url_buy: Optional[str] = None
+
+
+class VkGetStickersResponse(BaseModel):
+    user_id: int
+    count: int
+    count_all: int
+    stickers: List[Sticker]
+    price: Optional[StickersPrice] = None
+    system_comment: Optional[str] = None
+
+
+class VkGetStickers(BaseModel):
+    response: VkGetStickersResponse
