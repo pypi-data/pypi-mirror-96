fca1fdf2a0e803c0b060d2c2213c875fd6d171fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:01 +0000

    Add package stockai/1.1.0/stockai-1.1.0.tar.gz

---
+++ packages/stockai/stockai-1.1.0.tar.gz/stockai-1.1.0/stockai/yahoo/stock.py
@@ -0,0 +1,26 @@
+from .base import Base
+from ..utils import date_to_timestamp
+
+class Stock(Base):
+    def __init__(self, symbol):
+        super(Stock, self).__init__(symbol)
+        self.refresh()
+
+    # Summary Profile
+    def get_summary_profile(self):
+        return self.data_set['summaryProfile']
+
+    # Financial Data
+    def get_price(self):
+        return self.data_set['financialData']['currentPrice']['fmt']
+
+    def get_currency(self):
+        return self.data_set['financialData']['financialCurrency']
+
+    # Historical Prices
+    def get_historical_prices(self, start_date, end_date):        
+        if (date_to_timestamp(start_date) > date_to_timestamp(end_date)):
+            raise ValueError('Please check the order of start date and end date')
+
+        return self.get_historical(date_to_timestamp(start_date), date_to_timestamp(end_date))
+
