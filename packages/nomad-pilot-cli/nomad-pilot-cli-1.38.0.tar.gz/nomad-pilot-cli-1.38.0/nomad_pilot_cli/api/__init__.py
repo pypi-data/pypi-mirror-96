ec3410315141ee38ec87bb9181e7ebf4d4f4998a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:15 +0000

    Add package nomad-pilot-cli/1.38.0/nomad-pilot-cli-1.38.0.tar.gz

---
+++ packages/nomad-pilot-cli/nomad-pilot-cli-1.38.0.tar.gz/nomad-pilot-cli-1.38.0/nomad_pilot_cli/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nomad_pilot_cli.api.carriers_api import CarriersApi
+from nomad_pilot_cli.api.freight_forward_api import FreightForwardApi
+from nomad_pilot_cli.api.quick_ship_api import QuickShipApi
+from nomad_pilot_cli.api.ship_api import ShipApi
+from nomad_pilot_cli.api.ship_confirm_api import ShipConfirmApi
