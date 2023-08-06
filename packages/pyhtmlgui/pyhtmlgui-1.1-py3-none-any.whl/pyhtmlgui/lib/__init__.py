442c6d9c31bc360b3cd5e178ba187250229fdfa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:20 +0000

    Add package pyhtmlgui/1.1/pyhtmlgui-1.1-py3-none-any.whl

---
+++ packages/pyhtmlgui/pyhtmlgui-1.1-py3-none-any.whl/pyhtmlgui/lib/__init__.py
@@ -0,0 +1,6 @@
+from .eventset import EventSet
+from .weakfunctionreferences import WeakFunctionReferences
+from .browser import Browser
+from .observable import Observable
+from .observableDict import ObservableDict
+from .observableList import ObservableList
