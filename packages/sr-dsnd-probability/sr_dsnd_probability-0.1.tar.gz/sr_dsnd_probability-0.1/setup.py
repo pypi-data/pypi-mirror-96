7c5af6f2e4d70dff6ad6f70dbeefe5cad2b79052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:18 +0000

    Add package sr-dsnd-probability/0.1/sr_dsnd_probability-0.1.tar.gz

---
+++ packages/sr-dsnd-probability/sr_dsnd_probability-0.1.tar.gz/sr_dsnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sr_dsnd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sr_dsnd_probability'],
+      author = "Sangeetha Ramamurthy",
+      author_email = 'sramamurthy@ucdavis.edu',
+     zip_safe=False)
