fd10f30f07efbeed8c3b6595210359ac03398a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:51 +0000

    Add package graia-saya/0.0.6/graia-saya-0.0.6.tar.gz

---
+++ packages/graia-saya/graia-saya-0.0.6.tar.gz/graia-saya-0.0.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['graia',
+ 'graia.saya',
+ 'graia.saya.behaviour',
+ 'graia.saya.builtins',
+ 'graia.saya.builtins.broadcast']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graia-broadcast>=0.7.0', 'loguru>=0.5.3,<0.6.0']
+
+setup_kwargs = {
+    'name': 'graia-saya',
+    'version': '0.0.6',
+    'description': '',
+    'long_description': None,
+    'author': 'GreyElaina',
+    'author_email': '31543961+GreyElaina@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
