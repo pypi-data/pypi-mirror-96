7327950682887f056235d7c94a7d7f483267bfbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:54 +0000

    Add package allianceauth-afat/1.7.0/allianceauth_afat-1.7.0-py3-none-any.whl

---
+++ packages/allianceauth-afat/allianceauth_afat-1.7.0-py3-none-any.whl/afat/apps.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+app config
+"""
+
+from django.apps import AppConfig
+
+from afat import __version__
+
+
+class AfatConfig(AppConfig):
+    """
+    general config
+    """
+
+    name = "afat"
+    label = "afat"
+    verbose_name = f"AFAT - Another Fleet Activity Tracker v{__version__}"
---
+++ packages/allianceauth-afat/allianceauth_afat-1.7.0-py3-none-any.whl/afat/tests.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+for the tests
+"""
+
+# from django.test import TestCase
+
+# Create your tests here.
