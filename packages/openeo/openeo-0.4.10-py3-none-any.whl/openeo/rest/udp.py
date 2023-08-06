e4d3286010fc89a45d2cff2e62edaf21e5e29618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:33 +0000

    Add package openeo/0.4.10/openeo-0.4.10-py3-none-any.whl

---
+++ packages/openeo/openeo-0.4.10-py3-none-any.whl/openeo/rest/udp.py
@@ -0,0 +1,53 @@
+import typing
+from typing import List, Union
+
+from openeo.api.process import Parameter
+from openeo.internal.graph_building import as_flat_graph
+from openeo.internal.processes.builder import ProcessBuilderBase
+
+if hasattr(typing, 'TYPE_CHECKING') and typing.TYPE_CHECKING:
+    # Only import this for type hinting purposes. Runtime import causes circular dependency issues.
+    # Note: the `hasattr` check is necessary for Python versions before 3.5.2.
+    from openeo.rest.connection import Connection
+
+
+class RESTUserDefinedProcess:
+    def __init__(self, user_defined_process_id: str, connection: 'Connection'):
+        self.user_defined_process_id = user_defined_process_id
+        self._connection = connection
+
+    def store(
+            self, process_graph: Union[dict, ProcessBuilderBase], parameters: List[Union[Parameter, dict]] = None,
+            public: bool = False, summary: str = None, description: str = None
+    ):
+        req = {
+            'process_graph': as_flat_graph(process_graph),
+            'public': public
+        }
+        if parameters is not None:
+            req["parameters"] = [
+                (p if isinstance(p, Parameter) else Parameter(**p)).to_dict()
+                for p in parameters
+            ]
+        if summary is not None:
+            req["summary"] = summary
+        if description is not None:
+            req["description"] = description
+        self._connection.put(path="/process_graphs/{}".format(self.user_defined_process_id), json=req)
+
+    def update(
+            self, process_graph: Union[dict, ProcessBuilderBase], parameters: List[Union[Parameter, dict]] = None,
+            public: bool = False, summary: str = None, description: str = None
+    ):
+        # TODO: the openEO API does not allow partial updates, while this method might suggest that.
+        self.store(process_graph=process_graph, parameters=parameters, public=public, summary=summary,description=description)
+
+    def describe(self) -> dict:
+        # TODO: parse the "parameters" to Parameter objects?
+        return self._connection.get(path="/process_graphs/{}".format(self.user_defined_process_id)).json()
+
+    def delete(self) -> None:
+        self._connection.delete(path="/process_graphs/{}".format(self.user_defined_process_id), expected_status=204)
+
+    def validate(self) -> None:
+        raise NotImplementedError
