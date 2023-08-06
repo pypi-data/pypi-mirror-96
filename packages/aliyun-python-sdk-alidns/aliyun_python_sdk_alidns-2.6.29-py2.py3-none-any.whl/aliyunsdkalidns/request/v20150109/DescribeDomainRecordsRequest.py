97074734479e165fc4d0e32515b195342c9800e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:31 +0000

    Add package aliyun-python-sdk-alidns/2.6.29/aliyun_python_sdk_alidns-2.6.29-py2.py3-none-any.whl

---
+++ packages/aliyun-python-sdk-alidns/aliyun_python_sdk_alidns-2.6.29-py2.py3-none-any.whl/aliyunsdkalidns/request/v20150109/DescribeDomainRecordsRequest.py
@@ -0,0 +1,122 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkalidns.endpoint import endpoint_data
+
+class DescribeDomainRecordsRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Alidns', '2015-01-09', 'DescribeDomainRecords','alidns')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_ValueKeyWord(self):
+		return self.get_query_params().get('ValueKeyWord')
+
+	def set_ValueKeyWord(self,ValueKeyWord):
+		self.add_query_param('ValueKeyWord',ValueKeyWord)
+
+	def get_Line(self):
+		return self.get_query_params().get('Line')
+
+	def set_Line(self,Line):
+		self.add_query_param('Line',Line)
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_PageNumber(self):
+		return self.get_query_params().get('PageNumber')
+
+	def set_PageNumber(self,PageNumber):
+		self.add_query_param('PageNumber',PageNumber)
+
+	def get_PageSize(self):
+		return self.get_query_params().get('PageSize')
+
+	def set_PageSize(self,PageSize):
+		self.add_query_param('PageSize',PageSize)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
+
+	def get_KeyWord(self):
+		return self.get_query_params().get('KeyWord')
+
+	def set_KeyWord(self,KeyWord):
+		self.add_query_param('KeyWord',KeyWord)
+
+	def get_RRKeyWord(self):
+		return self.get_query_params().get('RRKeyWord')
+
+	def set_RRKeyWord(self,RRKeyWord):
+		self.add_query_param('RRKeyWord',RRKeyWord)
+
+	def get_Direction(self):
+		return self.get_query_params().get('Direction')
+
+	def set_Direction(self,Direction):
+		self.add_query_param('Direction',Direction)
+
+	def get_GroupId(self):
+		return self.get_query_params().get('GroupId')
+
+	def set_GroupId(self,GroupId):
+		self.add_query_param('GroupId',GroupId)
+
+	def get_DomainName(self):
+		return self.get_query_params().get('DomainName')
+
+	def set_DomainName(self,DomainName):
+		self.add_query_param('DomainName',DomainName)
+
+	def get_OrderBy(self):
+		return self.get_query_params().get('OrderBy')
+
+	def set_OrderBy(self,OrderBy):
+		self.add_query_param('OrderBy',OrderBy)
+
+	def get_SearchMode(self):
+		return self.get_query_params().get('SearchMode')
+
+	def set_SearchMode(self,SearchMode):
+		self.add_query_param('SearchMode',SearchMode)
+
+	def get_TypeKeyWord(self):
+		return self.get_query_params().get('TypeKeyWord')
+
+	def set_TypeKeyWord(self,TypeKeyWord):
+		self.add_query_param('TypeKeyWord',TypeKeyWord)
+
+	def get_Status(self):
+		return self.get_query_params().get('Status')
+
+	def set_Status(self,Status):
+		self.add_query_param('Status',Status)
---
+++ packages/aliyun-python-sdk-alidns/aliyun_python_sdk_alidns-2.6.29-py2.py3-none-any.whl/aliyunsdkalidns/request/v20150109/OperateBatchDomainRequest.py
@@ -0,0 +1,70 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from aliyunsdkcore.request import RpcRequest
+from aliyunsdkalidns.endpoint import endpoint_data
+
+class OperateBatchDomainRequest(RpcRequest):
+
+	def __init__(self):
+		RpcRequest.__init__(self, 'Alidns', '2015-01-09', 'OperateBatchDomain','alidns')
+		self.set_method('POST')
+		if hasattr(self, "endpoint_map"):
+			setattr(self, "endpoint_map", endpoint_data.getEndpointMap())
+		if hasattr(self, "endpoint_regional"):
+			setattr(self, "endpoint_regional", endpoint_data.getEndpointRegional())
+
+
+	def get_DomainRecordInfos(self):
+		return self.get_query_params().get('DomainRecordInfo')
+
+	def set_DomainRecordInfos(self, DomainRecordInfos):
+		for depth1 in range(len(DomainRecordInfos)):
+			if DomainRecordInfos[depth1].get('Rr') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Rr', DomainRecordInfos[depth1].get('Rr'))
+			if DomainRecordInfos[depth1].get('NewType') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.NewType', DomainRecordInfos[depth1].get('NewType'))
+			if DomainRecordInfos[depth1].get('NewValue') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.NewValue', DomainRecordInfos[depth1].get('NewValue'))
+			if DomainRecordInfos[depth1].get('Line') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Line', DomainRecordInfos[depth1].get('Line'))
+			if DomainRecordInfos[depth1].get('Domain') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Domain', DomainRecordInfos[depth1].get('Domain'))
+			if DomainRecordInfos[depth1].get('Type') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Type', DomainRecordInfos[depth1].get('Type'))
+			if DomainRecordInfos[depth1].get('Priority') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Priority', DomainRecordInfos[depth1].get('Priority'))
+			if DomainRecordInfos[depth1].get('Value') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Value', DomainRecordInfos[depth1].get('Value'))
+			if DomainRecordInfos[depth1].get('Ttl') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.Ttl', DomainRecordInfos[depth1].get('Ttl'))
+			if DomainRecordInfos[depth1].get('NewRr') is not None:
+				self.add_query_param('DomainRecordInfo.' + str(depth1 + 1) + '.NewRr', DomainRecordInfos[depth1].get('NewRr'))
+
+	def get_Type(self):
+		return self.get_query_params().get('Type')
+
+	def set_Type(self,Type):
+		self.add_query_param('Type',Type)
+
+	def get_Lang(self):
+		return self.get_query_params().get('Lang')
+
+	def set_Lang(self,Lang):
+		self.add_query_param('Lang',Lang)
