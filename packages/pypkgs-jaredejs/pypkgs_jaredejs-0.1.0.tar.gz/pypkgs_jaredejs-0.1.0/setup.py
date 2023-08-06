3d67a8efa66f72ce77c4289410ff4e1ac4459e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:41 +0000

    Add package pypkgs-jaredejs/0.1.0/pypkgs_jaredejs-0.1.0.tar.gz

---
+++ packages/pypkgs-jaredejs/pypkgs_jaredejs-0.1.0.tar.gz/pypkgs_jaredejs-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypkgs_jaredejs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.2.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pypkgs-jaredejs',
+    'version': '0.1.0',
+    'description': 'Python package for individual assignment 1 in UBC MDS DSCI 524',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
