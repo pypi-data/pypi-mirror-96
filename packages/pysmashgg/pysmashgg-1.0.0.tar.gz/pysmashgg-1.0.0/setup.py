2cb03765772de048a764d83289cf3fc1176061e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:13 +0000

    Add package pysmashgg/1.0.0/pysmashgg-1.0.0.tar.gz

---
+++ packages/pysmashgg/pysmashgg-1.0.0.tar.gz/pysmashgg-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysmashgg",
+    version="1.0.0",
+    author="Jeremy Skalla",
+    author_email="jthroughs@gmail.com",
+    description="Python Wrapper for smash.gg's GraphQL API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeremySkalla/SmashGGPythonWrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests']
+)
